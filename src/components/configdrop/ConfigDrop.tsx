ebca7ec9cfdf41fd3eef26b15b3d15b70fdf1557
Domino Weir <domino.weir@10gen.com>
2019-06-25 14:19:39 -0400

    add snackbar notifications for errors

--- src/components/configdrop/ConfigDrop.tsx
+++ src/components/configdrop/ConfigDrop.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, CardActions, CardContent } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, IconButton, Snackbar } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import { DropzoneArea } from 'material-ui-dropzone'
 import * as React from 'react';
 import { ClientConfig, ConvertToClientConfig } from '../../models/client_config';
@@ -9,6 +10,8 @@ class Props {
 }
 
 interface State {
+  snackbarOpen: boolean,
+  snackbarMessage: string,
   error: string
   newConfig: ClientConfig,
 }
@@ -18,6 +21,8 @@ export class ConfigDrop extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
+      snackbarOpen: false,
+      snackbarMessage: "",
       error: null,
       newConfig: null
     };
@@ -37,6 +42,9 @@ export class ConfigDrop extends React.Component<Props, State> {
             <Button variant="outlined" className="save" onClick={this.save()}>Save</Button>
           </CardActions>
         </Card>
+        <Snackbar open={this.state.snackbarOpen} message={this.state.snackbarMessage}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left", }} onClose={this.onCloseSnackbar}
+          action={[<IconButton key="close" color="inherit" onClick={this.onCloseSnackbar}><CloseIcon /></IconButton>,]} />
       </div>
     )
   }
@@ -51,22 +59,27 @@ export class ConfigDrop extends React.Component<Props, State> {
     }
   }
 
-
   private readConfig = (file: File) => {
     const reader = new FileReader();
     reader.onerror = () => {
-      console.log("Failed to read config file with error code: " + reader.error);
+      this.setState({
+        snackbarMessage: "Failed to read config file with error " + reader.error,
+        snackbarOpen: true
+      });
     };
     reader.onload = () => {
       const raw = reader.result.toString();
       const configObj = ConvertToClientConfig(raw);
       if (configObj.hasOwnProperty("user") && configObj.hasOwnProperty("api_key") &&
-        configObj.hasOwnProperty("ui_url") && configObj.hasOwnProperty("api_url") ) {
+        configObj.hasOwnProperty("ui_url") && configObj.hasOwnProperty("api_url")) {
         this.setState({
           newConfig: configObj,
         });
       } else {
-        console.log("Config file does not contain all required properties");
+        this.setState({
+          snackbarMessage: "Config file does not contain all required properties.",
+          snackbarOpen: true
+        });
       }
     };
     reader.readAsText(file);
@@ -75,7 +88,22 @@ export class ConfigDrop extends React.Component<Props, State> {
   private save = () => () => {
     if (this.state.newConfig !== null) {
       this.props.updateClientConfig(this.state.newConfig);
+      this.setState({
+        snackbarMessage: "Config file saved.",
+        snackbarOpen: true
+      });
+    } else {
+      this.setState({
+        snackbarMessage: "Please upload a valid config file before saving.",
+        snackbarOpen: true
+      });
     }
+  }
+
+  private onCloseSnackbar = (event: React.SyntheticEvent | React.MouseEvent, reason?: string) => {
+    this.setState({
+      snackbarOpen: false
+    });
   }
 }
 
