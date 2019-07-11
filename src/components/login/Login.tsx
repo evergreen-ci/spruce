86df5f5c7988475a3dae23a5b00cbf486f0f75f0
Domino Weir <domino.weir@10gen.com>
2019-07-11 16:08:53 -0400

    add login error handling

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,16 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Typography } from '@material-ui/core';
 import * as React from 'react';
 import * as rest from "../../rest/interface";
 import '../../styles.css';
 
 interface State {
-  open : boolean;
-  submitted : boolean
-  username : string
-  password : string
-  token : string
+  open: boolean;
+  submitted: boolean
+  username: string
+  password: string
+  token: string
   buttonText: string
+  hideErrorMessage: boolean
 }
 
 class Props {
@@ -22,12 +23,13 @@ export class Login extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      open : false,
-      submitted : false,
-      username : "",
-      password : "",
-      token : "",
-      buttonText: "Log In"
+      open: false,
+      submitted: false,
+      username: "",
+      password: "",
+      token: "",
+      buttonText: "Log In",
+      hideErrorMessage: true
     };
   }
 
@@ -41,7 +43,7 @@ export class Login extends React.Component<Props, State> {
           <DialogTitle id="form-dialog-title">Log In</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              Please enter your username and password. 
+              Please enter your username and password.
             </DialogContentText>
             <TextField
               autoFocus={false}
@@ -65,6 +67,11 @@ export class Login extends React.Component<Props, State> {
               onChange={this.updateSingleField(this.state, "password")}
               value={this.state.password}
             />
+            <div hidden={this.state.hideErrorMessage}>
+              <Typography color="error">
+                Invalid username or password
+              </Typography>
+            </div>
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClickClose} color="primary" id="cancel-button">
@@ -82,48 +89,54 @@ export class Login extends React.Component<Props, State> {
   private handleClickOpen = () => {
     if (this.state.buttonText === "Log In") {
       this.setState({
-        open : true,
+        open: true,
       });
     } else {
       this.setState({
         buttonText: "Log In"
       });
       this.props.updateUsername("");
-    } 
+    }
   }
 
   private handleClickClose = () => {
     this.setState({
-      open : false,
+      open: false,
       username: "",
       password: "",
+      hideErrorMessage: true
     });
   }
 
   private handleClickSubmit = () => {
     this.props.client.getToken((err, resp, body) => {
       if (err || resp.statusCode >= 300) {
-        console.log("got error " + err + " with status code " + resp.statusCode);
-        return;
+        if (resp.statusCode === 401) {
+          this.setState({
+            hideErrorMessage: false
+          });
+        } else {
+          console.log("got error " + err + " with status code " + resp.statusCode);
+        }
       } else {
         this.props.updateUsername(this.state.username);
+        this.setState({
+          open: false,
+          submitted: true,
+          buttonText: "Log Out"
+        });
       }
     }, this.state.username, this.state.password);
-    this.setState({
-      open : false,
-      submitted : true,
-      buttonText: "Log Out"
-    });
   }
- 
+
   private updateSingleField = <T, K extends keyof T>(state: T, key: K) => {
-    return (value:React.ChangeEvent<HTMLInputElement>) => {
+    return (value: React.ChangeEvent<HTMLInputElement>) => {
       const newState = Object.assign({}, state);
       newState[key] = value.target.value as any;
       this.setState(newState);
     }
   }
-  
+
 }
 
 export default Login;
