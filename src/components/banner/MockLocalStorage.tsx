29906a79a428ef07fcbda21c65088d2251390156
Domino Weir <domino.weir@10gen.com>
2019-07-29 16:41:26 -0400

    convert mock LocalStorage to something more typescript-y

--- src/components/banner/MockLocalStorage.tsx
+++ src/components/banner/MockLocalStorage.tsx
@@ -1,18 +1,27 @@
-const localStorageMock = (() => {
-  let store = {};
-  return {
-    getItem: (key: string) => {
-      return store[key] === undefined ? null : store[key];
-    },
-    setItem: (key: string, value: any) => {
-      store[key] = value.toString();
-    },
-    clear: () => {
-      store = {};
-    },
-    removeItem: (key: string) => {
-      delete store[key];
-    }
-  };
-})();
-Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+const globalAny:any = global;
+
+class LocalStorageMock {
+  private store: object;
+
+  constructor() {
+    this.store = {};
+  }
+
+  public clear() {
+    this.store = {};
+  }
+
+  public getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  public setItem(key: string, value: any) {
+    this.store[key] = value.toString();
+  }
+
+  public removeItem(key: string) {
+    delete this.store[key];
+  }
+};
+
+globalAny.localStorage = new LocalStorageMock;
