f89245b1cfc22b276e89ae4dea6bf38cbe1efe63
Domino Weir <domino.weir@10gen.com>
2019-08-01 10:45:00 -0400

    use real client initially

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -53,7 +53,7 @@ export class Evergreen extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      APIClient: rest.EvergreenClient("", "", "", "", true)
+      APIClient: rest.EvergreenClient("", "", "", "")
     }
     this.tryLoadConfig();
   }
