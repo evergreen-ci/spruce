e68737284927a89cb5788a691a92cdb9807b4eeb
Domino Weir <domino.weir@mongodb.com>
2019-08-07 13:17:46 -0400

    EVG-6548: add menu options in app bar (#20)
    * add menu options in app bar

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,6 @@
 import bugsnag, { Bugsnag } from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
-import { AppBar, createMuiTheme, /* IconButton, Menu, MenuItem, */ Toolbar, Typography } from '@material-ui/core';
+import { AppBar, createMuiTheme, /* IconButton, */ Link, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
 // import * as MenuIcon from '@material-ui/icons/Menu';
 import { ThemeProvider } from '@material-ui/styles';
 import * as models from 'evergreen.js/lib/models';
@@ -39,6 +39,7 @@ const theme = createMuiTheme({
 
 interface State {
   APIClient: rest.Evergreen;
+  PluginMenuAnchor?: HTMLElement;
   MenuAnchor?: HTMLElement;
   username?: string;
   bugsnag?: Bugsnag.Client;
@@ -64,7 +65,8 @@ export class Evergreen extends React.Component<Props, State> {
     const config = () => <ConfigDrop updateClientConfig={this.updateConfig} onLoadFinished={null} />
     const build = () => <BuildView client={this.state.APIClient} />
     let ErrorHandler: any;
-    // const menuOpen = Boolean(this.state.MenuAnchor);
+    const pluginMenuIsOpen = Boolean(this.state.PluginMenuAnchor);
+    // const menuIsOpen = Boolean(this.state.MenuAnchor);
     let app =
     <ThemeProvider theme={theme}>
       <div className="app">
@@ -75,9 +77,58 @@ export class Evergreen extends React.Component<Props, State> {
                 <AppBar className="app-bar" >
                   <Toolbar>
                     <img src={EvergreenIcon} className="app-icon" />
-                    <Typography variant="h5" color="inherit" noWrap={true}>
-                      Evergreen
+                    <Link href={this.state.APIClient.apiURL + "/waterfall"} underline="none">
+                      <Typography noWrap={true} className="menu-option">
+                        Waterfall
+                      </Typography>
+                    </Link>
+                    <Link href={this.state.APIClient.apiURL + "/timeline"} underline="none">
+                      <Typography noWrap={true} className="menu-option">
+                        Timeline
+                      </Typography>
+                    </Link>
+                    <Link href={this.state.APIClient.apiURL + "/grid"} underline="none">
+                      <Typography noWrap={true} className="menu-option">
+                        Summary
+                      </Typography>
+                    </Link>
+                    <Link href={this.state.APIClient.apiURL + "/patches"} underline="none">
+                      <Typography noWrap={true} className="menu-option">
+                        Patches
+                      </Typography>
+                    </Link>
+                    <Link href={this.state.APIClient.apiURL + "/task_timing"} underline="none">
+                      <Typography noWrap={true} className="menu-option">
+                        Stats
+                      </Typography>
+                    </Link>
+                    <Link href={this.state.APIClient.apiURL + "/hosts"} underline="none">
+                      <Typography noWrap={true} className="menu-option">
+                        Hosts
+                      </Typography>
+                    </Link>
+                    <Typography noWrap={true} className="menu-option" onClick={this.openPluginMenu}>
+                      Plugins
                     </Typography>
+                    <Menu id="mainAppMenu" open={pluginMenuIsOpen} anchorEl={this.state.PluginMenuAnchor}
+                      anchorOrigin={{ vertical: 'bottom', horizontal: 'center', }}
+                      transformOrigin={{ vertical: 'bottom', horizontal: 'center', }}
+                      onClose={this.closePluginMenu}>
+                      <MenuItem onClick={this.closePluginMenu}>
+                        <Link href={this.state.APIClient.apiURL + "/perfdiscovery"} underline="none">
+                          <Typography noWrap={true}>
+                            Performance Discovery
+                          </Typography>
+                        </Link>
+                      </MenuItem>
+                      <MenuItem onClick={this.closePluginMenu}>
+                        <Link href={this.state.APIClient.apiURL + "/perf-bb"} underline="none">
+                          <Typography noWrap={true}>
+                            Performance Baron
+                          </Typography>
+                        </Link>
+                      </MenuItem>
+                    </Menu>
                     <div className="spacer" />
                     {/* <IconButton className="menu" color="inherit" id="mainAppIcon" onClick={this.openMenu}>
                       <MenuIcon.default />
@@ -117,6 +168,14 @@ export class Evergreen extends React.Component<Props, State> {
     }
 
     return app;
+  }
+
+  private openPluginMenu = (event: React.MouseEvent<HTMLElement>) => {
+    this.setState({ PluginMenuAnchor: event.currentTarget });
+  };
+
+  private closePluginMenu = () => {
+    this.setState({ PluginMenuAnchor: null });
   }
 
   // private openMenu = (event: React.MouseEvent<HTMLElement>) => {
