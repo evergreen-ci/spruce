982dbc15922a11ca045f9390e17a94e050f2d0e6
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2019-11-11 20:35:06 -0500

    feat: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import { AppBar, Link, Toolbar, Typography } from "@material-ui/core";
+import * as rest from "../rest/interface";
+import * as EvergreenIcon from "../assets/evergreen_green.png";
+import { isDevelopment } from "../utils/isDevelopment";
+import { PluginsMenu } from "./navbar/PluginsMenu";
+import { DevMenu } from "./navbar/DevMenu";
+
+const { useState } = React;
+
+const APIClient = rest.EvergreenClient("", "", "", "", true);
+const { uiURL } = APIClient;
+
+export const Navbar: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  function handleClick(e: React.MouseEvent<HTMLElement>) {
+    setAnchorEl(e.currentTarget);
+  }
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  return (
+    <AppBar className="app-bar">
+      <Toolbar>
+        <img src={EvergreenIcon} className="app-icon" />
+        <Link href={`${uiURL}/waterfall`} underline="none">
+          <Typography noWrap={true} className="menu-option">
+            Waterfall
+          </Typography>
+        </Link>
+        <Link href={`${uiURL}/timeline`} underline="none">
+          <Typography noWrap={true} className="menu-option">
+            Timeline
+          </Typography>
+        </Link>
+        <Link href={`${uiURL}/grid`} underline="none">
+          <Typography noWrap={true} className="menu-option">
+            Summary
+          </Typography>
+        </Link>
+        <Link href={`${uiURL}/patches`} underline="none">
+          <Typography noWrap={true} className="menu-option">
+            Patches
+          </Typography>
+        </Link>
+        <Link href={`${uiURL}/task_timing`} underline="none">
+          <Typography noWrap={true} className="menu-option">
+            Stats
+          </Typography>
+        </Link>
+        <Link href={`${uiURL}/hosts`} underline="none">
+          <Typography noWrap={true} className="menu-option">
+            Hosts
+          </Typography>
+        </Link>
+        <Typography noWrap={true} className="menu-option" onClick={handleClick}>
+          Plugins
+        </Typography>
+        <PluginsMenu
+          anchorEl={anchorEl}
+          handleClose={handleClose}
+          uiURL={uiURL}
+        />
+        <div className="spacer" />
+        {isDevelopment() && <DevMenu apiClient={APIClient} />}
+      </Toolbar>
+    </AppBar>
+  );
+};
---
+++ src/components/navbar/DevMenu.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import * as MenuIcon from "@material-ui/icons/Menu";
+import { NavLink } from "react-router-dom";
+import { Login } from "../login/Login";
+import { Evergreen } from "../../rest/interface";
+
+const { useState } = React;
+
+type DevMenuProps = {
+  apiClient: Evergreen;
+};
+
+export const DevMenu: React.FC<DevMenuProps> = ({ apiClient }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [userName, setUserName] = useState("");
+
+  function handleOpenMenu(e: React.MouseEvent<HTMLElement>) {
+    setAnchorEl(e.currentTarget);
+  }
+  function handleClose() {
+    setAnchorEl(null);
+  }
+  function handleSetUsername(username: string) {
+    setUserName(username);
+  }
+
+  return (
+    <div>
+      <IconButton
+        className="menu"
+        color="inherit"
+        id="mainAppIcon"
+        onClick={handleOpenMenu}
+      >
+        <MenuIcon.default />
+      </IconButton>
+      <Menu
+        id="mainAppMenu"
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <NavLink to="/admin"> Admin Page</NavLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <NavLink to="/patches">My Patches</NavLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <NavLink to="/config">Upload Config File</NavLink>
+        </MenuItem>
+      </Menu>
+      <Login client={apiClient} updateUsername={handleSetUsername} />
+    </div>
+  );
+};
