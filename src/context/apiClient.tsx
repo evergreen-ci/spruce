285af9fb92941cded40cedd63574804ececf21b6
Trey Granderson <trey.granderson@mongodb.com>
2019-11-20 17:42:04 -0500

    Use environment variables to configure build environments (#28)
    * use env-cmd to set environment variables for different builds
    
    * delte Admin component
    
    * util env functions
    
    * rest.EvergreenClient generates api and ui urls from env variables
    
    * delete some code
    
    * use bugsnag ErrorBoundary using api key from env var
    
    * getBugsnagApiKey util
    
    * remove REACT_APP_BUGSNAG_API_KEY from version control
    
    * condense all env var util functions to a single file
    
    * fix file name and remove npm build:test
    
    * add start script for local evergreen server

--- src/context/apiClient.tsx
+++ src/context/apiClient.tsx
@@ -1,64 +1,30 @@
 import * as React from "react";
 import * as rest from "../rest/interface";
-import { ClientConfig, IsValidConfig } from "../models/client_config";
 
-const { createContext, useState, useEffect } = React;
-
-const configPath = "/config.json";
+const { createContext, useState } = React;
 
 interface ApiClientContextType {
   apiClient: rest.Evergreen;
   actions: {
-    tryLoadConfig: () => void;
-    updateConfig: (configObj: ClientConfig) => void;
+    updateConfig: (username: string, key?: string) => void;
   };
 }
 
 const ApiClientContext = createContext<ApiClientContextType | null>(null);
 
 const ApiClientProvider: React.FC = ({ children }) => {
-  const [apiClient, setApiClient] = useState(
-    rest.EvergreenClient("", "", "", "")
-  );
+  const [apiClient, setApiClient] = useState(rest.EvergreenClient());
 
-  useEffect(() => {
-    tryLoadConfig();
-  }, []);
+  const updateConfig = (username: string, key?: string) => {
+    setApiClient(rest.EvergreenClient({ username, key }));
+  };
 
   const contextValue: ApiClientContextType = {
     apiClient,
     actions: {
-      tryLoadConfig,
       updateConfig
     }
   };
-
-  async function updateConfig(configObj: ClientConfig) {
-    setApiClient(rest.EvergreenClient(configObj.api_url, configObj.ui_url));
-  }
-
-  // the configuration of Spruce's environment is currently accomplished by requesting a config file from S3
-  // that config file determines the api url and the ui url (which is just the domain)
-  // TODO: use env-cmd to configure arbitrary build environments instead of using the config file
-  function tryLoadConfig() {
-    fetch(configPath).then((resp: Response) => {
-      resp.json().then(
-        (config: object) => {
-          if (IsValidConfig(config)) {
-            updateConfig(config as ClientConfig);
-          } else {
-            console.log("Config is missing required fields");
-          }
-        },
-        (reason: any) => {
-          console.log(
-            "Error parsing config. You may need to manually drop a config file. Error: " +
-              reason
-          );
-        }
-      );
-    });
-  }
 
   return (
     <ApiClientContext.Provider value={contextValue as ApiClientContextType}>
---
+++ src/utils/getEnvironmentVariables.ts
@@ -0,0 +1,10 @@
+export const getApiUrl = () => process.env.REACT_APP_API_URL || "";
+
+export const getBugsnagApiKey = () =>
+  process.env.REACT_APP_BUGSNAG_API_KEY || "i-am-a-fake-key";
+
+export const getUiUrl = () => process.env.REACT_APP_UI_URL || "";
+
+export const isDevelopment = () => process.env.NODE_ENV === "development";
+
+export const isTest = () => process.env.NODE_ENV === "test";
