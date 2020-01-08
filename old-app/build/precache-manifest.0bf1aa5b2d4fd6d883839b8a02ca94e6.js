a9faca598d7ea57a697c7ac6041af8278197c8f3
Arjun Patel <arjun453@gmail.com>
2020-01-08 16:19:34 -0500

    move spruce into old-app folder

---
+++ old-app/build/precache-manifest.0bf1aa5b2d4fd6d883839b8a02ca94e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e19adcacd2ebfd20607b",
+    "url": "/static/css/main.661a25cb.chunk.css"
+  },
+  {
+    "revision": "e19adcacd2ebfd20607b",
+    "url": "/static/js/main.e19adcac.chunk.js"
+  },
+  {
+    "revision": "edf1c624e45e253fddac",
+    "url": "/static/js/1.edf1c624.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "efc476ffbe5cfab892b902e072e6c358",
+    "url": "/static/media/evergreen_green.efc476ff.png"
+  },
+  {
+    "revision": "082207a99615e09a403feb47cdc16d8c",
+    "url": "/index.html"
+  }
+];
