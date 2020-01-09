14166340ad7a2c761c6f52d6de6886b4724540d9
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-01-09 13:43:38 -0500

    delete test

--- src/components/login/Login.test.tsx
+++ src/components/login/Login.test.tsx
@@ -31,29 +31,4 @@ describe("Login", () => {
     // check that the state was updated correctly
     expect(wrapper.state("open")).toBe(false);
   });
-
-  it("clicking submit button closes login modal and saves token locally", done => {
-    // find the login button and click it
-    const loginButton = wrapper.find("#login-button").hostNodes();
-    loginButton.simulate("click");
-    // check that the state was set correctly
-    expect(wrapper.state("open")).toBe(true);
-    // fill in the username and password fields
-    wrapper.setState({
-      username: "domino.weir",
-      password: "password"
-    });
-    // find the submit button and click it
-    const submitButton = wrapper.find("#submit-button").hostNodes();
-    submitButton.simulate("click");
-    // check that the state reflects the submit button click
-    setImmediate(() => {
-      expect(wrapper.state("submitted")).toBe(true);
-      // check that the modal is now closed
-      expect(wrapper.state("open")).toBe(false);
-      // TODO: check that cookie was included in the response
-      // should be mci-token, located in response.request.response.headers["set-cookie"]
-      done();
-    });
-  });
 });
