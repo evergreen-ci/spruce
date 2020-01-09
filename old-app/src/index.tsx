1e5b3f5cd956e5c0ab3506dcc3e705e51717eb04
Arjun Patel <arjun453@gmail.com>
2020-01-09 16:25:39 -0500

    delete old-app folder

--- old-app/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import "./styles.css";
-import * as ReactDOM from "react-dom";
-import Evergreen from "./components/app/App";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<Evergreen />, document.getElementById("root") as HTMLElement);
-
-registerServiceWorker();
