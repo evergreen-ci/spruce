365c60388dd70c7005e8f7dd42745b4755410954
Arjun Patel <arjun453@gmail.com>
2020-01-10 09:59:49 -0500

    EVG 7159 Part 3 (#43)
    * move spruce into old-app folder
    
    * create new app
    
    * move GQLWrapper from old-app to new-app
    
    * install dependencies for GQLWrapper
    
    * adopt some tsconfig settings from old-app
    
    * move env var accessor file to new app
    
    * install env-cmd

---
+++ new-app/src/utils/getEnvironmentVariables.ts
@@ -0,0 +1,17 @@
+export const getApiUrl = () => process.env.REACT_APP_API_URL || "";
+
+export const getBugsnagApiKey = () =>
+  process.env.REACT_APP_BUGSNAG_API_KEY || "i-am-a-fake-key";
+
+export const getUiUrl = () => process.env.REACT_APP_UI_URL || "";
+
+export const isDevelopment = () => process.env.NODE_ENV === "development";
+
+export const isTest = () => process.env.NODE_ENV === "test";
+
+export const getSchemaString = () => process.env.REACT_APP_SCHEMA_STRING || "";
+
+export const getGQLUrl = () => process.env.REACT_APP_GQL_URL || "";
+
+export const shouldEnableGQLMockServer = () =>
+  process.env.REACT_APP_ENABLE_GQL_MOCK_SERVER === "true" ? true : false;
