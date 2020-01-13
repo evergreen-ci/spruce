10233104f2a3b4870baf72fba9a3632790dbae36
Arjun Patel <arjun453@gmail.com>
2020-01-13 13:10:56 -0500

    Evg 7159 part 7 (#50)

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import bugsnag from "@bugsnag/browser";
+import bugsnagReact from "@bugsnag/plugin-react";
+import GQLWrapper from "utils/gql/GQLWrapper";
+import {
+  getBugsnagApiKey,
+  getGQLUrl,
+  getSchemaString,
+  isDevelopment,
+  isTest,
+  shouldEnableGQLMockServer
+} from "./utils/getEnvironmentVariables";
+
+const bugsnagClient = bugsnag(getBugsnagApiKey());
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin("react");
+
+const App: React.FC = () => {
+  return (
+    <ErrorBoundary>
+      <GQLWrapper
+        gqlURL={getGQLUrl()}
+        isDevelopment={isDevelopment()}
+        isTest={isTest()}
+        schemaString={getSchemaString()}
+        credentials="include"
+        shouldEnableGQLMockServer={shouldEnableGQLMockServer()}
+      >
+        {/*add routes here*/}
+      </GQLWrapper>
+    </ErrorBoundary>
+  );
+};
+
+export default App;
