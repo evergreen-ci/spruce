0f3600281d444377ef27840f112decefa44b8880
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-01-15 11:47:06 -0500

    MCI-3108 cypress

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,11 @@
+describe("My First Test", function() {
+  it("Does not do much!", function() {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("My First Test", function() {
+  it("Does not do much!", function() {
+    expect(true).to.equal(false);
+  });
+});
