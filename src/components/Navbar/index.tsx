76805f807aadba0d0ecd20f4e755d4b6b7901dab
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-01-23 13:13:29 -0500

    logout from navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled/macro";
+import { logout, useAuthDispatchContext } from "../../context/auth";
 
 export const Navbar: React.FC = () => {
+  const dispatch = useAuthDispatchContext();
+
+  const logoutHandler = () => {
+    console.log("hello");
+    logout(dispatch);
+  };
+
   return (
     <Wrapper>
-      <InnerWrapper>logout</InnerWrapper>
+      <InnerWrapper>
+        <LogoutButton onClick={logoutHandler}>Logout</LogoutButton>
+      </InnerWrapper>
     </Wrapper>
   );
 };
@@ -23,4 +33,8 @@ const InnerWrapper = styled.div({
   alignItems: "center",
   justifyContent: "flex-end",
   height: "100%"
+});
+
+const LogoutButton = styled.div({
+  cursor: "pointer"
 });
