d46c8e048b2ad21bf4fe7badf3c7bcd8d82bc144
Arjun Patel <arjun453@gmail.com>
2020-01-27 16:24:24 -0500

    integration tests

---
+++ cypress/integration/taskroute.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe("Task Page Route", function() {
+  it("User is redirected to logs if they land on /task/{taskID}", function() {
+    cy.visit("/task/taskID");
+    cy.location("pathname").should("include", "/task/taskID/logs");
+  });
+
+  it("Browser history is placed when user lands on /task/{taskID}", function() {
+    cy.visit("/");
+    cy.visit("/task/taskID");
+    cy.go("back");
+    cy.location("pathname").should("eq", "/");
+  });
+});
