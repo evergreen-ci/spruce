eff5e59c75ff8e59e7b590bb2b75e2bb5bd56b11
Arjun Patel <arjun453@gmail.com>
2020-01-27 14:49:47 -0500

    if tab isn't specified, route to /logs

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,6 +1,15 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useParams, useHistory, useLocation } from "react-router-dom";
+const DEFAULT_TAB = "logs";
 const Task = () => {
+  const params = useParams();
+  const history = useHistory();
+  useEffect(() => {
+    if (!params.tab) {
+      history.replace(`/task/${params.taskID}/logs`);
+    }
+  }, [params.tab]);
+
   return <div>Task Page</div>;
 };
 
