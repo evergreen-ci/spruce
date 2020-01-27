7c5538b9e7e6675df0a78d8abd5791ced43f5dae
Arjun Patel <arjun453@gmail.com>
2020-01-27 16:24:11 -0500

    convert to tsx

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
+import { RouteComponentProps } from "react-router-dom";
 import { useParams, useHistory } from "react-router-dom";
 
 const DEFAULT_TAB = "logs";
 
-const Task = () => {
-  const params = useParams();
+const Task: React.FC<RouteComponentProps> = () => {
+  const params = useParams<{ tab?: string; taskID: string }>();
   const history = useHistory();
   useEffect(() => {
     if (!params.tab) {
       history.replace(`/task/${params.taskID}/${DEFAULT_TAB}`);
     }
-  }, [params.tab]);
+  }, [params.tab, params.taskID, history]);
 
-  return <div>Task Page</div>;
+  return <div>Task Page!!!!!!!!</div>;
 };
 
 export default Task;
