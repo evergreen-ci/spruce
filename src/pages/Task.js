532210702ae779acfc1d78fbfcc95fd67d31731c
Arjun Patel <arjun453@gmail.com>
2020-01-27 15:55:14 -0500

    remove unused import

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useParams, useHistory, useLocation } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 const DEFAULT_TAB = "logs";
 
