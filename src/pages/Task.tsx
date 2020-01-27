0b2f8f7c4720524202a8eadc29e3332a3b198125
Arjun Patel <arjun453@gmail.com>
2020-01-27 18:03:26 -0500

    use tab type

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -2,10 +2,11 @@ import React, { useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { useParams, useHistory } from "react-router-dom";
 
+type Tab = "logs" | "tests" | "files" | "build-baron";
 const DEFAULT_TAB = "logs";
 
 const Task: React.FC<RouteComponentProps> = () => {
-  const params = useParams<{ tab?: string; taskID: string }>();
+  const params = useParams<{ tab?: Tab; taskID: string }>();
   const history = useHistory();
   useEffect(() => {
     if (!params.tab) {
