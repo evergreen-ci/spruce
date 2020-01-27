d619ae22942b823697d348c3911b3b3edd8174e6
Arjun Patel <arjun453@gmail.com>
2020-01-27 14:50:15 -0500

    use constant

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { useParams, useHistory, useLocation } from "react-router-dom";
+
 const DEFAULT_TAB = "logs";
+
 const Task = () => {
   const params = useParams();
   const history = useHistory();
   useEffect(() => {
     if (!params.tab) {
-      history.replace(`/task/${params.taskID}/logs`);
+      history.replace(`/task/${params.taskID}/${DEFAULT_TAB}`);
     }
   }, [params.tab]);
 
