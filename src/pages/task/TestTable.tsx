f44b4948ae05c9a43086e1c8a7ac8c7e9e40817d
Arjun Patel <arjun453@gmail.com>
2020-01-28 11:48:11 -0500

    wip

--- src/pages/task/TestTable.tsx
+++ src/pages/task/TestTable.tsx
@@ -2,11 +2,15 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 
+enum RequiredQueryParams {
+  Sort = "sort",
+  Cat= "category"
+}
 export const TestTable: React.FC = () => {
   const { search } = useLocation();
   useEffect(() => {
     const parsed = queryString.parse(search);
-    console.log(parsed);
+    if(parsed["sort"] != )
   }, [search]);
   return <div>Test table</div>;
 };
