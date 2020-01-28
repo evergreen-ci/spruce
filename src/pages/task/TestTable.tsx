b10d4bc391d60a4a8417f0df0cec89ecf202b6b1
Arjun Patel <arjun453@gmail.com>
2020-01-28 11:38:23 -0500

    wip

--- src/pages/task/TestTable.tsx
+++ src/pages/task/TestTable.tsx
@@ -1,3 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
-export const TestTable = () => {};
+export const TestTable: React.FC = () => {
+  const { search } = useLocation();
+  useEffect(() => {
+    const parsed = queryString.parse(search);
+    console.log(parsed);
+  }, [search]);
+  return <div>Test table</div>;
+};
