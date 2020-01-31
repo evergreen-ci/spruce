b4f0d0c0928355512ed4d06c457005dea91147c1
Arjun Patel <arjun453@gmail.com>
2020-01-31 09:16:05 -0500

    refactor

--- src/pages/task/TestsTable.tsx
+++ src/pages/task/TestsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useHistory } from "react-router-dom";
 import queryString from "query-string";
 import { TestsTableCore } from "pages/task/TestsTableCore";
--- src/pages/task/TestsTableCore.tsx
+++ src/pages/task/TestsTableCore.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { TESTS_QUERY } from "utils/gql/queries";
 import { msToTime } from "utils/string";
 import { Tag, Spin } from "antd";
@@ -8,6 +8,7 @@ import { useQuery } from "@apollo/react-hooks";
 import queryString from "query-string";
 import { Categories, RequiredQueryParams, Sort } from "utils/enums";
 import get from "lodash.get";
+import { ColumnProps, TableProps } from "antd/es/table";
 
 interface TaskTests {
   id: String;
@@ -20,7 +21,7 @@ interface UpdateQueryArg {
   taskTests: [TaskTests];
 }
 
-const loadMoreContent = () => (
+const loadMoreContent = (): JSX.Element => (
   <div
     style={{
       textAlign: "center",
@@ -32,7 +33,7 @@ const loadMoreContent = () => (
     <Spin tip="Loading..." />
   </div>
 );
-const columns = [
+const columns: ColumnProps<TaskTests>[] = [
   {
     title: "Name",
     dataIndex: "testFile",
@@ -79,14 +80,14 @@ export const TestsTableCore: React.FC<{
   const { taskID } = useParams();
   const { search, pathname } = useLocation();
   const { replace } = useHistory();
-  const [latestQueryTs, setLatestQueryTs] = useState(Date.now());
-  const [isLoading, setIsLoading] = useState(0);
-  const { loading, error, data, fetchMore } = useQuery(TESTS_QUERY, {
+
+  const { data, fetchMore, networkStatus } = useQuery(TESTS_QUERY, {
     variables: {
       id: taskID,
       dir: initialSort === Sort.Asc ? "ASC" : "DESC",
       cat: initialCategory
-    }
+    },
+    notifyOnNetworkStatusChange: true
   });
   const parsed = queryString.parse(search);
   const category = (parsed[RequiredQueryParams.Category] || "")
@@ -94,71 +95,69 @@ export const TestsTableCore: React.FC<{
     .toUpperCase();
   const sort = parsed[RequiredQueryParams.Sort];
   useEffect(() => {
-    const dispatchTs = Date.now();
-    setIsLoading(isLoading + 1);
-    setLatestQueryTs(dispatchTs);
-    fetchMore({
-      variables: {
+    const elements = document.querySelectorAll(
+      "th.ant-table-column-has-actions.ant-table-column-has-sorters"
+    );
+    if (networkStatus < 7) {
+      elements.forEach(el => {
+        (el as HTMLElement).style["pointer-events"] = "none";
+      });
+    } else {
+      elements.forEach(el => {
+        (el as HTMLElement).style["pointer-events"] = "auto";
+      });
+    }
+  }, [networkStatus]);
+
+  useEffect(() => {
+    if (networkStatus >= 7) {
+      const variables = {
         cat: category,
         dir: sort === Sort.Asc ? "ASC" : "DESC",
         pageNum: 0
+      };
+      fetchMore({
+        variables,
+        updateQuery: (
+          prev: UpdateQueryArg,
+          { fetchMoreResult }: { fetchMoreResult: UpdateQueryArg }
+        ) => {
+          if (!fetchMoreResult) {
+            return prev;
+          }
+          return fetchMoreResult;
+        }
+      });
+    }
+  }, [category, sort]);
+
+  const dataSource: [TaskTests] = get(data, "taskTests", []);
+  const onFetch = () => {
+    fetchMore({
+      variables: {
+        pageNum: dataSource.length / 10,
+        cat: category,
+        dir: sort === Sort.Asc ? "ASC" : "DESC"
       },
       updateQuery: (
         prev: UpdateQueryArg,
         { fetchMoreResult }: { fetchMoreResult: UpdateQueryArg }
       ) => {
-        if (dispatchTs !== latestQueryTs) {
-          console.log("ignoring stale response from sorter");
-          return data;
+        if (!fetchMoreResult) {
+          return prev;
         }
-        if (!fetchMoreResult) return prev;
-        return fetchMoreResult;
+        return Object.assign({}, prev, {
+          taskTests: [...prev.taskTests, ...fetchMoreResult.taskTests]
+        });
       }
-    }).then(v => {
-      setIsLoading(isLoading - 1);
     });
-  }, [category, sort]);
-
-  // need to jus loading from useQuery... this only changes from on => off once
-  // so we need to listen to it
-  useEffect(() => {
-    setIsLoading(loading ? isLoading + 1 : isLoading - 1);
-  }, [loading]);
-
-  const dataSource = get(data, "taskTests", []);
-  const onFetch = () => {
-    if (!isLoading) {
-      setIsLoading(isLoading + 1);
-      const dispatchTs = Date.now();
-      setLatestQueryTs(dispatchTs);
-      fetchMore({
-        variables: {
-          pageNum: dataSource.length / 10,
-          cat: category,
-          dir: sort === Sort.Asc ? "ASC" : "DESC"
-        },
-        updateQuery: (
-          prev: UpdateQueryArg,
-          { fetchMoreResult }: { fetchMoreResult: UpdateQueryArg }
-        ) => {
-          // ignore out stale responses
-          if (dispatchTs !== latestQueryTs) {
-            console.log("ignoring stale response from loader");
-            return data;
-          }
-          if (!fetchMoreResult) {
-            return prev;
-          }
-          return Object.assign({}, prev, {
-            taskTests: [...prev.taskTests, ...fetchMoreResult.taskTests]
-          });
-        }
-      }).then(v => {
-        setIsLoading(isLoading - 1);
-      });
-    }
   };
-  const onChange = (pagination, filters, sorter, extra) => {
+  const onChange: TableProps<TaskTests>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
     const parsed = queryString.parse(search);
     const { order, columnKey } = sorter;
     let hasDiff = false;
@@ -188,15 +187,14 @@ export const TestsTableCore: React.FC<{
   // only need sort order set to reflect initial state in URL
   columns.find(({ key }) => key === initialCategory)["defaultSortOrder"] =
     initialSort === Sort.Asc ? "ascend" : "descend";
-
   return (
     <div>
       <InfinityTable
         key="key"
-        loading={isLoading}
+        loading={networkStatus < 7}
         onFetch={onFetch}
         pageSize={2000}
-        loadingIndicator={loadMoreContent()}
+        loadingIndicator={loadMoreContent}
         columns={columns}
         scroll={{ y: 300 }}
         dataSource={dataSource}
