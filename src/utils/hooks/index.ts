ead0cb54e5293d83e6cc312a23e6593e47595483
Arjun Patel <arjun453@gmail.com>
2020-01-31 10:59:43 -0500

    use generic

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from "react";
 
-export function usePrevious(value) {
-  const ref = useRef();
+export const usePrevious = <T>(state: T): T | undefined => {
+  const ref = useRef<T>();
   useEffect(() => {
-    ref.current = value;
+    ref.current = state;
   });
   return ref.current;
-}
+};
