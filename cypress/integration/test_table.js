f6fafea512e9a61860f63d1b7d83ae4effe5ab50
Arjun Patel <arjun453@gmail.com>
2020-02-05 17:48:28 -0500

    work around not being able to match post body in cypress

--- cypress/integration/test_table.js
+++ cypress/integration/test_table.js
@@ -3,11 +3,12 @@
 describe("tests table", function() {
   it("Navigate to tests table and click on table header to adjust query params", function() {
     cy.server();
-    cy.route("POST", "/graphql/query").as("gqlQuery");
     cy.login();
+    cy.route("POST", "/graphql/query").as("gqlQuery");
     cy.visit(
       "/task/mci_windows_test_agent_8a4f834ba24ddf91f93d0a96b90452e9653f4138_17_10_23_21_58_33/tests"
     );
+    cy.wait(["@gqlQuery"]);
     cy.wait(["@gqlQuery"]);
     cy.contains("Name").click();
     cy.location().should(loc => {
