f81c7751f3939b86c6999614b644e2a5399a2753
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-02-10 14:21:00 -0500

    change task in tests to one in local evergreen

--- cypress/integration/breadcrumbs.js
+++ cypress/integration/breadcrumbs.js
@@ -1,13 +1,13 @@
 /// <reference types="Cypress" />
 
 const taskRoute =
-  "/task/mci_windows_test_agent_8a4f834ba24ddf91f93d0a96b90452e9653f4138_17_10_23_21_58_33";
+  "/task/performance_linux_mmap_standalone_insert_37cb7ea09393e88662e3139bd20fa29e59f2a1a3_18_01_11_00_01_36";
 
 describe("TaskBreadcrumb", function() {
   it("Shows tasks display name", function() {
     cy.visit(taskRoute);
     cy.login();
-    cy.get("span[id=bc-task]").should("include.text", "test-agent");
+    cy.get("span[id=bc-task]").should("include.text", "insert");
   });
 
   it("Shows the patches name", function() {
@@ -19,7 +19,7 @@ describe("TaskBreadcrumb", function() {
     cy.get("span[id=bc-patch]").click();
     cy.url().should(
       "include",
-      "/patch/mci_8a4f834ba24ddf91f93d0a96b90452e9653f4138"
+      "/patch/performance_37cb7ea09393e88662e3139bd20fa29e59f2a1a3"
     );
   });
 
@@ -33,7 +33,7 @@ describe("TaskBreadcrumb", function() {
 
 describe("PatchBreadcrumb", function() {
   it("Shows the patches name", function() {
-    cy.visit("/patch/mci_8a4f834ba24ddf91f93d0a96b90452e9653f4138");
+    cy.visit("/patch/performance_37cb7ea09393e88662e3139bd20fa29e59f2a1a3");
     cy.login();
     // TODO: replace "Patch" with the actual patch's name once patch query is done
     cy.get("span[id=bc-patch]").should("include.text", "Patch");
