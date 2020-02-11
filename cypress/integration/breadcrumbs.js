9fb5abc42c182fcb27518f47eee0abf0a830ac68
Trey Granderson <trey.granderson@mongodb.com>
2020-02-11 14:06:29 -0500

    EVG-7289: fix e2e tests in ci (#73)
    * EVG-7289: sleep 10 before running cypress tests
    
    * change task in tests to one in local evergreen
    
    * change patch id in tests
    
    * lil changes
    
    * change tasks queried in tests
    
    * no log
    
    * delete log func

--- cypress/integration/breadcrumbs.js
+++ cypress/integration/breadcrumbs.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 const taskRoute =
-  "/task/mci_windows_test_agent_8a4f834ba24ddf91f93d0a96b90452e9653f4138_17_10_23_21_58_33";
+  "/task/logkeeper_ubuntu_test_edd78c1d581bf757a880777b00685321685a8e67_16_10_20_21_58_58";
 
 describe("TaskBreadcrumb", function() {
   before(() => {
@@ -10,7 +10,7 @@ describe("TaskBreadcrumb", function() {
 
   it("Shows tasks display name", function() {
     cy.visit(taskRoute);
-    cy.get("span[id=bc-task]").should("include.text", "test-agent");
+    cy.get("span[id=bc-task]").should("include.text", "test");
   });
 
   it("Shows the patches name", function() {
@@ -22,7 +22,7 @@ describe("TaskBreadcrumb", function() {
     cy.get("span[id=bc-patch]").click();
     cy.url().should(
       "include",
-      "/patch/mci_8a4f834ba24ddf91f93d0a96b90452e9653f4138"
+      "/patch/logkeeper_edd78c1d581bf757a880777b00685321685a8e67"
     );
   });
 
@@ -38,7 +38,7 @@ describe("PatchBreadcrumb", function() {
     cy.login();
   });
   it("Shows the patches name", function() {
-    cy.visit("/patch/mci_8a4f834ba24ddf91f93d0a96b90452e9653f4138");
+    cy.visit("/patch/logkeeper_edd78c1d581bf757a880777b00685321685a8e67");
     // TODO: replace "Patch" with the actual patch's name once patch query is done
     cy.get("span[id=bc-patch]").should("include.text", "Patch");
   });
