7ce9903089fb7a3b17afd6a012520a04397b22c4
Arjun Patel <arjun453@gmail.com>
2020-02-11 17:13:02 -0500

    use sdl schema from github for query linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+var fs = require("fs");
+let schema;
+try {
+  schema = fs.readFileSync("./sdlschema.graphql", "utf8");
+} catch (e) {
+  schema = "";
+}
+
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -15,19 +23,7 @@ module.exports = {
     "graphql/template-strings": [
       "error",
       {
-        // Import default settings for your GraphQL client. Supported values:
-        // 'apollo', 'relay', 'lokka', 'fraql', 'literal'
-        env: "apollo",
-        // Import your schema JSON here
-        schemaJson: require("./schema.json")
-
-        // OR provide absolute path to your schema JSON (but not if using `eslint --cache`!)
-        // schemaJsonFilepath: path.resolve(__dirname, './schema.json'),
-
-        // OR provide the schema in the Schema Language format
-        // schemaString: printSchema(schema),
-
-        // tagName is gql by default
+        schemaString: schema
       }
     ]
   }
