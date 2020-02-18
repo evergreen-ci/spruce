3e86360f5a5f78461c33bc87af2216be5e77f774
Arjun Patel <arjun453@gmail.com>
2020-02-18 11:57:42 -0500

    match on request body operationName

--- cypress/utils/networking.js
+++ cypress/utils/networking.js
@@ -1,8 +1,8 @@
-// this fn will wait until a query with queryName is executed on the aliased route
+// this fn will wait until a gql query with queryName is executed on the aliased route
 export const waitForGQL = (alias, queryName) => {
   const waitOnce = () => {
     cy.wait(alias).then(xhr => {
-      if (!xhr.requestBody || !xhr.requestBody.query.includes(queryName)) {
+      if (!xhr.requestBody || xhr.requestBody.operationName !== queryName) {
         waitOnce();
       }
     });
