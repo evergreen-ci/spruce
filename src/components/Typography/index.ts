2c62c9e0a272a5aecbbfd1362106bb6c1cd5a10e
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-02-20 10:42:35 -0500

    typography components

---
+++ src/components/Typography/index.ts
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled/macro";
+import { grayDark } from "../../contants/colors";
+
+export const H1 = styled.h1`
+  font-size: 30px;
+  line-height: 36px;
+  margin-bottom: 17px;
+  color: ${grayDark};
+`;
+
+export const H2 = styled.h2`
+  font-size: 18px;
+  line-height: 21px;
+  margin-bottom: 8px;
+  color: ${grayDark};
+`;
+
+export const H3 = styled.h3`
+  font-size: 15px;
+  line-height: 17px;
+  margin-bottom: 5px;
+  color: ${grayDark};
+`;
+
+export const P1 = styled.p`
+  font-size: 15px;
+  line-height: 17px;
+  margin-bottom: 5px;
+  color: ${grayDark};
+`;
+
+export const P2 = styled.p`
+  font-size: 12px;
+  line-height: 14px;
+  margin-bottom: 13px;
+  color: ${grayDark};
+`;
