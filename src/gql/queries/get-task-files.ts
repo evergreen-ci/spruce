b197366167286221b1f6f4b2bc71b1dfce9c2aa6
Arjun Patel <arjun453@gmail.com>
2020-02-27 11:58:15 -0500

    remove visibility

--- src/gql/queries/get-task-files.ts
+++ src/gql/queries/get-task-files.ts
@@ -1,31 +1,26 @@
-import gql from "graphql-tag"
+import gql from "graphql-tag";
 
 export const GET_TASK_FILES = gql`
-query taskFiles(
-    $id: String!
-) {
+  query taskFiles($id: String!) {
     taskFiles(taskId: $id) {
-        taskName
-        files {
-            name
-            link
-            visibility
-        }
+      taskName
+      files {
+        name
+        link
+      }
     }
-}
-`
+  }
+`;
 interface File {
-    name: string;
-    link: string;
-    visibility: string;
-} 
+  name: string;
+  link: string;
+}
 
 export interface TaskFilesData {
-    name: string;
-    files: [File]
+  name: string;
+  files: [File];
 }
 
 export interface TaskFilesVars {
-    id: string;
+  id: string;
 }
-
