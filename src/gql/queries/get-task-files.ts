720b4f684c62e36c3f07fee38fcdced88cc80d20
Arjun Patel <arjun453@gmail.com>
2020-02-27 11:40:26 -0500

    task files query

---
+++ src/gql/queries/get-task-files.ts
@@ -0,0 +1,27 @@
+import gql from "graphql-tag"
+
+export const GET_TASK_FILES = gql`
+query taskFiles(
+    $id: String!
+) {
+    taskFiles(taskId: $id) {
+        taskName
+        files {
+            name
+            link
+            visibility
+        }
+    }
+}
+`
+interface File {
+    name: string;
+    link: string;
+    visibility: string;
+} 
+
+export interface TaskFilesData {
+    name: string;
+    files: [File]
+}
+
