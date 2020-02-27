0bc72351f4dcae9076bed5a37c81f69d0b7de208
Arjun Patel <arjun453@gmail.com>
2020-02-27 11:54:33 -0500

    make files request

--- src/pages/task/FilesTables/index.tsx
+++ src/pages/task/FilesTables/index.tsx
@@ -1,5 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import {
+  GET_TASK_FILES,
+  TaskFilesData,
+  TaskFilesVars
+} from "gql/queries/get-task-files";
+import { useQuery } from "@apollo/react-hooks/lib/useQuery";
 
-export const FilesTables: React.FC = () => { 
-    return <div>Files tables</div>
-}
+export const FilesTables: React.FC = () => {
+  const { taskID } = useParams();
+  const { data, loading, error } = useQuery<TaskFilesData, TaskFilesVars>(
+    GET_TASK_FILES,
+    {
+      variables: {
+        id: taskID
+      }
+    }
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  console.log(data);
+
+  return <div>Files tables</div>;
+};
