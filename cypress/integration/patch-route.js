3eaadc0cd3937c97d47c84b13012d2e3917de7ea
Arjun Patel <arjun453@gmail.com>
2020-03-02 17:11:51 -0500

    adjust patch id

--- cypress/integration/patch-route.js
+++ cypress/integration/patch-route.js
@@ -1,8 +1,9 @@
 /// <reference types="Cypress" />
 
 const patch = {
-  id: "5e53f9c9a0182531515737ef",
-  desc: "'evergreen-ci/evergreen' pull request #3186 by bsamek: EVG-7425 Don't send ShouldExit to unprovisioned hosts (https://github.com/evergreen-ci/evergreen/pull/3186)"
+  id: "5e4ff3abe3c3317e352062e4",
+  desc:
+    "'evergreen-ci/evergreen' pull request #3186 by bsamek: EVG-7425 Don't send ShouldExit to unprovisioned hosts (https://github.com/evergreen-ci/evergreen/pull/3186)"
 };
 
 const badPatch = {
@@ -21,7 +22,9 @@ describe("Patch route", function() {
 
   it("'Base commit' link in metadata links to version page of legacy UI", function() {
     cy.visit(`/patch/${patch.id}`);
-    cy.get("a[id=patch-base-commit]").should("have.attr", "href").and("eq", "http://localhost:9090/version/5e4ff3abe3c3317e352062e4")
+    cy.get("a[id=patch-base-commit]")
+      .should("have.attr", "href")
+      .and("eq", "http://localhost:9090/version/5e4ff3abe3c3317e352062e4");
   });
 
   it("Shows an error page if there was a problem loading data", () => {
