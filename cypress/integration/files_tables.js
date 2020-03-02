d3b1fd074dac43ad83e5506f64343eab0b93c567
Arjun Patel <arjun453@gmail.com>
2020-03-02 12:37:46 -0500

    update test name

--- cypress/integration/files_tables.js
+++ cypress/integration/files_tables.js
@@ -6,7 +6,7 @@ const FILES_ROUTE = "/task/evergreen_ubuntu1604_89/files";
 const FILES_ROUTE_WITHOUT_FILES =
   "/task/evergreen_ubuntu1604_test_model_commitqueue_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48/files ";
 
-describe("tests table", function() {
+describe("files table", function() {
   beforeEach(() => {
     cy.server();
     cy.login();
