5f28b6d83a8dfd0bf4b639c4b75a4177afa2c9e5
Arjun Patel <arjun453@gmail.com>
2020-03-02 18:12:15 -0500

    fix test

--- cypress/integration/breadcrumbs.js
+++ cypress/integration/breadcrumbs.js
@@ -19,6 +19,8 @@ describe("TaskBreadcrumb", function() {
   });
 
   it("Clicking on the patch breadcrumb goes to patch for task", function() {
+    cy.login();
+    cy.visit(taskRoute);
     cy.get("span[id=bc-patch]").click();
     cy.url().should(
       "include",
