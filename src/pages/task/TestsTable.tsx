15192d0ce5079dbb8f09fee3bd50efd64715a7d9
Arjun Patel <arjun453@gmail.com>
2020-03-02 15:06:46 -0500

    try uppercase first letter

--- src/pages/task/TestsTable.tsx
+++ src/pages/task/TestsTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { TestsTableCore } from "./testsTable/TestsTableCore";
+import { TestsTableCore } from "./TestsTable/TestsTableCore";
 import { useLocation, useHistory } from "react-router-dom";
 import {
   RequiredQueryParams,
