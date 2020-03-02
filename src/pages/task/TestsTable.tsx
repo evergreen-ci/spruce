5a1fff179dbd2937b8b280b840232e6a28bbd080
Arjun Patel <arjun453@gmail.com>
2020-03-02 16:37:18 -0500

    rename folder to lowercase

--- src/pages/task/TestsTable.tsx
+++ src/pages/task/TestsTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { TestsTableCore } from "./TestsTable/TestsTableCore";
+import { TestsTableCore } from "./testsTable/TestsTableCore";
 import { useLocation, useHistory } from "react-router-dom";
 import {
   RequiredQueryParams,
