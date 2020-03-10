e343ea69c623d06416068a17065a926ee9efccef
Arjun Patel <arjun453@gmail.com>
2020-03-10 11:44:38 -0400

    read logtype from url

--- src/pages/task/Logs.tsx
+++ src/pages/task/Logs.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import queryString from "query-string";
+import { GET_TASK_LOGS, TaskLogsQuery } from "gql/queries/get-task-logs";
 import { Radio, RadioGroup } from "@leafygreen-ui/radio-group";
+import { useLocation, useHistory } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import queryString from "query-string";
 import styled from "@emotion/styled/macro";
-
-//import { GET_TASK_LOGS } from "gql/queries/get-task-logs";
 
 enum LogTypes {
   Agent = "agent",
@@ -18,22 +18,35 @@ enum QueryParams {
 }
 
 export const Logs: React.FC = props => {
-  const { search } = useLocation();
-  const parsed = queryString.parse(search);
+  const { search, pathname } = useLocation();
+  const { replace } = useHistory();
   const [currentLog, setCurrentLog] = useState<LogTypes>(DEFAULT_LOG_TYPE);
+  const { data, loading, error } = useQuery<TaskLogsQuery>(GET_TASK_LOGS);
+  const parsed = queryString.parse(search);
   const logTypeParam = (parsed[QueryParams.LogType] || "")
     .toString()
     .toLowerCase();
   useEffect(() => {
-    if (logTypeParam in LogTypes) {
+    if (
+      logTypeParam === LogTypes.Agent ||
+      logTypeParam === LogTypes.Event ||
+      logTypeParam === LogTypes.System ||
+      logTypeParam === LogTypes.Task
+    ) {
       setCurrentLog(logTypeParam as LogTypes);
     } else {
       setCurrentLog(DEFAULT_LOG_TYPE);
     }
   }, [logTypeParam]);
 
-  const onChangeLog = (event: React.ChangeEvent<HTMLInputElement>): void =>
-    setCurrentLog(event.target.value as LogTypes);
+  const onChangeLog = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const nextLogType = event.target.value as LogTypes;
+    replace(
+      `${pathname}?${queryString.stringify({
+        [QueryParams.LogType]: nextLogType
+      })}`
+    );
+  };
   return (
     <div>
       <StyledRadioGroup
