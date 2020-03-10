2b858f2956c6cfeea530f47e006c94ffd4af4013
Arjun Patel <arjun453@gmail.com>
2020-03-10 15:13:42 -0400

    color lines

---
+++ src/components/styles/LogLines.tsx
@@ -0,0 +1,54 @@
+import styled, { StyledComponent } from "@emotion/styled/macro";
+
+export const DebugMessage = styled.div`
+  color: #666;
+`;
+
+export const InfoMessage = styled.div`
+  color: #333;
+`;
+
+export const WarnMessage = styled.div`
+  color: #ffa500;
+`;
+
+export const ErrorMessage = styled.div`
+  color: #ff0000;
+`;
+
+export const DMessage = styled.div`
+  color: #666;
+`;
+
+export const IMessage = styled.div`
+  color: #333;
+`;
+
+export const WMessage = styled.div`
+  color: #ffa500;
+`;
+
+export const EMessage = styled.div`
+  color: #ff0000;
+`;
+
+export const getLogLineWrapper = (severity: string): React.FC => {
+  switch (severity) {
+    case "DEBUG":
+      return DebugMessage;
+    case "INFO":
+      return InfoMessage;
+    case "WARN":
+      return WarnMessage;
+    case "D":
+      return DMessage;
+    case "I":
+      return IMessage;
+    case "W":
+      return WMessage;
+    case "E":
+      return EMessage;
+    default:
+      return styled.div``;
+  }
+};
--- src/pages/task/Logs.tsx
+++ src/pages/task/Logs.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { GET_TASK_LOGS, TaskLogsQuery } from "gql/queries/get-task-logs";
+import {
+  GET_TASK_LOGS,
+  TaskLogsQuery,
+  TaskEventLogData,
+  LogMessage
+} from "gql/queries/get-task-logs";
 import { Radio, RadioGroup } from "@leafygreen-ui/radio-group";
-import { useLocation, useHistory } from "react-router-dom";
+import { useLocation, useHistory, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import queryString from "query-string";
 import styled from "@emotion/styled/macro";
+import { getLogLineWrapper } from "components/styles/LogLines";
 
 enum LogTypes {
   Agent = "agent",
@@ -12,16 +18,45 @@ enum LogTypes {
   Task = "task",
   Event = "event"
 }
+
 const DEFAULT_LOG_TYPE = LogTypes.Task;
 enum QueryParams {
   LogType = "logtype"
 }
 
+const LogMessageLine = ({
+  message,
+  timestamp,
+  severity
+}: {
+  message: string;
+  timestamp: string;
+  severity: string;
+}) => {
+  let time = "";
+  if (timestamp) {
+    const d = new Date(timestamp);
+    // formats time YYYY/MM/DD HH:MM:SS.MS
+    time = `[${d.getFullYear()}/${d.getMonth() +
+      1}/${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${d.getMilliseconds()}}] `;
+  }
+  const LogLineWrapper = getLogLineWrapper(severity);
+  return (
+    <LogLineWrapper>
+      {time}
+      {message}
+    </LogLineWrapper>
+  );
+};
+
 export const Logs: React.FC = props => {
+  const { id } = useParams<{ id: string }>();
   const { search, pathname } = useLocation();
   const { replace } = useHistory();
   const [currentLog, setCurrentLog] = useState<LogTypes>(DEFAULT_LOG_TYPE);
-  const { data, loading, error } = useQuery<TaskLogsQuery>(GET_TASK_LOGS);
+  const { data, loading, error } = useQuery<TaskLogsQuery>(GET_TASK_LOGS, {
+    variables: { id }
+  });
   const parsed = queryString.parse(search);
   const logTypeParam = (parsed[QueryParams.LogType] || "")
     .toString()
@@ -38,7 +73,12 @@ export const Logs: React.FC = props => {
       setCurrentLog(DEFAULT_LOG_TYPE);
     }
   }, [logTypeParam]);
-
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
   const onChangeLog = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const nextLogType = event.target.value as LogTypes;
     replace(
@@ -47,6 +87,24 @@ export const Logs: React.FC = props => {
       })}`
     );
   };
+
+  let currentData: [TaskEventLogData] | [LogMessage];
+  switch (currentLog) {
+    case LogTypes.Agent:
+      currentData = data.taskLogs.agentLogs;
+      break;
+    case LogTypes.Event:
+      currentData = data.taskLogs.systemLogs;
+      break;
+    case LogTypes.System:
+      currentData = data.taskLogs.systemLogs;
+      break;
+    case LogTypes.Task:
+      currentData = data.taskLogs.taskLogs;
+      break;
+    default:
+      currentData = null;
+  }
   return (
     <div>
       <StyledRadioGroup
@@ -68,6 +126,19 @@ export const Logs: React.FC = props => {
           Event Logs
         </Radio>
       </StyledRadioGroup>
+      {currentData == null || !currentData.length ? (
+        <div>No logs</div>
+      ) : currentLog !== LogTypes.Event ? (
+        currentData.map(({ message, timestamp, severity }) => (
+          <LogMessageLine
+            message={message}
+            timestamp={timestamp}
+            severity={severity}
+          />
+        ))
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
