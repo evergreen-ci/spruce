3fb7bcacb1dfd69573e3e95bc32b22e7f536a7e3
Arjun Patel <arjun453@gmail.com>
2020-03-11 12:24:12 -0400

    format date

--- src/pages/task/logs/LogMessageLine.tsx
+++ src/pages/task/logs/LogMessageLine.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { LogMessage } from "gql/queries/get-task-logs";
 import { getLogLineWrapper } from "components/styles/LogLines";
+import { format } from "date-fns";
+const FORMAT_STR = "yyyy/mm/d, HH:mm:ss.SSS";
 
 export const LogMessageLine: React.FC<LogMessage> = props => {
   const { timestamp, severity, message } = props;
-  let time = "";
-  if (timestamp) {
-    const d = new Date(timestamp);
-    // formats time YYYY/MM/DD HH:MM:SS.MS
-    time = `[${d.getFullYear()}/${d.getMonth() +
-      1}/${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${d.getMilliseconds()}}] `;
-  }
+  const time = timestamp ? `${format(new Date(timestamp), FORMAT_STR)}] ` : "";
   const LogLineWrapper = getLogLineWrapper(severity);
   return (
     <LogLineWrapper>
