37bf494d560d16be7d59a7108547b047b5413d7f
Arjun Patel <arjun453@gmail.com>
2020-03-12 12:30:02 -0400

    update taskID

--- cypress/integration/task_logs.js
+++ cypress/integration/task_logs.js
@@ -1,8 +1,7 @@
 /// <reference types="Cypress" />
-import { waitForGQL } from "../utils/networking";
 
 const LOGS_ROUTE =
-  "/task/evergreen_ubuntu1604_js_test_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48/logs";
+  "/task/evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48/logs";
 describe("task logs view", function() {
   beforeEach(() => {
     cy.server();
