5b76374d044caf58e042d95605f033dedae1f469
Arjun Patel <arjun453@gmail.com>
2020-03-12 11:55:39 -0400

    destructure in sig

--- src/pages/task/logs/LogMessageLine.tsx
+++ src/pages/task/logs/LogMessageLine.tsx
@@ -4,8 +4,11 @@ import { getLogLineWrapper } from "pages/task/logs/logMessageLine/LogLines";
 import { format } from "date-fns";
 const FORMAT_STR = "yyyy/MM/d, HH:mm:ss.SSS";
 
-export const LogMessageLine: React.FC<LogMessage> = props => {
-  const { timestamp, severity, message } = props;
+export const LogMessageLine: React.FC<LogMessage> = ({
+  timestamp,
+  severity,
+  message
+}) => {
   const time = timestamp ? `[${format(new Date(timestamp), FORMAT_STR)}] ` : "";
   const LogLineWrapper = getLogLineWrapper(severity);
   return (
