72bef277c183698cc0d26f24d5e197d9cf2a03bd
Arjun Patel <arjun453@gmail.com>
2020-03-12 10:06:00 -0400

    simplify log lines

--- src/pages/task/logs/logMessageLine/LogLines.tsx
+++ src/pages/task/logs/logMessageLine/LogLines.tsx
@@ -16,38 +16,20 @@ export const ErrorMessage = styled.div`
   color: #ff0000;
 `;
 
-export const DMessage = styled.div`
-  color: #666;
-`;
-
-export const IMessage = styled.div`
-  color: #333;
-`;
-
-export const WMessage = styled.div`
-  color: #ffa500;
-`;
-
-export const EMessage = styled.div`
-  color: #ff0000;
-`;
-
 export const getLogLineWrapper = (severity: string): React.FC => {
   switch (severity) {
+    case "D":
     case "DEBUG":
       return DebugMessage;
-    case "INFO":
-      return InfoMessage;
+    case "W":
     case "WARN":
       return WarnMessage;
-    case "D":
-      return DMessage;
     case "I":
-      return IMessage;
-    case "W":
-      return WMessage;
+    case "INFO":
+      return InfoMessage;
     case "E":
-      return EMessage;
+    case "ERROR":
+      return ErrorMessage;
     default:
       return styled.div``;
   }
