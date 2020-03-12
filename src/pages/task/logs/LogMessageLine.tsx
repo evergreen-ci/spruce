f78ea123cc2e87fd10484c6f50eb0771d9ea5daf
Arjun Patel <arjun453@gmail.com>
2020-03-12 09:55:08 -0400

    fix date format

--- src/pages/task/logs/LogMessageLine.tsx
+++ src/pages/task/logs/LogMessageLine.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LogMessage } from "gql/queries/get-task-logs";
 import { getLogLineWrapper } from "pages/task/logs/logMessageLine/LogLines";
 import { format } from "date-fns";
-const FORMAT_STR = "yyyy/mm/d, HH:mm:ss.SSS";
+const FORMAT_STR = "yyyy/MM/d, HH:mm:ss.SSS";
 
 export const LogMessageLine: React.FC<LogMessage> = props => {
   const { timestamp, severity, message } = props;
