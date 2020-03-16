402a06883ca6d769e566b63dd3b9e50a784911e4
Arjun Patel <arjun453@gmail.com>
2020-03-16 10:03:34 -0400

    EVG-7389: Render logs in task page (#94)

---
+++ src/pages/task/logs/logTypes/logMessageLine/LogLines.tsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled/macro";
+
+const getLogLineComp = (color: string) => styled.div`
+  color: ${color};
+`;
+
+export const getLogLineWrapper = (severity: string): React.FC => {
+  switch (severity) {
+    case "D":
+    case "DEBUG":
+      return getLogLineComp("#666");
+    case "W":
+    case "WARN":
+      return getLogLineComp("#ffa500");
+    case "I":
+    case "INFO":
+      return getLogLineComp("#333");
+    case "E":
+    case "ERROR":
+      return getLogLineComp("#ff0000");
+    default:
+      return styled.div``;
+  }
+};
