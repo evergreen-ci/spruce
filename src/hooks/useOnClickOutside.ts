345393b6196231e299e17ab6fa1edafbb59f3942
Arjun Patel <arjun453@gmail.com>
2020-03-18 16:48:54 -0400

    adjust dep array

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -5,17 +5,16 @@ export const useOnClickOutside = (
   cb: () => void
 ) => {
   useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        cb();
+      }
+    }
     // Bind the event listener
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       // Unbind the event listener on clean up
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
-
-  function handleClickOutside(event) {
-    if (ref.current && !ref.current.contains(event.target)) {
-      cb();
-    }
-  }
+  }, [cb, ref]);
 };
