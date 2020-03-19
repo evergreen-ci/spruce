5d6d2a1b65f16acb900dd923cc1dde3cab7f1844
Arjun Patel <arjun453@gmail.com>
2020-03-18 20:45:49 -0400

    tests

--- cypress/integration/test_table.js
+++ cypress/integration/test_table.js
@@ -5,119 +5,126 @@ const TABLE_SORT_SELECTOR = ".ant-table-column-title";
 const waitForTestsQuery = () => waitForGQL("@gqlQuery", "taskTests");
 const TESTS_ROUTE =
   "/task/evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48/tests";
-describe("tests table", function() {
+describe("Tests Table", function() {
   beforeEach(() => {
     cy.server();
     cy.login();
     cy.route("POST", "/graphql/query").as("gqlQuery");
   });
 
-  // it("Should display No Data when given an invalid TaskID in the url", () => {
-  //   cy.visit("/task/NO-SUCH-THANG/tests");
-  //   waitForGQL("@gqlQuery", "GetTask");
-  //   cy.get(".ant-table").should("not.exist");
-  // });
+  it("Should display No Data when given an invalid TaskID in the url", () => {
+    cy.visit("/task/NO-SUCH-THANG/tests");
+    waitForGQL("@gqlQuery", "GetTask");
+    cy.get(".ant-table").should("not.exist");
+  });
 
-  // it("Should have sort buttons disabled when fetching data", () => {
-  //   cy.visit(TESTS_ROUTE);
-  //   cy.contains(TABLE_SORT_SELECTOR, "Name").click();
-  //   cy.once("fail", err => {
-  //     expect(err.message).to.include(
-  //       "'pointer-events: none' prevents user mouse interaction."
-  //     );
-  //   });
-  // });
+  it("Should have sort buttons disabled when fetching data", () => {
+    cy.visit(TESTS_ROUTE);
+    cy.contains(TABLE_SORT_SELECTOR, "Name").click();
+    cy.once("fail", err => {
+      expect(err.message).to.include(
+        "'pointer-events: none' prevents user mouse interaction."
+      );
+    });
+  });
 
-  // it("Adjusts query params when table headers are clicked", () => {
-  //   cy.visit(TESTS_ROUTE);
-  //   waitForTestsQuery();
-  //   cy.contains(TABLE_SORT_SELECTOR, "Name").click();
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal(TESTS_ROUTE);
-  //     expect(loc.search).to.include("category=TEST_NAME");
-  //     expect(loc.search).to.include("sort=-1");
-  //   });
-  //   waitForTestsQuery();
-  //   cy.contains(TABLE_SORT_SELECTOR, "Status").click();
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal(TESTS_ROUTE);
-  //     expect(loc.search).to.include("category=STATUS");
-  //     expect(loc.search).to.include("sort=1");
-  //   });
-  //   waitForTestsQuery();
-  //   cy.contains(TABLE_SORT_SELECTOR, "Status").click();
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal(TESTS_ROUTE);
-  //     expect(loc.search).to.include("category=STATUS");
-  //     expect(loc.search).to.include("sort=-1");
-  //   });
-  //   waitForTestsQuery();
-  //   cy.contains(TABLE_SORT_SELECTOR, "Time").click();
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal(TESTS_ROUTE);
-  //     expect(loc.search).to.include("category=DURATION");
-  //     expect(loc.search).to.include("sort=1");
-  //   });
-  //   waitForTestsQuery();
-  //   cy.contains(TABLE_SORT_SELECTOR, "Time").click();
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal(TESTS_ROUTE);
-  //     expect(loc.search).to.include("category=DURATION");
-  //     expect(loc.search).to.include("sort=-1");
-  //   });
-  // });
+  it("Adjusts query params when table headers are clicked", () => {
+    cy.visit(TESTS_ROUTE);
+    waitForTestsQuery();
+    cy.contains(TABLE_SORT_SELECTOR, "Name").click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal(TESTS_ROUTE);
+      expect(loc.search).to.include("category=TEST_NAME");
+      expect(loc.search).to.include("sort=-1");
+    });
+    waitForTestsQuery();
+    cy.contains(TABLE_SORT_SELECTOR, "Status").click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal(TESTS_ROUTE);
+      expect(loc.search).to.include("category=STATUS");
+      expect(loc.search).to.include("sort=1");
+    });
+    waitForTestsQuery();
+    cy.contains(TABLE_SORT_SELECTOR, "Status").click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal(TESTS_ROUTE);
+      expect(loc.search).to.include("category=STATUS");
+      expect(loc.search).to.include("sort=-1");
+    });
+    waitForTestsQuery();
+    cy.contains(TABLE_SORT_SELECTOR, "Time").click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal(TESTS_ROUTE);
+      expect(loc.search).to.include("category=DURATION");
+      expect(loc.search).to.include("sort=1");
+    });
+    waitForTestsQuery();
+    cy.contains(TABLE_SORT_SELECTOR, "Time").click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal(TESTS_ROUTE);
+      expect(loc.search).to.include("category=DURATION");
+      expect(loc.search).to.include("sort=-1");
+    });
+  });
 
-  // it("Should not adjust URL params when clicking Logs tab", () => {
-  //   const assertInitialURLState = () =>
-  //     cy.location().should(loc => {
-  //       expect(loc.pathname).to.equal(TESTS_ROUTE);
-  //       expect(loc.search).to.include("category=TEST_NAME");
-  //       expect(loc.search).to.include("sort=1");
-  //     });
-  //   cy.visit(TESTS_ROUTE);
-  //   assertInitialURLState();
-  //   waitForTestsQuery();
-  //   cy.contains(TABLE_SORT_SELECTOR, "Logs").click();
-  //   assertInitialURLState();
-  // });
+  it("Should not adjust URL params when clicking Logs tab", () => {
+    const assertInitialURLState = () =>
+      cy.location().should(loc => {
+        expect(loc.pathname).to.equal(TESTS_ROUTE);
+        expect(loc.search).to.include("category=TEST_NAME");
+        expect(loc.search).to.include("sort=1");
+      });
+    cy.visit(TESTS_ROUTE);
+    assertInitialURLState();
+    waitForTestsQuery();
+    cy.contains(TABLE_SORT_SELECTOR, "Logs").click();
+    assertInitialURLState();
+  });
 
-  // it("Buttons in log column should have target=_blank attribute", () => {
-  //   cy.visit(TESTS_ROUTE);
-  //   waitForTestsQuery();
-  //   cy.get("#htmlBtn-356534666634326434653838666165613761393066306666").should(
-  //     "have.attr",
-  //     "target",
-  //     "_blank"
-  //   );
-  //   cy.get("#rawBtn-356534666634326434653838666165613761393066306666").should(
-  //     "have.attr",
-  //     "target",
-  //     "_blank"
-  //   );
-  // });
+  it("Buttons in log column should have target=_blank attribute", () => {
+    cy.visit(TESTS_ROUTE);
+    waitForTestsQuery();
+    cy.get("#htmlBtn-356534666634326434653838666165613761393066306666").should(
+      "have.attr",
+      "target",
+      "_blank"
+    );
+    cy.get("#rawBtn-356534666634326434653838666165613761393066306666").should(
+      "have.attr",
+      "target",
+      "_blank"
+    );
+  });
+
   describe("Test Status Selector", () => {
-    it("Status select says 'No filters selected' by default'", () => {
+    beforeEach(() => {
       cy.visit(TESTS_ROUTE);
+      cy.get("#cy-test-status-select > .cy-treeselect-bar").click();
+    });
+
+    it("Status select says 'No filters selected' by default", () => {
       cy.get("#cy-test-status-select").contains("No filters selected");
     });
 
-    it("Clicking on 'All' status checkbox adds all statuses to URL ", () => {
-      cy.visit(TESTS_ROUTE);
-
-      cy.get("#cy-test-status-select > .cy-treeselect-bar").click();
-      cy.get("#cy-test-status-select")
+    it("Clicking on 'All' checkbox adds all statuses to URL ", () => {
+      cy.get(".cy-checkbox")
         .contains("All")
         .click();
       cy.location().should(loc => {
         expect(loc.pathname).to.equal(TESTS_ROUTE);
-        expect(loc.search).to.include("statuses=pass,fail,skip,silentfail,all");
+        expect(loc.search).to.include("statuses=all,pass,fail,skip,silentfail");
       });
     });
 
+    const statuses = [
+      { display: "Pass", key: "pass" },
+      { display: "Silent Fail", key: "silentfail" },
+      { display: "Failed", key: "fail" },
+      { display: "Skip", key: "skip" }
+    ];
+
     statuses.forEach(({ display, key }) => {
       it(`Clicking on ${display} status checkbox adds ${key} status to URL and clicking again removes it`, () => {
-        cy.visit(TESTS_ROUTE);
-        cy.get("#cy-test-status-select > .cy-treeselect-bar").click();
         cy.get(".cy-checkbox")
           .contains(display)
           .click();
@@ -135,20 +142,11 @@ describe("tests table", function() {
       });
     });
 
-    it.skip("Checking all statuses will check All checkbox", () => {
-      cy.visit(TESTS_ROUTE);
-      cy.get("#cy-test-status-select > .cy-treeselect-bar").click();
-      statuses.forEach(({ display }) => {
-        cy.get(".cy-checkbox")
-          .contains(display)
-          .click();
-      });
+    it("Checking all statuses will check 'All' checkbox", () => {
       cy.get(".cy-checkbox > input").should("be.checked");
     });
 
-    it("Unchecking a status will uncheck All", () => {
-      cy.visit(TESTS_ROUTE);
-      cy.get("#cy-test-status-select > .cy-treeselect-bar").click();
+    it("Unchecking a status will uncheck 'All' checkbox", () => {
       statuses.forEach(({ display }) => {
         cy.get(".cy-checkbox")
           .contains(display)
@@ -162,7 +160,7 @@ describe("tests table", function() {
         .should("not.be.checked");
     });
 
-    it("Checking multiple statuses will add them to URL", () => {
+    it("Checking multiple statuses adds them to URL", () => {
       statuses.forEach(({ display }) => {
         cy.get(".cy-checkbox")
           .contains(display)
@@ -175,10 +173,3 @@ describe("tests table", function() {
     });
   });
 });
-
-const statuses = [
-  { display: "Pass", key: "pass" },
-  { display: "Silent Fail", key: "silentfail" },
-  { display: "Failed", key: "fail" },
-  { display: "Skip", key: "skip" }
-];
