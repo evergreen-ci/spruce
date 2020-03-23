04a44fe5499cf6ce69b5bdd2030f4ff6d97295e1
Arjun Patel <arjun453@gmail.com>
2020-03-23 14:13:33 -0400

    render log link buttons

--- src/pages/patch/patchTabs/CodeChanges.tsx
+++ src/pages/patch/patchTabs/CodeChanges.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { useParams } from "react-router-dom";
 import { Skeleton } from "antd";
+import { H2 } from "components/Typography";
 import {
   GET_CODE_CHANGES,
   GetCodeChangesQuery
 } from "gql/queries/get-code-changes";
+import Button from "@leafygreen-ui/button";
+import styled from "@emotion/styled";
 
 export const CodeChanges = () => {
   const { id } = useParams<{ id: string }>();
@@ -21,6 +24,30 @@ export const CodeChanges = () => {
   if (error) {
     return <div id="patch-error">{error.message}</div>;
   }
-  console.log(data);
-  return <div>Code changes 4 lyfe</div>;
+  const content = data.patch.moduleCodeChanges.map(modCodeChange => {
+    return (
+      <div>
+        <H2>Changes on {modCodeChange.branchName}: </H2>
+        <StyledButton
+          size="small"
+          title="Create an item"
+          href={modCodeChange.htmlLink}
+        >
+          HTML
+        </StyledButton>
+        <StyledButton
+          size="small"
+          title="Create an item"
+          href={modCodeChange.rawLink}
+        >
+          RAW
+        </StyledButton>
+      </div>
+    );
+  });
+  return <div>{content}</div>;
 };
+
+const StyledButton = styled(Button)`
+  margin-left: 16px;
+`;
