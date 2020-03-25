1e83054c46f8d801e1f0d3a38db1a47586ad0217
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-03-25 15:14:03 -0400

    comments for hook

--- src/hooks/useFilterInputChangeHandler.ts
+++ src/hooks/useFilterInputChangeHandler.ts
@@ -23,6 +23,17 @@ const arrayFormat = "comma";
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>;
 
+/**
+ * useFilterInputChangeHandler updates the url query param (urlSearchParam) by the
+ * the value user enters into the input field.
+ * @param  {string} urlSearchParam the url search param that should update
+ * @param  {string} pathname url pathname
+ * @param  {string} search url search
+ * @param  {(path:string)=>void} replace method from react router to update ur
+ * @return {[string, (e: InputEvent) => void]}
+ * pass {string} return value to input component as `value`
+ * pass {(e: InputEvent) => void} return value to input component as onChange prop
+ */
 export const useFilterInputChangeHandler = (
   urlSearchParam: string,
   pathname: string,
@@ -31,13 +42,11 @@ export const useFilterInputChangeHandler = (
 ): [string, (e: InputEvent) => void] => {
   const parsed = queryString.parse(search, { arrayFormat });
   const inputValue = (parsed[urlSearchParam] || "").toString();
-
   const [value, setValue] = useState(inputValue);
 
   const onChange = (e: InputEvent): void => {
     setValue(e.target.value);
     updateQueryParam(urlSearchParam, e.target.value, search, replace, pathname);
   };
-
   return [value, onChange];
 };
