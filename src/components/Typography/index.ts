143de2dc3f9b1f9502878a049df938e4ad7d8223
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-03-25 12:07:58 -0400

    update H2 line-height

--- src/components/Typography/index.ts
+++ src/components/Typography/index.ts
@@ -13,7 +13,7 @@ export const H1 = styled.h1`
 
 export const H2 = styled.h2`
   font-size: 20px;
-  line-height: 21px;
+  line-height: 23px;
   margin-bottom: 8px;
   color: ${gray.dark3};
   display: inline-flex;
