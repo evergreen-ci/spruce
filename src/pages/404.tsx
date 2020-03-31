ead34f1f547329a9e41e3c50090e149500790bc0
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-03-31 17:38:56 -0400

    EVG-7340: show 404 page for nonexistant routes

---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PageWrapper } from "components/styles";
+import { H2 } from "@leafygreen-ui/typography";
+import styled from "@emotion/styled";
+
+export const PageDoesNotExist = () => {
+  return (
+    <StyledPageWrapper>
+      <H2 data-cy="404">404 Page Does Not Exist</H2>
+    </StyledPageWrapper>
+  );
+};
+
+const StyledPageWrapper = styled(PageWrapper)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 35vh;
+`;
