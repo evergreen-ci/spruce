1a8b4e99bff95cef23f99b94eec3260a6df94423
Arjun Patel <arjun453@gmail.com>
2020-04-01 17:31:02 -0400

    rm outdated tests

--- cypress/integration/test_table_route.js
+++ cypress/integration/test_table_route.js
@@ -18,25 +18,6 @@ describe("Tests Table Route", function() {
     cy.login();
   });
 
-  it("Default query params are set when no required query params exist", function() {
-    cy.visit(taskPath);
-    cy.location().should(fallbackLocation);
-  });
-
-  it("Default query params are set when some required query params exist", function() {
-    cy.visit(`${taskPath}?sortBy=TEST_NAME`);
-    cy.location().should(fallbackLocation);
-  });
-
-  it("Default query params are not changed when all required query params exist and are valid", function() {
-    cy.visit(`${taskPath}?sortBy=DURATION&${DESCEND_PARAM}`);
-    cy.location().should(loc => {
-      expect(loc.pathname).to.equal(taskPath);
-      expect(loc.search).to.include("sortBy=DURATION");
-      expect(loc.search).to.include(DESCEND_PARAM);
-    });
-  });
-
   it("sortBy query param is case insensitve", function() {
     cy.visit(`${taskPath}?sortBy=DuRaTiOn&${DESCEND_PARAM}`);
     cy.location().should(loc => {
@@ -73,20 +54,6 @@ describe("Tests Table Route", function() {
         expect(loc.search).to.include("sortBy=TEST_NAME");
         expect(loc.search).to.include(ASCEND_PARAM);
       });
-    });
-
-    it("Default query params are set if page query param is an array", function() {
-      cy.visit(
-        `${taskPath}?sortBy=TEST_NAME&page=[0,1,3,4]&limit=4&${ASCEND_PARAM}`
-      );
-      cy.location().should(fallbackLocation);
-    });
-
-    it("Default query params are set if limit query param is an array", function() {
-      cy.visit(
-        `${taskPath}?sortBy=TEST_NAME&page=0&limit=[4,3,4]&${ASCEND_PARAM}]`
-      );
-      cy.location().should(fallbackLocation);
     });
   });
 });
