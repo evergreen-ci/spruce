0d761e2b7823d7258eccf806038ac1ef6891b3f1
Arjun Patel <arjun453@gmail.com>
2020-04-07 11:48:47 -0400

    comments

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -9,15 +9,46 @@ declare namespace Cypress {
      * @example cy.dataCy('greeting')
      */
     dataCy(value: string): Chainable<Element>;
-    waitForGQL(queryName: string, options?: WaitForGQLOptions);
+    /**
+     * Custom command to wait for a gqlQuery with queryName.
+     * @param queryName Name of the query to wait for
+     * @param options Key is the path in WaitXHR object. Value
+     * is a function that accepts WaitXHR value at key path and returns a boolean
+     * to indicate a match or is a string value that is matched
+     * with WaitXHR at the key path
+     * @example cy.dataCy("taskTests", {"path.to.value": "value-to-match"})
+     * @example cy.dataCy("taskTests", {"path.to.value": (value) => value && value.length > 2})
+     */
+    waitForGQL(
+      queryName: string,
+      options?: WaitForGQLOptions
+    ): Chainable<WaitXHR>;
+    /**
+     * Custom command to preserve cookie between tests.
+     * @example cy.preserveCookies()
+     */
     preserveCookies(): void;
+    /**
+     * Custom command to navigate to login page and login.
+     * @example cy.login()
+     */
     login(): void;
+    /**
+     * Custom command to listen to GQL requests. Call this before
+     * your tests to use cy.waitForGQL
+     * @example cy.listenGQL()
+     */
     listenGQL(): void;
+    /**
+     * Custom command to enter credentials in username and password input
+     * click submit
+     * @example cy.listenGQL()
+     */
     enterCredentials(): void;
   }
 }
 
-type truthyFunction = (v: any) => boolean;
+type truthyFunction = (v: Cypress.WaitXHR) => boolean;
 interface WaitForGQLOptions {
   [index: string]: string | truthyFunction;
 }
