9c94d3c8c9c024edd5027af2c8276befd793482e
Arjun Patel <arjun453@gmail.com>
2020-04-07 10:17:28 -0400

    command types

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,22 @@
+// in cypress/support/index.d.ts
+// load type definitions that come with Cypress module
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to select DOM element by data-cy attribute.
+     * @example cy.dataCy('greeting')
+     */
+    dataCy(value: string): Chainable<Element>;
+    waitForGQL(queryName: string, options?: WaitForGQLOptions);
+    preserveCookies(): void;
+    login(): void;
+    listenGQL(): void;
+  }
+}
+
+type truthyFunction = (v: any) => boolean;
+interface WaitForGQLOptions {
+  [index: string]: string | truthyFunction;
+}
