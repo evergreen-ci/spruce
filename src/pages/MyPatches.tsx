547c2113130a6f6159247ed7712cc2db42f3632d
Domino Weir <domino.weir@mongodb.com>
2020-04-07 11:15:08 -0700

    initial commit

--- src/pages/MyPatches.tsx
+++ src/pages/MyPatches.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { FiltersWrapper, StyledInput, PageWrapper } from "components/styles";
+import { TreeSelect } from "components/TreeSelect";
+import { PageTitle } from "components/PageTitle";
+import Icon from "@leafygreen-ui/icon";
 
 export const MyPatches = () => {
-  return <div>My Patches Page</div>;
+  return (
+    <PageWrapper>
+      <PageTitle
+        loading={false}
+        hasData={true}
+        title="My Patches"
+        badge={null}
+      />
+      <FiltersWrapper>
+        <StyledInput
+          data-cy="patch-name-input"
+          placeholder="Search Patches"
+          suffix={<Icon glyph="MagnifyingGlass" />}
+        />
+        <TreeSelect
+          onChange={null}
+          state={[]}
+          tData={[]}
+          inputLabel="Patch Status: "
+          dataCy="patch-status-filter"
+        />
+      </FiltersWrapper>
+    </PageWrapper>
+  );
 };
