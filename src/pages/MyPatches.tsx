d1ee93be38727d922320401712710c0bef2479fc
Domino Weir <domino.weir@mongodb.com>
2020-04-07 09:27:50 -0700

    initial commit

--- src/pages/MyPatches.tsx
+++ src/pages/MyPatches.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import queryString from "query-string";
+import Checkbox from "@leafygreen-ui/checkbox";
 
 export const MyPatches = () => {
-  return <div>My Patches Page</div>;
+  const { search, pathname } = useLocation();
+  const history = useHistory();
+  const parsed = queryString.parse(search);
+  let showCommitQueue = (parsed.commitQueue === "true");
+  return (
+    <div>
+      <div>My Patches Page</div>
+      <Checkbox
+        onChange={() => {
+          showCommitQueue = !showCommitQueue;
+          history.push({
+            pathname: pathname,
+            search: "?commitQueue=" + showCommitQueue
+          })
+        }}
+        label="Show Commit Queue"
+        checked={showCommitQueue}
+      />
+    </div>
+  );
 };
