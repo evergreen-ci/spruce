bede7cd9ca8a693ceb7c1e0acae74418a9890203
Domino Weir <domino.weir@mongodb.com>
2020-04-07 09:50:53 -0700

    linter fix

--- src/pages/MyPatches.tsx
+++ src/pages/MyPatches.tsx
@@ -3,22 +3,25 @@ import { useLocation, useHistory } from "react-router-dom";
 import queryString from "query-string";
 import Checkbox from "@leafygreen-ui/checkbox";
 
-export const MyPatches = () => {
+export const MyPatches: React.FC = () => {
   const { search, pathname } = useLocation();
   const history = useHistory();
   const parsed = queryString.parse(search);
   let showCommitQueue = parsed.commitQueue === "true";
+
+  function onCheckboxChange() {
+    showCommitQueue = !showCommitQueue;
+    history.push({
+      pathname: pathname,
+      search: "?commitQueue=" + showCommitQueue
+    });
+  }
+
   return (
     <div>
       <div>My Patches Page</div>
       <Checkbox
-        onChange={() => {
-          showCommitQueue = !showCommitQueue;
-          history.push({
-            pathname: pathname,
-            search: "?commitQueue=" + showCommitQueue
-          });
-        }}
+        onChange={onCheckboxChange}
         label="Show Commit Queue"
         checked={showCommitQueue}
       />
