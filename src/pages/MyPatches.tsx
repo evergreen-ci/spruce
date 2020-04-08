491ffe6e247ddcea7952f20b4f6dfa6e1678c38f
Domino Weir <domino.weir@mongodb.com>
2020-04-08 13:35:54 -0700

    remove out of scope stuff

--- src/pages/MyPatches.tsx
+++ src/pages/MyPatches.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { FiltersWrapper, StyledInput, PageWrapper } from "components/styles";
-import { TreeSelect } from "components/TreeSelect";
+import { PageWrapper } from "components/styles";
 import { PageTitle } from "components/PageTitle";
-import Icon from "@leafygreen-ui/icon";
 import { useLocation, useHistory } from "react-router-dom";
 import queryString from "query-string";
 import Checkbox from "@leafygreen-ui/checkbox";
@@ -12,16 +10,17 @@ export const MyPatches = () => {
   const { search, pathname } = useLocation();
   const { replace } = useHistory();
   const parsed = queryString.parse(search);
-  const showCommitQueue = parsed.commitQueue === "true" || parsed.commitQueue === undefined;
+  const showCommitQueue =
+    parsed.commitQueue === "true" || parsed.commitQueue === undefined;
 
   const onCheckboxChange = () => {
     replace(
       `${pathname}?${queryString.stringify({
         ...queryString.parse(search),
-        [MyPatchesQueryParams.CommitQueue]: !showCommitQueue
+        [MyPatchesQueryParams.CommitQueue]: !showCommitQueue,
       })}`
     );
-  }
+  };
 
   return (
     <PageWrapper>
@@ -31,26 +30,12 @@ export const MyPatches = () => {
         title="My Patches"
         badge={null}
       />
-      <FiltersWrapper>
-        <StyledInput
-          data-cy="patch-name-input"
-          placeholder="Search Patches"
-          suffix={<Icon glyph="MagnifyingGlass" />}
-        />
-        <TreeSelect
-          onChange={null}
-          state={[]}
-          tData={[]}
-          inputLabel="Patch Status: "
-          dataCy="patch-status-filter"
-        />
-        <Checkbox
+      <Checkbox
         data-cy="commit-queue-checkbox"
         onChange={onCheckboxChange}
         label="Show Commit Queue"
         checked={showCommitQueue}
       />
-      </FiltersWrapper>
     </PageWrapper>
-  )
-}
+  );
+};
