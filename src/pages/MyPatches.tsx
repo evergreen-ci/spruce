3a39515474048986e18d7f285627ac5b8e32a74d
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-04-09 09:13:42 -0400

    Revert "Merge branch 'EVG-7546' of github.com:evergreen-ci/spruce into new-ui-feature-branch"
    This reverts commit 8d8aa5780d0c46e62baace1f25ec6ae1157fc89a, reversing
    changes made to 8f56e5116bd853b50078c2909225512753af6bc4.

--- src/pages/MyPatches.tsx
+++ src/pages/MyPatches.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { PageWrapper } from "components/styles";
-import { PageTitle } from "components/PageTitle";
 import { useLocation, useHistory } from "react-router-dom";
 import queryString from "query-string";
 import Checkbox from "@leafygreen-ui/checkbox";
@@ -10,32 +8,26 @@ export const MyPatches = () => {
   const { search, pathname } = useLocation();
   const { replace } = useHistory();
   const parsed = queryString.parse(search);
-  const showCommitQueue =
-    parsed.commitQueue === "true" || parsed.commitQueue === undefined;
+  const showCommitQueue = parsed.commitQueue === "true" || parsed.commitQueue === undefined;
 
   const onCheckboxChange = () => {
     replace(
       `${pathname}?${queryString.stringify({
         ...queryString.parse(search),
-        [MyPatchesQueryParams.CommitQueue]: !showCommitQueue,
+        [MyPatchesQueryParams.CommitQueue]: !showCommitQueue
       })}`
     );
-  };
+  }
 
   return (
-    <PageWrapper>
-      <PageTitle
-        loading={false}
-        hasData={true}
-        title="My Patches"
-        badge={null}
-      />
+    <div>
+      <div>My Patches Page</div>
       <Checkbox
         data-cy="commit-queue-checkbox"
         onChange={onCheckboxChange}
         label="Show Commit Queue"
         checked={showCommitQueue}
       />
-    </PageWrapper>
+    </div>
   );
 };
