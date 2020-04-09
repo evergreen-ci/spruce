f2b5f03149948aa202a3ab2e433326274ceb0c19
Arjun Patel <arjun453@gmail.com>
2020-04-09 11:49:21 -0400

    EVG-7529: Don't login before every cy.visit (#123)

--- cypress/integration/breadcrumbs.ts
+++ cypress/integration/breadcrumbs.ts
@@ -1,11 +1,15 @@
 /// <reference types="Cypress" />
 
-const taskRoute =
-  "/task/evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48";
-
 describe("TaskBreadcrumb", function() {
+  const taskRoute =
+    "/task/evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48";
   before(() => {
     cy.login();
+  });
+
+  beforeEach(() => {
+    cy.preserveCookies();
+    cy.listenGQL();
   });
 
   it("Shows tasks display name", function() {
@@ -18,14 +22,12 @@ describe("TaskBreadcrumb", function() {
   });
 
   it("Clicking on the patch breadcrumb goes to patch for task", function() {
-    cy.login();
     cy.visit(taskRoute);
     cy.get("#bc-patch").click();
     cy.url().should("include", "/patch/5e4ff3abe3c3317e352062e4");
   });
 
   it("Clicking 'My Patches' breadcrumb goes to /my-patches route", function() {
-    cy.login();
     cy.visit(taskRoute);
     cy.get("#bc-my-patches").click();
     cy.url().should("include", "/my-patches");
@@ -34,11 +36,11 @@ describe("TaskBreadcrumb", function() {
 
 describe("PatchBreadcrumb", function() {
   beforeEach(() => {
-    cy.login();
-    cy.visit("/patch/5e4ff3abe3c3317e352062e4");
+    cy.preserveCookies();
   });
 
   it("Shows the patches name", function() {
+    cy.visit("/patch/5e4ff3abe3c3317e352062e4");
     cy.get("#bc-patch").should("include.text", "Patch 2567");
   });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
+import { waitForGQL } from "../utils/networking";
+
+const GQL_QUERY = "gqlQuery";
+
 function enterLoginCredentials() {
   cy.get("input[name=username]").type("admin");
   cy.get("input[name=password]").type("password");
@@ -12,3 +16,22 @@ Cypress.Commands.add("login", () => {
 Cypress.Commands.add("enterLoginCredentials", () => {
   enterLoginCredentials();
 });
+
+Cypress.Commands.add("preserveCookies", () => {
+  Cypress.Cookies.preserveOnce(
+    "mci-token",
+    "mci-session",
+    "mci-project-cookie"
+  );
+});
+
+Cypress.Commands.add("listenGQL", () => {
+  cy.server();
+  cy.route("POST", "/graphql/query").as(GQL_QUERY);
+});
+
+Cypress.Commands.add("waitForGQL", (queryName, options = {}) =>
+  waitForGQL(`@${GQL_QUERY}`, queryName, options)
+);
+
+Cypress.Commands.add("dataCy", (value) => cy.get(`[data-cy=${value}]`));
