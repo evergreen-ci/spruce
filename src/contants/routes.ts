935adee0394e904fd377ad5de1cf763425bf2f65
Mohamed <mohamed.khelif@mongodb.com>
2020-04-20 14:06:30 -0400

    Add commit Queue route

--- src/contants/routes.ts
+++ src/contants/routes.ts
@@ -2,12 +2,14 @@ export const paths = {
   login: "/login",
   myPatches: "/my-patches",
   task: "/task",
-  patch: "/patch"
+  patch: "/patch",
+  commitQueue: "/commitQueue"
 };
 
 export const routes = {
   login: paths.login,
   myPatches: paths.myPatches,
   task: `${paths.task}/:id/:tab?`,
-  patch: `${paths.patch}/:id/:tab?`
+  patch: `${paths.patch}/:id/:tab?`,
+  commitQueue: `${paths.commitQueue}/:id`
 };
