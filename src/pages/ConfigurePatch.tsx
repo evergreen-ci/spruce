cb68d827befcedba6d7b54561b7c3c7c10931c55
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-04-20 16:25:57 -0400

    render all ConfigurePatch within a page wrapper

--- src/pages/ConfigurePatch.tsx
+++ src/pages/ConfigurePatch.tsx
@@ -4,18 +4,22 @@ import { useQuery } from "@apollo/react-hooks";
 import { GET_PATCH_CONFIGURE, PatchQuery } from "gql/queries/patch";
 import { ConfigurePatchCore } from "pages/configurePatch/ConfigurePatchCore";
 import { PatchAndTaskFullPageLoad } from "components/Loading/PatchAndTaskFullPageLoad";
+import { PageWrapper } from "components/styles";
 
 export const ConfigurePatch: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { data, loading, error } = useQuery<PatchQuery>(GET_PATCH_CONFIGURE, {
     variables: { id },
   });
-  if (loading) {
-    return <PatchAndTaskFullPageLoad />;
-  }
-  if (error) {
-    // TODO: replace with full page error
-    return <div>{error.message}</div>;
-  }
-  return <ConfigurePatchCore patch={data.patch} />;
+  return (
+    <PageWrapper>
+      {loading ? (
+        <PatchAndTaskFullPageLoad />
+      ) : error ? (
+        <div>{error.message}</div>
+      ) : (
+        <ConfigurePatchCore patch={data.patch} />
+      )}
+    </PageWrapper>
+  );
 };
