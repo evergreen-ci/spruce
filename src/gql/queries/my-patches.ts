a10d9d16b9e49181593fadab68b48179e9fce853
Arjun Patel <arjun453@gmail.com>
2020-04-22 13:15:38 -0400

    GET_MY_PATCHES query

---
+++ src/gql/queries/my-patches.ts
@@ -0,0 +1,56 @@
+import gql from "graphql-tag";
+
+export const GET_MY_PATCHES = gql`
+  query userPatches(
+    $page: Int
+    $limit: Int
+    $statuses: [String!]!
+    $patchName: String
+    $includeCommitQueue: Boolean
+  ) {
+    userPatches(
+      page: $page
+      limit: $limit
+      statuses: $statuses
+      patchName: $patchName
+      includeCommitQueue: $includeCommitQueue
+    ) {
+      projectID
+      description
+      status
+      createTime
+      builds {
+        id
+        buildVariant
+        status
+        predictedMakespan
+        actualMakespan
+      }
+    }
+  }
+`;
+
+export interface Build {
+  id: string;
+  buildVariant: string;
+  status: string;
+  predictedMakespan: number;
+  actualMakespan: number;
+}
+export interface UserPatchesVars {
+  $page: number;
+  $limit: number;
+  $statuses: string[];
+  $patchName: string;
+  $includeCommitQueue: boolean;
+}
+export interface Patch {
+  projectID: string;
+  description: string;
+  status: string;
+  createTime: string;
+  builds: Build[];
+}
+export interface UserPatchesData {
+  userPatches: Patch[];
+}
