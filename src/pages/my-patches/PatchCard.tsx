678f7fc0018ddf941a092cfe2329b4d535451e4f
Arjun Patel <arjun453@gmail.com>
2020-04-23 14:59:32 -0400

    adjust props; import BuildStatusIcon

--- src/pages/my-patches/PatchCard.tsx
+++ src/pages/my-patches/PatchCard.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Build } from "gql/queries/my-patches";
-import { BuildStatus } from "types/build";
 import styled from "@emotion/styled";
 import { PatchStatusBadge } from "components/PatchStatusBadge";
-import Icon from "@leafygreen-ui/icon";
-import { uiColors } from "@leafygreen-ui/palette";
-import { ProgressCircle } from "pages/my-patches/patch-card/ProgressCircle";
-
+import { Patch } from "gql/queries/my-patches";
+import { BuildStatusIcon } from "pages/my-patches/patch-card/BuildStatusIcon";
 interface Props {
   description: string;
   status: string;
@@ -15,30 +12,26 @@ interface Props {
   buildStatuses: Build[];
 }
 
-export const PatchCard: React.FC<Props> = ({
+export const PatchCard: React.FC<Patch> = ({
   description,
   status,
-  dateStr,
-  branch,
-  buildStatuses,
+  createTime,
+  projectID,
+  builds,
 }) => {
   return (
     <Container>
       <div>
         <div>{description}</div>
         <div>
-          {dateStr} on {branch}
+          {createTime} on {projectID}
         </div>
       </div>
       <div>
         <PatchStatusBadge status={status} />
       </div>
       <div>
-        {buildStatuses.map((b, i) => {
-          const Comp: (props: Build) => JSX.Element =
-            buildStatusToIcon[b.status];
-          return Comp ? <Comp key={i} {...b} /> : null;
-        })}
+        {builds.map((b, i) => <BuildStatusIcon key={i} {...b} /> }
       </div>
     </Container>
   );
@@ -47,26 +40,3 @@ export const PatchCard: React.FC<Props> = ({
 const Container = styled.div`
   display: flex;
 `;
-
-const GrayCircle = styled.div`
-  border-radius: 50%;
-  color: ${uiColors.gray.light1}
-  width: 10px;
-  height: 10px
-`;
-
-const buildStatusToIcon = {
-  [BuildStatus.Created]: (props: Build) => <GrayCircle />,
-  [BuildStatus.Failed]: (props: Build) => (
-    <Icon color={uiColors.red.base} glyph="Warning" />
-  ),
-  [BuildStatus.Started]: (props: Build) => {
-    const percentFill = props.predictedMakespan
-      ? (props.actualMakespan / props.predictedMakespan) * 100
-      : 75;
-    return <ProgressCircle diameter={22} percentFill={percentFill} />;
-  },
-  [BuildStatus.Succeeded]: (props: Build) => (
-    <Icon color={uiColors.green.base} glyph="Checkmark" />
-  ),
-};
