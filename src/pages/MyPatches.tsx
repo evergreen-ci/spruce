43cdd079ed7b1e02c1be588c1a1c7869f3881f61
Arjun Patel <arjun453@gmail.com>
2020-04-27 12:25:07 -0400

    folder structure

--- src/pages/MyPatches.tsx
+++ src/pages/MyPatches.tsx
@@ -20,7 +20,7 @@ import { useQuery } from "@apollo/react-hooks";
 import { useFilterInputChangeHandler } from "hooks";
 import styled from "@emotion/styled";
 import get from "lodash/get";
-import { PatchCard } from "./my-patches/PatchCard";
+import { PatchCard } from "./my-patches/patch-card-list/PatchCard";
 
 export const MyPatches = () => {
   const { replace, listen } = useHistory();
---
+++ src/pages/my-patches/PatchCardList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PatchCardList = () => {
+  <div>Patch card list</div>;
+};
