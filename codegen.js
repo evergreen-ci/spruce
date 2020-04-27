e2af87da75a371bf0c037febe9b4766e2939fd9a
Mohamed <mohamed.khelif@mongodb.com>
2020-04-27 11:36:44 -0400

    Add codegeneration scripts

---
+++ codegen.js
@@ -0,0 +1,22 @@
+module.exports = {
+  schema: [
+    {
+      "http://localhost:9090/graphql/query": {
+        headers: {
+          cookie: process.env.REACT_APP_GQL_COOKIE,
+        },
+      },
+    },
+  ],
+  documents: ["./src/**/*.ts"],
+  overwrite: true,
+  generates: {
+    "./src/gql/generated/types.ts": {
+      plugins: ["typescript", "typescript-operations"],
+      config: {
+        skipTypename: true,
+        preResolveTypes: true,
+      },
+    },
+  },
+};
