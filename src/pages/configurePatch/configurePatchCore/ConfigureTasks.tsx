7acac62836cad66185f86a8241208ab531fe48c2
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-04-29 16:55:29 -0400

    disable schedule button if loading

--- src/pages/configurePatch/configurePatchCore/ConfigureTasks.tsx
+++ src/pages/configurePatch/configurePatchCore/ConfigureTasks.tsx
@@ -91,7 +91,7 @@ export const ConfigureTasks: React.FC<Props> = ({
           data-cy="schedule-patch"
           variant={Variant.Primary}
           onClick={onClickSchedule}
-          disabled={isEmpty(selectedVariantTasks)}
+          disabled={isEmpty(selectedVariantTasks) || loading}
         >
           {loading && <StyledIcon type="loading" />}
           Schedule
