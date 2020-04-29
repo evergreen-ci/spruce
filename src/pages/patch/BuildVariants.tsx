47395613ca970f09ee2ad5eeb70165dbcb5aa9b1
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-04-29 18:50:48 -0400

    EVG-7515: patch build variants update in real time

--- src/pages/patch/BuildVariants.tsx
+++ src/pages/patch/BuildVariants.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_PATCH_BUILD_VARIANTS } from "gql/queries/get-patch-build-variants";
@@ -12,15 +12,24 @@ import { Skeleton } from "antd";
 import { TaskSquare } from "./buildVariants/TaskSquare";
 import { H3, P1 } from "components/Typography";
 import styled from "@emotion/styled/macro";
+import every from "lodash/every";
+import { TaskStatus } from "types/task";
+import get from "lodash/get";
 
 export const BuildVariants: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const { data, loading, error } = useQuery<
+  const { data, loading, error, stopPolling } = useQuery<
     PatchBuildVariantsQuery,
     PatchBuildVariantsQueryVariables
   >(GET_PATCH_BUILD_VARIANTS, {
     variables: { patchId: id },
+    pollInterval: 2000,
   });
+  useEffect(() => stopPolling, []);
+  const buildVariants = get(data, "patchBuildVariants", []);
+  if (data && allTasksHaveFinished(buildVariants)) {
+    stopPolling();
+  }
   return (
     <SiderCard>
       <H3>Build Variants</H3>
@@ -45,10 +54,22 @@ export const BuildVariants: React.FC = () => {
   );
 };
 
+const allTasksHaveFinished = (
+  buildVariants: PatchBuildVariantsQuery["patchBuildVariants"]
+) =>
+  every(buildVariants, ({ tasks }) =>
+    every(
+      tasks,
+      ({ status }) =>
+        status !== TaskStatus.Started &&
+        status !== TaskStatus.Dispatched &&
+        status !== TaskStatus.Undispatched
+    )
+  );
+
 const BuildVariant = styled.div`
   margin-bottom: 8px;
 `;
-
 const VariantTasks = styled.div`
   display: flex;
   flex-wrap: wrap;
