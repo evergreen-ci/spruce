6d53a5666f4226f736f62e2f1d1ede8c71086862
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-05-01 09:20:37 -0400

    colors

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React from "react";
 import styled from "@emotion/styled";
+import { uiColors } from "@leafygreen-ui/palette";
 
-type BannerType = "success" | "error" | "info" | "warning"
+const { green, red, yellow, blue } = uiColors;
+
+type BannerType = "success" | "error" | "info" | "warning";
 interface Props {
-    type: BannerType
+  type: BannerType;
 }
-export const Banner: React.FC<Props> = ({type}) => {
-    return (
-        <BannerDiv type={type}>
-            
-        </BannerDiv>
-    )
-}
+export const Banner: React.FC<Props> = ({ children, type }) => {
+  return <BannerDiv type={type}></BannerDiv>;
+};
 
-const mapTypeToBorderColor: {[type: BannerType]: string} = {
-    
-}
-const mapTypeToBackgroundColor: {[type: BannerType]: string} = {
-
-}
+const mapTypeToBorderColor: { [type: string]: string } = {
+  success: green.light2,
+  error: red.light2,
+  info: yellow.light2,
+  warning: blue.light2,
+};
+const mapTypeToBackgroundColor: { [type: string]: string } = {
+  success: green.light3,
+  error: red.light3,
+  info: yellow.light3,
+  warning: blue.light3,
+};
+const mapTypeToTextColor: { [type: string]: string } = {
+  success: green.dark2,
+  error: red.dark2,
+  info: yellow.dark2,
+  warning: blue.dark2,
+};
 
 const BannerDiv = styled.div`
-    border-color: ${({type}: Props) => }
-`
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+  min-height: 44px;
+  border-color: ${({ type }: Props) => mapTypeToBorderColor[type]};
+  background-color: ${({ type }: Props) => mapTypeToBackgroundColor[type]};
+  color: ${({ type }: Props) => mapTypeToTextColor[type]};
+`;
