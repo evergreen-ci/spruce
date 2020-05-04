63f78bedbe70b9c8cc53a95b44b579ba514c1f18
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-05-04 16:01:00 -0400

    Revert "EVG-7839: remove project dropdown from navbar"
    This reverts commit 4e47a2c7e79b43dda02fd9fb39030584991f6863.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import styled from "@emotion/styled/macro";
 import { useAuthDispatchContext, useAuthStateContext } from "context/auth";
-import { ProjectSelectProps } from "components/ProjectSelect";
+import { ProjectSelect, ProjectSelectProps } from "components/ProjectSelect";
 import { Layout } from "antd";
 
 const { Header } = Layout;
 
-export const Navbar: React.FC<ProjectSelectProps> = () => {
+export const Navbar: React.FC<ProjectSelectProps> = ({ data, loading }) => {
   const { logout } = useAuthDispatchContext();
   const { isAuthenticated } = useAuthStateContext();
 
   if (!isAuthenticated) {
     return null;
   }
+
   return (
     <StyledHeader>
       <InnerWrapper>
+        <ProjectSelect data={data} loading={loading} />
         <LogoutButton id="logout" onClick={logout}>
           Logout
         </LogoutButton>
@@ -29,12 +31,14 @@ const StyledHeader = styled(Header)`
   background: #20313c;
   margin-bottom: 16px;
 `;
+
 const InnerWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
   height: 100%;
 `;
+
 const LogoutButton = styled.div`
   cursor: pointer;
 `;
