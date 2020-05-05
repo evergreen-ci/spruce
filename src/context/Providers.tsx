5e74c6e4c692d90348c1cd594aad6a62580a2bf6
Mohamed <mohamed.khelif@mongodb.com>
2020-05-05 14:20:38 -0400

    Add leafygreen provider

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import LeafyGreenProvider from "@leafygreen-ui/leafygreen-provider";
 import { AuthProvider } from "./auth";
 import { ToastProvider } from "./toast";
 
 export const ContextProviders: React.FC = ({ children }) => {
   return (
     <AuthProvider>
-      <ToastProvider>{children}</ToastProvider>
+      <LeafyGreenProvider baseFontSize={16}>
+        <ToastProvider>{children}</ToastProvider>
+      </LeafyGreenProvider>
     </AuthProvider>
   );
 };
