913fb0c330c300208651bd5a310f6f7fa62a455c
Arjun Patel <arjun453@gmail.com>
2020-05-08 15:35:17 -0400

    page size selector

---
+++ src/components/PageSizeSelector.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Select } from "antd";
+import queryString from "query-string";
+import { useHistory, useLocation } from "react-router-dom";
+
+const { Option } = Select;
+
+interface Props {
+  value: number;
+}
+
+export const PageSizeSelector = ({ value }: Props) => {
+  const { replace } = useHistory();
+  const { search, pathname } = useLocation();
+
+  const handleChange = (pageSize: number) => {
+    replace(
+      `${pathname}?${queryString.stringify(
+        {
+          ...queryString.parse(search, { arrayFormat }),
+          limit: pageSize,
+        },
+        { arrayFormat }
+      )}`
+    );
+  };
+
+  return (
+    <Select value={value} style={{ width: 120 }} onChange={handleChange}>
+      {PAGE_SIZES.map((limit) => (
+        <Option value={limit}>{`${limit} / page`}</Option>
+      ))}
+    </Select>
+  );
+};
+
+export const PAGE_SIZES = [10, 20, 50, 100];
+const arrayFormat = "comma";
