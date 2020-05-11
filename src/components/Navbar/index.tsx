a8789917dc06047187444754ff2edba3db59126e
Trey Granderson <trey.granderson@mongodb.com>
2020-05-11 16:46:15 -0400

    EVG-7840: Evergreen logo in navbar (#165)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,39 +1,50 @@
 import React from "react";
 import styled from "@emotion/styled/macro";
-import { useAuthDispatchContext, useAuthStateContext } from "context/auth";
+import { useAuthStateContext } from "context/auth";
 import { Layout } from "antd";
+import { EvergreenLogo } from "components/icons";
+import { Subtitle } from "@leafygreen-ui/typography";
+import { uiColors } from "@leafygreen-ui/palette";
+import { Link } from "react-router-dom";
+import { paths } from "constants/routes";
 
 const { Header } = Layout;
 
 export const Navbar: React.FC = () => {
-  const { logout } = useAuthDispatchContext();
   const { isAuthenticated } = useAuthStateContext();
-
   if (!isAuthenticated) {
     return null;
   }
   return (
     <StyledHeader>
       <InnerWrapper>
-        <LogoutButton id="logout" onClick={logout}>
-          Logout
-        </LogoutButton>
+        <Link to={paths.myPatches}>
+          <Logo>
+            <EvergreenLogo />
+            <StyledSubtitle>Evergreen</StyledSubtitle>
+          </Logo>
+        </Link>
       </InnerWrapper>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled(Header)`
-  background-color: #20313c;
-  background: #20313c;
+  background-color: ${uiColors.gray.dark3};
   margin-bottom: 16px;
+  padding: 0 36px;
 `;
 const InnerWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: flex-start;
   height: 100%;
 `;
-const LogoutButton = styled.div`
-  cursor: pointer;
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const StyledSubtitle = styled(Subtitle)`
+  color: ${uiColors.white};
+  margin-left: 8px;
 `;
