31d030d588c2db3429a8f4b551d2ca7c87737435
Mohamed <mohamed.khelif@mongodb.com>
2020-05-12 14:06:34 -0400

    Remove unnecessary component

--- src/pages/patch/patchModal/PatchStatusCheckboxContainer.tsx
+++ src/pages/patch/patchModal/PatchStatusCheckboxContainer.tsx
@@ -19,45 +19,21 @@ export const PatchStatusCheckboxContainer: React.FC<PatchStatusCheckboxContainer
 }) => (
   <ScrollableContainer data-cy="patch-status-selector-container">
     {tasks.map((task) => (
-      <PatchStatusCheckbox
-        key={task.id}
-        {...task}
-        selectedTasks={selectedTasks}
-        toggleSelectedTask={toggleSelectedTask}
+      <Checkbox
+        data-cy="task-status-checkbox"
+        onChange={() => toggleSelectedTask(task.id)}
+        label={
+          <PatchStateItemWrapper>
+            <Square color={mapVariantTaskStatusToColor[task.status]} />{" "}
+            {task.name}
+          </PatchStateItemWrapper>
+        }
+        checked={selectedTasks.includes(task.id)}
+        bold={false}
       />
     ))}
   </ScrollableContainer>
 );
-
-interface PatchStatusCheckboxProps {
-  status: string;
-  name: string;
-  id: string;
-  selectedTasks: string[];
-  toggleSelectedTask: (id: string) => void;
-}
-const PatchStatusCheckbox: React.FC<PatchStatusCheckboxProps> = ({
-  status,
-  name,
-  id,
-  selectedTasks,
-  toggleSelectedTask,
-}) => {
-  const square = <Square color={mapVariantTaskStatusToColor[status]} />;
-  return (
-    <Checkbox
-      data-cy="task-status-checkbox"
-      onChange={() => toggleSelectedTask(id)}
-      label={
-        <PatchStateItemWrapper>
-          {square} {name}
-        </PatchStateItemWrapper>
-      }
-      checked={selectedTasks.includes(id)}
-      bold={false}
-    />
-  );
-};
 
 const ScrollableContainer = styled("div")`
   max-height: 250px;
