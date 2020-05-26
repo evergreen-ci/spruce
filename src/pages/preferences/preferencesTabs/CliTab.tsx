f975ebb1c576cef3cdaa1291c1237cd357b3a7f8
Mohamed <mohamed.khelif@mongodb.com>
2020-05-26 12:56:17 -0400

    Add cli tab

---
+++ src/pages/preferences/preferencesTabs/CliTab.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Styled from "@emotion/styled";
+import {
+  DownloadCard,
+  AuthenticationCard,
+  ConfirmationCard,
+  NodeList,
+} from "./cliTab/index";
+
+export const CliTab = () => (
+  <RelativeContainer>
+    <NodeList list={list} />
+  </RelativeContainer>
+);
+
+const list = [
+  {
+    title: "Download the Command-Line Client.",
+    child: <DownloadCard />,
+  },
+  {
+    title:
+      "Move the command-line client to somewhere in your PATH. On many systems this will be /usr/local/bin.",
+  },
+  {
+    title: "Download the authentication file.",
+    child: <AuthenticationCard />,
+  },
+  {
+    title: "Move the authentication file to ~/.evergreen.yml.",
+  },
+  {
+    title: "Make sure you are good to go!",
+    child: <ConfirmationCard />,
+  },
+];
+
+const RelativeContainer = Styled.div`
+ // This is required for the NodeList component to allow the vertical line to render properly
+ // It has to do with absolute positioning in relation to the nearest relative parent.
+  position: relative;
+`;
