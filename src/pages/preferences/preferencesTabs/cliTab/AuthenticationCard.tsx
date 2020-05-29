4119587f00fd2a1b31b965706aa1bb5f1d99522d
Mohamed <mohamed.khelif@mongodb.com>
2020-05-29 15:50:10 -0400

    Add ability to download a file with the auth code for the cli

--- src/pages/preferences/preferencesTabs/cliTab/AuthenticationCard.tsx
+++ src/pages/preferences/preferencesTabs/cliTab/AuthenticationCard.tsx
@@ -24,8 +24,7 @@ export const AuthenticationCard = () => {
     return <Skeleton active paragraph={{ rows: 6 }} />;
   }
   const config = get(data, "userConfig");
-  const authCode = `
-user: "${config.user}"
+  const authCode = `user: "${config.user}"
 api_key: "${config.api_key}"
 api_server_host: "${config.api_server_host}"
 ui_server_host: "${config.ui_server_host}"
@@ -35,13 +34,29 @@ ui_server_host: "${config.ui_server_host}"
     await post("/settings/newkey", {});
     refetch();
   };
+  const downloadFile = (e) => {
+    // This creates a text blob with the contents of `authCode`
+    // It then creates a `a` element and generates an objectUrl pointing to the
+    // text blob which is used to get the browser to download it as if it were a file
+    e.preventDefault();
+    const element = document.createElement("a");
+    const file = new Blob([authCode], { type: "text/plain" });
+    element.href = URL.createObjectURL(file);
+    element.download = `.evergreen.yml`;
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+    URL.revokeObjectURL(element.href);
+    document.body.removeChild(element);
+  };
   return (
     <Container>
       <Subtitle>Authentication</Subtitle>
       <CodeContainer>
         <Code language="none">{authCode}</Code>
       </CodeContainer>
-      <StyledButton variant={Variant.Primary}>Download File</StyledButton>
+      <StyledButton variant={Variant.Primary} onClick={downloadFile}>
+        Download File
+      </StyledButton>
       <Button onClick={resetKey}>Reset Key</Button>
     </Container>
   );
