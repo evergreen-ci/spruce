a216aae17f5966947b3febbbee97723024f16f6b
Mohamed <mohamed.khelif@mongodb.com>
2020-06-04 16:26:05 -0400

    Add comment for array function

--- src/utils/array/index.ts
+++ src/utils/array/index.ts
@@ -1,3 +1,5 @@
+// This takes in an array and an id and safely inserts or removes the value from
+// the array so that there is never a duplicate value
 export const toggleArray = (id: string, array: any[]) => {
   const tempArray = [...array];
   const idIndex = tempArray.indexOf(id);
