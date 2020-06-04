b336c2f47948c7b34e5c820fed2d3d3712a01aec
Mohamed Khelif <mohamed.khelif@mongodb.com>
2020-06-04 16:42:50 -0400

    EVG-7974 Add support for selecting multiple tasks in the configure tab (#202)

---
+++ src/utils/array/index.ts
@@ -0,0 +1,12 @@
+// This takes in an array and an id and safely inserts or removes the value from
+// the array so that there is never a duplicate value
+export const toggleArray = (id: string, array: any[]) => {
+  const tempArray = [...array];
+  const idIndex = tempArray.indexOf(id);
+  if (idIndex !== -1) {
+    tempArray.splice(idIndex, 1);
+  } else {
+    tempArray.push(id);
+  }
+  return tempArray;
+};
