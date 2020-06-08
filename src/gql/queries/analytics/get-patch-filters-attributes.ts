a137a74c7cdba26c29313c3e2e2a5098f7946241
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-06-08 14:15:25 -0400

    remove eslint rule

--- src/gql/queries/analytics/get-patch-filters-attributes.ts
+++ src/gql/queries/analytics/get-patch-filters-attributes.ts
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
 
-/* eslint-disable graphql/template-strings */
+/* eslint-disable */
 export const GET_PATCH_FILTERS_EVENT_DATA = gql`
   query GetPatchFiltersEventData($id: String!) {
     patch(id: $id) {
