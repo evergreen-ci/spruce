667a5a9f34b9716b9ccfee07896e7248311021b7
Mohamed <mohamed.khelif@mongodb.com>
2020-06-08 12:08:00 -0400

    Remove console statments from request test

--- src/utils/request.test.js
+++ src/utils/request.test.js
@@ -19,15 +19,15 @@ test("posts", async () => {
 });
 
 test("should handle a bad response", async () => {
-  const { warn } = console;
-  console.warn = jest.fn();
+  // const { warn } = console;
+  // console.warn = jest.fn();
 
   const errorCallback = jest.fn();
   axios.post.mockImplementationOnce(() => Promise.resolve(jsonMessage));
 
   expect(await post(API_URL, {}, { onFailure: errorCallback })).toBe(undefined);
-  expect(console.warn).toHaveBeenCalledTimes(1);
+  // expect(console.warn).toHaveBeenCalledTimes(1);
   expect(errorCallback).toHaveBeenCalledTimes(1);
 
-  console.warn = warn;
+  // console.warn = warn;
 });
