365c81ebaf79d6706233bc91ce3ba3effbe003cd
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-06-10 14:58:26 -0400

    EVG-7852: userPatches analytics

---
+++ src/analytics/userPatches/useUserPatchesAnalytics.ts
@@ -0,0 +1,29 @@
+import {
+  addPageAction,
+  Properties,
+  Analytics as A,
+} from "analytics/addPageAction";
+import { useGetUserQuery } from "analytics/useGetUserQuery";
+
+type Action =
+  | { name: "Filter Patches" }
+  | { name: "Filter Commit Queue" }
+  | { name: "Change Page Size" }
+  | { name: "Click patch link" }
+  | { name: "Click Reconfigure Link" };
+
+interface P extends Properties {}
+interface Analytics extends A<Action> {}
+
+export const useUserPatchesAnalytics = (): Analytics => {
+  const userId = useGetUserQuery();
+
+  const sendEvent: Analytics["sendEvent"] = (action) => {
+    addPageAction<Action, P>(action, {
+      object: "UserPatches",
+      userId,
+    });
+  };
+
+  return { sendEvent };
+};
