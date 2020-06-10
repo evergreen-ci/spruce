c305da71f8ef2c0aca13382efb70c3c0220c4654
Mohamed Khelif <mohamed.khelif@mongodb.com>
2020-06-10 11:45:25 -0400

    EVG-7851 Add Nav Dropdown (#207)
    * Add Nav Dropdown
    
    * Add link to User patches page
    
    * Add tests for nav dropdown
    
    * fetch userid from localstorage

--- src/constants/externalResources.ts
+++ src/constants/externalResources.ts
@@ -1,2 +1,8 @@
 export const cliDocumentationUrl =
   "https://github.com/evergreen-ci/evergreen/wiki/Using-the-Command-Line-Tool";
+
+export const legacyRoutes = {
+  distros: "/distros",
+  hosts: "/spawn",
+  projects: "/projects",
+};
