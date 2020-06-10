f6456316d7b101cc1c86c390edc4f8c9a6672152
Arjun Patel <arjun453@gmail.com>
2020-06-10 11:44:49 -0400

    EVG-8106: hide logs and html buttons when no logs found or gql error occurs (#215)

--- src/pages/task/Logs.tsx
+++ src/pages/task/Logs.tsx
@@ -1,33 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Radio, RadioGroup } from "@leafygreen-ui/radio-group";
-import { useLocation, useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import queryString from "query-string";
-import styled from "@emotion/styled/macro";
+
 import {
   EventLog,
   AgentLog,
   SystemLog,
   TaskLog,
+  LogTypes,
+  QueryParams,
 } from "pages/task/logs/LogTypes";
 import Maybe from "graphql/tsutils/Maybe";
-import { Button } from "components/Button";
 
-enum LogTypes {
-  Agent = "agent",
-  System = "system",
-  Task = "task",
-  Event = "event",
-}
 const DEFAULT_LOG_TYPE = LogTypes.Task;
-enum QueryParams {
-  LogType = "logtype",
-}
 
 const options = {
-  [LogTypes.Agent]: <AgentLog />,
-  [LogTypes.System]: <SystemLog />,
-  [LogTypes.Task]: <TaskLog />,
-  [LogTypes.Event]: <EventLog />,
+  [LogTypes.Agent]: AgentLog,
+  [LogTypes.System]: SystemLog,
+  [LogTypes.Task]: TaskLog,
+  [LogTypes.Event]: EventLog,
 };
 
 interface LogLinks {
@@ -42,8 +33,7 @@ interface Props {
   logLinks: LogLinks;
 }
 export const Logs: React.FC<Props> = ({ logLinks }) => {
-  const { search, pathname } = useLocation();
-  const { replace } = useHistory();
+  const { search } = useLocation();
   const [currentLog, setCurrentLog] = useState<LogTypes>(DEFAULT_LOG_TYPE);
   const parsed = queryString.parse(search);
   const logTypeParam = (parsed[QueryParams.LogType] || "")
@@ -64,63 +54,14 @@ export const Logs: React.FC<Props> = ({ logLinks }) => {
     }
   }, [logTypeParam]);
 
-  const onChangeLog = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const nextLogType = event.target.value as LogTypes;
-    replace(
-      `${pathname}?${queryString.stringify(
-        {
-          [QueryParams.LogType]: nextLogType,
-        },
-        { arrayFormat: "comma" }
-      )}`
-    );
-  };
   const { htmlLink, rawLink } = getLinks(logLinks, currentLog);
+  const LogComp = options[currentLog];
   return (
-    <div>
-      <StyledRadioGroup
-        variant="default"
-        onChange={onChangeLog}
-        value={currentLog}
-        name="log-select"
-      >
-        <ButtonContainer>
-          {htmlLink && (
-            <Button dataCy="html-log-btn" target="_blank" href={htmlLink}>
-              HTML
-            </Button>
-          )}
-          {rawLink && (
-            <Button dataCy="raw-log-btn" target="_blank" href={rawLink}>
-              Raw
-            </Button>
-          )}
-        </ButtonContainer>
-        <Radio id="cy-task-radio" value={LogTypes.Task}>
-          Task Logs
-        </Radio>
-        <Radio id="cy-agent-radio" value={LogTypes.Agent}>
-          Agent Logs
-        </Radio>
-        <Radio id="cy-system-radio" value={LogTypes.System}>
-          System Logs
-        </Radio>
-        <Radio id="cy-event-radio" value={LogTypes.Event}>
-          Event Logs
-        </Radio>
-      </StyledRadioGroup>
-      {options[currentLog] || <></>}
-    </div>
+    LogComp && (
+      <LogComp htmlLink={htmlLink} rawLink={rawLink} currentLog={currentLog} />
+    )
   );
 };
-
-const StyledRadioGroup = styled(RadioGroup)`
-  display: flex;
-  align-items: center;
-  label {
-    margin-left: 24px;
-  }
-`;
 
 interface GetLinksResult {
   htmlLink?: string;
@@ -146,9 +87,3 @@ const getLinks = (logLinks: LogLinks, logType: LogTypes): GetLinksResult => {
   }
   return { htmlLink: url, rawLink: `${url}&text=true` };
 };
-
-const ButtonContainer = styled.div`
-  a:first-of-type {
-    margin-right: 8px;
-  }
-`;
