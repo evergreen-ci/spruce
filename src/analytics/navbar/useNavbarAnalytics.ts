730625f7a059dd4829d84a3f971a2300c7406a47
Mohamed <mohamed.khelif@mongodb.com>
2020-06-11 16:51:11 -0400

    Add waterfall link

--- src/analytics/navbar/useNavbarAnalytics.ts
+++ src/analytics/navbar/useNavbarAnalytics.ts
@@ -5,7 +5,10 @@ import {
 } from "analytics/addPageAction";
 import { useGetUserQuery } from "analytics/useGetUserQuery";
 
-type Action = { name: "Click Legacy UI Link" } | { name: "Click Logo Link" };
+type Action =
+  | { name: "Click Legacy UI Link" }
+  | { name: "Click Logo Link" }
+  | { name: "Click Waterfall Link" };
 
 interface P extends Properties {}
 interface Analytics extends A<Action> {}
