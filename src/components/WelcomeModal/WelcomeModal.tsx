e664872bccfbf3c8a230eaffa6a34a27aa1981ab
Mohamed <mohamed.khelif@mongodb.com>
2020-06-15 18:46:00 -0400

    Increase delay

--- src/components/WelcomeModal/WelcomeModal.tsx
+++ src/components/WelcomeModal/WelcomeModal.tsx
@@ -99,7 +99,7 @@ const WelcomeModal = () => {
       <Carousel
         afterChange={(number) => setActiveSlide(number)}
         autoplay
-        autoplaySpeed={7000}
+        autoplaySpeed={8000}
         dots={false}
         slickGoTo={activeSlide}
         ref={slider}
