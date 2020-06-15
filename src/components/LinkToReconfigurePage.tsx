c96955fcbf53b30f7b82094e2febb4140f14e8f7
Arjun Patel <arjun453@gmail.com>
2020-06-15 12:07:16 -0400

    add analytics to reconfigure link

--- src/components/LinkToReconfigurePage.tsx
+++ src/components/LinkToReconfigurePage.tsx
@@ -3,13 +3,23 @@ import { paths } from "constants/routes";
 import { Link } from "react-router-dom";
 import { DropdownItem } from "components/ButtonDropdown";
 import { Disclaimer } from "@leafygreen-ui/typography";
+import { usePatchAnalytics } from "analytics";
 
-export const LinkToReconfigurePage: React.FC<{ patchId: string }> = ({
-  patchId,
-}) => (
-  <Link data-cy="reconfigure-link" to={`${paths.patch}/${patchId}/configure`}>
-    <DropdownItem disabled={false}>
-      <Disclaimer>Reconfigure Tasks/Variants</Disclaimer>
-    </DropdownItem>
-  </Link>
-);
+export const LinkToReconfigurePage: React.FC<{
+  patchId: string;
+}> = ({ patchId }) => {
+  const patchAnalytics = usePatchAnalytics();
+  return (
+    <Link
+      onClick={() => {
+        patchAnalytics.sendEvent({ name: "Click Reconfigure Link" });
+      }}
+      data-cy="reconfigure-link"
+      to={`${paths.patch}/${patchId}/configure`}
+    >
+      <DropdownItem disabled={false}>
+        <Disclaimer>Reconfigure Tasks/Variants</Disclaimer>
+      </DropdownItem>
+    </Link>
+  );
+};
