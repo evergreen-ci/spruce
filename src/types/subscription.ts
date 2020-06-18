36a49946f43cacde1928d80ff807d11cb4417d9c
Arjun Patel <arjun.patel@mongodb.com>
2020-06-18 14:39:12 -0400

    update copy

--- src/types/subscription.ts
+++ src/types/subscription.ts
@@ -5,15 +5,15 @@ export interface SubscriptionMethod {
 
 export const SUBSCRIPTION_JIRA_COMMENT: SubscriptionMethod = {
   value: "jira-comment",
-  label: "comment on a JIRA issue",
+  label: "Comment on a JIRA issue",
 };
 
 export const SUBSCRIPTION_SLACK: SubscriptionMethod = {
   value: "slack",
-  label: "send a slack message",
+  label: "Slack message",
 };
 
 export const SUBSCRIPTION_EMAIL: SubscriptionMethod = {
   value: "email",
-  label: "send an email",
+  label: "Email",
 };
