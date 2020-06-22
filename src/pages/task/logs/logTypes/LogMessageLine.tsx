ea9666411af749cfba4f5432218bd2f24006a92e
Arjun Patel <arjun.patel@mongodb.com>
2020-06-22 17:10:16 -0400

    import syntax

--- src/pages/task/logs/logTypes/LogMessageLine.tsx
+++ src/pages/task/logs/logTypes/LogMessageLine.tsx
@@ -3,9 +3,9 @@ import { LogMessage } from "gql/generated/types";
 import { getLogLineWrapper } from "pages/task/logs/logTypes/logMessageLine/LogLines";
 import { format } from "date-fns";
 import linkifyHtml from "linkifyjs/html";
-var Convert = require("ansi-to-html");
-var convert = new Convert({ newline: true, escapeXML: true });
+import Convert from "ansi-to-html";
 
+const convert = new Convert({ newline: true, escapeXML: true });
 const FORMAT_STR = "yyyy/MM/d, HH:mm:ss.SSS";
 
 export const LogMessageLine: React.FC<LogMessage> = ({
