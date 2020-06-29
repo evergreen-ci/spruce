8ed9c6fbffb1d41eb92ab0aaa46f48571291017a
Arjun Patel <arjun453@gmail.com>
2020-06-29 14:11:27 -0400

    EVG-8052: style subscription modal (#255)

--- src/types/subscription.ts
+++ src/types/subscription.ts
@@ -5,15 +5,15 @@ export interface SubscriptionMethod {
 
 export const SUBSCRIPTION_JIRA_COMMENT: SubscriptionMethod = {
   value: "jira-comment",
-  label: "making a comment on a JIRA issue",
+  label: "Comment on a JIRA issue",
 };
 
 export const SUBSCRIPTION_SLACK: SubscriptionMethod = {
   value: "slack",
-  label: "sending a slack message",
+  label: "Slack message",
 };
 
 export const SUBSCRIPTION_EMAIL: SubscriptionMethod = {
   value: "email",
-  label: "sending an email",
+  label: "Email",
 };
