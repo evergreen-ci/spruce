0267f944d6b38a074f827ff4b520f62f3882b756
Mohamed <mohamed.khelif@mongodb.com>
2020-07-08 13:16:31 -0400

    Add Sitewide notification banner

--- src/components/Banners/SiteBanner.tsx
+++ src/components/Banners/SiteBanner.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import styled from "@emotion/styled";
 import { uiColors } from "@leafygreen-ui/palette";
+import IconButton from "@leafygreen-ui/icon-button";
 import Icon from "@leafygreen-ui/icon";
 import Cookies from "js-cookie";
 import get from "lodash.get";
@@ -12,10 +13,12 @@ const { yellow, blue, green, red } = uiColors;
 export const SiteBanner = () => {
   const { data, loading } = useQuery<SiteBannerQuery>(GET_SITE_BANNER);
   const { text, theme } = get(data, "siteBanner", {});
-  const shouldShowBanner =
-    text && text.length > 0 && Cookies.get(text) === undefined;
-  console.log(text);
-  const [showBanner, setShowBanner] = useState(shouldShowBanner);
+  const [showBanner, setShowBanner] = useState(false);
+  useEffect(() => {
+    if (text !== "" && Cookies.get(text) === undefined) {
+      setShowBanner(true);
+    }
+  }, [text]);
   if (loading) return null;
 
   const hideBanner = () => {
@@ -25,30 +28,41 @@ export const SiteBanner = () => {
     Cookies.set(text, "viewed", { expires: 7 });
   };
 
-  return (
+  return showBanner ? (
     <Banner theme={theme}>
-      {showBanner && (
-        <>
-          {text} <Icon glyph="X" onClick={hideBanner} />
-        </>
-      )}
+      {text}{" "}
+      <IconButton
+        aria-label="Close Site Banner"
+        variant="light"
+        onClick={hideBanner}
+      >
+        <Icon glyph="X" onClick={hideBanner} />{" "}
+      </IconButton>
     </Banner>
+  ) : (
+    <BannerPadding />
   );
 };
+
 type BannerProps = {
   theme: string;
 };
 
 const Banner = styled.div`
-  display: flex;
+  transition: max-height 0.3s ease-in-out;
   align-items: center;
-  justify-content: space-between;
   ${(props: BannerProps) =>
     props.theme && `background-color: ${bannerTypeToColor[props.theme]}`};
+  display: flex;
+  justify-content: space-between;
   padding-left: 15px;
   padding-right: 15px;
   padding-top: 5px;
   padding-bottom: 5px;
+  margin-bottom: 15px;
+`;
+
+const BannerPadding = styled.div`
   margin-bottom: 16px;
 `;
 
