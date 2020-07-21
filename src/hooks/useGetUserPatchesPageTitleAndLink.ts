2369bd7f39ef0ee645e42a84a0d6dc5ee5183479
Arjun Patel <arjun.patel@mongodb.com>
2020-07-21 10:24:03 -0400

    useGetUserPatchesPageTitleAndLink hook

---
+++ src/hooks/useGetUserPatchesPageTitleAndLink.ts
@@ -0,0 +1,34 @@
+import { GET_OTHER_USER } from "gql/queries";
+import {
+  GetOtherUserQuery,
+  GetOtherUserQueryVariables,
+} from "gql/generated/types";
+import { useQuery } from "@apollo/react-hooks";
+import { getUserPatchesRoute } from "constants/routes";
+
+export const useGetUserPatchesPageTitleAndLink = (userId: string) => {
+  const { data, loading, error } = useQuery<
+    GetOtherUserQuery,
+    GetOtherUserQueryVariables
+  >(GET_OTHER_USER);
+  const link = getUserPatchesRoute(userId);
+
+  if (loading || error) {
+    return { error, link, loading, title: "" };
+  }
+
+  if (userId === data?.currentUser.userId) {
+    return { error, link, loading, title: "My Patches" };
+  }
+
+  const otherUserDisplayName = data?.otherUser.displayName ?? "";
+  const possessiveModifier =
+    otherUserDisplayName.slice(-1) === "s" ? "'" : "'s";
+
+  return {
+    error,
+    link,
+    loading,
+    title: `${otherUserDisplayName}${possessiveModifier} Patches`,
+  };
+};
