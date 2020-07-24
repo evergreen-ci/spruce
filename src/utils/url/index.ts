a5905c789e44896c2f2594e8aacdeaee5985fd1a
Trey Granderson <trey.granderson@mongodb.com>
2020-07-24 11:54:22 -0400

    undo url util edits

--- src/utils/url/index.ts
+++ src/utils/url/index.ts
@@ -5,22 +5,16 @@ const arrayFormat = "comma";
 const pageKey = "page";
 const limitKey = "limit";
 
-export const validatePage = (page) =>
-  !Number.isNaN(page) && page >= 0 ? page : 0;
-
 export const getPageFromSearch = (search: string): number => {
   const parsed = queryString.parse(search, { arrayFormat });
   const page = parseInt((parsed[pageKey] ?? "").toString(), 10);
-  return validatePage(page);
+  return !Number.isNaN(page) && page >= 0 ? page : 0;
 };
-
-export const validateLimit = (limit) =>
-  !Number.isNaN(limit) && PAGE_SIZES.includes(limit)
-    ? limit
-    : DEFAULT_PAGE_SIZE;
 
 export const getLimitFromSearch = (search: string): number => {
   const parsed = queryString.parse(search, { arrayFormat });
   const limit = parseInt((parsed[limitKey] ?? "").toString(), 10);
-  return validateLimit(limit);
+  return !Number.isNaN(limit) && PAGE_SIZES.includes(limit)
+    ? limit
+    : DEFAULT_PAGE_SIZE;
 };
