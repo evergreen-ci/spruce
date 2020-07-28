ef09dfcff636ce61c0ef98e33f8a5a37a7db7a19
Jonathan Brill <jonathan.brill@10gen.com>
2020-07-28 16:55:50 -0400

    EVG-7952 enqueue a patch (#281)

--- src/components/PatchActionButtons/index.tsx
+++ src/components/PatchActionButtons/index.tsx
@@ -2,4 +2,5 @@ export { SchedulePatchTasks } from "./SchedulePatchTasks";
 export { UnschedulePatchTasks } from "./UnschedulePatchTasks";
 export { RestartPatch } from "./RestartPatch";
 export { SetPatchPriority } from "./SetPatchPriority";
+export { EnqueuePatch } from "./EnqueuePatch";
 export { AddNotification } from "./AddNotification";
