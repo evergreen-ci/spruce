4a71eab7eeb1a43deb2623a26a653b14978c8afb
Chaya Malik <chaya.malik@10gen.com>
2020-08-03 16:34:18 -0400

    EVG-12517 Fix colors on Display title and status (#327)

--- src/pages/Host.tsx
+++ src/pages/Host.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { GET_HOST } from "gql/queries/get-host";
-import get from "lodash/get";
 import { useQuery } from "@apollo/react-hooks";
 import { HostQuery, HostQueryVariables } from "gql/generated/types";
 import { usePageTitle } from "hooks/usePageTitle";
@@ -16,9 +15,9 @@ export const Host: React.FC = () => {
   const { data, loading } = useQuery<HostQuery, HostQueryVariables>(GET_HOST, {
     variables: { id },
   });
-  const host = get(data, "host");
-  const hostUrl = get(host, "hostUrl");
-  const status = get(host, "status") as HostStatus;
+  const host = data?.host;
+  const hostUrl = host?.hostUrl;
+  const status = host?.status as HostStatus;
 
   usePageTitle(`Host${hostUrl ? ` - ${hostUrl}` : ""}`);
 
