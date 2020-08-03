04f2bbcc9e99a0e011d20cd71992daee83c9c2bb
Chaya Malik <chaya.malik@10gen.com>
2020-08-03 16:10:51 -0400

    fix color

--- src/pages/Host.tsx
+++ src/pages/Host.tsx
@@ -16,8 +16,8 @@ export const Host: React.FC = () => {
   const { data, loading } = useQuery<HostQuery, HostQueryVariables>(GET_HOST, {
     variables: { id },
   });
-  const host = get(data, "host");
-  const hostUrl = get(host, "hostUrl");
+  const host = data?.host;
+  const hostUrl = host?.hostUrl;
   const status = get(host, "status") as HostStatus;
 
   usePageTitle(`Host${hostUrl ? ` - ${hostUrl}` : ""}`);
