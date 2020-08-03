cebe58bb1518e62f4606456a106698ac55dd523b
Trey Granderson <trey.granderson@mongodb.com>
2020-08-03 09:12:18 -0400

    EVG-12526: hosts table dropdown filters (#319)

--- src/components/Table/Filters.tsx
+++ src/components/Table/Filters.tsx
@@ -19,24 +19,29 @@ export const InputFilter: React.FC<InputFilterProps> = ({
   placeholder,
   value,
   onChange,
-  dataCy,
   updateUrlParam,
   resetUrlParam,
+  dataCy,
 }) => (
-  <FilterWrapper>
+  <FilterWrapper data-cy={`${dataCy}-wrapper`}>
     <Input
-      data-cy={dataCy}
+      data-cy="input-filter"
       placeholder={placeholder}
       value={value}
       onChange={onChange}
     />
     <ButtonsWrapper>
       <ButtonWrapper>
-        <Button size="small" onClick={resetUrlParam}>
+        <Button dataCy="reset-button" size="small" onClick={resetUrlParam}>
           Reset
         </Button>
       </ButtonWrapper>
-      <Button size="small" variant="primary" onClick={updateUrlParam}>
+      <Button
+        dataCy="filter-button"
+        size="small"
+        variant="primary"
+        onClick={updateUrlParam}
+      >
         Search
       </Button>
     </ButtonsWrapper>
@@ -54,17 +59,18 @@ export const getColumnSearchFilterProps = ({
   filterDropdown: () => (
     <InputFilter
       {...{
-        dataCy,
         placeholder,
         value,
         onChange,
         updateUrlParam,
         resetUrlParam,
+        dataCy,
       }}
     />
   ),
   filterIcon: () => (
     <Icon
+      data-cy={dataCy}
       type="search"
       style={{ color: value ? uiColors.blue.base : undefined }}
     />
@@ -81,22 +87,27 @@ export interface CheckboxFilterProps {
 }
 
 export const CheckboxFilter: React.FC<CheckboxFilterProps> = ({
-  dataCy,
   statuses,
   value,
   onChange,
   updateUrlParam,
   resetUrlParam,
+  dataCy,
 }) => (
-  <FilterWrapper data-cy={dataCy}>
+  <FilterWrapper data-cy={`${dataCy}-wrapper`}>
     <CheckboxGroup value={value} data={statuses} onChange={onChange} />
     <ButtonsWrapper>
       <ButtonWrapper>
-        <Button onClick={resetUrlParam} size="small">
+        <Button dataCy="reset-button" onClick={resetUrlParam} size="small">
           Reset
         </Button>
       </ButtonWrapper>
-      <Button size="small" variant="primary" onClick={updateUrlParam}>
+      <Button
+        dataCy="filter-button"
+        size="small"
+        variant="primary"
+        onClick={updateUrlParam}
+      >
         Filter
       </Button>
     </ButtonsWrapper>
@@ -125,6 +136,7 @@ export const getColumnCheckboxFilterProps = ({
   ),
   filterIcon: () => (
     <Icon
+      data-cy={dataCy}
       type="filter"
       style={{ color: value.length ? uiColors.blue.base : undefined }}
     />
