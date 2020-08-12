e53d52d59b063ae9857ac3808d2e3d07eaa16d65
Arjun Patel <arjun.patel@mongodb.com>
2020-08-12 11:52:22 -0400

    render Add New Key btn

--- src/pages/preferences/preferencesTabs/PublicKeysTab.tsx
+++ src/pages/preferences/preferencesTabs/PublicKeysTab.tsx
@@ -1,3 +1,11 @@
 import React from "react";
+import Button from "@leafygreen-ui/button";
+import Icon from "@leafygreen-ui/icon";
 
-export const PublicKeysTab: React.FC = () => <div>Manage Public Keys</div>;
+export const PublicKeysTab: React.FC = () => (
+  <div>
+    <Button size="small" data-cy="add-key-button" glyph={<Icon glyph="Plus" />}>
+      Add New Key
+    </Button>
+  </div>
+);
