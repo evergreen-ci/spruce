f5ddbad202c0b16bcd6bc115288e3d4ce576078c
Mohamed <mohamed.khelif@mongodb.com>
2020-08-17 18:31:45 -0400

    Fix test selectors

--- cypress/integration/spawn/host.ts
+++ cypress/integration/spawn/host.ts
@@ -1,28 +1,27 @@
 // / <reference types="Cypress" />
 
-const tableRow = "tr.ant-table-row";
+const hostTableRow = ".ant-table-row";
 const hostColumnHeader =
   ":nth-child(1) > .ant-table-header-column > .ant-table-column-sorters";
 
-const unsortedSpawnHostOrder = [
-  "rhel71-ppc-1.pic.build.10gen",
-  "i-04ade558e1e26b0ad",
-];
+// const hostColumnHeader =
+//   ".ant-table-column-sort > .ant-table-header-column > .ant-table-column-sorters";
+const unsortedSpawnHostOrder = ["i-092593689871a50dc", "i-04ade558e1e26b0ad"];
 const ascendingSortSpawnHostOrderByHostId = [
   "i-04ade558e1e26b0ad",
-  "rhel71-ppc-1.pic.build.10gen",
+  "i-092593689871a50dc",
 ];
 const descendingSortSpawnHostOrderByHostId = [
-  "rhel71-ppc-1.pic.build.10gen",
+  "i-092593689871a50dc",
   "i-04ade558e1e26b0ad",
 ];
 
 const descendingSortSpawnHostOrderByExpiration = [
   "i-04ade558e1e26b0ad",
-  "rhel71-ppc-1.pic.build.10gen",
+  "i-092593689871a50dc",
 ];
 const ascendingSortSpawnHostOrderByExpiration = [
-  "rhel71-ppc-1.pic.build.10gen",
+  "i-092593689871a50dc",
   "i-04ade558e1e26b0ad",
 ];
 
@@ -37,46 +36,46 @@ describe("Navigating to Spawn Host page", () => {
 
   it("Visiting the spawn host page should display all of your spawned hosts", () => {
     cy.visit("/spawn/host");
-    cy.get(tableRow).should("have.length", 2);
+    cy.get(hostTableRow).should("have.length", 2);
   });
   it("Visiting the spawn host page should display all of your spawned hosts not sorted by default", () => {
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(unsortedSpawnHostOrder[index])
     );
   });
   it("Clicking on the host column header should sort spawn hosts by ascending order by id", () => {
     cy.get(hostColumnHeader).click();
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(ascendingSortSpawnHostOrderByHostId[index])
     );
   });
   it("Clicking on the host column header a second time should sort spawn hosts by decending order by id", () => {
     cy.get(hostColumnHeader).click();
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(descendingSortSpawnHostOrderByHostId[index])
     );
   });
   it("Clicking on the host column header a third  time should return the spawn host table to its original state", () => {
     cy.get(hostColumnHeader).click();
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(unsortedSpawnHostOrder[index])
     );
   });
   it("Clicking on the expiration column header should sort the hosts by ascending order", () => {
     cy.contains("Expires In").click();
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(ascendingSortSpawnHostOrderByExpiration[index])
     );
   });
   it("Clicking on the expiration column header should sort the hosts by descending order", () => {
     cy.contains("Expires In").click();
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(descendingSortSpawnHostOrderByExpiration[index])
     );
   });
   it("Clicking on the expiration column header a third  time should return the spawn host table to its original state", () => {
     cy.contains("Expires In").click();
-    cy.get(tableRow).each(($el, index) =>
+    cy.get(hostTableRow).each(($el, index) =>
       cy.wrap($el).contains(unsortedSpawnHostOrder[index])
     );
   });
