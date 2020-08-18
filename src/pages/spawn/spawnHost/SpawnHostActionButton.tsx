7f42757f08901f1d4b939a5010c18e9f07b2983c
Mohamed Khelif <mohamed.khelif@mongodb.com>
2020-08-18 10:33:58 -0400

    EVG-12576 Add spawn host Table  (#349)

---
+++ src/pages/spawn/spawnHost/SpawnHostActionButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import Button, { Size } from "@leafygreen-ui/button";
+import Icon from "components/icons/Icon";
+import { Host } from "gql/generated/types";
+
+import { HostStatus } from "types/host";
+
+export const SpawnHostActionButton: React.FC<{ host: Host }> = ({ host }) => {
+  const action = mapStatusToAction[host.status];
+
+  return action ? (
+    <PaddedButton glyph={<Icon glyph={action} />} size={Size.XSmall} />
+  ) : null;
+};
+const mapStatusToAction = {
+  [HostStatus.Running]: "Pause",
+  [HostStatus.Stopped]: "Start",
+};
+
+const PaddedButton = styled(Button)`
+  margin-left: 5px;
+  margin-right: 5px;
+`;
