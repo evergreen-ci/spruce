5c15d0355ea24f3f358937e3d4e2b4c068256f52
Arjun Patel <arjun.patel@mongodb.com>
2020-08-18 15:30:03 -0400

    tests

--- cypress/integration/preferences/public_key_managment.ts
+++ cypress/integration/preferences/public_key_managment.ts
@@ -40,7 +40,7 @@ describe("Public Key Management Page", () => {
     it("Displays errors when the modal opens", () => {
       cy.visit(route);
       cy.dataCy("add-key-button").click();
-      cy.dataCy("error-messages").each(($el, index) =>
+      cy.dataCy("error-message").each(($el, index) =>
         cy.wrap($el).contains([err1, err2][index])
       );
     });
@@ -48,7 +48,7 @@ describe("Public Key Management Page", () => {
     it("Error messages go away after typing valid input", () => {
       cy.dataCy("key-name-input").type(keyName3);
       cy.dataCy("key-value-input").type("ssh-dss");
-      cy.dataCy("error-messages").should("have.length", 0);
+      cy.dataCy("error-message").should("have.length", 0);
     });
 
     it("Should include the public in the public key list after adding", () => {
@@ -66,15 +66,13 @@ describe("Public Key Management Page", () => {
     it("Should show an error if the key name already exists", () => {
       cy.dataCy("add-key-button").click();
       cy.dataCy("key-name-input").type(keyName3);
-      cy.dataCy("error-messages").each(($el, index) =>
-        cy.wrap($el).contains([err3][index])
-      );
+      cy.dataCy("error-message")
+        .first()
+        .contains(err3);
     });
 
     it("Modal has correct title", () => {
       cy.dataCy("modal-title").contains("Add Public Key");
-      cy.dataCy("cancel-subscription-button").click();
-      cy.dataCy("save-key-button").click();
     });
   });
 
@@ -84,7 +82,7 @@ describe("Public Key Management Page", () => {
       cy.dataCy("edit-btn")
         .first()
         .click();
-      cy.dataCy("error-messages").should("have.length", 0);
+      cy.dataCy("error-message").should("have.length", 0);
     });
 
     it("After submitting, the key name and key value are updated", () => {
@@ -110,8 +108,9 @@ describe("Public Key Management Page", () => {
 
   describe("Network errors", () => {
     it("should show an error banner after submitting an invalid public key", () => {
+      cy.visit(route);
       cy.dataCy("add-key-button").click();
-      cy.dataCy("key-name-input").type(keyName4);
+      cy.dataCy("key-name-input").type("rsioeantarsn");
       cy.dataCy("key-value-input").type("ssh-rsa");
       cy.dataCy("save-key-button").click();
       cy.dataCy("banner")
