a019aa0c63b0e5d924579eaf13f5863df172936c
Trey Granderson <trey.granderson@mongodb.com>
2020-08-21 11:32:21 -0400

    change test route

--- cypress/integration/test_table.ts
+++ cypress/integration/test_table.ts
@@ -267,9 +267,11 @@ describe("Tests Table", () => {
     });
   });
 
-  xdescribe("Changing page number", () => {
+  describe("Changing page number", () => {
     before(() => {
-      cy.visit(TESTS_ROUTE);
+      cy.visit(
+        "/task/clone_evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48/tests?execution=1&limit=10&page=0"
+      );
     });
 
     it("Displays the next page of results and updates URL when right arrow is clicked and next page exists", () => {
