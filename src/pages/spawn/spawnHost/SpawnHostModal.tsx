0282f1e223bf3a34cabbb3334d4c49c36a80c219
Mohamed <mohamed.khelif@mongodb.com>
2020-08-27 23:26:30 -0400

    Add sub forms to spawn host modal

--- src/pages/spawn/spawnHost/SpawnHostModal.tsx
+++ src/pages/spawn/spawnHost/SpawnHostModal.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { Modal, AutoComplete, Input } from "antd";
+import React, { useState } from "react";
+import { Modal, AutoComplete, Input, Select } from "antd";
 import { useQuery } from "@apollo/client";
+import styled from "@emotion/styled";
 import { Subtitle, H2 } from "@leafygreen-ui/typography";
 import Button, { Variant } from "@leafygreen-ui/button";
+import { uiColors } from "@leafygreen-ui/palette";
 import Icon from "components/icons/Icon";
-import { GET_DISTROS } from "gql/queries";
-import { DistrosQuery, DistrosQueryVariables } from "gql/generated/types";
+import { InputLabel } from "components/styles";
+import { GET_DISTROS, GET_MY_PUBLIC_KEYS, GET_AWS_REGIONS } from "gql/queries";
+import {
+  DistrosQuery,
+  DistrosQueryVariables,
+  GetMyPublicKeysQuery,
+  GetMyPublicKeysQueryVariables,
+  AwsRegionsQuery,
+  AwsRegionsQueryVariables,
+} from "gql/generated/types";
+import {
+  PublicKeyForm,
+  publicKeyStateType,
+} from "./spawnHostModal/PublicKeyForm";
+import {
+  HostDetailsForm,
+  hostDetailsStateType,
+} from "./spawnHostModal/HostDetailsForm";
+
+const { Option } = Select;
+const { gray } = uiColors;
 
 interface SpawnHostModalProps {
   visible: boolean;
@@ -26,10 +47,41 @@ export const SpawnHostModal: React.FC<SpawnHostModalProps> = ({
     },
   });
 
-  const distros = distrosData?.distros;
-  if (distroLoading) {
-    return <>Loading...</>;
+  const { data: awsData, loading: awsLoading } = useQuery<
+    AwsRegionsQuery,
+    AwsRegionsQueryVariables
+  >(GET_AWS_REGIONS);
+
+  const { data: publicKeysData, loading: publicKeyLoading } = useQuery<
+    GetMyPublicKeysQuery,
+    GetMyPublicKeysQueryVariables
+  >(GET_MY_PUBLIC_KEYS);
+
+  const [publicKeyState, setPublicKeyState] = useState<publicKeyStateType>({
+    publicKey: {
+      name: "",
+      key: "",
+    },
+    savePublicKey: false,
+  });
+  const [hostDetailsState, setHostDetailsState] = useState<
+    hostDetailsStateType
+  >({
+    hasUserDataScript: false,
+    userDataScript: "",
+    expiration: new Date(),
+    noExpiration: false,
+  });
+  const [distro, setDistro] = useState("");
+  const [awsRegion, setAWSRegion] = useState("");
+
+  if (distroLoading || publicKeyLoading || awsLoading) {
+    return null;
   }
+
+  const distros = distrosData?.distros;
+  const publicKeys = publicKeysData?.myPublicKeys;
+  const awsRegions = awsData?.awsRegions;
   return (
     <Modal
       title={<H2>Spawn New Host</H2>}
@@ -52,17 +104,78 @@ export const SpawnHostModal: React.FC<SpawnHostModalProps> = ({
         "data-cy": "spawn-host-modal",
       }}
     >
-      <Subtitle> Required Host Information</Subtitle>
-      <AutoComplete
-        style={{ width: 200, marginLeft: 0 }}
-        dataSource={distros?.map((d) => d.name)}
-      >
-        <Input
-          style={{ width: 200 }}
-          placeholder="Search for Distro"
-          suffix={<Icon glyph="MagnifyingGlass" />}
-        />
-      </AutoComplete>
+      <Container>
+        <Subtitle> Required Host Information</Subtitle>
+        <Section>
+          <InputLabel htmlFor="distroSearchBox">Distro</InputLabel>
+          <AutoComplete
+            style={{ width: 200, marginLeft: 0 }}
+            options={distros?.map((d) => wrapOption(d.name))}
+            id="distroSearchBox"
+            onChange={setDistro}
+          >
+            <Input
+              value={distro}
+              style={{ width: 200 }}
+              placeholder="Search for Distro"
+              suffix={<Icon glyph="MagnifyingGlass" />}
+            />
+          </AutoComplete>
+        </Section>
+        <Section>
+          <InputLabel htmlFor="awsSelectDropown">Region</InputLabel>
+          <Select
+            id="awsSelectDropown"
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Select existing key"
+            onChange={(v) => setAWSRegion(v)}
+            value={awsRegion}
+          >
+            {awsRegions?.map((region) => (
+              <Option value={region} key={`region_option_${region}`}>
+                {region}
+              </Option>
+            ))}
+          </Select>
+        </Section>
+        <Section>
+          <PublicKeyForm
+            publicKeys={publicKeys}
+            data={publicKeyState}
+            onChange={setPublicKeyState}
+          />
+        </Section>
+        <HR />
+        <Section>
+          <HostDetailsForm
+            data={hostDetailsState}
+            onChange={setHostDetailsState}
+          />
+        </Section>
+      </Container>
     </Modal>
   );
 };
+
+// antd requires options to be in this format.
+// For some reason something is preventing me from inlining this in the map statement above
+const wrapOption = (str: string) => ({
+  value: str,
+});
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Section = styled(Container)`
+  margin-top: 20px;
+`;
+
+const HR = styled("hr")`
+  background-color: ${gray.light2};
+  border: 0;
+  height: 1px;
+  width: 100%;
+`;
