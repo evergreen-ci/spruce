da5bea255253dcad865f931636b087d3874eef52
Mohamed <mohamed.khelif@mongodb.com>
2020-09-03 13:40:02 -0400

    Clean up deploy process and messaging

--- cli-scripts/git-tag-proc.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require("child_process");
-
-const getLatestReleaseTag = () =>
-  execSync("git describe --tags `git rev-list --tags --max-count=1`", {
-    encoding: "utf-8",
-  }).trim();
-
-module.exports = getLatestReleaseTag;
