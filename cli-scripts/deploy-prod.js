7ca584d8fb0e83b96dc78fd9540633217050fe36
Mohamed Khelif <mohamed.khelif@mongodb.com>
2020-09-08 10:46:11 -0400

    EVG-12932 Clean up deploy process and messaging (#389)

--- cli-scripts/deploy-prod.js
+++ cli-scripts/deploy-prod.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 const colors = require("colors/safe");
 const prompt = require("prompt");
 const promptRun = require("prompt-run");
 const simpleGit = require("simple-git");
 const path = require("path");
-const getLatestReleaseTag = require("./git-tag-proc");
+const { getLatestCommitsSinceLastRelease } = require("./deploy-utils");
 
 const git = simpleGit(path.resolve(__dirname, ".."));
 
@@ -20,41 +21,6 @@ const checkIfOnMaster = async () => {
   return true;
 };
 
-// Fetches all the commits since the last release tag if there are no commits
-// since the last release it returns false otherwise prints out all the commits
-const getLatestCommitsSinceLastRelease = async () => {
-  try {
-    const latestRelease = getLatestReleaseTag();
-    const latestCommits = await git.log(latestRelease, "HEAD");
-    if (latestCommits.all.length === 0) {
-      console.log(colors.red("No Changes found since last release\n"));
-      return false;
-    }
-    console.log(colors.red("Changes since last release\n"));
-    prettyPrintCommitLogs(latestCommits.all);
-    return true;
-  } catch (e) {
-    console.log(
-      colors.bgRed(
-        `Error occured when trying to parse commits.\nThere is likely something wrong with your commit history:\n ${e}`
-      )
-    );
-    return false;
-  }
-};
-
-// Prints out all the commits since the commit body is optional it will conditionally print it
-const prettyPrintCommitLogs = (latestCommits) => {
-  latestCommits.forEach((commit) => {
-    console.log(`hash: ${colors.yellow(commit.hash)}`);
-    console.log(`author: ${colors.cyan(commit.author_name)}`);
-    console.log(`email: ${colors.cyan(commit.author_email)}`);
-    console.log(`change: ${colors.cyan(commit.message)}`);
-    if (commit.body) console.log(`body: ${colors.cyan(commit.body)}`);
-    console.log("\n");
-  });
-};
-
 // Runs the script to build and deploy to production
 const deployProcess = () => {
   promptRun({
@@ -68,14 +34,22 @@ const deployProcess = () => {
     childProcess.on("close", () => {
       console.log("Deploy Complete! 🎉");
       console.log("Don't forget to push tags");
-      console.log(colors.green("git push upstream && git push upstream --tags"));
+      console.log(
+        colors.green("git push upstream && git push upstream --tags")
+      );
     });
   });
 };
 
 const deployProd = async () => {
   const onMaster = await checkIfOnMaster();
-  const anyChangesFound = await getLatestCommitsSinceLastRelease();
+  const latestCommits = await getLatestCommitsSinceLastRelease();
+  const anyChangesFound = latestCommits.length > 0;
+  if (anyChangesFound) {
+    console.log(latestCommits);
+  } else {
+    console.log(colors.red("No Changes found"));
+  }
   if (onMaster && anyChangesFound) {
     prompt.start();
     prompt.get(
--- cli-scripts/notify-email.js
+++ cli-scripts/notify-email.js
@@ -1,20 +1,17 @@
+/* eslint-disable no-console */
 const { exec } = require("child_process");
-const simpleGit = require("simple-git");
-const { gitDescribeSync } = require("git-describe");
-const path = require("path");
-
-const git = simpleGit(path.resolve(__dirname, ".."));
+const { getLatestCommitsSinceLastRelease } = require("./deploy-utils");
 
 // this script gets the current user email, the latest git commit hash of
 // the current branch and sends an email to a recipient that will forward
 // the message to a slack channel
-const sendEmail = () =>
+const sendEmail = async () =>
   exec("git config user.email", async (emailErr, email) => {
     if (emailErr) {
       console.error(errorMessage, emailErr);
     } else {
-      const formattedEmail = email.replace(lineBreakRegex, "");
-      exec("git rev-parse HEAD", (commitHashErr, commitHash) => {
+      const formattedEmail = email.trim();
+      exec("git rev-parse HEAD", async (commitHashErr, commitHash) => {
         if (commitHashErr) {
           console.error(errorMessage, commitHashErr);
         } else {
@@ -25,54 +22,25 @@ const sendEmail = () =>
           const dateStr = `${today.getFullYear()}/${month}/${day}`;
           const recipient = process.env.REACT_APP_DEPLOYS_EMAIL;
           const subject = `${dateStr} Spruce deploy ${formattedCommitHash}`;
-          getLatestCommitsSinceLastRelease().then((messageBody) => {
-            const cleanedMessageBody = messageBody
-              .replace(/\(/gm, "")
-              .replace(/\)/gm, "");
-            exec(
-              `evergreen notify email -f ${formattedEmail} -r ${recipient} -s "${subject}" -b "${cleanedMessageBody} check it out <a href="${process.env.REACT_APP_SITE_URL}">here</a>"`,
-              (notifyErr, notifyStdOut, notifyStdErr) => {
-                if (notifyErr) {
-                  console.error(errorMessage, notifyErr);
-                } else {
-                  console.log(notifyStdOut);
-                  console.log(notifyStdErr);
-                }
+          const messageBody = await getLatestCommitsSinceLastRelease();
+          const formattedBody = messageBody.replace("\n", "<br />");
+          exec(
+            `evergreen notify email -f ${formattedEmail} -r ${recipient} -s "${subject}" -b "${formattedBody} <br/>Check it out <a href="${process.env.REACT_APP_SITE_URL}">here!</a>"`,
+            (notifyErr, notifyStdOut, notifyStdErr) => {
+              if (notifyErr) {
+                console.error(errorMessage, notifyErr);
+              } else {
+                console.log(notifyStdOut);
+                console.log(notifyStdErr);
               }
-            );
-          });
+            }
+          );
         }
       });
     }
   });
 
-// Fetches all the commits since the last release tag if there are no commits
-// since the last release it returns a message otherwise it returns all the commits
-const getLatestCommitsSinceLastRelease = async () => {
-  const latestRelease = gitDescribeSync(__dirname);
-  const latestCommits = await git.log(latestRelease.tag, "HEAD");
-  if (latestCommits.all.length === 0) {
-    return "No Changes found since last release\n";
-  }
-  return prettifyCommitLogs(latestCommits.all);
-};
-
-// Returns all the commits formatted
-const prettifyCommitLogs = (latestCommits) => {
-  let commitBody = "<u><i>Changes since last release</i></u><br>";
-  latestCommits.forEach((commit) => {
-    let commitString = "";
-    commitString += `hash: <b>${commit.hash}</b><br>`;
-    commitString += `author: <b>${commit.author_name}</b><br>`;
-    commitString += `change: <b>${commit.message}</b><br>`;
-    commitString += "<br>";
-    commitBody += commitString;
-  });
-  return commitBody;
-};
-
 sendEmail();
 
 const errorMessage = "Error sending message to slack";
-const lineBreakRegex = /(\r\n|\n|\r)/gm;
 const prependZero = (strNum) => (strNum.length < 2 ? `0${strNum}` : strNum);
