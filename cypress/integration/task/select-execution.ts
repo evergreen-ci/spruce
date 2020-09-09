85d01338b725b4bdbc028afc8c3c7bfd925b8111
Arjun Patel <arjun.patel@mongodb.com>
2020-09-09 11:15:18 -0400

    update tests

--- cypress/integration/task/select-execution.ts
+++ cypress/integration/task/select-execution.ts
@@ -15,14 +15,14 @@ describe("Selecting Task Execution", () => {
   it("Should be able to switch to the new execution", () => {
     cy.dataCy("execution-select").click();
     cy.dataCy("execution-1").click();
-    cy.dataCy("task-status-badge").contains("undispatched");
+    cy.dataCy("task-status-badge").contains("Undispatched");
   });
 
   it("should display different executions", () => {
-    cy.dataCy("task-status-badge").contains("undispatched");
+    cy.dataCy("task-status-badge").contains("Undispatched");
     cy.dataCy("execution-select").click();
     cy.dataCy("execution-1").click();
-    cy.dataCy("task-status-badge").contains("undispatched");
+    cy.dataCy("task-status-badge").contains("Undispatched");
     cy.location("search").should("include", "execution=2");
   });
 });
