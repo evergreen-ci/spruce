9a3584883fedc199594c80eed5faaab44a6a8c2d
Arjun Patel <arjun.patel@mongodb.com>
2020-09-09 13:23:08 -0400

    size limit label

--- src/pages/spawn/spawnVolume/SpawnVolumeButton.tsx
+++ src/pages/spawn/spawnVolume/SpawnVolumeButton.tsx
@@ -1,4 +1,21 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { uiColors } from "@leafygreen-ui/palette";
+import { Disclaimer } from "@leafygreen-ui/typography";
 import { PlusButton } from "components/Spawn";
 
-export const SpawnVolumeButton = () => <PlusButton>Create Volume</PlusButton>;
+export const SpawnVolumeButton = () => (
+  <div>
+    <PlusButton>Create Volume</PlusButton>
+    <Info>Limit 1500 GiB per User</Info>
+  </div>
+);
+
+const Info = styled(Disclaimer)`
+  font-weight: 300;
+  padding-left: 8px;
+  position: relative;
+  top: -2px;
+  font-style: italic;
+  color: ${uiColors.gray.dark2};
+`;
