6b883bf7faa4e650602cb5d044bdba5278829725
Arjun Patel <arjun.patel@mongodb.com>
2020-09-10 13:48:49 -0400

    placeholder comps

---
+++ src/pages/spawn/spawnVolume/spawnVolumeTable/SpawnVolumeCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Volume } from "gql/generated/types";
+
+interface Props {
+  volume: Volume;
+}
+export const SpawnVolumeCard: React.FC<Props> = () => {
+  return <div>spawn volume card</div>;
+};
---
+++ src/pages/spawn/spawnVolume/spawnVolumeTable/SpawnVolumeTableActions.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SpawnVolumeTableActions = () => {
+  return <div>table actions</div>;
+};
