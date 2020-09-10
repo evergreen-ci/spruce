36904c49db0f3453d6eea0f039414b317d008251
Arjun Patel <arjun.patel@mongodb.com>
2020-09-10 14:02:39 -0400

    import shared comp

--- src/pages/spawn/spawnVolume/SpawnVolumeTable.tsx
+++ src/pages/spawn/spawnVolume/SpawnVolumeTable.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import styled from "@emotion/styled";
-import { Table } from "antd";
 import { useLocation } from "react-router";
-import Icon from "components/icons/Icon";
+import { SpawnTable } from "components/Spawn";
 import { Volume } from "gql/generated/types";
 import { SpawnVolumeCard } from "pages/spawn/spawnVolume/spawnVolumeTable/SpawnVolumeCard";
 import { parseQueryString } from "utils";
@@ -18,27 +16,14 @@ export const SpawnHostTable: React.FC<SpawnVolumeTableProps> = ({
   const queryParams = parseQueryString(search);
   const volume = queryParams?.volume;
   return (
-    <Container>
-      <Table
-        rowKey={(record) => record.id}
-        pagination={false}
-        expandRowByClick
-        expandIcon={({ expanded }) => (
-          <Icon glyph={expanded ? "CaretDown" : "CaretRight"} />
-        )}
-        expandedRowRender={(record: Volume) => (
-          <SpawnVolumeCard volume={record} />
-        )}
-        columns={columns}
-        dataSource={volumes}
-        defaultExpandedRowKeys={[volume as string]}
-      />
-    </Container>
+    <SpawnTable
+      expandedRowRender={(record: Volume) => (
+        <SpawnVolumeCard volume={record} />
+      )}
+      columns={columns}
+      dataSource={volumes}
+      defaultExpandedRowKeys={[volume as string]}
+    />
   );
 };
 const columns = [];
-
-const Container = styled.div`
-  width: 100%;
-  padding-right: 1%;
-`;
