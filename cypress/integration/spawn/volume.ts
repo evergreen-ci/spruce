e0bec5dddee8393f8eedd15b115fd2adc2f4949c
Arjun Patel <arjun453@gmail.com>
2020-09-10 09:33:44 -0400

    EVG-12588: Volumes Page title + badges (#395)

---
+++ cypress/integration/spawn/volume.ts
@@ -0,0 +1,17 @@
+// / <reference types="Cypress" />
+
+describe("Navigating to Spawn Volume page", () => {
+  before(() => {
+    cy.login();
+  });
+
+  beforeEach(() => {
+    cy.preserveCookies();
+  });
+
+  it("Visiting the spawn volume page should display the number of free and mounted volumes ", () => {
+    cy.visit("/spawn/volume");
+    cy.dataCy("mounted-badge").contains("9 Mounted");
+    cy.dataCy("free-badge").contains("1 Free");
+  });
+});
