97975e8d906d05a278a0e2917cb36646e4594149
Mohamed <mohamed.khelif@mongodb.com>
2020-09-15 00:45:50 -0400

    Fix formatting for consistency

--- src/pages/spawn/spawnHost/EditSpawnHostModal.tsx
+++ src/pages/spawn/spawnHost/EditSpawnHostModal.tsx
@@ -4,6 +4,7 @@ import styled from "@emotion/styled";
 import Button, { Variant } from "@leafygreen-ui/button";
 import { Body } from "@leafygreen-ui/typography";
 import { Input, Select } from "antd";
+import { diff } from "deep-object-diff";
 import isEqual from "lodash.isequal";
 import { Modal } from "components/Modal";
 import { InputLabel } from "components/styles";
@@ -13,11 +14,13 @@ import {
   InstanceTypesQueryVariables,
   MyVolumesQuery,
   MyHostsQueryVariables,
+  InstanceTag,
 } from "gql/generated/types";
 import { GET_INSTANCE_TYPES, GET_MY_VOLUMES } from "gql/queries";
 import {
   HostExpirationField,
   VolumesField,
+  UserTagsField,
 } from "pages/spawn/spawnHost/fields";
 
 const { Option } = Select;
@@ -28,6 +31,8 @@ interface editSpawnHostState {
   displayName?: string;
   instanceType?: string;
   volume?: string;
+  addedInstanceTags?: InstanceTag[];
+  deletedInstanceTags?: InstanceTag[];
 }
 interface EditSpawnHostModalProps {
   visible: boolean;
@@ -40,13 +45,15 @@ export const EditSpawnHostModal: React.FC<EditSpawnHostModalProps> = ({
   onCancel,
   host,
 }) => {
-  const { expiration, noExpiration } = host;
-  const defaultEditSpawnHostState = {
+  const { expiration, noExpiration, instanceTags } = host;
+  const defaultEditSpawnHostState: editSpawnHostState = {
     displayName: host.displayName,
     expiration,
     noExpiration,
     instanceType: host.instanceType,
     volume: host.homeVolumeID,
+    addedInstanceTags: [],
+    deletedInstanceTags: [],
   };
   const [editSpawnHostState, setEditSpawnHostState] = useState<
     editSpawnHostState
@@ -70,23 +77,31 @@ export const EditSpawnHostModal: React.FC<EditSpawnHostModalProps> = ({
 
   const hasChanges = isEqual(defaultEditSpawnHostState, editSpawnHostState);
 
+  // This will be used for the mutation submission we are submitting the diff
+  // Between the changes and the default values so we don't submit an unchanged field
+  const mutationParams = diff(defaultEditSpawnHostState, editSpawnHostState);
+  console.log({ mutationParams });
+
   return (
     <Modal
       title="Edit Host Details"
       visible={visible}
       onCancel={onCancel}
       footer={[
-        <WideButton onClick={onCancel}>Cancel</WideButton>,
+        <WideButton onClick={onCancel} key="cancel_button">
+          Cancel
+        </WideButton>,
         <WideButton
           data-cy="save-spawn-host-button"
           disabled={hasChanges}
           onClick={() => undefined}
           variant={Variant.Primary}
+          key="save_spawn_host_button"
         >
           Save
         </WideButton>,
       ]}
-      data-cy="spawn-host-modal"
+      data-cy="edit-spawn-host-modal"
     >
       <Container>
         <SectionContainer>
@@ -106,6 +121,7 @@ export const EditSpawnHostModal: React.FC<EditSpawnHostModalProps> = ({
           </Section>
         </SectionContainer>
         <SectionContainer>
+          <SectionLabel weight="medium">Expiration</SectionLabel>
           <HostExpirationField
             data={editSpawnHostState}
             onChange={setEditSpawnHostState}
@@ -113,7 +129,6 @@ export const EditSpawnHostModal: React.FC<EditSpawnHostModalProps> = ({
         </SectionContainer>
         <SectionContainer>
           <SectionLabel weight="medium">Instance Type</SectionLabel>
-
           <Section>
             <InputLabel htmlFor="instanceTypeDropdown">
               Instance Types
@@ -150,6 +165,14 @@ export const EditSpawnHostModal: React.FC<EditSpawnHostModalProps> = ({
             volumes={volumes}
           />
         </SectionContainer>
+        <SectionContainer>
+          <SectionLabel weight="medium">User Tags</SectionLabel>
+          <UserTagsField
+            data={editSpawnHostState}
+            onChange={setEditSpawnHostState}
+            instanceTags={instanceTags}
+          />
+        </SectionContainer>
       </Container>
     </Modal>
   );
@@ -160,7 +183,6 @@ const FlexContainer = styled.div`
 `;
 
 const SectionContainer = styled(FlexContainer)`
-  align-items: center;
   margin-top: 15px;
 `;
 
