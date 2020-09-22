3188628f8bc466f34a0913d0e1900706b760f29a
Arjun Patel <arjun.patel@mongodb.com>
2020-09-22 14:31:53 -0400

    render order and badge copy

--- cypress/integration/spawn/volume.ts
+++ cypress/integration/spawn/volume.ts
@@ -12,12 +12,26 @@ describe("Navigating to Spawn Volume page", () => {
   it("Visiting the spawn volume page should display the number of free and mounted volumes.", () => {
     cy.visit("/spawn/volume");
     cy.dataCy("mounted-badge").contains("9 Mounted");
-    cy.dataCy("free-badge").contains("2 Free");
+    cy.dataCy("free-badge").contains("4 Free");
   });
 
   it("The table initally displays volumes with status ascending.", () => {
-    const expectedVolNames = [];
-    cy.get("vol-name").each(($el, index) =>
+    const expectedVolNames = [
+      "1da0e996608e6871b60a92f6564bbc9cdf66ce90be1178dfb653920542a0d0f0",
+      "vol-0c66e16459646704d",
+      "vol-0583d66433a69f136",
+      "vol-0ea662ac92f611ed4",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      "vol-0ae8720b445b771b6",
+    ];
+    cy.dataCy("vol-name").each(($el, index) =>
       cy.wrap($el).contains(expectedVolNames[index])
     );
   });
