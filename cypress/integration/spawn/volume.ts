4dcd69a2b81b960138642c7402fb7bb96e65aa33
Arjun Patel <arjun.patel@mongodb.com>
2020-09-30 16:25:44 -0400

    increase viewport even more

--- cypress/integration/spawn/volume.ts
+++ cypress/integration/spawn/volume.ts
@@ -8,7 +8,7 @@ describe("Navigating to Spawn Volume page", () => {
 
   beforeEach(() => {
     cy.preserveCookies();
-    cy.viewport(1920, 1080);
+    cy.viewport(1920, 1600);
   });
 
   it("Visiting the spawn volume page should display the number of free and mounted volumes.", () => {
