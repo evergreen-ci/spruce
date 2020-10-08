82003a47d0365d1fbc0afdb329b337f96e2cf108
Mohamed <mohamed.khelif@mongodb.com>
2020-10-08 14:22:42 -0400

    Code cleanup

--- src/pages/patch/BuildVariants.tsx
+++ src/pages/patch/BuildVariants.tsx
@@ -106,20 +106,17 @@ const groupTasksByColor = (tasks: PatchBuildVariantTask[]) => {
     [key: string]: { count: number; statuses: string[]; textColor: string };
   } = {};
   tasks.forEach((task) => {
-    if (taskColors[mapVariantTaskStatusToColor[task.status]]) {
-      taskColors[mapVariantTaskStatusToColor[task.status]].count += 1;
-      if (
-        !taskColors[mapVariantTaskStatusToColor[task.status]].statuses.includes(
-          task.status
-        )
-      ) {
-        taskColors[mapVariantTaskStatusToColor[task.status]].statuses = [
-          ...taskColors[mapVariantTaskStatusToColor[task.status]].statuses,
+    const taskStatusToColor = mapVariantTaskStatusToColor[task.status];
+    if (taskColors[taskStatusToColor]) {
+      taskColors[taskStatusToColor].count += 1;
+      if (!taskColors[taskStatusToColor].statuses.includes(task.status)) {
+        taskColors[taskStatusToColor].statuses = [
+          ...taskColors[taskStatusToColor].statuses,
           task.status,
         ];
       }
     } else {
-      taskColors[mapVariantTaskStatusToColor[task.status]] = {
+      taskColors[taskStatusToColor] = {
         count: 1,
         statuses: [task.status],
         textColor: mapVariantTaskStatusToDarkColor[task.status],
