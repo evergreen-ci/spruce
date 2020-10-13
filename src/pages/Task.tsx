a104d0ebd25ee0109cd6b1f7d3adca41775e4aa6
John Liu <john.m.liu2@gmail.com>
2020-10-13 09:33:13 -0700

    EVG-8181 preserve query part of url on task page

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -133,9 +133,7 @@ const TaskCore: React.FC = () => {
     tabToIndexMap,
     defaultTab: TaskTab.Logs,
     path: `${paths.task}/${id}`,
-    query: new URLSearchParams(
-      `${RequiredQueryParams.Execution}=${ExecutionAsDisplay(execution)}`
-    ),
+    query: new URLSearchParams(location.search),
     sendAnalyticsEvent: (newTab: string) =>
       taskAnalytics.sendEvent({ name: "Change Tab", tab: newTab }),
   });
