56d1cd610b3c2c0ab54dc8b8b9114f666275ce96
Arjun Patel <arjun.patel@mongodb.com>
2020-10-20 12:52:15 -0400

    SizeSelector component

---
+++ src/pages/spawn/spawnVolume/spawnVolumeButton/spawnVolumeModal/SizeSelector.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { InputNumber } from "antd";
+import { ModalContent, SectionContainer, SectionLabel } from "components/Spawn";
+import { InputLabel } from "components/styles";
+
+interface Props {
+  value: number;
+  onChange: (s: number) => void;
+}
+
+export const SizeSelector: React.FC<Props> = ({ value, onChange }) => (
+  <SectionContainer>
+    <SectionLabel weight="medium">Volume Size</SectionLabel>
+    <ModalContent>
+      <InputLabel htmlFor="volumeSize">Size</InputLabel>
+      <InputNumber min={1} max={500} value={value} onChange={onChange} />
+    </ModalContent>
+  </SectionContainer>
+);
