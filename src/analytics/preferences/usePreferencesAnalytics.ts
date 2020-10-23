89ea8f333c2f26e70ff39de890dc0620b072a179
Arjun Patel <arjun.patel@mongodb.com>
2020-10-23 17:25:05 -0400

    CLI Download

--- src/analytics/preferences/usePreferencesAnalytics.ts
+++ src/analytics/preferences/usePreferencesAnalytics.ts
@@ -11,7 +11,8 @@ type Action =
   | { name: "Save Profile Info"; params: UpdateUserSettingsMutationVariables }
   | { name: "Log Me Out Everywhere" }
   | { name: "Save Notifications"; params: UpdateUserSettingsMutationVariables }
-  | { name: "Clear Subscriptions" };
+  | { name: "Clear Subscriptions" }
+  | { name: "CLI Download Link"; downloadName: string };
 
 interface P extends Properties {}
 interface Analytics extends A<Action> {}
