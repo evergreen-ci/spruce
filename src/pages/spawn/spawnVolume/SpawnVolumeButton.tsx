c9dbe5ea948e9465fe7c14f09826ef10d739765b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2020-10-27 10:36:14 -0400

    EVG-13320 Use volume limit for spawn volume page + misc improvements (#490)

--- src/pages/spawn/spawnVolume/SpawnVolumeButton.tsx
+++ src/pages/spawn/spawnVolume/SpawnVolumeButton.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
 import { uiColors } from "@leafygreen-ui/palette";
 import { Disclaimer } from "@leafygreen-ui/typography";
 import { PlusButton } from "components/Spawn";
+import { GetSpruceConfigQuery } from "gql/generated/types";
+import { GET_SPRUCE_CONFIG } from "gql/queries";
 import { SpawnVolumeModal } from "./spawnVolumeButton/SpawnVolumeModal";
 
 export const SpawnVolumeButton: React.FC = () => {
   const [openModal, setOpenModal] = useState(false);
+  const { data } = useQuery<GetSpruceConfigQuery>(GET_SPRUCE_CONFIG);
+  const volumeLimit = data?.spruceConfig?.providers?.aws?.maxVolumeSizePerUser;
   return (
     <PaddedContainer>
       <PlusButton data-cy="spawn-volume-btn" onClick={() => setOpenModal(true)}>
         Spawn a Volume
       </PlusButton>
-      <Info>Limit 1500 GiB per User</Info>
+      <Info>Limit {volumeLimit} GiB per User</Info>
       <SpawnVolumeModal
         visible={openModal}
         onCancel={() => setOpenModal(false)}
