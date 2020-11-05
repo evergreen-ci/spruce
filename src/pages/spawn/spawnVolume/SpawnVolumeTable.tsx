94145bf0aa204a0d921ef886cd11eaa815460067
Arjun Patel <arjun.patel@mongodb.com>
2020-11-05 15:31:17 -0500

    link to spawn/host from spawn/volume

--- src/pages/spawn/spawnVolume/SpawnVolumeTable.tsx
+++ src/pages/spawn/spawnVolume/SpawnVolumeTable.tsx
@@ -6,7 +6,7 @@ import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { DoesNotExpire, SpawnTable } from "components/Spawn";
 import { wordBreakCss } from "components/Typography";
-import { getHostRoute } from "constants/routes";
+import { routes } from "constants/routes";
 import { SpawnVolumeCard } from "pages/spawn/spawnVolume/spawnVolumeTable/SpawnVolumeCard";
 import { MyVolume } from "types/spawn";
 import { parseQueryString } from "utils";
@@ -60,7 +60,10 @@ const columns: Array<ColumnProps<MyVolume>> = [
     key: "mountedOn",
     sorter: sortByHost,
     render: (_, volume: MyVolume) => (
-      <Link data-cy="host-link" to={getHostRoute(volume.hostID)}>
+      <Link
+        data-cy="host-link"
+        to={`${routes.spawnHost}?host=${volume.hostID}`}
+      >
         <WordBreak> {getHostDisplayName(volume)}</WordBreak>
       </Link>
     ),
