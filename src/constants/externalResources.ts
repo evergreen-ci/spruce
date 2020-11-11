adedb935f14c749db49d7770c75ee21ed8a34909
Chaya Malik <chaya.malik@10gen.com>
2020-11-11 12:58:30 -0500

    EVG-13323 Display link to JIRA search query when task annotations are populated with BB plugin results (#521)

--- src/constants/externalResources.ts
+++ src/constants/externalResources.ts
@@ -6,3 +6,6 @@ export const legacyRoutes = {
   hosts: "/spawn",
   projects: "/projects",
 };
+
+export const getJiraSearchUrl = (jiraHost: string, jqlEscaped: string) =>
+  `https://${jiraHost}/secure/IssueNavigator.jspa?jql=${jqlEscaped}`;
