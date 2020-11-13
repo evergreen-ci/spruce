d6faed6969c3ff1be2b31a5df7148451893a403d
Mohamed <mohamed.khelif@mongodb.com>
2020-11-13 16:19:14 -0500

    Create conditional wrapper component

---
+++ src/components/ConditionalWrapper/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ConditionalWrapperProps {
+  condition: boolean;
+  wrapper: (children: any) => JSX.Element;
+  altWrapper?: (children: any) => JSX.Element;
+  children: JSX.Element;
+}
+export const ConditionalWrapper: React.FC<ConditionalWrapperProps> = ({
+  condition,
+  wrapper,
+  altWrapper,
+  children,
+}) => {
+  if (condition) {
+    return wrapper(children);
+  }
+  return altWrapper ? altWrapper(children) : children;
+};
