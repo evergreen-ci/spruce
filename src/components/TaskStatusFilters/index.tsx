5924a391625bbefe3814d602bfe11a0347e0f25f
Arjun Patel <arjun.patel@mongodb.com>
2020-11-18 16:50:25 -0500

    TaskStatusFilters

---
+++ src/components/TaskStatusFilters/index.tsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { TreeSelect, ALL_VALUE } from "components/TreeSelect";
+import { pollInterval } from "constants/index";
+import {
+  GetPatchTaskStatusesQuery,
+  GetPatchTaskStatusesQueryVariables,
+} from "gql/generated/types";
+import { GET_PATCH_TASK_STATUSES } from "gql/queries";
+import { useNetworkStatus } from "hooks";
+import { Status } from "pages/patch/patchTabs/tasks/TaskFilters";
+import { TaskStatus } from "types/task";
+import { getCurrentStatuses } from "utils/statuses/getCurrentStatuses";
+
+interface Props {
+  patchId: string;
+  selectedStatuses: string[];
+  selectedBaseStatuses: string[];
+  onChangeStatusFilter: (s: string[]) => void;
+  onChangeBaseStatusFilter: (s: string[]) => void;
+}
+
+export const TaskStatusFilters: React.FC<Props> = ({
+  onChangeBaseStatusFilter,
+  onChangeStatusFilter,
+  patchId,
+  selectedBaseStatuses,
+  selectedStatuses,
+}) => {
+  const { data, startPolling, stopPolling } = useQuery<
+    GetPatchTaskStatusesQuery,
+    GetPatchTaskStatusesQueryVariables
+  >(GET_PATCH_TASK_STATUSES, { variables: { id: patchId }, pollInterval });
+
+  useNetworkStatus(startPolling, stopPolling);
+
+  const statuses = data?.patch.taskStatuses ?? [];
+  const baseStatuses = data?.patch.baseTaskStatuses ?? [];
+
+  return (
+    <>
+      <TreeSelect
+        state={selectedStatuses}
+        tData={getCurrentStatuses(statuses, statusesTreeData)}
+        inputLabel="Task Status: "
+        dataCy="task-status-filter"
+        width="25%"
+        onChange={onChangeStatusFilter}
+      />
+      <TreeSelect
+        state={selectedBaseStatuses}
+        tData={getCurrentStatuses(baseStatuses, statusesTreeData)}
+        inputLabel="Task Base Status: "
+        dataCy="task-base-status-filter"
+        width="25%"
+        onChange={onChangeBaseStatusFilter}
+      />
+    </>
+  );
+};
+
+const statusesTreeData: Status[] = [
+  {
+    title: "All",
+    value: ALL_VALUE,
+    key: ALL_VALUE,
+  },
+  {
+    title: "Failures",
+    value: "all-failures",
+    key: "all-failures",
+    children: [
+      {
+        title: "Failed",
+        value: TaskStatus.Failed,
+        key: TaskStatus.Failed,
+      },
+      {
+        title: "Task Timed Out",
+        value: TaskStatus.TaskTimedOut,
+        key: TaskStatus.TaskTimedOut,
+      },
+      {
+        title: "Test Timed Out",
+        value: TaskStatus.TestTimedOut,
+        key: TaskStatus.TestTimedOut,
+      },
+    ],
+  },
+  {
+    title: "Success",
+    value: TaskStatus.Succeeded,
+    key: TaskStatus.Succeeded,
+  },
+  {
+    title: "Dispatched",
+    value: TaskStatus.Dispatched,
+    key: TaskStatus.Dispatched,
+  },
+  {
+    title: "Running",
+    value: TaskStatus.Started,
+    key: TaskStatus.Started,
+  },
+  {
+    title: "Scheduled",
+    value: "scheduled",
+    key: "scheduled",
+    children: [
+      {
+        title: "Unstarted",
+        value: TaskStatus.Unstarted,
+        key: TaskStatus.Unstarted,
+      },
+      {
+        title: "Undispatched or Blocked",
+        value: TaskStatus.Undispatched,
+        key: TaskStatus.Undispatched,
+      },
+    ],
+  },
+  {
+    title: "System Issues",
+    value: "system-issues",
+    key: "system-issues",
+    children: [
+      {
+        title: "System Failed",
+        value: TaskStatus.SystemFailed,
+        key: TaskStatus.SystemFailed,
+      },
+      {
+        title: "System Timed Out",
+        value: TaskStatus.SystemTimedOut,
+        key: TaskStatus.SystemTimedOut,
+      },
+      {
+        title: "System Unresponsive",
+        value: TaskStatus.SystemUnresponsive,
+        key: TaskStatus.SystemUnresponsive,
+      },
+    ],
+  },
+  {
+    title: "Setup Failed",
+    value: TaskStatus.SetupFailed,
+    key: TaskStatus.SetupFailed,
+  },
+  {
+    title: "Blocked",
+    value: TaskStatus.StatusBlocked,
+    key: TaskStatus.StatusBlocked,
+  },
+  {
+    title: "Won't Run",
+    value: TaskStatus.Inactive,
+    key: TaskStatus.Inactive,
+  },
+];
