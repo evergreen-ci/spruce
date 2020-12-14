a03a09fc59f0cb41c0712f8479ac611e2bbae66e
Mohamed Khelif <mohamed.khelif@mongodb.com>
2020-12-14 11:41:23 -0500

    EVG-13517 Refactor user Tags component to be more generic (#557)

---
+++ src/components/EditableTagField/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import { InstanceTag, ParameterInput } from "gql/generated/types";
+import { convertArrayToObject } from "utils/array";
+import { TagRow } from "./TagRow";
+
+type Tag = InstanceTag | ParameterInput;
+type EditableTagFieldProps = {
+  onChange: (data: Tag[]) => void;
+  inputTags: Tag[];
+  visible?: boolean;
+};
+
+export const EditableTagField: React.FC<EditableTagFieldProps> = ({
+  onChange,
+  inputTags,
+  visible,
+}) => {
+  const [visibleTags, setVisibleTags] = useState(inputTags);
+  // Convert this tag array to an object it makes searching through them faster if there are allot of tags
+  const visibleTagsAsObject = convertArrayToObject(visibleTags, "key");
+
+  const deleteHandler = (key: string) => {
+    const tags = { ...visibleTagsAsObject };
+    delete tags[key];
+    const tagsBackToArray = Object.values(tags);
+    setVisibleTags(tagsBackToArray);
+    onChange(tagsBackToArray);
+  };
+
+  // tag is the value to replace
+  const updateTagHandler = (tag: Tag, deleteKey: string) => {
+    const tags = { ...visibleTagsAsObject };
+    if (deleteKey) {
+      tags[deleteKey] = tag;
+    } else {
+      tags[tag.key] = tag;
+    }
+    const tagsBackToArray = Object.values(tags);
+    setVisibleTags(tagsBackToArray);
+    onChange(tagsBackToArray);
+  };
+
+  const validateKey = (key: string) => {
+    if (visibleTagsAsObject[key]) {
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    setVisibleTags(inputTags);
+  }, [visible]); // eslint-disable-line react-hooks/exhaustive-deps
+  return (
+    <FlexColumnContainer>
+      {visibleTags.map((tag) => (
+        <TagRow
+          tag={tag}
+          onDelete={deleteHandler}
+          key={tag.key}
+          onUpdateTag={updateTagHandler}
+          isValidKey={validateKey}
+        />
+      ))}
+      <TagRow
+        onUpdateTag={updateTagHandler}
+        isNewTag
+        isValidKey={validateKey}
+      />
+    </FlexColumnContainer>
+  );
+};
+
+const FlexColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 10px;
+`;
