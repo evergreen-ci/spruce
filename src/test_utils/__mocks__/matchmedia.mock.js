dbfb3f89c67086e5baadf740c09730d694ba7f80
Mohamed <mohamed.khelif@mongodb.com>
2020-12-14 22:57:38 -0500

    Update match media mock

--- src/test_utils/__mocks__/matchmedia.mock.js
+++ src/test_utils/__mocks__/matchmedia.mock.js
@@ -1,14 +1,9 @@
-// Provides a mock for the window.matchMedia this is used by some antd components
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+// Provides a mock for the global.matchMedia this is used by some antd components
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  };
