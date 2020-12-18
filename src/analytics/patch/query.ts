beee004160b33ad23392aa7ce490f0f78093f728
Mohamed <mohamed.khelif@mongodb.com>
2020-12-18 13:15:03 -0500

    enable eslint back

--- src/analytics/patch/query.ts
+++ src/analytics/patch/query.ts
@@ -1,6 +1,5 @@
 import { gql } from "@apollo/client";
 
-/* eslint-disable */
 export const GET_PATCH_EVENT_DATA = gql`
   query GetPatchEventData($id: String!) {
     patch(id: $id) {
--- src/analytics/task/query.ts
+++ src/analytics/task/query.ts
@@ -1,6 +1,5 @@
 import { gql } from "@apollo/client";
 
-/* eslint-disable */
 export const GET_TASK_EVENT_DATA = gql`
   query GetTaskEventData($taskId: String!) {
     task(taskId: $taskId) {
