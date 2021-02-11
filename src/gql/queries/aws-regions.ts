a7e0bcc4ed29b6280311ea7df2fde694e0c8d996
Mohamed <mohamed.khelif@mongodb.com>
2021-02-11 16:36:09 -0500

    Rename queries to graphql files

--- src/gql/queries/aws-regions.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_AWS_REGIONS = gql`
-  query AWSRegions {
-    awsRegions
-  }
-`;
--- src/gql/queries/get-instance-types.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_INSTANCE_TYPES = gql`
-  query InstanceTypes {
-    instanceTypes
-  }
-`;
