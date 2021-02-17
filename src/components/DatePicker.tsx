1baf29551ca4987a67c5be83c860d1a7f543c3a6
Mohamed <mohamed.khelif@mongodb.com>
2021-02-17 14:53:16 -0500

    Load date picker and time picker css manually

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,6 +1,8 @@
 import generatePicker from "antd/lib/date-picker/generatePicker";
 import dateFnsGenerateConfig from "rc-picker/lib/generate/dateFns";
 
+import "antd/es/date-picker/style/css";
+
 const DatePicker = generatePicker<Date>(dateFnsGenerateConfig);
 
 export default DatePicker;
--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -3,6 +3,8 @@ import { Omit } from "antd/es/_util/type";
 import { PickerTimeProps } from "antd/es/date-picker/generatePicker";
 import DatePicker from "./DatePicker";
 
+import "antd/es/time-picker/style/css";
+
 export interface TimePickerProps
   extends Omit<PickerTimeProps<Date>, "picker"> {}
 
