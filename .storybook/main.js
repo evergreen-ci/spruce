379e4ef4259aa392af3aee7d0b05bf416721d4c4
Mohamed <mohamed.khelif@mongodb.com>
2021-02-17 16:41:35 -0500

    Make storybook work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,35 @@
+const path = require("path");
+const fs = require("fs");
+
+const { merge } = require("webpack-merge");
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, "package.json"))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(
+        `Could not find package.json in the parent directories starting from ${filepath}.`
+      );
+    }
+    currDir = dir;
+  }
+}
 module.exports = {
-  stories: ["../src/**/*.stories.*@(js|ts|jsx|tsx)"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-  ],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links"],
+  presets: ["@storybook/preset-ant-design"],
+  webpackFinal: async (config) => {
+    return merge(config, {
+      resolve: {
+        alias: {
+          "@emotion/core": getPackageDir("@emotion/react"),
+          "@emotion/styled": getPackageDir("@emotion/styled"),
+          "emotion-theming": getPackageDir("@emotion/react"),
+        },
+      },
+    });
+  },
 };
