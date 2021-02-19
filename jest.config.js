c3e9038a21c4f47af76342f62d12e04fad3152e4
Mohamed <mohamed.khelif@mongodb.com>
2021-02-18 23:08:15 -0500

    Add storyshot testing

--- jest.config.js
+++ jest.config.js
@@ -27,6 +27,7 @@ module.exports = {
   roots: ["<rootDir>/src"],
   setupFiles: ["react-app-polyfill/jsdom"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  snapshotSerializers: ["@emotion/jest/serializer"],
   testEnvironment: "jsdom",
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
