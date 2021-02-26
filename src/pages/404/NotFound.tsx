ae396ec6cfdfdef4457be57adaab2650f077a756
Mohamed <mohamed.khelif@mongodb.com>
2021-02-26 12:42:31 -0500

    Add datacy

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,5 +1,7 @@
 import notFound from "./notFound.svg";
 
-const NotFound = () => <img src={notFound} alt="Page not found" />;
+const NotFound = () => (
+  <img src={notFound} alt="Page not found" data-cy="404" />
+);
 
 export default NotFound;
