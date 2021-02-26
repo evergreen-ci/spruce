f495d7925c1947163a27c0843b70f2b3b7a812a2
Mohamed <mohamed.khelif@mongodb.com>
2021-02-26 11:36:59 -0500

    Create 404 and error boundarys

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,4 @@
 import React from "react";
-import styled from "@emotion/styled";
-import { H2 } from "@leafygreen-ui/typography";
-import { PageWrapper } from "components/styles";
+import NotFound from "./404/NotFound";
 
-export const PageDoesNotExist: React.FC = () => (
-  <StyledPageWrapper>
-    <H2 data-cy="404">404 Page Does Not Exist</H2>
-  </StyledPageWrapper>
-);
-
-const StyledPageWrapper = styled(PageWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 35vh;
-`;
+export const PageDoesNotExist: React.FC = () => <NotFound />;
---
+++ src/pages/404/NotFound.tsx
@@ -0,0 +1,5 @@
+import notFound from "./notFound.svg";
+
+const NotFound = () => <img src={notFound} alt="Page not found" />;
+
+export default NotFound;
