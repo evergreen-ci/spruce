99ea9d7cdf0b2b574877c1012709a7e9b99753f5
Mohamed <mohamed.khelif@mongodb.com>
2021-03-01 14:48:33 -0500

    Add proper mocks

--- src/storybook.test.ts
+++ src/storybook.test.ts
@@ -1,5 +1,6 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { v4 as uuid } from "uuid";
+import "test_utils/__mocks__/matchmedia.mock";
 
 jest.mock("uuid");
 uuid.mockImplementation(() => "SOME_RANDOM_VALUE");
