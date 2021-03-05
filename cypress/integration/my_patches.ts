af3cb6cc6b6f60b2abadab101d128041c29dfb11
Mohamed <mohamed.khelif@mongodb.com>
2021-03-04 21:04:08 -0500

    Refactor tests

--- cypress/integration/my_patches.ts
+++ cypress/integration/my_patches.ts
@@ -1,8 +1,6 @@
 // / <reference types="Cypress" />
-import get from "lodash/get";
 import {
   urlSearchParamsAreUpdated,
-  assertQueryVariables,
   clickOnPageBtnAndAssertURLandTableResults,
   clickOnPageSizeBtnAndAssertURLandTableSize,
 } from "../utils";
@@ -19,7 +17,6 @@ describe("My Patches Page", () => {
 
   beforeEach(() => {
     cy.preserveCookies();
-    cy.listenGQL();
   });
 
   it("Redirects user to user patches route from `/user/:id`", () => {
@@ -34,17 +31,14 @@ describe("My Patches Page", () => {
     cy.contains("My Patches").should("exist");
   });
 
-  it("The page title should be 'Bob Hicks' Patches' when viewing Bob Hicks' patches page", () => {
+  it("The page title should reflect another users patches when viewing another users patches page", () => {
     cy.visit(BOB_HICKS_PATCHES_ROUTE);
     cy.contains("Bob Hicks' Patches").should("exist");
-  });
-
-  it("The page title should be 'Regular User's Patches' when viewing Regular Users's patches page", () => {
     cy.visit(REGULAR_USER_PATCHES_ROUTE);
     cy.contains("Regular User's Patches").should("exist");
   });
 
-  it.skip("Typing in patch description input updates the url, requests patches and renders patches", () => {
+  it("Typing in patch description input updates the url, requests patches and renders patches", () => {
     cy.visit(MY_PATCHES_ROUTE);
     const inputVal = "testtest";
     cy.dataCy("patch-description-input").type(inputVal);
@@ -53,21 +47,15 @@ describe("My Patches Page", () => {
       paramName: "patchName",
       search: inputVal,
     });
-    resultsAreFetchedAndRendered({
-      queryName: "UserPatches",
-      responseName: "userPatches.patches",
-      requestVariables: { patchName: inputVal },
-      tableRow,
-    });
+    cy.dataCy("patch-description-input").clear();
   });
 
   it("Searching for a nonexistent patch shows 'No patches found'", () => {
-    cy.visit(MY_PATCHES_ROUTE);
     cy.dataCy("patch-description-input").type("satenarstharienht");
     cy.dataCy("no-patches-found").contains("No patches found");
   });
 
-  it("Build status icon should link to version page with appropiate filters", () => {
+  it("Build status icon should link to version page with appropriate filters", () => {
     cy.visit(MY_PATCHES_ROUTE);
     cy.dataCy("build-status-icon-link")
       .eq(1)
@@ -78,38 +66,27 @@ describe("My Patches Page", () => {
       );
   });
 
-  it.skip("Clicking the commit queue checkbox updates the URL, requests patches and renders patches", () => {
+  it("Clicking the commit queue checkbox updates the URL, requests patches and renders patches", () => {
     cy.visit(MY_PATCHES_ROUTE);
+    cy.dataCy("commit-queue-checkbox").should("be.checked");
+    cy.contains(patchOnCommitQueue);
+
     cy.dataCy("commit-queue-checkbox").click({ force: true });
     urlSearchParamsAreUpdated({
       pathname: MY_PATCHES_ROUTE,
       paramName: "commitQueue",
       search: "false",
     });
-    resultsAreFetchedAndRendered({
-      queryName: "UserPatches",
-      responseName: "userPatches.patches",
-      requestVariables: { includeCommitQueue: (v) => v === false },
-      tableRow,
-    });
+    cy.contains(patchOnCommitQueue).should("not.exist");
     cy.dataCy("commit-queue-checkbox").click({ force: true });
-    urlSearchParamsAreUpdated({
-      pathname: MY_PATCHES_ROUTE,
-      paramName: "commitQueue",
-      search: "true",
-    });
-    resultsAreFetchedAndRendered({
-      queryName: "UserPatches",
-      responseName: "userPatches.patches",
-      requestVariables: { includeCommitQueue: (v) => v === true },
-      tableRow,
-    });
   });
 
   describe("Changing page number and page size", () => {
-    it("Displays the next page of results and updates URL when right arrow is clicked and next page exists", () => {
+    before(() => {
       cy.login();
       cy.visit(MY_PATCHES_ROUTE);
+    });
+    it("Displays the next page of results and updates URL when right arrow is clicked and next page exists", () => {
       clickOnPageBtnAndAssertURLandTableResults(
         dataCyNextPage,
         secondPageDisplayNames,
@@ -161,7 +138,6 @@ describe("My Patches Page", () => {
   describe("Clicking on status checkbox requests and renders patches for that status", () => {
     beforeEach(() => {
       cy.preserveCookies();
-      cy.listenGQL();
       cy.get("[data-cy=my-patch-status-select] > .cy-treeselect-bar").click();
     });
     before(() => {
@@ -223,6 +199,8 @@ const dataCyNextPage = "[data-cy=my-patches-pagination] > .ant-pagination-next";
 const dataCyPrevPage = "[data-cy=my-patches-pagination] > .ant-pagination-prev";
 const dataCyTableRows = tableRow;
 
+const patchOnCommitQueue =
+  "'evergreen-ci/evergreen' pull request #3186 by bsamek: EVG-7425 Don't send ShouldExit to unprovisioned hosts (https://github.com/evergreen-ci/evergreen/pull/3186)";
 const firstPageDisplayNames = [
   "main: EVG-7823 add a commit queue message (#4048)",
   "dist",
@@ -254,10 +232,6 @@ const thirdPageDisplayNames = [
   "windows-64,windows-32,solaris-64-bit",
   "no description",
 ];
-// TODO: These functions were adopted from cypress/utils/index and fixes two issues
-// which caused the incorrect query to be tracked. The functions in the util folder
-// should be updated as well as any tests that rely on them
-
 const clickingCheckboxUpdatesUrlAndRendersFetchedResults = ({
   selector = ".cy-checkbox",
   checkboxDisplayName,
@@ -274,24 +248,3 @@ const clickingCheckboxUpdatesUrlAndRendersFetchedResults = ({
 
   urlSearchParamsAreUpdated({ pathname, paramName, search: null });
 };
-
-const resultsAreFetchedAndRendered = ({
-  queryName,
-  responseName,
-  requestVariables,
-  tableRow: tableRowVal,
-} = {}) =>
-  assertQueryVariables(queryName, requestVariables).then((xhr) => {
-    const { response } = xhr;
-    const numberOfResults = get(response, `body.data.${responseName}`, [])
-      .length;
-    if (numberOfResults === 0) {
-      cy.get(tableRowVal).should("not.exist");
-    } else {
-      cy.get(tableRowVal)
-        .invoke("toArray")
-        .then((filteredResults) => {
-          expect(filteredResults.length >= numberOfResults).eq(true);
-        });
-    }
-  });
