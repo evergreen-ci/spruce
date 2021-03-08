0d60bcbd6415283521a6a10c13841c610bd08332
Mohamed <mohamed.khelif@mongodb.com>
2021-03-08 17:55:36 -0500

    Update task result type

--- src/components/Table/TasksTable.tsx
+++ src/components/Table/TasksTable.tsx
@@ -6,7 +6,7 @@ import { TaskStatusBadge } from "components/TaskStatusBadge";
 import { WordBreak } from "components/Typography";
 import { getTaskRoute } from "constants/routes";
 import {
-  TaskResult,
+  Task,
   SortDirection,
   TaskSortCategory,
   SortOrder,
@@ -15,7 +15,7 @@ import { TableOnChange } from "types/task";
 
 interface TasksTableProps {
   tasks: any;
-  tableChangeHandler?: TableOnChange<TaskResult>;
+  tableChangeHandler?: TableOnChange<Task>;
   onExpand?: (expanded: boolean) => void;
   onClickTaskLink?: (taskId: string) => void;
   sorts?: SortOrder[];
@@ -47,7 +47,7 @@ export const TasksTable: React.FC<TasksTableProps> = ({
   />
 );
 
-const getColumnDefs = (onClickTaskLink): ColumnProps<TaskResult>[] => [
+const getColumnDefs = (onClickTaskLink): ColumnProps<Task>[] => [
   {
     title: "Name",
     dataIndex: "displayName",
@@ -55,7 +55,7 @@ const getColumnDefs = (onClickTaskLink): ColumnProps<TaskResult>[] => [
     sorter: (a, b) => a.displayName.localeCompare(b.displayName),
     width: "40%",
     className: "cy-task-table-col-NAME",
-    render: (name: string, { id }: TaskResult): JSX.Element => (
+    render: (name: string, { id }: Task): JSX.Element => (
       <TaskLink onClick={onClickTaskLink} taskName={name} taskId={id} />
     ),
   },
@@ -87,7 +87,7 @@ const getColumnDefs = (onClickTaskLink): ColumnProps<TaskResult>[] => [
 const getColumnDefsControlled = (
   sortOrder: SortOrder[],
   onClickTaskLink: (taskId: string) => void
-): ColumnProps<TaskResult>[] => {
+): ColumnProps<Task>[] => {
   const getSortDir = (
     key: string,
     sorts: SortOrder[]
@@ -135,7 +135,7 @@ const getColumnDefsControlled = (
 
 const renderStatusBadge = (
   status: string,
-  { blocked }: TaskResult
+  { blocked }: Task
 ): null | JSX.Element => {
   if (status === "" || !status) {
     return null;
