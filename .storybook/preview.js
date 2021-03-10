2f9b4af8ca4e8c75f61716b8e908d7d52061ae3c
Mohamed <mohamed.khelif@mongodb.com>
2021-03-10 13:27:48 -0500

    Add mock apollo to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { MockedProvider } from "@apollo/client/testing";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  apolloClient: {
+    MockedProvider,
+    // any props you want to pass to MockedProvider on every story
+  },
 };
