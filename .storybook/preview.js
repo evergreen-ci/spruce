54dd9d0d4d3922829054dcb204f9d5e65c43b4a2
Mohamed <mohamed.khelif@mongodb.com>
2021-03-13 09:55:13 -0500

    Add apollo support for storyshots

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import { MockedProvider } from "@apollo/client/testing";
+import { WithApolloClient } from "storybook-addon-apollo-client/dist/decorators";
 
+// Enable storyshots support
+export const decorators = [WithApolloClient];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   apolloClient: {
