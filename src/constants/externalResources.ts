1751da27a49d15b7b47bbd022540fe25051cc2ab
SupaJoon <arjun453@gmail.com>
2021-03-22 11:27:55 -0400

    fix type error

--- src/constants/externalResources.ts
+++ src/constants/externalResources.ts
@@ -31,4 +31,4 @@ export const getLobsterTestLogUrl = (
     lineNum ? `#bookmarks=${lineNum}` : ""
   }`;
 
-export const isLobsterLink = (url: string) => url.split("/")[3] === "lobster";
+export const isLobsterLink = (url: string) => url?.split("/")[3] === "lobster";
