df52a49f225c864a7d509f4bedc11e647bf10240
Mohamed <mohamed.khelif@mongodb.com>
2021-04-06 19:43:07 -0400

    add tests

--- src/utils/url/index.ts
+++ src/utils/url/index.ts
@@ -19,4 +19,20 @@ export const getLimitFromSearch = (search: string): number => {
     : getDefaultPageSize();
 };
 
+// Takes a query param and a value and returns an array
+// if the query param already contains the value it does not modify it otherwise
+// it will add the value to the query param
+export const upsertQueryParam = (params: string[] | string, value: string) => {
+  if (
+    params === undefined ||
+    (typeof params === "string" && params === value)
+  ) {
+    return [value];
+  }
+  if (Array.isArray(params) && params.find((param) => param === value)) {
+    return [...params];
+  }
+  return Array.isArray(params) ? [...params, value] : [params, value];
+};
+
 export { updateUrlQueryParam } from "./updateUrlQueryParam";
