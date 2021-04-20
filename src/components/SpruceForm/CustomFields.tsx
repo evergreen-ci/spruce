692b75baab43bcbb23c3bc5a3735750561f65062
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-04-20 16:59:45 -0400

    Add label attribute

--- src/components/SpruceForm/CustomFields.tsx
+++ src/components/SpruceForm/CustomFields.tsx
@@ -1,9 +1,17 @@
 import styled from "@emotion/styled";
+import { Label } from "@leafygreen-ui/typography";
+import { FieldProps } from "@rjsf/core";
 import { EditableTagField } from "components/EditableTagField";
 
-export const ArrayField = ({ onChange, formData }) => (
+export const ArrayField: React.FC<FieldProps> = ({
+  onChange,
+  formData,
+  schema: { title },
+}) => (
   <ElementWrapper>
+    <Label htmlFor={`editable_tag_field_${title}`}>{title}</Label>
     <EditableTagField
+      id={`editable_tag_field_${title}`}
       inputTags={formData}
       onChange={onChange}
       buttonText="Add New Expansion"
