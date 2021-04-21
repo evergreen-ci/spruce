05253d1964d0f3ab69ce5e21e47388e92d551181
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-04-21 11:35:48 -0400

    Always show scroll bar

--- src/pages/configurePatch/configurePatchCore/ConfigureBuildVariants.tsx
+++ src/pages/configurePatch/configurePatchCore/ConfigureBuildVariants.tsx
@@ -193,5 +193,17 @@ const StyledBadge = styled(Badge)`
 
 const ScrollableBuildVariantContainer = styled.div`
   overflow: scroll;
-  max-height: 400px;
+  max-height: 60vh;
+
+  // Styles to always show scrollbar
+  ::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 7px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+  }
 `;
