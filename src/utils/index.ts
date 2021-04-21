7b5c12aa4d52c116950d67c8a8613cb220d723d2
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-04-21 19:21:33 -0400

    export color from utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
-import * as statuses from "utils/statuses";
 import * as array from "./array";
+import * as color from "./color";
 import * as queryString from "./queryString";
+import * as statuses from "./statuses";
 import * as url from "./url";
 
-export { queryString, statuses, array, url };
+export { queryString, statuses, array, url, color };
