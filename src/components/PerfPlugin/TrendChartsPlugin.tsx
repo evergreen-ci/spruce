84d92cb6ccd25c3ee327bd207fe88c9b0e342fbd
Alexander Costas <alexander.costas@mongodb.com>
2021-04-30 08:36:50 -0400

    nit

--- src/components/PerfPlugin/TrendChartsPlugin.tsx
+++ src/components/PerfPlugin/TrendChartsPlugin.tsx
@@ -4,11 +4,6 @@ import { environmentalVariables } from "utils";
 
 const { getSignalProcessingUrl } = environmentalVariables;
 
-const StyledIframe = styled.iframe`
-width: 100%;
-height: 1000px;
-`;
-
 interface Props {
   taskId: string;
 }
@@ -16,5 +11,10 @@ interface Props {
 const TrendChartsPlugin: React.FC<Props> = ({ taskId }) => (
     <StyledIframe src={`${getSignalProcessingUrl()}/task/${taskId}/performanceData`} title="Task Performance Data"/>
 );
+
+const StyledIframe = styled.iframe`
+width: 100%;
+height: 1000px;
+`;
 
 export default TrendChartsPlugin;
