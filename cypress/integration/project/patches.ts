b6bb2612d34ef404e56bddb8c4411d5c4baaf5a0
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-05-21 16:53:00 -0400

    Remove unnecessary test

--- cypress/integration/project/patches.ts
+++ cypress/integration/project/patches.ts
@@ -5,9 +5,6 @@ const route = "/project/evergreen/patches";
 describe("Project Patches Page", () => {
   before(() => {
     cy.login();
-  });
-
-  beforeEach(() => {
     cy.preserveCookies();
   });
 
@@ -15,21 +12,5 @@ describe("Project Patches Page", () => {
     cy.visit("/user/admin/patches");
     cy.dataCy("project-patches-link").first().click();
     cy.location("pathname").should("eq", route);
-  });
-
-  it("Page title and data rows are displayed.", () => {
-    cy.visit(route);
-    cy.dataCy("patches-page-title").contains("evergreen smoke test Patches");
-    const patchDisplayNames = [
-      "Commit Queue Merge",
-      "commit queue",
-      "dist",
-      "dist",
-      "test meee",
-      "'evergreen-ci/evergreen' pull request #3186 by bsamek: EVG-7425 Don't send ShouldExit to unprovisioned hosts (https://github.com/evergreen-ci/evergreen/pull/3186)",
-    ];
-    cy.dataCy("patch-card").each(($el, index) => {
-      cy.wrap($el).contains(patchDisplayNames[index]);
-    });
   });
 });
