3df5feeb3c3f3aeb5d16770d688cfb826dab97bc
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-05-21 16:31:31 -0400

    Fix hot tests

--- cypress/integration/hosts/update_status_modal.ts
+++ cypress/integration/hosts/update_status_modal.ts
@@ -3,17 +3,16 @@ const hostsRoute = "/hosts";
 describe("Update Status Modal", () => {
   before(() => {
     cy.login();
+    cy.preserveCookies();
   });
 
   beforeEach(() => {
-    cy.preserveCookies();
     cy.visit(`${hostsRoute}?limit=100&page=0`);
   });
 
   it("Update status for selected hosts", () => {
-    cy.get(".ant-table-selection-column").within(() => {
-      cy.get(".ant-checkbox-input").check({ force: true });
-    });
+    cy.get(".ant-checkbox-input").should("not.be.disabled");
+    cy.get(".ant-checkbox-input").check({ force: true });
 
     cy.dataCy("update-status-button").click();
 
@@ -28,30 +27,6 @@ describe("Update Status Modal", () => {
     cy.dataCy("toast").should("exist");
 
     // MODAL FORM VALUES SHOULD BE CLEARED AFTER MUTATION
-    cy.dataCy("update-status-button").click();
-
-    cy.dataCy("host-status-select").within(() => {
-      cy.get(".ant-select-selection-item").should("not.exist");
-    });
-
-    cy.dataCy("host-status-notes").invoke("val").should("eq", "");
-  });
-
-  it("Clears form values when modal is closed", () => {
-    cy.get(".ant-table-selection-column").within(() => {
-      cy.get(".ant-checkbox-input").check({ force: true });
-    });
-
-    cy.dataCy("update-status-button").click();
-
-    cy.dataCy("host-status-select").click();
-
-    cy.dataCy("decommissioned-option").click();
-
-    cy.dataCy("host-status-notes").type("notes");
-
-    cy.dataCy("modal-cancel-button").click();
-
     cy.dataCy("update-status-button").click();
 
     cy.dataCy("host-status-select").within(() => {
