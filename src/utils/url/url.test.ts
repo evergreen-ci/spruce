b5d38420a05ef46b040c1ac1362f09932115b4bc
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-06-10 13:56:25 -0400

    Update some test names

--- src/utils/url/url.test.ts
+++ src/utils/url/url.test.ts
@@ -6,14 +6,14 @@ describe("getLimitFromSearch", () => {
     localStorage.clear();
   });
 
-  test("should return the value of the 'limit' query variable from the given search string if it is a valid page size.", () => {
+  it("should return the value of the 'limit' query variable from the given search string if it is a valid page size.", () => {
     expect(getLimitFromSearch("&limit=10")).toEqual(10);
     expect(getLimitFromSearch("&limit=20")).toEqual(20);
     expect(getLimitFromSearch("&limit=50")).toEqual(50);
     expect(getLimitFromSearch("&limit=100")).toEqual(100);
   });
 
-  test("should return the recent page size value in local storage when the value in local storage is a valid page size and the given search string does not contain a valid limit,", () => {
+  it("should return the recent page size value in local storage when the value in local storage is a valid page size and the given search string does not contain a valid limit,", () => {
     localStorage.setItem(RECENT_PAGE_SIZE_KEY, "50");
     expect(getLimitFromSearch("&limit=11")).toEqual(50);
     expect(getLimitFromSearch("&limit")).toEqual(50);
