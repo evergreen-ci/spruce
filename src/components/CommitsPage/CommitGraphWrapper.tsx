9fae928fa8760602a896fc3ae6c1d4d12151e413
ziyielenachen <elena.chen@berkeley.edu>
2021-06-22 17:51:36 -0400

    done

--- src/components/CommitsPage/CommitGraphWrapper.tsx
+++ src/components/CommitsPage/CommitGraphWrapper.tsx
@@ -1,46 +1,39 @@
 import React from "react";
 import { ApolloError } from "@apollo/client";
 import styled from "@emotion/styled";
+import { Array } from "@ungap/global-this";
 import { Skeleton } from "antd";
 import { PageWrapper } from "components/styles";
 import { MainlineCommitsQuery } from "gql/generated/types";
 import { CommitGraph } from "./CommitGraph";
-import { CommitGraphHTML } from "./CommitGraphHTML";
 
 export const CommitGraphWrapper: React.FC<{
   versions: MainlineCommitsQuery["mainlineCommits"]["versions"];
   error?: ApolloError;
-}> = ({ versions, error }) => {
+  graphType: "percentage" | "absolute";
+}> = ({ versions, error, graphType }) => {
+  let max = -1;
   if (error) {
     return <PageWrapper>ERROR</PageWrapper>;
   }
   if (!versions) {
     return <StyledSkeleton active title={false} paragraph={{ rows: 4 }} />;
   }
+  if (graphType === "absolute") {
+    max = findMax(data);
+  }
   if (versions?.length !== 0) {
     return (
       <>
         <FlexRowContainer>
-          <CommitGraph taskCounts={taskCounts} />
-          <CommitGraph taskCounts={taskCounts2} />
-          <CommitGraph taskCounts={taskCounts3} />
-          <CommitGraph taskCounts={taskCounts2} />
-          <CommitGraph taskCounts={taskCounts} />
-          <CommitGraph taskCounts={taskCounts4} />
-          <CommitGraph taskCounts={taskCounts3} />
-        </FlexRowContainer>
-        <FlexRowContainer>
-          <CommitGraphHTML taskCounts={taskCountsHTML} />
-          <CommitGraphHTML taskCounts={taskCountsHTML2} />
-          <CommitGraphHTML taskCounts={taskCountsHTML3} />
-          <CommitGraphHTML taskCounts={taskCountsHTML2} />
-          <CommitGraphHTML taskCounts={taskCountsHTML} />
-          <CommitGraphHTML taskCounts={taskCountsHTML4} />
-          <CommitGraphHTML taskCounts={taskCountsHTML3} />
+          {data.map((value) => (
+            <CommitGraph taskCounts={value} max={max} graphType={graphType} />
+          ))}
         </FlexRowContainer>
       </>
     );
   }
+  return <NoResults data-cy="no-patches-found">No commits found</NoResults>;
 };
 
 const StyledSkeleton = styled(Skeleton)`
@@ -52,61 +45,81 @@ const FlexRowContainer = styled.div`
   contents-direction: row;
   justify-content: flex-start;
   align-items: flex-end;
-  height: 240px;
+  height: 222px;
+  width: 100%;
 `;
 
-const taskCounts = {
-  success: 6,
-  failure: 2,
-  dispatched: 4,
-  scheduled: 2,
-  unscheduled: 5,
-};
-const taskCounts2 = {
-  success: 4,
-  failure: 3,
-  dispatched: 5,
-  setupFailure: 2,
-};
-const taskCounts3 = {
-  success: 6,
-};
-const taskCounts4 = {
-  success: 3,
-  failure: 4,
-  dispatched: 6,
-  scheduled: 5,
-  unscheduled: 1,
-  systemFailure: 4,
-  setupFailure: 2,
-};
+const NoResults = styled.div`
+  margin-top: 12px;
+`;
 
-const taskCountsHTML = {
-  success: 6,
-  failure: 2,
-  dispatched: 4,
-  scheduled: 2,
-  unscheduled: 5,
-  total: 19,
-};
-const taskCountsHTML2 = {
-  success: 4,
-  failure: 3,
-  dispatched: 5,
-  setupFailure: 2,
-  total: 14,
-};
-const taskCountsHTML3 = {
-  success: 6,
-  total: 6,
-};
-const taskCountsHTML4 = {
-  success: 3,
-  failure: 4,
-  dispatched: 6,
-  scheduled: 5,
-  unscheduled: 1,
-  systemFailure: 4,
-  setupFailure: 2,
-  total: 25,
-};
+function findMax(data: Array<Object>) {
+  let max = -1;
+  data.forEach((item) => {
+    max = Math.max(
+      max,
+      Math.max.apply(
+        null,
+        Object.keys(item).map((x) => item[x])
+      )
+    );
+  });
+  return max;
+}
+
+// Hard-coded data for now:
+const data = [
+  {
+    success: 6,
+    failure: 2,
+    dispatched: 4,
+    scheduled: 2,
+    unscheduled: 5,
+    total: 19,
+    max: 6,
+  },
+  {
+    success: 4,
+    failure: 3,
+    dispatched: 5,
+    setupFailure: 2,
+    total: 14,
+    max: 5,
+  },
+  {
+    success: 30,
+    total: 30,
+    max: 30,
+  },
+  {
+    success: 3,
+    failure: 4,
+    dispatched: 6,
+    scheduled: 5,
+    unscheduled: 1,
+    systemFailure: 4,
+    setupFailure: 2,
+    total: 25,
+    max: 6,
+  },
+  {
+    success: 6,
+    failure: 2,
+    unscheduled: 5,
+    total: 13,
+    max: 6,
+  },
+  {
+    success: 4,
+    failure: 3,
+    dispatched: 5,
+    total: 12,
+    max: 5,
+  },
+  {
+    success: 10,
+    failure: 20,
+    total: 30,
+    max: 20,
+  },
+];
