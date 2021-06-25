fed9644307ffa7e5a14caa314218e344da190680
ziyielenachen <elena.chen@berkeley.edu>
2021-06-25 13:31:27 -0400

    resolved some comments

--- src/pages/Commits.tsx
+++ src/pages/Commits.tsx
@@ -13,7 +13,6 @@ import {
 import { GET_MAINLINE_COMMITS } from "gql/queries";
 import { usePageTitle, useNetworkStatus } from "hooks";
 import { PageDoesNotExist } from "pages/404";
-import { taskData } from "pages/commits/commitGraph/CommitGraph.stories";
 import { CommitGraphWrapper } from "pages/commits/commitGraph/CommitGraphWrapper";
 import { ProjectFilterOptions } from "types/commits";
 import { ProjectSelect } from "./commits/ProjectSelect";
@@ -57,7 +56,7 @@ export const Commits = () => {
         <FilterBadges />
       </BadgeWrapper>
       <CommitGraphWrapper
-        taskCounts={taskData}
+        taskCounts={[]}
         error={error}
         graphType="percentage"
         isLoading={loading}
