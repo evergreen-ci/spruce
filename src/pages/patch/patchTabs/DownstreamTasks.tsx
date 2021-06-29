e29705c6591b8aa8840bb4410ac5d0cd50022db7
Chaya Malik <chaya.malik@10gen.com>
2021-06-29 10:02:36 -0400

    fix styling

--- src/pages/patch/patchTabs/DownstreamTasks.tsx
+++ src/pages/patch/patchTabs/DownstreamTasks.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Table } from "antd";
 import { Patch } from "gql/generated/types";
 import { DownstreamProjectAccordion } from "./DownstreamProjectAccordion";
 
 type childPatchesType = Patch["childPatches"];
-type childPatch = childPatchesType[0];
 
 interface DownstreamTasksProps {
   childPatches: childPatchesType;
@@ -12,16 +10,16 @@ interface DownstreamTasksProps {
 
 export const DownstreamTasks: React.FC<DownstreamTasksProps> = ({
   childPatches,
-}) => {
-  return (
-    <>
-    {childPatches.map(({project,status,patchID, taskCount}) =>   <DownstreamProjectAccordion
-          key={`downstream_project_${patchID}`}
-          projectName={project}
-          status={status}
-          childPatchId={patchID}
-          taskCount={taskCount}
-        />)
-    </>
-  );
-};
+}) => (
+  <>
+    {childPatches.map(({ project, status, patchID, taskCount }) => (
+      <DownstreamProjectAccordion
+        key={`downstream_project_${patchID}`}
+        projectName={project}
+        status={status}
+        childPatchId={patchID}
+        taskCount={taskCount}
+      />
+    ))}
+  </>
+);
