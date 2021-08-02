170d0b5c8dca0e3cdafdff0e305ecb476abe62dc
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-08-02 17:33:17 -0400

    Update codechanges tests

--- cypress/integration/version/unscheduled_patch/code_changes_table.ts
+++ cypress/integration/version/unscheduled_patch/code_changes_table.ts
@@ -1,46 +1,29 @@
 // / <reference types="Cypress" />
 // / <reference path="../support/index.d.ts" />
 
-const patchWithChanges = "5e4ff3abe3c3317e352062e4";
-const CODE_CHANGES_ROUTE = `version/${patchWithChanges}/changes`;
-const NO_CODE_CHANGES_ROUTE =
-  "patch/5e6bb9e23066155a993e0f1a/configure/changes";
+const patchId = "5ecedafb562343215a7ff297";
+
 describe("Code Changes Table", () => {
   before(() => {
     cy.login();
+    cy.visit(`/version/${patchId}/changes`);
   });
 
   beforeEach(() => {
     cy.preserveCookies();
   });
-
-  it("Shows all columns even when the file name is super long.", () => {
-    cy.visit("/version/52a630633ff1227909000021/changes");
-    cy.contains(
-      "superloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg name"
-    ).should("be.visible");
-    cy.dataCy("additions-column").should("be.visible");
-    cy.dataCy("deletions-column").should("be.visible");
-  });
-
   it("Should display at least one table when there are code changes", () => {
-    cy.visit(CODE_CHANGES_ROUTE);
     cy.dataCy("code-changes-table").should("exist");
   });
   it("Should link to code changes when they exist", () => {
     cy.dataCy("fileLink")
       .should("have.attr", "href")
-      .and("include", `filediff/${patchWithChanges}`);
+      .and("include", `filediff/${patchId}`);
     cy.dataCy("html-diff-btn")
       .should("have.attr", "href")
-      .and("include", `filediff/${patchWithChanges}`);
+      .and("include", `filediff/${patchId}`);
     cy.dataCy("raw-diff-btn")
       .should("have.attr", "href")
-      .and("include", `rawdiff/${patchWithChanges}`);
-  });
-
-  it("Should display 'No code changes' when there are no code changes", () => {
-    cy.visit(NO_CODE_CHANGES_ROUTE);
-    cy.contains("No code changes");
+      .and("include", `rawdiff/${patchId}`);
   });
 });
