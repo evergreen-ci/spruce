73260bebc61a8871c3f04625ee0e2d1f3bae12da
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-09-24 16:17:38 -0400

    Use datacy instead of testid

--- cypress/integration/version/subscription_modal.ts
+++ cypress/integration/version/subscription_modal.ts
@@ -15,12 +15,9 @@ describe("Version Subscription Modal", () => {
   });
 
   describe("Regex selector inputs", () => {
-    const openModal = () =>
-      openSubscriptionModal(route, dataCyToggleModalButton);
-
     it("Clicking on 'Add additional criteria' adds a regex selector row", () => {
-      openModal();
-      cy.dataTestId("trigger_5-option").click();
+      openSubscriptionModal(route, dataCyToggleModalButton);
+      cy.dataCy("trigger_5-option").click();
       cy.dataCy("1-regex-selector-container").should("not.exist");
       cy.contains("Add additional criteria").click();
       cy.dataCy("1-regex-selector-container").should("exist");
@@ -34,15 +31,15 @@ describe("Version Subscription Modal", () => {
 
     it("'Regex' input should be disabled when the 'Field name' is empty and enabled otherwise", () => {
       cy.dataCy("0-regex-selector-input").should("be.disabled");
-      cy.dataTestId("0-regex-selector-dropdown").click();
-      cy.dataTestId("0-display-name-option").click();
+      cy.dataCy("0-regex-selector-dropdown").click();
+      cy.dataCy("0-display-name-option").click();
       cy.dataCy("0-regex-selector-input").should("not.be.disabled");
     });
 
     it("Selecting a regex selector type will disable that option in other regex selector type dropdowns", () => {
       cy.contains("Add additional criteria").click();
-      cy.dataTestId("1-regex-selector-dropdown").click();
-      cy.dataTestId("1-display-name-option").should(
+      cy.dataCy("1-regex-selector-dropdown").click();
+      cy.dataCy("1-display-name-option").should(
         "have.css",
         "user-select",
         "auto"
@@ -50,11 +47,11 @@ describe("Version Subscription Modal", () => {
     });
 
     it("Regex selectors are optional for triggers that offer them", () => {
-      openModal();
-      cy.dataTestId("trigger_5-option").click();
-      cy.dataTestId("notify-by-select").click();
-      cy.dataTestId("jira-comment-option").click();
-      cy.dataTestId("jira-comment-input").type("EVG-2000");
+      openSubscriptionModal(route, dataCyToggleModalButton);
+      cy.dataCy("trigger_5-option").click();
+      cy.dataCy("notify-by-select").click();
+      cy.dataCy("jira-comment-option").click();
+      cy.dataCy("jira-comment-input").type("EVG-2000");
       cy.dataCy("save-subscription-button").should("not.be.disabled");
       cy.dataCy("toast").should("not.exist");
       cy.dataCy("save-subscription-button").click();
@@ -62,58 +59,58 @@ describe("Version Subscription Modal", () => {
     });
 
     it("Switching between Event types should either hide or reset regex selector inputs", () => {
-      openModal();
-      cy.dataTestId("trigger_5-option").click();
-      cy.dataTestId("0-regex-selector-dropdown").click();
-      cy.dataTestId("0-display-name-option").click();
+      openSubscriptionModal(route, dataCyToggleModalButton);
+      cy.dataCy("trigger_5-option").click();
+      cy.dataCy("0-regex-selector-dropdown").click();
+      cy.dataCy("0-display-name-option").click();
       cy.dataCy("0-regex-selector-input")
         .type("stuff")
         .should("have.value", "stuff");
       cy.contains("Add additional criteria").click();
-      cy.dataTestId("1-regex-selector-dropdown").click();
-      cy.dataTestId("1-build-variant-option").click();
+      cy.dataCy("1-regex-selector-dropdown").click();
+      cy.dataCy("1-build-variant-option").click();
       cy.dataCy("1-regex-selector-input")
         .type("more stuff")
         .should("have.value", "more stuff");
-      cy.dataTestId("when-select").click();
-      cy.dataTestId("trigger_6-option").click();
+      cy.dataCy("when-select").click();
+      cy.dataCy("trigger_6-option").click();
       cy.dataCy("0-regex-selector-input").should("have.value", "");
       cy.dataCy("1-regex-selector-input").should("not.exist");
-      cy.dataTestId("when-select").click();
-      cy.dataTestId("trigger_2-option").click();
+      cy.dataCy("when-select").click();
+      cy.dataCy("trigger_2-option").click();
       cy.dataCy("0-regex-selector-container").should("not.exist");
     });
 
     it("Changing the regex selector dropdown should reset the regex selector input", () => {
-      openModal();
-      cy.dataTestId("trigger_5-option").click();
-      cy.dataTestId("0-regex-selector-dropdown").click();
-      cy.dataTestId("0-display-name-option").click();
+      openSubscriptionModal(route, dataCyToggleModalButton);
+      cy.dataCy("trigger_5-option").click();
+      cy.dataCy("0-regex-selector-dropdown").click();
+      cy.dataCy("0-display-name-option").click();
       cy.dataCy("0-regex-selector-input")
         .type("stuff")
         .should("have.value", "stuff");
-      cy.dataTestId("0-regex-selector-dropdown").click();
-      cy.dataTestId("0-build-variant-option").click();
+      cy.dataCy("0-regex-selector-dropdown").click();
+      cy.dataCy("0-build-variant-option").click();
       cy.dataCy("0-regex-selector-input").should("have.value", "");
     });
 
     it("Display success toast after submitting a valid form with regex selectors inputs and request succeeds", () => {
-      openModal();
-      cy.dataTestId("trigger_5-option").click();
-      cy.dataTestId("0-regex-selector-dropdown").click();
-      cy.dataTestId("0-display-name-option").click();
+      openSubscriptionModal(route, dataCyToggleModalButton);
+      cy.dataCy("trigger_5-option").click();
+      cy.dataCy("0-regex-selector-dropdown").click();
+      cy.dataCy("0-display-name-option").click();
       cy.dataCy("0-regex-selector-input").type("stuff");
-      cy.dataTestId("notify-by-select").click();
-      cy.dataTestId("jira-comment-option").click();
-      cy.dataTestId("jira-comment-input").type("EVG-2000");
+      cy.dataCy("notify-by-select").click();
+      cy.dataCy("jira-comment-option").click();
+      cy.dataCy("jira-comment-input").type("EVG-2000");
       cy.dataCy("toast").should("not.exist");
       cy.dataCy("save-subscription-button").click();
       cy.dataCy("toast").contains("Your subscription has been added");
     });
 
     it("'Add Additional Criteria' button should be disabled when there are enough 'Field name' dropdowns to represent all possible regex selector types for a trigger", () => {
-      openModal();
-      cy.dataTestId("trigger_5-option").click();
+      openSubscriptionModal(route, dataCyToggleModalButton);
+      cy.dataCy("trigger_5-option").click();
       cy.contains("Add additional criteria").should("not.be.disabled");
       cy.contains("Add additional criteria").click();
       cy.contains("Add additional criteria").should("be.disabled");
