c372a942ddddfabade77fa81d79a2553bc06ed28
Chaya Malik <chaya.malik@10gen.com>
2021-10-14 10:46:50 -0400

    gen

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -176,6 +176,7 @@ export type QueryBbGetCreatedTicketsArgs = {
 
 export type QueryMainlineCommitsArgs = {
   options: MainlineCommitsOptions;
+  buildVariantOptions?: Maybe<BuildVariantOptions>;
 };
 
 export type QueryTaskNamesForBuildVariantArgs = {
