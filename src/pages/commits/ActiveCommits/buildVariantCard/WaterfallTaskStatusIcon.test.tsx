fd9a77b12a463490011a708d0647edc97a4a9a4e
Arjun Patel <arjun453@gmail.com>
2021-10-20 14:25:41 -0400

    Update src/pages/commits/ActiveCommits/buildVariantCard/WaterfallTaskStatusIcon.test.tsx
    Co-authored-by: Mohamed Khelif <khelif96@gmail.com>

--- src/pages/commits/ActiveCommits/buildVariantCard/WaterfallTaskStatusIcon.test.tsx
+++ src/pages/commits/ActiveCommits/buildVariantCard/WaterfallTaskStatusIcon.test.tsx
@@ -2,7 +2,7 @@ import { MockedProvider } from "@apollo/client/testing";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import { GET_FAILED_TASK_STATUS_ICON_TOOLTIP } from "gql/queries";
-import { act, render, waitFor } from "test_utils/test-utils";
+import {  renderWithRouterMatch as render,  waitFor } from "test_utils/test-utils";
 import { WaterfallTaskStatusIcon } from "./WaterfallTaskStatusIcon";
 
 const props = {
