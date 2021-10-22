c2d9b6fb954d4bb911d2f3235a02f2bfceed5a59
minnakt <47064971+minnakt@users.noreply.github.com>
2021-10-22 12:51:20 -0400

    EVG-14778 - Refactor font styles
    * Add WOFF2 files

--- src/App.tsx
+++ src/App.tsx
@@ -3,93 +3,18 @@ import { Global, css } from "@emotion/react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Content } from "components/Content";
 import { ErrorBoundary } from "components/ErrorBoundary";
+import { akzidenzFont } from "components/styles/Fonts";
 import { routes } from "constants/routes";
 import { ContextProviders } from "context/Providers";
 import GQLWrapper from "gql/GQLWrapper";
 import { Login } from "pages/Login";
 
 const globalStyles = css`
-  // Regular
-  @font-face {
-    font-family: "Akzidenz";
-    src: url("/static/font/akzidenz/akzidgrostdreg.eot");
-    src: url("/static/font/akzidenz/akzidgrostdreg.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdreg.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdreg.ttf") format("truetype");
-    font-style: normal;
-    font-weight: normal;
-  }
-  // Regular Italic
-  @font-face {
-    font-family: "Akzidenz";
-    src: url("/static/font/akzidenz/akzidgrostdita.eot");
-    src: url("/static/font/akzidenz/akzidgrostdita.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdita.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdita.ttf") format("truetype");
-    font-style: italic;
-    font-weight: normal;
-  }
-  // Bold
-  @font-face {
-    font-family: "Akzidenz";
-    src: url("/static/font/akzidenz/akzidgrostdmed.eot");
-    src: url("/static/font/akzidenz/akzidgrostdmed.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdmed.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdmed.ttf") format("truetype");
-    font-style: normal;
-    font-weight: bold;
-  }
-  // Bold Italic
-  @font-face {
-    font-family: "Akzidenz";
-    src: url("/static/font/akzidenz/akzidgrostdmedita.eot");
-    src: url("/static/font/akzidenz/akzidgrostdmedita.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdmedita.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdmedita.ttf") format("truetype");
-    font-style: italic;
-    font-weight: bold;
-  }
-  // Light
-  @font-face {
-    font-family: "Akzidenz";
-    src: url("/static/font/akzidenz/akzidgrostdlig.eot");
-    src: url("/static/font/akzidenz/akzidgrostdlig.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdlig.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdlig.ttf") format("truetype");
-    font-style: normal;
-    font-weight: 200;
-  }
-  // Light Italic
-  @font-face {
-    font-family: "Akzidenz";
-    src: url("/static/font/akzidenz/akzidgrostdligita.eot");
-    src: url("/static/font/akzidenz/akzidgrostdligita.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdligita.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdligita.ttf") format("truetype");
-    font-style: italic;
-    font-weight: 200;
-  }
-  //Condensed weights and styles
-  @font-face {
-    font-family: "Akzidenz Cnd";
-    src: url("/static/font/akzidenz/akzidgrostdligcnd.eot");
-    src: url("/static/font/akzidenz/akzidgrostdligcnd.eot?#iefix")
-        format("embedded-opentype"),
-      url("/static/font/akzidenz/akzidgrostdligcnd.woff") format("woff"),
-      url("/static/font/akzidenz/akzidgrostdligcnd.ttf") format("truetype");
-    font-style: normal;
-    font-weight: 200;
-  }
+  ${akzidenzFont}
   background-color: white;
   background: white;
   body {
-    font-family: "Akzidenz", "Helvetica Neue", sans serif;
+    font-family: "Akzidenz", "Helvetica Neue", sans-serif;
   }
 `;
 
---
+++ src/components/styles/Fonts.tsx
@@ -0,0 +1,140 @@
+import { css } from "@emotion/react";
+
+// Akzidenz Light
+const akzidenzLight = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: 200;
+    font-style: normal;
+    src: url("/static/font/akzidenz/akzidgrostdlig-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdlig-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Light Italic
+const akzidenzLightItalic = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: 200;
+    font-style: italic;
+    src: url("/static/font/akzidenz/akzidgrostdligita-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdligita-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Regular
+const akzidenzRegular = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: normal;
+    src: url("/static/font/akzidenz/akzidgrostdreg-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdreg-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Regular Italic
+const akzidenzRegularItalic = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-style: italic;
+    src: url("/static/font/akzidenz/akzidgrostdita-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdita-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Medium - MongoDB uses Medium as default bold for Akzidenz
+const akzidenzMedium = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: bold;
+    src: url("/static/font/akzidenz/akzidgrostdmed-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdmed-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Medium Italic
+const akzidenzMediumItalic = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: bold;
+    font-style: italic;
+    src: url("/static/font/akzidenz/akzidgrostdmedita-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdmedita-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Bold
+const akzidenzBold = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: 800;
+    src: url("/static/font/akzidenz/akzidgrostdbol-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdbol-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Bold Italic
+const akzidenzBoldItalic = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: 800;
+    font-style: italic;
+    src: url("/static/font/akzidenz/akzidgrostdbolita-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdbolita-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Super
+const akzidenzSuper = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: 900;
+    font-style: normal;
+    src: url("/static/font/akzidenz/akzidgrostdsup-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdsup-webfont.woff") format("woff");
+  }
+`;
+
+// Akzidenz Super Italic
+const akzidenzSuperItalic = css`
+  @font-face {
+    font-display: swap;
+    font-family: "Akzidenz";
+    font-weight: 900;
+    font-style: italic;
+    src: url("/static/font/akzidenz/akzidgrostdsupita-webfont.woff2")
+        format("woff2"),
+      url("/static/font/akzidenz/akzidgrostdsupita-webfont.woff") format("woff");
+  }
+`;
+
+export const akzidenzFont = css`
+  ${akzidenzLight}
+  ${akzidenzLightItalic}
+  ${akzidenzRegular}
+  ${akzidenzRegularItalic}
+  ${akzidenzMedium}
+  ${akzidenzMediumItalic}
+  ${akzidenzBold}
+  ${akzidenzBoldItalic}
+  ${akzidenzSuper}
+  ${akzidenzSuperItalic}
+`;
