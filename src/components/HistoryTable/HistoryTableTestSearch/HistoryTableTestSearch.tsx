abc700b9e825082a3b8e3a7ece1148040909aeb2
minnakt <47064971+minnakt@users.noreply.github.com>
2021-11-02 19:02:43 -0400

    EVG-15728: Modify History Table Test Search component for user testing (#942)

--- src/components/HistoryTable/HistoryTableTestSearch/HistoryTableTestSearch.tsx
+++ src/components/HistoryTable/HistoryTableTestSearch/HistoryTableTestSearch.tsx
@@ -1,77 +1,35 @@
 import { useState } from "react";
 import styled from "@emotion/styled";
 import Icon from "@leafygreen-ui/icon";
-import { RadioBox, RadioBoxGroup } from "@leafygreen-ui/radio-box-group";
 import TextInput from "@leafygreen-ui/text-input";
 import { useLocation } from "react-router-dom";
 import { useUpdateURLQueryParams } from "hooks/useUpdateURLQueryParams";
-import { TestStatus } from "types/history";
 import { queryString, url } from "utils";
 
-const { upsertQueryParam, removeQueryParam } = url;
+const { upsertQueryParam } = url;
 const { parseQueryString } = queryString;
 
 export const HistoryTableTestSearch = () => {
   const [input, setInput] = useState("");
-  const [radioSelection, setRadioSelection] = useState<string>(
-    TestStatus.Failed
-  );
 
   const updateQueryParams = useUpdateURLQueryParams();
   const { search } = useLocation();
   const queryParams = parseQueryString(search);
 
-  const updateAndRemoveQueryParams = (
-    selectedParams: string[],
-    removeKey: string
-  ) => {
-    const updatedParams = upsertQueryParam(selectedParams, input);
-    const removedParams = removeQueryParam(queryParams[removeKey], input);
-    updateQueryParams({ [removeKey]: removedParams, all: updatedParams });
-    setInput("");
-  };
-
+  // Currently, users are only able to filter by failed tests
   const onClick = () => {
-    const allParams = convertToArray((queryParams.all as string[]) || []);
-    const failedParams = convertToArray((queryParams.failed as string[]) || []);
-    const passedParams = convertToArray((queryParams.passed as string[]) || []);
-
-    if (allParams.includes(input)) {
-      setInput("");
-      return;
-    }
-    if (radioSelection !== TestStatus.Failed && failedParams.includes(input)) {
-      updateAndRemoveQueryParams(allParams, TestStatus.Failed);
-      return;
-    }
-    if (radioSelection !== TestStatus.Passed && passedParams.includes(input)) {
-      updateAndRemoveQueryParams(allParams, TestStatus.Passed);
-      return;
-    }
-
-    const selectedParams = queryParams[radioSelection] as string[];
+    const selectedParams = queryParams.failed as string[];
     const updatedParams = upsertQueryParam(selectedParams, input);
-    updateQueryParams({ [radioSelection]: updatedParams });
+    updateQueryParams({ failed: updatedParams });
     setInput("");
   };
 
   return (
     <ContentWrapper>
-      <RadioBoxWrapper>
-        <StyledRadioBoxGroup
-          value={radioSelection}
-          size="full"
-          onChange={(e) => setRadioSelection(e.target.value)}
-        >
-          <RadioBox value={TestStatus.Failed}>Failed test</RadioBox>
-          <RadioBox value={TestStatus.Passed}>Passed test</RadioBox>
-          <RadioBox value={TestStatus.All}>All test</RadioBox>
-        </StyledRadioBoxGroup>
-      </RadioBoxWrapper>
-
       <TextInputWrapper>
         <TextInput
           type="search"
+          label="Filter by Failed Tests"
           aria-label="history-table-test-search-input"
           value={input}
           placeholder="Search Test Name"
@@ -84,25 +42,12 @@ export const HistoryTableTestSearch = () => {
   );
 };
 
-const convertToArray = (params: string[] | string) =>
-  Array.isArray(params) ? [...params] : [params];
-
 const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 40%;
-  min-width: 500px; // to prevent radio buttons from becoming visually squished if Content div becomes too small
+  min-width: 200px; //temporary
   padding-right: 30px;
-`;
-
-const RadioBoxWrapper = styled.div`
-  margin-bottom: 14px;
-  width: 85%;
-`;
-
-// @ts-expect-error
-const StyledRadioBoxGroup = styled(RadioBoxGroup)`
-  height: 32px;
 `;
 
 const TextInputWrapper = styled.div`
@@ -114,7 +59,7 @@ const TextInputWrapper = styled.div`
 const StyledIcon = styled(Icon)`
   position: absolute;
   height: 100%;
-  margin-top: auto;
+  top: 10px; //temporary
   align-self: flex-end;
   margin-right: 10px;
   &:hover {
