72a8a8752e0983bab200de21d98c279c1544005a
SupaJoon <arjun.patel@mongodb.com>
2021-11-09 11:23:34 -0500

    rm last passing version query

--- src/gql/queries/index.ts
+++ src/gql/queries/index.ts
@@ -48,7 +48,6 @@ import GET_USER_SETTINGS from "./get-user-settings.graphql";
 import GET_USER from "./get-user.graphql";
 import GET_VERSION from "./get-version.graphql";
 import HOSTS from "./hosts.graphql";
-import GET_LAST_PASSING_VERSION from "./last-passing-version.graphql";
 import GET_PROJECT_PATCHES from "./project-patches.graphql";
 import GET_SPAWN_EXPIRATION_INFO from "./spawn-expiration.graphql";
 import GET_SPAWN_TASK from "./spawn-task.graphql";
@@ -112,6 +111,5 @@ export {
   GET_PATCH,
   GET_VERSION,
   GET_PATCH_CONFIGURE,
-  GET_LAST_PASSING_VERSION,
   GET_FAILED_TASK_STATUS_ICON_TOOLTIP,
 };
