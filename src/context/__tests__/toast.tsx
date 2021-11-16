d0830837c17013df21828c83efffbd4fe91ed141
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-11-16 17:25:53 -0500

    Update documentation

--- src/context/__tests__/toast.tsx
+++ src/context/__tests__/toast.tsx
@@ -11,11 +11,11 @@ type DispatchToast = ReturnType<typeof useToastContext>;
  * @returns {Object} response - An object with the following properties:
  * @returns {React.FC} response.Component - A React Component which renders the component with the context mocked out
  * @returns {Object} response.dispatchToast - A series of jest.fn() methods which can be used to assert that the toast context was called with the correct parameters
- * @returns {Object} response.dispatchToast.success - A jest.fn() method which can be used to assert that the success toast context was called
- * @returns {Object} response.dispatchToast.error - A jest.fn() method which can be used to assert that the error toast context was called
- * @returns {Object} response.dispatchToast.info - A jest.fn() method which can be used to assert that the info toast context was called
- * @returns {Object} response.dispatchToast.warning - A jest.fn() method which can be used to assert that the warning toast context was called
- * @returns {Object} response.dispatchToast.hide - A jest.fn() method which can be used to assert that the hide toast context was called
+ * @returns {Function} response.dispatchToast.success - A jest.fn() method which can be used to assert that the success toast context was called
+ * @returns {Function} response.dispatchToast.error - A jest.fn() method which can be used to assert that the error toast context was called
+ * @returns {Function} response.dispatchToast.info - A jest.fn() method which can be used to assert that the info toast context was called
+ * @returns {Function} response.dispatchToast.warning - A jest.fn() method which can be used to assert that the warning toast context was called
+ * @returns {Function} response.dispatchToast.hide - A jest.fn() method which can be used to assert that the hide toast context was called
  * @returns {React.FC} response.HookWrapper - A React Component which wraps a hook and has the useToastContext mocked out but utilizes the same methods
  * @returns {Function} response.useToastContext - A jest.fn() method which can be used to assert that the useToastContext hook was called
  */
