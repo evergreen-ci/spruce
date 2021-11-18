11186a94f486c979d6d5bd2827a6a07d7dd7f133
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-11-17 21:16:42 -0500

    Add util for get requests

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,18 +4,38 @@ import { reportError } from "./errorReporting";
 
 type optionsType = {
   onFailure?: (e) => void;
+  sameDomain?: boolean;
 };
-export const post = async (
-  url: string,
-  body: unknown,
-  options: optionsType = {}
-) => {
+
+const defaultOptions: optionsType = {
+  sameDomain: true,
+};
+export const post = async (url: string, body: unknown, opts?: optionsType) => {
+  const options = { ...defaultOptions, ...opts };
   try {
-    const response = await axios.post(getLoginDomain() + url, {
+    const baseDomain = options.sameDomain ? getLoginDomain() : "";
+    const response = await axios.post(baseDomain + url, {
       body,
     });
     if (isBadResponse(response)) {
       throw new Error(getErrorMessage(response, "POST"));
+    }
+    return response;
+  } catch (e) {
+    if (options.onFailure) {
+      options.onFailure(e);
+    }
+    handleError(e);
+  }
+};
+
+export const get = async (url: string, opts?: optionsType) => {
+  const options = { ...defaultOptions, ...opts };
+  try {
+    const baseDomain = options.sameDomain ? getLoginDomain() : "";
+    const response = await axios.get(baseDomain + url);
+    if (isBadResponse(response)) {
+      throw new Error(getErrorMessage(response, "GET"));
     }
     return response;
   } catch (e) {
