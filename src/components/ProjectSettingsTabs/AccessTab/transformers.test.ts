bcb317ba9cba211e7ad6386b50bb51ce9b7ee7ea
sophstad <sophie.stadler@mongodb.com>
2021-12-06 10:51:05 -0500

    Fix eslint warnings

--- src/components/ProjectSettingsTabs/AccessTab/transformers.test.ts
+++ src/components/ProjectSettingsTabs/AccessTab/transformers.test.ts
@@ -8,11 +8,11 @@ describe("repo data", () => {
     form,
     gql: { input, result },
   } = repo;
-  test("correctly converts from GQL to a form", () => {
+  it("correctly converts from GQL to a form", () => {
     expect(gqlToForm(input)).toStrictEqual(form);
   });
 
-  test("correctly converts from a form to GQL", () => {
+  it("correctly converts from a form to GQL", () => {
     expect(formToGql(form, "123")).toStrictEqual(result);
   });
 });
@@ -22,11 +22,11 @@ describe("project data", () => {
     form,
     gql: { input, result },
   } = project;
-  test("correctly converts from GQL to a form", () => {
+  it("correctly converts from GQL to a form", () => {
     expect(gqlToForm(input)).toStrictEqual(form);
   });
 
-  test("correctly converts from a form to GQL and omits empty strings", () => {
+  it("correctly converts from a form to GQL and omits empty strings", () => {
     expect(
       formToGql(
         {
