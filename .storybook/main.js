36f3106c590471dfb32beae2737f4d5b90221926
sophstad <sophie.stadler@mongodb.com>
2021-12-09 11:28:01 -0500

    Fix tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,22 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const { merge } = require("webpack-merge");
 
-// https://stackoverflow.com/questions/65894711/module-not-found-error-cant-resolve-emotion-styled-base-when-running-story
-function getPackageDir(filepath) {
-  let currDir = path.dirname(require.resolve(filepath));
-  while (true) {
-    if (fs.existsSync(path.join(currDir, "package.json"))) {
-      return currDir;
-    }
-    const { dir, root } = path.parse(currDir);
-    if (dir === root) {
-      throw new Error(
-        `Could not find package.json in the parent directories starting from ${filepath}.`
-      );
-    }
-    currDir = dir;
-  }
-}
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -30,15 +14,4 @@ module.exports = {
     "storybook-addon-apollo-client",
   ],
   presets: ["@storybook/preset-ant-design"],
-  webpackFinal: async (config) => {
-    return merge(config, {
-      resolve: {
-        alias: {
-          "@emotion/core": getPackageDir("@emotion/react"),
-          "@emotion/styled": getPackageDir("@emotion/styled"),
-          "emotion-theming": getPackageDir("@emotion/react"),
-        },
-      },
-    });
-  },
 };
