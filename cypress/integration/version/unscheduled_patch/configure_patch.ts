cad097fbd4ae403860308552c040815d9fe8ac96
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-12-10 13:42:17 -0500

    Use different cypress stubbing

--- cypress/integration/version/unscheduled_patch/configure_patch.ts
+++ cypress/integration/version/unscheduled_patch/configure_patch.ts
@@ -520,10 +520,11 @@ describe("Configure Patch Page", () => {
       const val = "hello world";
       cy.dataCy(`patch-name-input`).as("patchNameInput").clear().type(val);
       cy.dataCy("task-checkbox").first().check({ force: true });
-      cy.route({
-        method: "POST",
-        url: "/graphql/query",
-        response: mockedSuccessfulConfigureResponse,
+      // TODO: Replace this with cy.intercept() once we upgrade to > Cypress 6.0.0
+      cy.route2("/graphql/query", (req) => {
+        req.reply((res) => {
+          res.body = mockedSuccessfulConfigureResponse;
+        });
       });
       cy.dataCy("schedule-patch").click();
       cy.location("pathname").should(
@@ -536,11 +537,11 @@ describe("Configure Patch Page", () => {
       const val = "hello world";
       cy.dataCy(`patch-name-input`).as("patchNameInput").clear().type(val);
       cy.dataCy("task-checkbox").first().check({ force: true });
-
-      cy.route({
-        method: "POST",
-        url: "/graphql/query",
-        response: mockedErrorConfigureResponse,
+      // TODO: Replace this with cy.intercept() once we upgrade to > Cypress 6.0.0
+      cy.route2("/graphql/query", (req) => {
+        req.reply((res) => {
+          res.body = mockedErrorConfigureResponse;
+        });
       });
       cy.dataCy("schedule-patch").click();
       cy.location("pathname").should(
