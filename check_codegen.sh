da7050a15c297447f879e43de1f420449888f751
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-12-23 09:41:10 -0500

    update test

--- check_codegen.sh
+++ check_codegen.sh
@@ -3,5 +3,6 @@ if [[ $(git status --porcelain | grep src/gql/generated/types.ts) ]]; then
   echo "did you forget to run 'yarn codegen'?"
   exit 1
 else
+  echo "src/gql/types.ts is up to date"
   exit 0
 fi
