8ad1554f3d5c3ce19f4833415d3ad1ec769abcf5
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-12-23 10:11:08 -0500

    Update script

--- check_codegen.sh
+++ check_codegen.sh
@@ -1,4 +1,8 @@
-if [[ $(git status --porcelain | grep src/gql/generated/types.ts) ]]; then
+# This is a script which checks if yarn codegen was run it used by the check_codegen evergreen task
+BEFORE=$(git status --porcelain | grep src/gql/generated/types.ts)
+yarn codegen
+AFTER=$(git status --porcelain | grep src/gql/generated/types.ts)
+if [ "$BEFORE" != "$AFTER" ]; then
   echo "src/gql/types.ts is not up to date"
   echo "did you forget to run 'yarn codegen'?"
   exit 1
