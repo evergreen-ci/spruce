9183466b735639c4d4c1693e784af5ca679ae6b5
Chaya Malik <chaya.malik@10gen.com>
2022-01-24 19:26:12 -0500

    add validate

--- src/components/ProjectSettingsTabs/PluginsTab/PluginsTab.tsx
+++ src/components/ProjectSettingsTabs/PluginsTab/PluginsTab.tsx
@@ -14,6 +14,7 @@ export const PluginsTab: React.FC<TabProps> = ({
 }) => {
   const { getTab, updateForm } = useProjectSettingsContext();
   const { formData } = getTab(tab);
+  console.log(formData);
 
   const initialFormState = projectData || repoData;
   usePopulateForm(initialFormState, tab);
@@ -38,7 +39,77 @@ export const PluginsTab: React.FC<TabProps> = ({
       formData={formData}
       onChange={onChange}
       schema={schema}
+      validate={validate}
       uiSchema={uiSchema}
     />
   );
+};
+
+/* Display an error and prevent saving if a user enters something invalid. */
+const validate = (formData, errors) => {
+  const searchProject = formData?.buildBaronSettings?.ticketSearchProjects;
+
+  // if a search project is defined, a create project must be defined, and vice versa
+  const searchProjectDefined =
+    searchProject?.length !== 0 &&
+    searchProject?.[0]?.searchProject !== undefined;
+  const createProjectDefined =
+    formData?.buildBaronSettings?.ticketCreateProject?.createProject !== "";
+
+  if (searchProjectDefined && !createProjectDefined) {
+    errors.buildBaronSettings?.ticketCreateProject?.createProject.addError(
+      "You must specify a create project."
+    );
+  }
+
+  if (createProjectDefined && !searchProjectDefined) {
+    errors.buildBaronSettings?.ticketCreateProject?.createProject.addError(
+      "You must also specify at least one ticket search project above."
+    );
+  }
+
+  // a webhook must contain both a secret and an endpoint
+  const fileTicketWebhookDefined =
+    formData?.buildBaronSettings?.fileTicketWebhook?.endpoint !== "";
+  const fileTicketSecretDefined =
+    formData?.buildBaronSettings?.fileTicketWebhook?.secret !== "";
+
+  if (fileTicketSecretDefined && !fileTicketWebhookDefined) {
+    errors.buildBaronSettings?.fileTicketWebhook?.endpoint.addError(
+      "You must specify a webhook to use with the secret."
+    );
+  }
+
+  if (fileTicketWebhookDefined && !fileTicketSecretDefined) {
+    errors.buildBaronSettings?.fileTicketWebhook?.secret?.addError(
+      "You must specify a secret to use with the webhook."
+    );
+  }
+
+  // each jira custom field must contain both a display text and a custom field
+  formData.buildBaronSettings?.taskAnnotationSettings?.jiraCustomFields.forEach(
+    (field, i) => {
+      if (field?.field === undefined) {
+        formData.buildBaronSettings?.taskAnnotationSettings?.jiraCustomFields?.[
+          i
+        ]?.field?.addValue("");
+      }
+      const fieldUndefined = field?.field === undefined || field?.field === "";
+      const displayTextUndefined =
+        field?.displayText === undefined || field?.displayText === "";
+
+      if (!fieldUndefined && displayTextUndefined) {
+        errors.buildBaronSettings?.taskAnnotationSettings?.jiraCustomFields?.[
+          i
+        ]?.field?.addError("You must also specify a display text below.");
+      }
+      if (!displayTextUndefined && fieldUndefined) {
+        errors.buildBaronSettings?.taskAnnotationSettings?.jiraCustomFields?.[
+          i
+        ]?.displayText?.addError("You must also specify a custom field above.");
+      }
+    }
+  );
+
+  return errors;
 };
