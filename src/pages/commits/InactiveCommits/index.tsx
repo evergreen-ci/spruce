849af925d3dfe9027f94ffca0caba75ce3455c57
minnakt <minna.kt@mongodb.com>
2022-01-28 15:52:35 -0500

    EVG-16180: Improve tooltip readability

--- src/pages/commits/InactiveCommits/index.tsx
+++ src/pages/commits/InactiveCommits/index.tsx
@@ -6,7 +6,7 @@ import { CommitRolledUpVersions } from "types/commits";
 import { string } from "utils";
 import { commitChartHeight, tooltipZIndex } from "../constants";
 
-const { getDateCopy, shortenGithash } = string;
+const { getDateCopy, shortenGithash, trimStringFromMiddle } = string;
 const { gray } = uiColors;
 
 export const InactiveCommitsLine = () => (
@@ -29,6 +29,7 @@ export const InactiveCommitButton: React.FC<InactiveCommitsProps> = ({
 
   const tooltipType = hasFilters ? "Unmatching" : "Inactive";
   let returnedCommits = [];
+
   if (shouldSplitCommits) {
     const hiddenCommitCount = versionCount - MAX_COMMIT_COUNT;
     returnedCommits = [
@@ -58,7 +59,7 @@ export const InactiveCommitButton: React.FC<InactiveCommitsProps> = ({
   }
 
   return (
-    <Tooltip
+    <StyledTooltip
       usePortal={false}
       align="bottom"
       justify="middle"
@@ -80,31 +81,36 @@ export const InactiveCommitButton: React.FC<InactiveCommitsProps> = ({
         </TitleText>
         {returnedCommits}
       </TooltipContainer>
-    </Tooltip>
+    </StyledTooltip>
   );
 };
 
-const getCommitCopy = (v: CommitRolledUpVersions[0]) =>
-  `${shortenGithash(v.revision)} ${getDateCopy(v.createTime)} ${v.author} ${
-    v.message
-  } (#${v.order})`;
+const getCommitCopy = (v: CommitRolledUpVersions[0]) => (
+  <>
+    <CommitTitleText>
+      {shortenGithash(v.revision)} — {getDateCopy(v.createTime)}
+    </CommitTitleText>
+    <CommitBodyText>
+      {v.author} - {trimStringFromMiddle(v.message, 120)} (#{v.order})
+    </CommitBodyText>
+  </>
+);
 
-const CommitText = styled(Body)`
-  padding: 4px 0;
-`;
+const tooltipWidth = 300;
+const padding = 16;
 
 const HiddenCommitsWrapper = styled.div`
   width: 60%;
+  border-top: 1px solid ${gray.dark2};
   border-bottom: 1px solid ${gray.dark2};
   text-align: center;
   padding: 4px 0;
   align-self: center;
-  margin: 32px 0;
+  margin: 16px 0;
 `;
 
 const TooltipContainer = styled.div`
-  width: 300px;
-  margin: auto;
+  width: ${tooltipWidth}px;
   display: flex;
   justify-content: center;
   align-items: flex-start;
@@ -138,6 +144,26 @@ const TitleText = styled(Body)`
 const Container = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+// @ts-ignore-error
+const StyledTooltip = styled(Tooltip)`
+  max-height: 350px;
+  overflow-y: scroll;
+`;
+
+const CommitText = styled(Body)`
+  padding: 8px 0;
+  word-break: break-all;
+`;
+
+const CommitTitleText = styled(Body)`
+  font-weight: bold;
+`;
+
+const CommitBodyText = styled(Body)`
+  width: ${tooltipWidth - padding}px;
+  padding-left: ${padding}px;
 `;
 
 const MAX_COMMIT_COUNT = 5;
