524252814eb75959c9f92faaa8f7df96ee7b115c
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-01-31 13:12:32 +0100

    magic shell script

--- appVersion.js
+++
@@ -1,5 +0,0 @@
-// This script writes the version number to the stdout so it can be read by appVersion.sh
-
-const appVersion = require("./package.json").version;
-
-console.log(appVersion);
--- appVersion.sh
+++ appVersion.sh
@@ -1,3 +1,4 @@
 # This script runs appVersion.js which fetches the version number from the
 # package.json file and writes it to a env variable.
-export REACT_APP_VERSION=`node appVersion.js`
+export REACT_APP_VERSION=$(node -pe "require('./package.json').version")
+echo $REACT_APP_VERSION
