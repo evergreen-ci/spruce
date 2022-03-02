2e9a40339638e5ae224b12c830a216229c4c1d69
sophstad <sophie.stadler@mongodb.com>
2022-03-02 16:41:17 -0500

    We do need to actually save the private variable value at some point...

--- src/components/ProjectSettingsTabs/VariablesTab/transformers.ts
+++ src/components/ProjectSettingsTabs/VariablesTab/transformers.ts
@@ -24,13 +24,14 @@ export const formToGql: FormToGqlFunction = (
   id
 ) => {
   const vars = varsData.reduce(
-    (acc, { varName, varValue, isPrivate, isAdminOnly }) => {
+    (acc, { varName, varValue, isPrivate, isAdminOnly, isDisabled }) => {
       if (!varName || !varValue) return acc;
 
       let val = varValue;
       if (isPrivate) {
         acc.privateVarsList.push(varName);
-        val = "";
+        // Overwrite {REDACTED} for variables that have been previously saved as private variables
+        if (isDisabled) val = "";
       }
       if (isAdminOnly) {
         acc.adminOnlyVarsList.push(varName);
