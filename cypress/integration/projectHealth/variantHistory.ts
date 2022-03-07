ea4fa9244bc2f8964646fdde11c9ba9e7939873b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-03-06 23:53:38 -0500

    Add variant history tests

---
+++ cypress/integration/projectHealth/variantHistory.ts
@@ -0,0 +1,104 @@
+// / <reference types="Cypress" />
+
+describe("variant history", () => {
+  before(() => {
+    cy.login();
+  });
+  beforeEach(() => {
+    cy.preserveCookies();
+  });
+
+  it("should be linked to the variant history page from the project health page", () => {
+    cy.visit("/commits/spruce");
+    cy.dataCy("variant-header").should("exist");
+    cy.dataCy("variant-header").scrollIntoView();
+    cy.dataCy("variant-header").should("be.visible");
+    cy.dataCy("variant-header").should("contain.text", "Ubuntu 16.04");
+    cy.dataCy("variant-header").click();
+    cy.location("pathname").should("eq", "/variant-history/spruce/ubuntu1604");
+  });
+  it("should be able to paginate column headers", () => {
+    cy.dataCy("header-cell").should("have.length", 7);
+    cy.dataCy("next-page-button").click();
+    cy.dataCy("header-cell").should("have.length", 2);
+    cy.dataCy("prev-page-button").click();
+    cy.dataCy("header-cell").should("have.length", 7);
+  });
+  it("should be able to filter column headers", () => {
+    cy.dataCy("header-cell").should("have.length", 7);
+    cy.getInputByLabel("Tasks").click();
+    cy.getInputByLabel("Tasks").within(($el) => {
+      cy.wrap($el)
+        .dataCy("searchable-dropdown-option")
+        .contains("compile")
+        .click();
+      cy.wrap($el)
+        .dataCy("searchable-dropdown-option")
+        .contains("e2e_test")
+        .click();
+    });
+    cy.getInputByLabel("Tasks").click();
+    cy.dataCy("header-cell").should("have.length", 2);
+  });
+  it("removing column header filters should restore all columns", () => {
+    cy.dataCy("header-cell").should("have.length", 2);
+    cy.getInputByLabel("Tasks").click();
+    cy.getInputByLabel("Tasks").within(($el) => {
+      cy.wrap($el)
+        .dataCy("searchable-dropdown-option")
+        .contains("compile")
+        .click();
+      cy.wrap($el)
+        .dataCy("searchable-dropdown-option")
+        .contains("e2e_test")
+        .click();
+    });
+    cy.getInputByLabel("Tasks").click();
+    cy.dataCy("header-cell").should("have.length", 7);
+  });
+  it("hovering over a failing task should show test results", () => {
+    cy.dataCy("history-table-icon")
+      .get("[data-status=failed]")
+      .should("have.length", 1);
+    cy.dataCy("history-table-icon")
+      .get("[data-status=failed]")
+      .should("be.visible");
+    cy.dataCy("history-table-icon")
+      .get("[data-status=failed]")
+      .first()
+      .trigger("mouseover");
+    cy.dataCy("test-tooltip").should("be.visible");
+    cy.dataCy("test-tooltip").contains("JustAFakeTestInALonelyWorld");
+    cy.dataCy("history-table-icon")
+      .get("[data-status=failed]")
+      .first()
+      .trigger("mouseout");
+  });
+  describe("applying a test filter", () => {
+    before(() => {
+      cy.getInputByLabel("Filter by Failed Tests").should("exist");
+      cy.getInputByLabel("Filter by Failed Tests").type(
+        "JustAFakeTestInALonelyWorld"
+      );
+      cy.getInputByLabel("Filter by Failed Tests").type("{enter}");
+      cy.dataCy("filter-badge").should("exist");
+      cy.dataCy("filter-badge").should("contain.text", "JustAFake");
+    });
+    it("should disable non matching tasks", () => {
+      cy.dataCy("history-table-icon")
+        .get("[data-status=success]")
+        .each(($el) => {
+          cy.wrap($el).should("have.attr", "aria-disabled", "true");
+        });
+    });
+    it("should display a message on matching tasks", () => {
+      cy.contains("1 / 1 Failing Tests").should("exist");
+    });
+    it("should have a tooltip on matching tests with test results", () => {
+      cy.contains("1 / 1 Failing Tests").should("exist");
+      cy.contains("1 / 1 Failing Tests").trigger("mouseover");
+      cy.dataCy("test-tooltip").should("be.visible");
+      cy.dataCy("test-tooltip").contains("JustAFakeTestInALonelyWorld");
+    });
+  });
+});
