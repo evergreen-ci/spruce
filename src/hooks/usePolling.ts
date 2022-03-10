60e52f23755829e4c1f4de4c563d6fb7ada53d0b
minnakt <minna.kt@mongodb.com>
2022-03-10 11:47:50 -0500

    Add analytics events

--- src/hooks/usePolling.ts
+++ src/hooks/usePolling.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { usePollingAnalytics } from "analytics";
 import { pollInterval } from "constants/index";
 import { useNetworkStatus } from "./useNetworkStatus";
 import { usePageVisibility } from "./usePageVisibility";
@@ -19,24 +20,29 @@ type usePollingType = {
  * @returns boolean - true if polling, false if not polling
  */
 export const usePolling: usePollingType = (startPolling, stopPolling) => {
+  const { sendEvent } = usePollingAnalytics();
+
   const [isPolling, setIsPolling] = useState(true);
   const isOnline = useNetworkStatus();
   const isVisible = usePageVisibility();
 
   // If offline and polling, stop polling.
   if (!isOnline && isPolling) {
+    sendEvent({ name: "Tab Not Active", status: "offline" });
     setIsPolling(false);
     stopPolling();
   }
 
   // If not visible and polling, stop polling.
   if (!isVisible && isPolling) {
+    sendEvent({ name: "Tab Not Active", status: "hidden" });
     setIsPolling(false);
     stopPolling();
   }
 
   // If online and visible and not polling, start polling.
   if (isOnline && isVisible && !isPolling) {
+    sendEvent({ name: "Tab Active" });
     setIsPolling(true);
     startPolling(pollInterval);
   }
