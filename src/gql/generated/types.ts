000302e68d2106b7c6ebdae83789a5d2816a9e20
Chaya Malik <chaya.malik@10gen.com>
2022-03-16 10:24:53 -0400

    EVG-16367 Add canCreateProject check to the createProject Modal (#1148)

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -3933,6 +3933,12 @@ export type GetUserConfigQuery = {
   }>;
 };
 
+export type GetUserPermissionsQueryVariables = Exact<{ [key: string]: never }>;
+
+export type GetUserPermissionsQuery = {
+  user: { userId: string; permissions: { canCreateProject: boolean } };
+};
+
 export type GetUserSettingsQueryVariables = Exact<{ [key: string]: never }>;
 
 export type GetUserSettingsQuery = {
