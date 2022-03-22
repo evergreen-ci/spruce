038d1e65f215dcd75a768138c308fedb16f1fcb7
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-03-22 18:14:25 -0400

    Fix typings for custom test utils hook

--- src/components/HistoryTable/hooks/test-utils.tsx
+++ src/components/HistoryTable/hooks/test-utils.tsx
@@ -4,26 +4,22 @@ import {
   useHistoryTable as useHistoryTableActual,
 } from "../HistoryTableContext";
 
-// generic T is a hook type
-// generic R is the return type of the hook
-type HookType<T, R> = (args: T) => R;
-
-type UseHistoryTableTestHookType = <T, R>(
-  useHook: HookType<T, R>
+type UseHistoryTableTestHookType = <T extends (...args: any) => any | any[]>(
+  useHook: T
 ) => (
-  props: T
+  ...args: Parameters<T>
 ) => {
-  hookResponse: R;
+  hookResponse: ReturnType<T>;
   historyTable: ReturnType<typeof useHistoryTableActual>;
 };
 /** useHistoryTableTestHook takes a hook and useHistoryTable hooks
  * and combines them into a shared hook which can be rendered under the same wrapper context
  * and can be used together */
 const useHistoryTableTestHook: UseHistoryTableTestHookType = (useHook) => (
-  props
+  args
 ) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const hookResponse = useHook(props);
+  const hookResponse = useHook(...args);
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const historyTable = useHistoryTableActual();
   return {
