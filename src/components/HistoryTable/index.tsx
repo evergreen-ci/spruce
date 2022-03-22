fa40ad30e84ec23a8d832d7c800178cdc31d4f9a
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-03-22 16:04:38 -0400

    Extract test filter logic into hook

--- src/components/HistoryTable/index.tsx
+++ src/components/HistoryTable/index.tsx
@@ -3,8 +3,19 @@ import ColumnPaginationButtons from "./ColumnPaginationButtons";
 import HistoryTable from "./HistoryTable";
 import * as context from "./HistoryTableContext";
 import Row from "./HistoryTableRow/Row";
+import { HistoryTableTestSearch } from "./HistoryTableTestSearch";
 import useTestResults from "./hooks/useTestResults";
 import * as types from "./types";
+import useTestFilters from "./useTestFilters";
 
-export { Row, Cell, ColumnPaginationButtons, context, types, useTestResults };
+export {
+  Row,
+  Cell,
+  ColumnPaginationButtons,
+  context,
+  types,
+  useTestResults,
+  HistoryTableTestSearch,
+  useTestFilters,
+};
 export default HistoryTable;
