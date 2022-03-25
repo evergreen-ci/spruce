6952604a79bb5e8a8e2cb5a1c4cc2b6ba6d8eea4
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-03-25 17:15:01 -0400

    EVG-16598 Refactor history pages and extract common logic into hooks (#1165)

--- src/components/HistoryTable/constants.ts
+++ src/components/HistoryTable/constants.ts
@@ -1,4 +1,20 @@
-export const FOLDED_COMMITS_HEIGHT = 40;
-export const COMMIT_HEIGHT = 120;
-export const DATE_SEPARATOR_HEIGHT = 40;
-export const DEFAULT_HEIGHT = 100;
+import { TestStatus } from "types/history";
+
+const queryParamsToDisplay = new Set([
+  TestStatus.Failed,
+  TestStatus.Passed,
+  TestStatus.All,
+]);
+
+const FOLDED_COMMITS_HEIGHT = 40;
+const COMMIT_HEIGHT = 120;
+const DATE_SEPARATOR_HEIGHT = 40;
+const DEFAULT_HEIGHT = 100;
+
+export {
+  queryParamsToDisplay,
+  FOLDED_COMMITS_HEIGHT,
+  COMMIT_HEIGHT,
+  DATE_SEPARATOR_HEIGHT,
+  DEFAULT_HEIGHT,
+};
