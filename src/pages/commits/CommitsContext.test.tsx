fbaa18983aa2d1f211bf448e2bd2602203baf149
minnakt <minna.kt@mongodb.com>
2022-03-28 16:01:46 -0400

    Update tests

--- src/pages/commits/CommitsContext.test.tsx
+++ src/pages/commits/CommitsContext.test.tsx
@@ -3,7 +3,7 @@ import { CommitsProvider, useCommits } from "./CommitsContext";
 
 const wrapper = ({ children }) => <CommitsProvider>{children}</CommitsProvider>;
 
-describe("historyTableContext", () => {
+describe("commitsContext", () => {
   it("initializes with the default state", () => {
     const { result } = renderHook(() => useCommits(), { wrapper });
     expect(result.current).toStrictEqual({
@@ -11,12 +11,15 @@ describe("historyTableContext", () => {
       setTaskIcon: expect.any(Function),
     });
   });
-  it("should properly update the selectedTaskIcon", () => {
-    const { result } = renderHook(() => useCommits(), { wrapper });
-    const hoverTask = "ubuntu-1604check_codegen";
+  it("should properly update the selectedTaskIcon", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useCommits(), {
+      wrapper,
+    });
+    const hoverTask = "ubuntu-1604-check_codegen";
     act(() => {
       result.current.setTaskIcon(hoverTask);
     });
+    await waitForNextUpdate();
     expect(result.current.hoveredTaskIcon).toBe(hoverTask);
   });
 });
