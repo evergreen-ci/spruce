ffa53898d2413d1569bef2ffefcb416708abe554
SupaJoon <arjun.patel@mongodb.com>
2022-03-28 14:47:38 -0400

    refactor

--- src/components/HistoryTable/Cell/Cell.tsx
+++ src/components/HistoryTable/Cell/Cell.tsx
@@ -8,7 +8,7 @@ import { inactiveElementStyle, StyledRouterLink } from "components/styles";
 import { getTaskRoute } from "constants/routes";
 import { size } from "constants/tokens";
 import { TaskStatus } from "types/task";
-import { COLUMN_LABEL_WIDTH } from "../constants";
+import { COLUMN_LABEL_WIDTH, ROW_LABEL_WIDTH } from "../constants";
 import { HistoryTableIcon } from "../HistoryTableIcon";
 
 const { gray } = uiColors;
@@ -122,7 +122,7 @@ const HeaderCell = styled(Cell)`
 
 // LabelCellContainer is used to provide padding for the first column in the table since we do not have a header for it
 const LabelCellContainer = styled.div`
-  width: 200px;
+  width: ${ROW_LABEL_WIDTH}px;
   margin-right: 40px;
 `;
 
