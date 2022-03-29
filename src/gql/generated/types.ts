345da020487255d1b9fb87bed4ceb98397a0c5a5
Sophie Stadler <sophstad@users.noreply.github.com>
2022-03-29 11:10:42 -0400

    EVG-16195: Disable github fields when collision is found (#1172)

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -3186,6 +3186,18 @@ export type EventLogsQuery = {
   };
 };
 
+export type GithubProjectConflictsQueryVariables = Exact<{
+  projectId: Scalars["String"];
+}>;
+
+export type GithubProjectConflictsQuery = {
+  githubProjectConflicts: {
+    commitQueueIdentifiers?: Maybe<Array<string>>;
+    prTestingIdentifiers?: Maybe<Array<string>>;
+    commitCheckIdentifiers?: Maybe<Array<string>>;
+  };
+};
+
 export type GetHasVersionQueryVariables = Exact<{
   id: Scalars["String"];
 }>;
