90a36c61b31cc8bf7e0b4db9ffb88a4e6e0383c5
SupaJoon <arjun.patel@mongodb.com>
2022-03-31 16:22:47 -0400

    sort actions

--- src/analytics/projectHealth/useProjectHealthAnalytics.ts
+++ src/analytics/projectHealth/useProjectHealthAnalytics.ts
@@ -9,30 +9,31 @@ import { ProjectSelectAnalytics } from "components/projectSelect";
 import { TupleAnalytics } from "components/TupleSelect";
 
 type Action =
-  | { name: "Click commit label version link"; versionId: string }
-  | { name: "Click commit label jira link"; jiraTicket: string }
-  | { name: "Click commit chart view option"; viewOption: string }
-  | ({ name: "Submit commit chart variant/task tuple" } & TupleAnalytics)
-  | { name: "Filter commit chart by task status"; statuses: string[] }
-  | { name: "Filter commit chart by requester"; requesters: string[] }
   | ({ name: "Select commit chart project" } & ProjectSelectAnalytics)
-  | { name: "Click commit chart grouped status badge"; statuses: string[] }
-  | { name: "Click commit chart task status icon"; status: string }
-  | ({
-      name: "Submit variant history failed test filter";
-    } & HistoryTableTestSearchAnalytics)
+  | ({ name: "Submit commit chart variant/task tuple" } & TupleAnalytics)
   | ({
       name: "Submit task history failed test filter";
     } & HistoryTableTestSearchAnalytics)
-  | { name: "Remove commit chart badge" }
+  | ({
+      name: "Submit variant history failed test filter";
+    } & HistoryTableTestSearchAnalytics)
   | { name: "Clear all commit chart badges" }
-  | { name: "Remove task history badge" }
   | { name: "Clear all task history badges" }
-  | { name: "Remove variant history badge" }
   | { name: "Clear all variant history badges" }
-  | { name: "Click variant history task selector"; tasks: string[] }
-  | { name: "Click variant history column header"; task: string }
+  | { name: "Click commit chart grouped status badge"; statuses: string[] }
+  | { name: "Click commit chart task status icon"; status: string }
+  | { name: "Click commit chart variant label"; variant: string }
+  | { name: "Click commit chart view option"; viewOption: string }
+  | { name: "Click commit label jira link"; jiraTicket: string }
+  | { name: "Click commit label version link"; versionId: string }
   | { name: "Click task history column header"; variant: string }
+  | { name: "Click variant history column header"; task: string }
+  | { name: "Click variant history task selector"; tasks: string[] }
+  | { name: "Filter commit chart by requester"; requesters: string[] }
+  | { name: "Filter commit chart by task status"; statuses: string[] }
+  | { name: "Remove commit chart badge" }
+  | { name: "Remove task history badge" }
+  | { name: "Remove variant history badge" }
   | {
       name: "Submit task history build variant selector";
       buildVariants: string[];
