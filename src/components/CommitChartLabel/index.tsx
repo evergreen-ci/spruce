ddbebe07b4733a9538ae4e8eedbccf56cb67e573
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-04-11 19:13:17 -0400

    Add upstreamProject field to versions and mainline commits

--- src/components/CommitChartLabel/index.tsx
+++ src/components/CommitChartLabel/index.tsx
@@ -4,13 +4,14 @@ import { uiColors } from "@leafygreen-ui/palette";
 import { Body } from "@leafygreen-ui/typography";
 import ExpandedText from "components/ExpandedText";
 import { StyledRouterLink } from "components/styles";
-import { getVersionRoute } from "constants/routes";
+import { getVersionRoute, getTaskRoute } from "constants/routes";
 import { size, zIndex } from "constants/tokens";
 import {
   GetSpruceConfigQuery,
   GetSpruceConfigQueryVariables,
 } from "gql/generated/types";
 import { GET_SPRUCE_CONFIG } from "gql/queries";
+import { ProjectTriggerLevel } from "types/triggers";
 import { string } from "utils";
 import { shortenGithash } from "utils/string";
 import { jiraLinkify } from "utils/string/jiraLinkify";
@@ -24,6 +25,17 @@ interface Props {
   author: string;
   message: string;
   versionId: string;
+  upstreamProject?: {
+    triggerType: string;
+    project: string;
+
+    task?: {
+      id: string;
+    };
+    version?: {
+      id: string;
+    };
+  };
 }
 
 const CommitChartLabel: React.VFC<Props> = ({
@@ -32,6 +44,7 @@ const CommitChartLabel: React.VFC<Props> = ({
   author,
   message,
   versionId,
+  upstreamProject,
 }) => {
   const createDate = new Date(createTime);
   const shortenMessage = message.length > MAX_CHAR;
@@ -41,7 +54,13 @@ const CommitChartLabel: React.VFC<Props> = ({
     GetSpruceConfigQueryVariables
   >(GET_SPRUCE_CONFIG);
   const jiraHost = configData?.spruceConfig?.jira?.host;
-
+  const {
+    triggerType,
+    project: upstreamProjectIdentifier,
+    task: upstreamTask,
+    task: upstreamVersion,
+  } = upstreamProject || {};
+  console.log(upstreamProject);
   return (
     <LabelContainer data-cy="commit-label">
       <LabelText>
@@ -50,6 +69,21 @@ const CommitChartLabel: React.VFC<Props> = ({
         </StyledRouterLink>{" "}
         <b>{shortDate(createDate)}</b>
       </LabelText>
+      {upstreamProject && (
+        <LabelText>
+          Triggered from:{" "}
+          <StyledRouterLink
+            to={
+              triggerType === ProjectTriggerLevel.TASK
+                ? getTaskRoute(upstreamTask?.id)
+                : getVersionRoute(upstreamVersion?.id)
+            }
+          >
+            {upstreamProjectIdentifier}
+          </StyledRouterLink>
+        </LabelText>
+      )}
+
       <LabelText>{author} -</LabelText>
       <LabelText>
         {jiraLinkify(shortenMessage ? shortenedMessage : message, jiraHost)}
