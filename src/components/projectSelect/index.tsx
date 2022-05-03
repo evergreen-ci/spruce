cc09a2395084a9892ca44e272d3d24fe460d4bfe
Sophie Stadler <sophstad@users.noreply.github.com>
2022-05-03 11:18:58 -0400

    EVG-15068: Add Duplicate Project functionality (#1241)

--- src/components/projectSelect/index.tsx
+++ src/components/projectSelect/index.tsx
@@ -11,7 +11,6 @@ import {
   GetViewableProjectRefsQueryVariables,
 } from "gql/generated/types";
 import { GET_PROJECTS, GET_VIEWABLE_PROJECTS } from "gql/queries";
-import { errorReporting } from "utils";
 import { ProjectOptionGroup } from "./ProjectOptionGroup";
 
 type Project = {
@@ -129,13 +128,8 @@ export const ProjectSelect: React.VFC<ProjectSelectProps> = ({
   );
 };
 
-const { reportError } = errorReporting;
-
 export const getRepoId = (projects: Project[]) => {
   if (!projects || projects.length === 0) {
-    reportError(
-      new Error("No projects provided when fetching repo id")
-    ).warning();
     return "";
   }
   return projects[0].repoRefId;
