2c102685edd8f4b95f95c575acef56e24b3fa18e
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-05-04 13:45:31 -0400

    Update new ui modal

---
+++ src/components/WelcomeModal/WelcomeModal.stories.tsx
@@ -0,0 +1,114 @@
+import { MockedProvider } from "@apollo/client/testing";
+import CarouselCard from "./CarouselCard";
+import WelcomeModal from "./WelcomeModal";
+
+export default {
+  title: "Welcome Modal",
+  component: WelcomeModal,
+};
+
+export const NewSpruceUser = () => (
+  <WelcomeModal
+    title="Welcome to the New Evergreen UI!"
+    param="hasUsedSpruceBefore"
+    carouselCards={[
+      {
+        img: "welcome_modal/mypatch_gif_06_10.gif",
+        subtitle: "Discover your new and improved patches workflow!",
+        description:
+          "We&apos;ve made your patches workflow better by adding more filtering options, reducing load times, and improving the design.",
+      },
+      {
+        img: "welcome_modal/patch_gif_06_10.gif",
+        subtitle: "We&apos;ve also updated the patch page!",
+        description:
+          "We&apos;ve made it easier to navigate through your tasks and find the information you&apos;re looking for.",
+        href: "https://evergreen.mongodb.com/patch/",
+      },
+      {
+        img: "welcome_modal/newui_gif_06_10.gif",
+        subtitle:
+          "We really hope you enjoy the new UI, but just in case you miss the old Evergreen…",
+        description:
+          "We&apos;re still working every day to make this better, adding new features and new pages all the time. In case you want to opt out of the new UI and miss all the updates... navigate to your preferences to do so.",
+      },
+    ]}
+  />
+);
+
+export const NewMainlineCommitsUser = () => (
+  <MockedProvider>
+    <WelcomeModal
+      param="hasUsedMainlineCommitsBefore"
+      carouselCards={[
+        {
+          img: "mainline_commits/carousel_1_05_4.png",
+          subtitle: "Welcome to the new mainline commits experience!",
+          description:
+            "We designed this UI to make navigation faster and more intuitive. By default it displays failed tasks, but you can customize this view with filters.",
+          href:
+            "https://app.tango.us/app/workflow/Evergreen--Onboarding-guide-for-the-new-project-health-page--7b74b28c80f448869a01730a450bc246",
+        },
+        {
+          movie: "mainline_commits/carousel_2_05_4.webm",
+          subtitle: "Get a quick overview of your project&apos;s health",
+          description:
+            "A graph at the top of the page gives you an overview of the project&apos;s health. You can toggle the graph between an absolute number  and percentage.",
+          href: "https://evergreen.mongodb.com/patch/",
+        },
+        {
+          movie: "mainline_commits/carousel_3_05_4.webm",
+          subtitle: "Newly designed icons",
+          description:
+            "New status icons make it easier to differentiate between task statuses. You can find an expandable legend explaining what each icon represents.",
+          href:
+            "https://app.tango.us/app/workflow/Status-icon-behavior--1db9909b454f4800b05774fa408f2924",
+        },
+        {
+          movie: "mainline_commits/carousel_4_05_4.webm",
+          subtitle: "New way to recognize patterns",
+          description:
+            "Identifying the same task across multiple build variants is an important part of the workflow. When you hover over a task, we will display the name of the task and highlight that task across other commits.",
+          href:
+            "https://app.tango.us/app/workflow/Status-icon-behavior--1db9909b454f4800b05774fa408f2924",
+        },
+        {
+          movie: "mainline_commits/carousel_5_05_4.webm",
+          subtitle: "Improved task history experience",
+          description:
+            "You can get to the task history from the task details page by clicking on the see history button at the top of the page. You can also filter for specific test results by clicking see history on a test result. ",
+          href:
+            "https://app.tango.us/app/workflow/Task-History--23e6b3f043234a19988d6ab0a0729598",
+        },
+        {
+          img: "mainline_commits/carousel_6_05_4.png",
+          subtitle: "New Build Variant History page",
+          description:
+            "Selecting a Build Variant will direct you to the Build Variant History page, which shows tasks across a build variant over time.",
+          href:
+            "https://app.tango.us/app/workflow/Variant-History-fa73d48662f24e48842fc315130c483f",
+        },
+      ]}
+    />
+  </MockedProvider>
+);
+
+export const MovieCard = () => (
+  <MockedProvider>
+    <CarouselCard
+      movie="mainline_commits/carousel_2_05_4.webm"
+      subtitle="A Subtitle"
+      description="A description"
+    />
+  </MockedProvider>
+);
+
+export const ImageCard = () => (
+  <MockedProvider>
+    <CarouselCard
+      img="welcome_modal/mypatch_gif_06_10.gif"
+      subtitle="A Subtitle"
+      description="A description"
+    />
+  </MockedProvider>
+);
