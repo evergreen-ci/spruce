94a7b27641ab4ceb7e6648cf167e4bdb20fdd2c3
Chaya Malik <chaya.malik@10gen.com>
2022-05-04 18:49:59 -0400

    EVG-15089  Create the Event Log Page (#1232)

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -1036,6 +1036,7 @@ export type UpdateVolumeInput = {
 export type IssueLinkInput = {
   url: Scalars["String"];
   issueKey: Scalars["String"];
+  confidenceScore?: Maybe<Scalars["Float"]>;
 };
 
 export type SortOrder = {
@@ -2050,6 +2051,7 @@ export type IssueLink = {
   url?: Maybe<Scalars["String"]>;
   source?: Maybe<Source>;
   jiraTicket?: Maybe<JiraTicket>;
+  confidenceScore?: Maybe<Scalars["Float"]>;
 };
 
 export type Source = {
@@ -2501,7 +2503,6 @@ export type RepoPluginsSettingsFragment = {
 export type ProjectEventSettingsFragment = {
   projectRef?: Maybe<
     {
-      id: string;
       identifier: string;
       repoRefId: string;
       versionControlEnabled?: Maybe<boolean>;
@@ -2524,7 +2525,6 @@ export type ProjectEventSettingsFragment = {
 export type RepoEventSettingsFragment = {
   projectRef?: Maybe<
     {
-      id: string;
       displayName: string;
       versionControlEnabled: boolean;
       tracksPushEvents: boolean;
