2d96d5f228ffabf67093e64e344fb4fd057ef597
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-05-09 18:39:26 -0400

    Remove toast requirement from user settings hook

--- src/hooks/useUserSettings.ts
+++ src/hooks/useUserSettings.ts
@@ -1,21 +1,21 @@
 import { useQuery } from "@apollo/client";
-import { useToastContext } from "context/toast";
 import {
   GetUserSettingsQuery,
   GetUserSettingsQueryVariables,
 } from "gql/generated/types";
 import { GET_USER_SETTINGS } from "gql/queries";
 
-export const useUserSettings = () => {
-  const dispatchToast = useToastContext();
+type UseUserSettingsOptions = {
+  onError?: (error: Error) => void;
+};
+
+export const useUserSettings = (options?: UseUserSettingsOptions) => {
   const { data, loading } = useQuery<
     GetUserSettingsQuery,
     GetUserSettingsQueryVariables
   >(GET_USER_SETTINGS, {
     onError(err) {
-      dispatchToast.error(
-        `There was an error fetching your user settings: ${err.message}`
-      );
+      options?.onError?.(err);
     },
   });
   const { userSettings } = data || {};
