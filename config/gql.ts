63f41cdb74107e7c7c00645d60ed029d7f9161e1
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-06-06 17:31:18 -0400

    lint

--- config/gql.ts
+++ config/gql.ts
@@ -9,31 +9,34 @@ function getCode(schema: string) {
 }
 
 function transform(src: string, id: string) {
-  if (id.endsWith('.graphql') || id.endsWith('.gql')) {
-
-    src = findAndReplaceFragments(src, id);
+  if (id.endsWith(".graphql") || id.endsWith(".gql")) {
+    const transformedQueries = findAndReplaceFragments(src, id);
     return {
-      code: getCode(JSON.stringify(src)),
-      map: null
+      code: getCode(JSON.stringify(transformedQueries)),
+      map: null,
     };
   }
 }
 
-const findAndReplaceFragments = (fragment:string, relativePath:string) => {
-    const matches = fragment.match(/#import ".+"/gm);
-    if(matches) {
-        matches.forEach(match => {
-            const importPath = match.replace(/#import "(.+)"/, '$1');
-            // get absolute path
-            const absolutePath = path.resolve(path.dirname(relativePath), importPath);
-            // read file
-            const importCode = fs.readFileSync(absolutePath).toString();
-            // recursively navigate through fragments and replace them
-            fragment = fragment.replace(match, findAndReplaceFragments(importCode, absolutePath));
-        })
-    }
-    return fragment;
-}
+const findAndReplaceFragments = (fragment: string, relativePath: string) => {
+  let updatedFragment = fragment;
+  const matches = updatedFragment.match(/#import ".+"/gm);
+  if (matches) {
+    matches.forEach((match) => {
+      const importPath = match.replace(/#import "(.+)"/, "$1");
+      // get absolute path
+      const absolutePath = path.resolve(path.dirname(relativePath), importPath);
+      // read file
+      const importCode = fs.readFileSync(absolutePath).toString();
+      // recursively navigate through fragments and replace them
+      updatedFragment = updatedFragment.replace(
+        match,
+        findAndReplaceFragments(importCode, absolutePath)
+      );
+    });
+  }
+  return updatedFragment;
+};
 
 /**
  * SpruceVitePluginGQL is a vite plugin that transforms graphql files and also handles processing fragments
@@ -41,7 +44,7 @@ const findAndReplaceFragments = (fragment:string, relativePath:string) => {
  */
 export default function SpruceVitePluginGQL() {
   return {
-    name: 'spruce-plugin-gql',
-    transform
+    name: "spruce-plugin-gql",
+    transform,
   };
 }
