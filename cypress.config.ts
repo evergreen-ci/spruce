bf7d2e28e68335d3d2836e7cf4e0a4c3b3dbad77
SupaJoon <arjun.patel@mongodb.com>
2022-06-15 14:14:26 -0400

    v10

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,24 +1,9 @@
 import { defineConfig } from "cypress";
 
-import webpack from "@cypress/webpack-preprocessor";
-import webpackOptions from "./cypress/plugins/webpack.config";
-
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
-    projectId: "yshv48",
-    reporterOptions: {
-      mochaFile: "bin/cypress/junit-[hash].xml",
+    setupNodeEvents() {
+      // implement node event listeners here
     },
-    defaultCommandTimeout: 15000,
-    retries: {
-      runMode: 0,
-      openMode: 0,
-    },
-    setupNodeEvents(on) {
-      console.log("00000000000000000000000000000");
-      on("file:preprocessor", webpack({ webpackOptions }));
-    },
-    supportFile: "cypress/support/e2e.ts",
   },
 });
