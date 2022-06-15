bf4eca229095c35e8a67e4c17e8f21dfac4a78ca
SupaJoon <arjun.patel@mongodb.com>
2022-06-15 08:43:41 -0400

    10

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "cypress";
 
+import webpack from "@cypress/webpack-preprocessor";
+import webpackOptions from "./cypress/plugins/webpack.config";
+
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
@@ -12,5 +15,10 @@ export default defineConfig({
       runMode: 0,
       openMode: 0,
     },
+    setupNodeEvents(on) {
+      console.log("00000000000000000000000000000");
+      on("file:preprocessor", webpack({ webpackOptions }));
+    },
+    supportFile: "cypress/support/e2e.ts",
   },
 });
