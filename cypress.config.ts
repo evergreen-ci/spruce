aa87827968f931b2bd538a7f8c3b645941e8bb38
SupaJoon <arjun.patel@mongodb.com>
2022-06-16 08:49:19 -0400

    vite for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "cypress";
 
-import webpack from "@cypress/webpack-preprocessor";
-import webpackOptions from "./cypress/plugins/webpack.config";
-
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
@@ -14,10 +11,6 @@ export default defineConfig({
     retries: {
       runMode: 0,
       openMode: 0,
-    },
-    setupNodeEvents(on) {
-      console.log("00000000000000000000000000000");
-      on("file:preprocessor", webpack({ webpackOptions }));
     },
     supportFile: "cypress/support/e2e.ts",
   },
