ed49691660478d7968e69c9443cd144a2eaffc98
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-06-23 12:13:19 -0400

    Use es module

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import InteractionRing from "@leafygreen-ui/interaction-ring";
 import { uiColors } from "@leafygreen-ui/palette";
-import generatePicker from "antd/lib/date-picker/generatePicker";
+import generatePicker from "antd/es/date-picker/generatePicker";
 import dateFnsGenerateConfig from "rc-picker/lib/generate/dateFns";
 import { size } from "constants/tokens";
 import "antd/es/date-picker/style/index";
