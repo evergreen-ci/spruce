669e81b698bc4ad406660161df0b8a9a6240d177
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-06-28 16:48:32 -0400

    Spacing

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -18,5 +18,5 @@ export const parameters = {
 export const decorators = [
   withRouter,
   // storybook-addon-apollo-client will not add the mockedprovider decorator in the test environment which causes snapshot tests to fail.
-  ...(isTest ?[(Story) => <MockedProvider>{<Story />}</MockedProvider>] : []),
+  ...(isTest ? [(Story) => <MockedProvider>{<Story />}</MockedProvider>] : []),
 ];
