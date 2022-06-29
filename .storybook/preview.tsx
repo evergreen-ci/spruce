449f47658142a5712a31969418f91f173620d933
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-06-29 09:54:23 -0400

    Support decorators in snapshot tests

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,9 @@
 import { MockedProvider } from "@apollo/client/testing";
+import { MemoryRouter } from "react-router-dom";
 import { withRouter } from "storybook-addon-react-router-v6";
 
 const isTest = process.env.NODE_ENV === "test";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -15,8 +17,5 @@ export const parameters = {
   },
 };
 
-export const decorators = [
-  withRouter,
-  // storybook-addon-apollo-client will not add the mockedprovider decorator in the test environment which causes snapshot tests to fail.
-  ...(isTest ? [(Story) => <MockedProvider>{<Story />}</MockedProvider>] : []),
-];
+// @storybook/addon-storyshots isn't successfully wrapping the decorators from storybbook-addon-react-router-v6 and storybook-addon-apollo-client so we need to manually wrap the stories.
+export const decorators = !isTest ?  [withRouter] : [(Story) => <MemoryRouter>{<Story />}</MemoryRouter>, (Story) => <MockedProvider>{<Story />}</MockedProvider>];
