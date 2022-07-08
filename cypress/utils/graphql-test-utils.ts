dc4a0f48c425b5a084711d2d037e366e8cc0a5ac
SupaJoon <arjun.patel@mongodb.com>
2022-07-08 17:06:14 -0400

    type + refactor

--- cypress/utils/graphql-test-utils.ts
+++ cypress/utils/graphql-test-utils.ts
@@ -1,24 +1,15 @@
+import { CyHttpMessages } from "cypress/types/net-stubbing";
+
 // Utility to match GraphQL mutation based on the operation name
-export const hasOperationName = (req, operationName) => {
+export const hasOperationName = (
+  req: CyHttpMessages.IncomingHttpRequest,
+  operationName: string
+): boolean => {
   const { body } = req;
   return (
     Object.prototype.hasOwnProperty.call(body, "operationName") &&
     body.operationName === operationName
   );
-};
-
-// Alias query if operationName matches
-export const aliasQuery = (req, operationName) => {
-  if (hasOperationName(req, operationName)) {
-    req.alias = `gql${operationName}Query`;
-  }
-};
-
-// Alias mutation if operationName matches
-export const aliasMutation = (req, operationName) => {
-  if (hasOperationName(req, operationName)) {
-    req.alias = `gql${operationName}Mutation`;
-  }
 };
 
 export const GQL_URL = "http://localhost:9090/graphql/query";
