6b97738f1b76f20e59ee28d3e9539eb36251f572
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-07-18 14:28:10 -0400

    Handle the codegen script failing

--- check_codegen.sh
+++ check_codegen.sh
@@ -1,30 +1,29 @@
 # This is a script which checks if yarn codegen was run it used by the check_codegen evergreen task
 
-# save types file variable
+# path to generated types
 types_file="src/gql/generated/types.ts"
 {
   BEFORE=$(git status --porcelain | grep "$types_file")
   yarn codegen
   AFTER=$(git status --porcelain | grep "$types_file")
-  if [ "$BEFORE" != "$AFTER" ]; then
-    echo "$types_file is not up to date"
-    echo "did you forget to run 'yarn codegen'?"
-    if [ -d "bin" ] 
-    then
-        echo "Directory /bin exists." 
-    else
-        echo "Directory /bin does not exist."
-        mkdir bin
-    fi
-    git diff $types_file >> bin/codegen.diff
-    exit 1
-  else
-    echo "$types_file is up to date"
-    exit 0
-  fi
-
 } || {
   echo "script failed"
   exit 1
 }
+
+if [ "$BEFORE" != "$AFTER" ]; then
+  echo "$types_file is not up to date"
+  echo "did you forget to run 'yarn codegen'?"
+  # Check if bin directory exists so we can save the diff to it
+  if [ -d "bin" ] 
+  then
+      echo "Directory /bin already exists." 
+  else
+      mkdir bin
+  fi
+  git diff $types_file >> bin/codegen.diff
+  exit 1
+else
+  echo "$types_file is up to date"
+fi
 
