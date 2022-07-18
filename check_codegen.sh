32b1f4e95fcd4e2e63d23a024b2aacd83104c35b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-07-18 13:33:20 -0400

    Save codegen diff to evergreen task

--- check_codegen.sh
+++ check_codegen.sh
@@ -3,10 +3,12 @@ BEFORE=$(git status --porcelain | grep src/gql/generated/types.ts)
 yarn codegen
 AFTER=$(git status --porcelain | grep src/gql/generated/types.ts)
 if [ "$BEFORE" != "$AFTER" ]; then
-  echo "src/gql/types.ts is not up to date"
+  echo "src/gql/generated/types.ts is not up to date"
   echo "did you forget to run 'yarn codegen'?"
+  mkdir tmp
+  git diff >> tmp/codegen.diff
   exit 1
 else
-  echo "src/gql/types.ts is up to date"
+  echo "src/gql/generated/types.ts is up to date"
   exit 0
 fi
