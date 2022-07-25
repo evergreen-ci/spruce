321db0bd4b1817ce52a0dbd9e335339ee210dc13
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-07-25 10:58:27 -0400

    EVG-17326 Skunkworks - Save codegen diff to evergreen task (#1373)

--- check_codegen.sh
+++ check_codegen.sh
@@ -1,12 +1,27 @@
 # This is a script which checks if yarn codegen was run it used by the check_codegen evergreen task
-BEFORE=$(git status --porcelain | grep src/gql/generated/types.ts)
-yarn codegen
-AFTER=$(git status --porcelain | grep src/gql/generated/types.ts)
+
+# path to generated types
+types_file="src/gql/generated/types.ts"
+BEFORE=$(git status --porcelain | grep "$types_file")
+yarn codegen || {
+  echo "yarn codegen failed to run"
+  exit 1
+}
+AFTER=$(git status --porcelain | grep "$types_file")
+
 if [ "$BEFORE" != "$AFTER" ]; then
-  echo "src/gql/types.ts is not up to date"
+  echo "$types_file is not up to date"
   echo "did you forget to run 'yarn codegen'?"
+  # Check if bin directory exists so we can save the diff to it
+  if [ -d "bin" ] 
+  then
+      echo "Directory /bin already exists." 
+  else
+      mkdir bin
+  fi
+  git diff $types_file >> bin/codegen.diff
   exit 1
 else
-  echo "src/gql/types.ts is up to date"
-  exit 0
+  echo "$types_file is up to date"
 fi
+
