ea71bad6e27d116209868345234e2a76635973a3
Sophie Stadler <sophie.stadler@mongodb.com>
2022-08-01 12:54:21 -0400

    Remove identifier state

--- src/hooks/useLegacyUIURL.ts
+++ src/hooks/useLegacyUIURL.ts
@@ -7,7 +7,6 @@ import { environmentalVariables } from "utils";
 const { getUiUrl } = environmentalVariables;
 export const useLegacyUIURL = (): string | null => {
   const [id, setId] = useState("");
-  const [identifier, setIdentifier] = useState("");
   const [legacyUIUrl, setLegacyUIUrl] = useState(null);
   const { pathname } = useLocation();
 
@@ -22,19 +21,21 @@ export const useLegacyUIURL = (): string | null => {
       [routes.spawnHost]: `${uiURL}/spawn#?resourcetype=hosts`,
       [routes.spawnVolume]: `${uiURL}/spawn#?resourcetype=volumes`,
       [`${routes.commits}/:id`]: `${uiURL}/waterfall/${id}`,
-      [`${routes.projectSettings}/*`]: `${uiURL}/projects##${identifier}`,
+      [`${routes.projectSettings}/*`]: `${uiURL}/projects##${id}`,
     };
     const legacyUIKeys = Object.keys(legacyUIMap);
     for (let i = 0; i < legacyUIKeys.length; i++) {
       const matchedPath = matchPath(legacyUIKeys[i], pathname);
       if (matchedPath !== null) {
-        setId(get(matchedPath, "params.id", ""));
-        setIdentifier(get(matchedPath, "params.identifier", ""));
+        setId(
+          get(matchedPath, "params.id", "") ||
+            get(matchedPath, "params.identifier", "")
+        );
         setLegacyUIUrl(legacyUIMap[legacyUIKeys[i]]);
         break;
       }
     }
-  }, [id, identifier, pathname, uiURL]);
+  }, [id, pathname, uiURL]);
 
   return legacyUIUrl;
 };
