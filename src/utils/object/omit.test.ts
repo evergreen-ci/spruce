ca7458223599b942d341c9e82fe48f8ce1f0907e
Arjun Patel <arjun453@gmail.com>
2022-08-31 13:57:51 -0400

    wip

--- src/utils/object/omit.test.ts
+++ src/utils/object/omit.test.ts
@@ -2,14 +2,13 @@ import { omit } from "utils/object";
 
 describe("omit", () => {
   it("returns a new object without the supplied key", () => {
-    expect(omit<{ b?: number }, ["b"]>({}, ["b"])).toMatchObject({
-      a: 1,
-    });
+    expect(omit<{ b?: number }, ["b"]>({}, ["b"])).toMatchObject({});
     expect(omit({ a: 1 }, ["a"])).toMatchObject({});
   });
   it("returns a new object reference even when the input and outupt object have deep equality", () => {
     const inputObj = {};
     const resultObj = omit<{ b?: number }, ["b"]>(inputObj, ["b"]);
     expect(resultObj).toMatchObject(inputObj);
+    expect(resultObj).not.toBe(inputObj);
   });
 });
