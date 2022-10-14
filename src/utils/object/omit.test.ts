178650a675a1629d2b685917dee47601abaac400
Arjun Patel <arjun453@gmail.com>
2022-10-14 10:10:32 -0400

    update test

--- src/utils/object/omit.test.ts
+++ src/utils/object/omit.test.ts
@@ -2,14 +2,15 @@ import { omit } from "utils/object";
 
 describe("omit", () => {
   it("returns an object without the supplied key(s)", () => {
-    expect(omit({} as { a?: number }, ["a"])).toMatchObject({});
+    const inputObj: { a?: number } = {};
+    expect(omit(inputObj, ["a"])).toMatchObject({});
     expect(omit({ a: 1 }, ["a"])).toMatchObject({});
     expect(omit({ a: 1, b: 1 }, ["b"])).toMatchObject({ a: 1 });
     expect(omit({ a: 1, b: 1, c: 1 }, ["b", "c"])).toMatchObject({ a: 1 });
   });
   it("returns a new object reference", () => {
-    const inputObj = {};
-    const resultObj = omit<{ b?: number }, ["b"]>(inputObj, ["b"]);
+    const inputObj: { b?: number } = {};
+    const resultObj = omit(inputObj, ["b"]);
     expect(resultObj).toMatchObject(inputObj);
     expect(resultObj).not.toBe(inputObj);
   });
