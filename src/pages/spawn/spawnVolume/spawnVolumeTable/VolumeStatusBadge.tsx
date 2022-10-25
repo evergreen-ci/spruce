06ca6756d92968ee389db86fb0801209d68ecb07
Sophie Stadler <sophstad@users.noreply.github.com>
2022-10-25 15:41:03 -0400

    EVG-17489: Display migrating badge (#1472)

--- src/pages/spawn/spawnVolume/spawnVolumeTable/VolumeStatusBadge.tsx
+++ src/pages/spawn/spawnVolume/spawnVolumeTable/VolumeStatusBadge.tsx
@@ -1,15 +1,39 @@
-import Badge, { Variant } from "components/Badge";
+import Badge, { Variant } from "@leafygreen-ui/badge";
 import { MyVolume } from "types/spawn";
 
 interface Props {
   volume: MyVolume;
 }
 
-export const VolumeStatusBadge = ({ volume }: Props) => (
-  <Badge
-    data-cy="volume-status-badge"
-    variant={volume.hostID ? Variant.Green : Variant.Blue}
-  >
-    {volume.hostID ? "Mounted" : "Free"}
-  </Badge>
-);
+export const VolumeStatusBadge = ({ volume }: Props) => {
+  const { text, variant } = getBadge(volume.migrating, volume.hostID);
+  return (
+    <Badge data-cy="volume-status-badge" variant={variant}>
+      {text}
+    </Badge>
+  );
+};
+
+const getBadge = (
+  migrating: boolean,
+  hostID?: string
+): { text: string; variant: Variant } => {
+  if (migrating) {
+    return {
+      text: "Migrating",
+      variant: Variant.Yellow,
+    };
+  }
+
+  if (hostID) {
+    return {
+      text: "Mounted",
+      variant: Variant.Green,
+    };
+  }
+
+  return {
+    text: "Free",
+    variant: Variant.Blue,
+  };
+};
