fa41ed3bec928c2eca2cdfc6a3c2595eba930d0e
Arjun Patel <arjun453@gmail.com>
2022-11-01 12:02:05 -0400

    type props

--- src/components/Spawn/spawnHostModal/useVirtualWorkstationDefaultExpiration.ts
+++ src/components/Spawn/spawnHostModal/useVirtualWorkstationDefaultExpiration.ts
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { usePrevious } from "hooks";
 import { getDefaultExpiration } from "./getFormSchema";
+import { FormState } from "./types";
 
+interface Props {
+  setFormState: React.Dispatch<React.SetStateAction<FormState>>;
+  formState: FormState;
+  disableExpirationCheckbox: boolean;
+}
 export const useVirtualWorkstationDefaultExpiration = ({
   setFormState,
   formState,
   disableExpirationCheckbox,
-}) => {
+}: Props) => {
   const isVirtualWorkstation = !!formState?.distro?.isVirtualWorkstation;
   // Default virtual workstations to unexpirable upon selection if possible
   const prevIsVirtualWorkStation = usePrevious(isVirtualWorkstation);
