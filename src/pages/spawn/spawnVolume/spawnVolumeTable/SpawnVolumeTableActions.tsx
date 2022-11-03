10c0db3391d57e98f2f352d032f9f0628d16885b
Arjun Patel <arjun453@gmail.com>
2022-11-03 12:40:09 -0400

    simplify condition

--- src/pages/spawn/spawnVolume/spawnVolumeTable/SpawnVolumeTableActions.tsx
+++ src/pages/spawn/spawnVolume/spawnVolumeTable/SpawnVolumeTableActions.tsx
@@ -1,8 +1,5 @@
-import { useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
 import { size } from "constants/tokens";
-import { MyHostsQuery, MyHostsQueryVariables } from "gql/generated/types";
-import { GET_MY_HOSTS } from "gql/queries";
 import { MyVolume } from "types/spawn";
 import { DeleteVolumeBtn } from "./spawnVolumeTableActions/DeleteVolumeBtn";
 import { EditButton } from "./spawnVolumeTableActions/EditButton";
@@ -15,41 +12,22 @@ interface Props {
 }
 
 export const SpawnVolumeTableActions: React.VFC<Props> = ({ volume }) => {
-  const { data: myHostsData } = useQuery<MyHostsQuery, MyHostsQueryVariables>(
-    GET_MY_HOSTS
-  );
-  const myHosts = myHostsData?.myHosts ?? [];
-  // Show the migrate button if the volume is a home volume and mounted to a virtual workstation
-  const showMigrateBtn = !!myHosts?.find(
-    (h) => h.homeVolumeID === volume.id && h.distro?.isVirtualWorkStation
-  );
+  const { homeVolume, host, id, displayName } = volume;
   return (
     <FlexRow
       onClick={(e) => {
         e.stopPropagation();
       }}
     >
-      <DeleteVolumeBtn
-        data-cy={`trash-${volume.displayName || volume.id}`}
-        volume={volume}
-      />
-      {showMigrateBtn && <MigrateBtn volume={volume} />}
-      {volume.host && !showMigrateBtn && (
-        <UnmountBtn
-          data-cy={`unmount-${volume.displayName || volume.id}`}
-          volume={volume}
-        />
+      <DeleteVolumeBtn data-cy={`trash-${displayName || id}`} volume={volume} />
+      {homeVolume && <MigrateBtn volume={volume} />}
+      {host && !homeVolume && (
+        <UnmountBtn data-cy={`unmount-${displayName || id}`} volume={volume} />
       )}
-      {!volume.host && !showMigrateBtn && (
-        <MountBtn
-          data-cy={`mount-${volume.displayName || volume.id}`}
-          volume={volume}
-        />
+      {!host && !homeVolume && (
+        <MountBtn data-cy={`mount-${displayName || id}`} volume={volume} />
       )}
-      <EditButton
-        data-cy={`edit-${volume.displayName || volume.id}`}
-        volume={volume}
-      />
+      <EditButton data-cy={`edit-${displayName || id}`} volume={volume} />
     </FlexRow>
   );
 };
