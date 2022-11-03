04cd5a0ad4bed75ca037104cf97cd170c721faf9
Sophie Stadler <sophstad@users.noreply.github.com>
2022-11-03 11:55:20 -0400

    EVG-18154: Upgrade tabs (#1486)

--- src/components/styles/StyledTabs.tsx
+++ src/components/styles/StyledTabs.tsx
@@ -1,23 +1,10 @@
 import styled from "@emotion/styled";
-import { Tabs, Tab } from "@leafygreen-ui/tabs";
+import { Tabs } from "@leafygreen-ui/tabs";
+import { size } from "constants/tokens";
+import { TabsType } from "types/leafygreen";
 
-type StyledTabsProps = React.ComponentProps<typeof Tabs>;
-export const StyledTabs: React.VFC<StyledTabsProps> = ({
-  children,
-  ...rest
-}) => (
-  <Tabs {...rest}>
-    {children.map(
-      (c) =>
-        c && (
-          <Tab {...c.props} key={`styled_tab_${c.key || c.props.name}`}>
-            <PaddedContainer>{c.props.children}</PaddedContainer>
-          </Tab>
-        )
-    )}
-  </Tabs>
-);
-
-const PaddedContainer = styled.div`
-  margin-top: 12px;
+export const StyledTabs = styled<TabsType>(Tabs)`
+  [role="tablist"] {
+    margin-bottom: ${size.s};
+  }
 `;
