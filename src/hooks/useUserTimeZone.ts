2571639c8a47cdde215c545b39be6ca83de966ad
SupaJoon <arjun453@gmail.com>
2022-11-14 15:13:27 -0500

    EVG-18035: Disable setting spawn host expiration beyond 30 days (#1507)

--- src/hooks/useUserTimeZone.ts
+++ src/hooks/useUserTimeZone.ts
@@ -4,9 +4,6 @@ import { GET_USER_SETTINGS } from "gql/queries";
 
 // get the timezone for the user
 export const useUserTimeZone = () => {
-  const { data: userSettingsData } =
-    useQuery<GetUserSettingsQuery>(GET_USER_SETTINGS);
-  const { userSettings } = userSettingsData || {};
-  const { timezone } = userSettings || {};
-  return timezone;
+  const { data } = useQuery<GetUserSettingsQuery>(GET_USER_SETTINGS);
+  return data?.userSettings?.timezone;
 };
