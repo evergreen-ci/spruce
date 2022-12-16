00fe67d7e0cd9ba19f3f092b18eaaa60bdb1f12f
Sophie Stadler <sophie.stadler@mongodb.com>
2022-12-16 13:15:56 -0500

    EVG-15703: Restart only failed execution tasks (#1582)

--- cypress/integration/task/task_actions.ts
+++ cypress/integration/task/task_actions.ts
@@ -57,6 +57,14 @@ describe("Task Action Buttons", () => {
       cy.validateToast("success", "Priority for task updated to 0");
     });
   });
+
+  describe("restarting a display task", () => {
+    it("does not allow restarting only failed execution tasks when all execution tasks were successful", () => {
+      cy.visit(tasks[4]);
+      cy.dataCy("restart-task").click();
+      cy.validateToast("success", restartSuccessBannerText);
+    });
+  });
 });
 
 const prioritySuccessBannerText = "Priority for task updated to 99";
@@ -67,4 +75,5 @@ const tasks = {
   1: "/task/clone_evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48",
   2: "/task/evergreen_lint_lint_service_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48",
   3: "/task/evergreen_ubuntu1604_dist_patch_33016573166a36bd5f46b4111151899d5c4e95b1_5ecedafb562343215a7ff297_20_05_27_21_39_46",
+  4: "/task/mci_ubuntu1604_display_asdf_patch_a1d2c8f70bf5c543de8b9641ac1ec08def1ddb26_5f74d99ab2373627c047c5e5_20_09_30_19_16_47/execution-tasks?execution=0&sorts=STATUS%3AASC",
 };
--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -3493,6 +3493,7 @@ export type RestartJasperMutation = { restartJasper: number };
 
 export type RestartTaskMutationVariables = Exact<{
   taskId: Scalars["String"];
+  failedOnly: Scalars["Boolean"];
 }>;
 
 export type RestartTaskMutation = {
