1869dc2569d3ba840b18320fcd6d38bc17a11f60
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-12-17 16:03:13 -0500

    Create VisbilityContainer Component

---
+++ src/components/VisibilityContainer/index.tsx
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect, useState } from "react";
+
+interface VisibilityContainerProps {
+  children: React.ReactNode;
+}
+/**
+ * `VisibilityContainer` is a component that will only render its children when it is visible in the viewport.
+ */
+const VisibilityContainer: React.VFC<VisibilityContainerProps> = ({
+  children,
+}) => {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const currentRef = containerRef.current;
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    });
+    observer.observe(currentRef);
+    return () => observer.unobserve(currentRef);
+  }, []);
+
+  return <div ref={containerRef}>{isVisible ? children : null}</div>;
+};
+
+export default VisibilityContainer;
