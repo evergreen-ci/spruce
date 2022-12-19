a282a194ee5238e4f7fc3edaddd18f65a3be7ca0
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-12-19 16:29:19 -0500

    Add tests

---
+++ src/components/VisibilityContainer/VisibilityContainer.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "test_utils";
+import VisibilityContainer from ".";
+
+describe("visibilityContainer", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should not render children when not visible in viewport", () => {
+    const mockIntersectionObserver = jest.fn((callback) => {
+      callback([
+        {
+          isIntersecting: false,
+        },
+      ]);
+      return {
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+      };
+    });
+
+    // @ts-expect-error
+    window.IntersectionObserver = mockIntersectionObserver;
+    render(
+      <VisibilityContainer>
+        <div>Visible content</div>
+      </VisibilityContainer>
+    );
+
+    expect(screen.queryByText("Visible content")).toBeNull();
+  });
+
+  it("should render children when visible in viewport", () => {
+    const mockIntersectionObserver = jest.fn((callback) => {
+      callback([
+        {
+          isIntersecting: true,
+        },
+      ]);
+      return {
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+      };
+    });
+
+    // @ts-expect-error
+    window.IntersectionObserver = mockIntersectionObserver;
+    render(
+      <VisibilityContainer>
+        <div>Visible content</div>
+      </VisibilityContainer>
+    );
+    expect(screen.getByText("Visible content")).toBeInTheDocument();
+  });
+});
