42395fb90e5b3e4171ea6058c9ce914c5dc8668b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-12-20 12:01:26 -0500

    Bigger tree

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -140,6 +140,7 @@ const SecondaryLink = styled(StyledLink)`
 `;
 
 const ChristmasTreeIcon = styled.img`
-  height: 32px;
-  width: 32px;
+  height: 46px;
+  width: 46px;
+  position: relative;
 `;
