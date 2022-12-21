7dc74dc20cb1236accbfd3f912e7f159c6439b76
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-12-21 14:15:21 -0500

    Refactor spawn host button

--- src/pages/spawn/spawnHost/SpawnHostButton.tsx
+++ src/pages/spawn/spawnHost/SpawnHostButton.tsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
 import Tooltip from "@leafygreen-ui/tooltip";
-import { useLocation } from "react-router-dom";
 import { useSpawnAnalytics } from "analytics";
 import { PlusButton } from "components/Buttons";
 import { size } from "constants/tokens";
 import { MyHostsQuery, MyHostsQueryVariables } from "gql/generated/types";
 import { GET_MY_HOSTS } from "gql/queries";
-import { useSpruceConfig, useUpdateURLQueryParams } from "hooks";
+import { useSpruceConfig } from "hooks";
+import { useQueryParam } from "hooks/useQueryParam";
 import { HostStatus } from "types/host";
-import { queryString } from "utils";
 import { QueryParams } from "../types";
 import { SpawnHostModal } from "./spawnHostButton/SpawnHostModal";
 
-const { parseQueryString } = queryString;
-
 export const SpawnHostButton: React.VFC = () => {
-  const updateQueryParams = useUpdateURLQueryParams();
   const { data: myHostsData } = useQuery<MyHostsQuery, MyHostsQueryVariables>(
     GET_MY_HOSTS
   );
+  const [spawnHostParam, setSpawnHostParam] = useQueryParam<
+    boolean | undefined
+  >(QueryParams.SpawnHost, false);
 
   const spruceConfig = useSpruceConfig();
-  const { search } = useLocation();
-  const initialOpen =
-    parseQueryString(search)[QueryParams.SpawnHost] === "True";
-  const [openModal, setOpenModal] = useState(initialOpen);
+
+  const [openModal, setOpenModal] = useState(spawnHostParam);
   const spawnAnalytics = useSpawnAnalytics();
 
   const maxHosts = spruceConfig?.spawnHost?.spawnHostsPerUser || 0;
@@ -36,11 +33,6 @@ export const SpawnHostButton: React.VFC = () => {
   );
   const currentHostCount = nonTerminatedHosts?.length || 0;
   const reachedMaxNumHosts = currentHostCount >= maxHosts;
-  useEffect(() => {
-    if (!openModal) {
-      updateQueryParams({ spawnHost: undefined });
-    }
-  }, [openModal, updateQueryParams]);
 
   return (
     <PaddedContainer>
@@ -66,7 +58,15 @@ export const SpawnHostButton: React.VFC = () => {
       >
         {`You have reached the maximum number of hosts (${maxHosts}). Delete some hosts to spawn more.`}
       </Tooltip>
-      {openModal && <SpawnHostModal open={openModal} setOpen={setOpenModal} />}
+      {openModal && (
+        <SpawnHostModal
+          open={openModal}
+          setOpen={(open) => {
+            setOpenModal(open);
+            setSpawnHostParam(undefined);
+          }}
+        />
+      )}
     </PaddedContainer>
   );
 };
