db576784c774dccd7d9a9d2e233b719f2b8fb13d
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-04 17:44:48 -0500

    Use correct app-version script

--- appVersion.sh
+++
@@ -1,4 +0,0 @@
-# This script runs appVersion.js which fetches the version number from the
-# package.json file and writes it to a env variable.
-export REACT_APP_VERSION=$(node -pe "require('./package.json').version")
-echo $REACT_APP_VERSION
--- scripts/upload-bugsnag-sourcemaps.js
+++ scripts/upload-bugsnag-sourcemaps.js
@@ -3,7 +3,7 @@ const { browser } = require("@bugsnag/source-maps");
 browser.uploadMultiple({
   apiKey: process.env.REACT_APP_BUGSNAG_API_KEY,
   appVersion: process.env.REACT_APP_VERSION,
-  detectAppVersion: true,
+  overwrite: true,
   directory: "./build/assets",
   baseUrl: `${process.env.REACT_APP_SPRUCE_URL}/assets`,
 });
