feba6703fd8c8e2d2f6d1f591883d51c73cf24a4
Arjun Patel <arjun453@gmail.com>
2023-01-27 15:55:43 -0500

    useDimension tests

---
+++ src/hooks/tests/useDimensions.test.ts
@@ -0,0 +1,133 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
+import { act, renderHook } from "@testing-library/react-hooks";
+import { useDimensions } from "hooks/useDimensions";
+
+describe("useDimensions", () => {
+  it("validate default value", () => {
+    (window as any).ResizeObserver = class ResizeObserver {};
+    const { result } = renderHook(() => useDimensions({ current: null }));
+    expect(result.current).toMatchObject({
+      width: 0,
+      height: 0,
+    });
+  });
+
+  it("synchronously sets up ResizeObserver listener", () => {
+    let listener = null;
+    (window as any).ResizeObserver = class ResizeObserver {
+      constructor(ls) {
+        listener = ls;
+      }
+
+      observe() {}
+
+      disconnect() {}
+    };
+    renderHook(() => useDimensions({ current: null }));
+    expect(typeof listener).toBe("function");
+  });
+
+  it("tracks DOM dimensions", () => {
+    let listener = null;
+    (window as any).ResizeObserver = class ResizeObserver {
+      constructor(l) {
+        listener = l;
+      }
+
+      observe() {}
+
+      disconnect() {}
+    };
+
+    const { result } = renderHook(() =>
+      useDimensions({ current: document.createElement("div") })
+    );
+
+    act(() => {
+      listener!([
+        {
+          contentRect: {
+            width: 200,
+            height: 200,
+          },
+        },
+      ]);
+    });
+
+    expect(result.current).toMatchObject({
+      width: 200,
+      height: 200,
+    });
+  });
+
+  it("tracks multiple updates", () => {
+    let listener = null;
+    (window as any).ResizeObserver = class ResizeObserver {
+      constructor(l) {
+        listener = l;
+      }
+
+      observe() {}
+
+      disconnect() {}
+    };
+
+    const { result } = renderHook(() =>
+      useDimensions({ current: document.createElement("div") })
+    );
+
+    act(() => {
+      listener!([
+        {
+          contentRect: {
+            width: 200,
+            height: 200,
+          },
+        },
+      ]);
+    });
+
+    expect(result.current).toMatchObject({
+      width: 200,
+      height: 200,
+    });
+
+    act(() => {
+      listener!([
+        {
+          contentRect: {
+            width: 100,
+            height: 100,
+          },
+        },
+      ]);
+    });
+
+    expect(result.current).toMatchObject({
+      width: 100,
+      height: 100,
+    });
+  });
+
+  it("calls .disconnect() on ResizeObserver when component unmounts", () => {
+    const spy = jest.fn();
+    (window as any).ResizeObserver = class ResizeObserver {
+      observe() {}
+
+      disconnect() {
+        spy();
+      }
+    };
+
+    const { unmount } = renderHook(() =>
+      useDimensions({ current: document.createElement("div") })
+    );
+
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    unmount();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
