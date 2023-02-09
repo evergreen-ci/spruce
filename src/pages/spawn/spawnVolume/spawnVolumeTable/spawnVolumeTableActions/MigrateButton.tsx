77577ceee70e18032ea4fbd6e3415916fcbc0853
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-02-09 13:37:47 -0500

    Migrate button to use FirstTimeGuideCue

--- src/pages/spawn/spawnVolume/spawnVolumeTable/spawnVolumeTableActions/MigrateButton.tsx
+++ src/pages/spawn/spawnVolume/spawnVolumeTable/spawnVolumeTableActions/MigrateButton.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState } from "react";
 import Button, { Size } from "@leafygreen-ui/button";
-import { GuideCue } from "@leafygreen-ui/guide-cue";
-import Cookies from "js-cookie";
+import FirstTimeGuideCue from "components/FirstTimeGuideCue";
 import { SEEN_MIGRATE_GUIDE_CUE } from "constants/cookies";
 import { TableVolume } from "types/spawn";
 import { MigrateVolumeModal } from "./migrateButton/MigrateVolumeModal";
@@ -12,26 +11,23 @@ interface Props {
 
 export const MigrateButton: React.VFC<Props> = ({ volume }) => {
   const [openModal, setOpenModal] = useState(false);
-  const [openGuideCue, setOpenGuideCue] = useState(volume.showMigrateBtnCue);
   const triggerRef = useRef(null);
+  const [openGuideCue, setOpenGuideCue] = useState(volume.showMigrateBtnCue);
   const onHideCue = () => {
-    Cookies.set(SEEN_MIGRATE_GUIDE_CUE, "true");
     setOpenGuideCue(false);
   };
   return (
     <>
-      <GuideCue
+      <FirstTimeGuideCue
         data-cy="migrate-cue"
-        open={openGuideCue}
-        setOpen={setOpenGuideCue}
+        defaultOpen={openGuideCue}
+        cookieName={SEEN_MIGRATE_GUIDE_CUE}
         title="New feature!"
         refEl={triggerRef}
         numberOfSteps={1}
         currentStep={1}
-        onPrimaryButtonClick={onHideCue}
-      >
-        You can now migrate your home volume to a new spawn host!
-      </GuideCue>
+        description="You can now migrate your home volume to a new spawn host!"
+      />
       <Button
         ref={triggerRef}
         size={Size.XSmall}
