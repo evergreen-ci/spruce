a6988f729b5b226db275654dc969452419341206
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-02-10 14:42:54 -0500

    Fix stories

--- src/components/FirstTimeGuideCue/FirstTimeGuideCue.stories.tsx
+++ src/components/FirstTimeGuideCue/FirstTimeGuideCue.stories.tsx
@@ -1,5 +1,4 @@
 import { useRef, useEffect } from "react";
-import Button from "@leafygreen-ui/button";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Cookies from "js-cookie";
 import FirstTimeGuideCue from ".";
@@ -11,28 +10,26 @@ export default {
 
 export const Default: ComponentStory<typeof FirstTimeGuideCue> = (args) => {
   const ref = useRef(null);
-
   useEffect(() => {
     Cookies.set("test", "");
   }, []);
   return (
     <div>
-      <FirstTimeGuideCue
-        numberOfSteps={1}
-        currentStep={1}
-        refEl={ref}
-        cookieName="test"
-        {...args}
-      />
-      <Button ref={ref}>Click me</Button>
+      <FirstTimeGuideCue refEl={ref} {...args} />
+      <span ref={ref}>I&apos;m an element</span>
     </div>
   );
 };
 
 Default.args = {
   title: "First Time Guide Cue",
-  description: "This is a description",
+  description: "I will not show up again if you click the close button.",
   buttonText: "Close",
   numberOfSteps: 1,
   currentStep: 1,
+  cookieName: "test",
+};
+
+Default.parameters = {
+  storyshots: { disable: true },
 };
