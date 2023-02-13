cf3b4e6eb794b3849d53472e484bcfd40106532a
Arjun Patel <arjun453@gmail.com>
2023-02-13 17:45:05 -0500

    jsdoc

--- src/hooks/useDimensions/index.ts
+++ src/hooks/useDimensions/index.ts
@@ -5,6 +5,12 @@ interface DimensionState {
   height: number;
 }
 
+/**
+ * useDimensions returns the pixel width and height of a given ref.
+ * The ref should be applied to an HTML Element.
+ * @param ref The ref of the element to track the dimensions of.
+ * @returns An object containing the current height and width of the ref.
+ */
 export const useDimensions = (ref: MutableRefObject<HTMLElement>) => {
   const [state, setState] = useState<DimensionState>({ width: 0, height: 0 });
 
