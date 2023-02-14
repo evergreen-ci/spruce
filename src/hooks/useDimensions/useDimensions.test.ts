e454b0772cdd287cd29e5952947bde904f6c2d96
Arjun Patel <arjun453@gmail.com>
2023-02-13 19:10:47 -0500

    refactor tests

--- src/hooks/useDimensions/useDimensions.test.ts
+++ src/hooks/useDimensions/useDimensions.test.ts
@@ -4,14 +4,23 @@ import { act, renderHook } from "@testing-library/react-hooks";
 import { useDimensions } from "hooks/useDimensions";
 
 describe("useDimensions", () => {
+  let listener;
+  let disconnectSpy;
+
+  beforeEach(() => {
+    disconnectSpy = jest.fn();
+    // eslint-disable-next-line jest/prefer-spy-on
+    window.ResizeObserver = jest.fn().mockImplementation((l) => {
+      listener = l;
+      return {
+        observe: () => {},
+        disconnect: disconnectSpy,
+        unobserve: () => {},
+      };
+    });
+  });
+
   it("validate default value", () => {
-    window.ResizeObserver = class ResizeObserver {
-      observe() {}
-
-      disconnect() {}
-
-      unobserve() {}
-    };
     const { result } = renderHook(() => useDimensions({ current: null }));
     expect(result.current).toMatchObject({
       width: 0,
@@ -20,36 +29,11 @@ describe("useDimensions", () => {
   });
 
   it("synchronously sets up ResizeObserver listener", () => {
-    let listener = null;
-    window.ResizeObserver = class ResizeObserver {
-      constructor(ls) {
-        listener = ls;
-      }
-
-      observe() {}
-
-      disconnect() {}
-
-      unobserve() {}
-    };
     renderHook(() => useDimensions({ current: null }));
     expect(typeof listener).toBe("function");
   });
 
   it("tracks DOM dimensions", () => {
-    let listener = null;
-    window.ResizeObserver = class ResizeObserver {
-      constructor(l) {
-        listener = l;
-      }
-
-      observe() {}
-
-      disconnect() {}
-
-      unobserve() {}
-    };
-
     const { result } = renderHook(() =>
       useDimensions({ current: document.createElement("div") })
     );
@@ -72,19 +56,6 @@ describe("useDimensions", () => {
   });
 
   it("tracks multiple updates", () => {
-    let listener = null;
-    window.ResizeObserver = class ResizeObserver {
-      constructor(l) {
-        listener = l;
-      }
-
-      observe() {}
-
-      disconnect() {}
-
-      unobserve() {}
-    };
-
     const { result } = renderHook(() =>
       useDimensions({ current: document.createElement("div") })
     );
@@ -123,25 +94,14 @@ describe("useDimensions", () => {
   });
 
   it("calls .disconnect() on ResizeObserver when component unmounts", () => {
-    const spy = jest.fn();
-    window.ResizeObserver = class ResizeObserver {
-      observe() {}
-
-      disconnect() {
-        spy();
-      }
-
-      unobserve() {}
-    };
-
     const { unmount } = renderHook(() =>
       useDimensions({ current: document.createElement("div") })
     );
 
-    expect(spy).toHaveBeenCalledTimes(0);
+    expect(disconnectSpy).toHaveBeenCalledTimes(0);
 
     unmount();
 
-    expect(spy).toHaveBeenCalledTimes(1);
+    expect(disconnectSpy).toHaveBeenCalledTimes(1);
   });
 });
