b00e32a078b2675811a82e90560ce62c7c780522
bynn <bynn.lee@mongodb.com>
2023-02-13 23:56:17 -0500

    codegen

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -480,25 +480,6 @@ export type LogMessage = {
   version?: Maybe<Scalars["Int"]>;
 };
 
-export type LogkeeperBuild = {
-  buildNum: Scalars["Int"];
-  builder: Scalars["String"];
-  id: Scalars["String"];
-  taskExecution: Scalars["Int"];
-  taskId: Scalars["String"];
-  tests: Array<LogkeeperTest>;
-};
-
-export type LogkeeperTest = {
-  buildId: Scalars["String"];
-  command: Scalars["String"];
-  id: Scalars["String"];
-  name: Scalars["String"];
-  phase: Scalars["String"];
-  taskExecution: Scalars["Int"];
-  taskId: Scalars["String"];
-};
-
 export type MainlineCommitVersion = {
   rolledUpVersions?: Maybe<Array<Version>>;
   version?: Maybe<Version>;
@@ -1259,7 +1240,6 @@ export type Query = {
   hostEvents: HostEvents;
   hosts: HostsResponse;
   instanceTypes: Array<Scalars["String"]>;
-  logkeeperBuildMetadata: LogkeeperBuild;
   mainlineCommits?: Maybe<MainlineCommits>;
   myHosts: Array<Host>;
   myPublicKeys: Array<PublicKey>;
@@ -1275,6 +1255,8 @@ export type Query = {
   subnetAvailabilityZones: Array<Scalars["String"]>;
   task?: Maybe<Task>;
   taskAllExecutions: Array<Task>;
+  /** @deprecated taskFiles is deprecated. Use task.taskFiles instead. */
+  taskFiles: TaskFiles;
   taskLogs: TaskLogs;
   taskNamesForBuildVariant?: Maybe<Array<Scalars["String"]>>;
   taskQueueDistros: Array<TaskQueueDistro>;
@@ -1344,10 +1326,6 @@ export type QueryHostsArgs = {
   statuses?: InputMaybe<Array<Scalars["String"]>>;
 };
 
-export type QueryLogkeeperBuildMetadataArgs = {
-  buildId: Scalars["String"];
-};
-
 export type QueryMainlineCommitsArgs = {
   buildVariantOptions?: InputMaybe<BuildVariantOptions>;
   options: MainlineCommitsOptions;
@@ -1387,6 +1365,11 @@ export type QueryTaskArgs = {
 };
 
 export type QueryTaskAllExecutionsArgs = {
+  taskId: Scalars["String"];
+};
+
+export type QueryTaskFilesArgs = {
+  execution?: InputMaybe<Scalars["Int"]>;
   taskId: Scalars["String"];
 };
 
