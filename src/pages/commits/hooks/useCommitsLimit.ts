be809debdcbd49a7adc5c47cc8dc7ab55796660b
Arjun Patel <arjun453@gmail.com>
2023-02-15 10:54:06 -0500

    utilize useQueryParam

--- src/pages/commits/hooks/useCommitsLimit.ts
+++ src/pages/commits/hooks/useCommitsLimit.ts
@@ -1,21 +1,22 @@
 import { MutableRefObject, useEffect, useRef } from "react";
 import { useDimensions } from "hooks/useDimensions";
-import { useQueryParams } from "hooks/useQueryParam";
+import { useQueryParam } from "hooks/useQueryParam";
 import { MainlineCommitQueryParams } from "types/commits";
 
 export const useCommitsLimit = <T extends HTMLElement>(): [
   MutableRefObject<T>,
   number
 ] => {
-  const [params, setParams] = useQueryParams();
+  const [, setSkipOrderNumber] = useQueryParam(
+    MainlineCommitQueryParams.SkipOrderNumber,
+    0
+  );
   const commitsContainerRef = useRef<T>();
   const { width } = useDimensions(commitsContainerRef);
   const nextLimit = Math.max(Math.round(width / COL_WIDTH), MIN_LIMIT);
 
   useEffect(() => {
-    const nextParams = { ...params };
-    delete nextParams[MainlineCommitQueryParams.SkipOrderNumber];
-    setParams(nextParams);
+    setSkipOrderNumber(undefined);
   }, [nextLimit]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return [commitsContainerRef, nextLimit];
