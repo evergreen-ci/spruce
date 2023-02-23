0adf2d070692a1d4f4e429c05ba4c736db3cac8d
SupaJoon <arjun453@gmail.com>
2023-02-23 16:22:30 -0500

    EVG-16928: Dynamic mainline commit count (#1688)

---
+++ src/hooks/useDimensions/index.ts
@@ -0,0 +1,37 @@
+import { MutableRefObject, useLayoutEffect, useMemo, useState } from "react";
+
+interface DimensionState {
+  width: number;
+  height: number;
+}
+
+/**
+ * useDimensions returns the pixel width and height of a given ref.
+ * The ref should be applied to an HTML Element.
+ * @param ref The ref of the element to track the dimensions of.
+ * @returns An object containing the current height and width of the ref.
+ */
+export const useDimensions = (ref: MutableRefObject<HTMLElement>) => {
+  const [state, setState] = useState<DimensionState>({ width: 0, height: 0 });
+
+  const observer = useMemo(
+    () =>
+      new ResizeObserver((entries) => {
+        requestAnimationFrame(() => {
+          const { width, height } = entries[0]?.contentRect ?? {};
+          setState({ width, height });
+        });
+      }),
+    []
+  );
+
+  useLayoutEffect(() => {
+    if (!ref?.current) return;
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return state;
+};
---
+++ src/hooks/useDimensions/useDimensions.test.ts
@@ -0,0 +1,107 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+import { useDimensions } from "hooks/useDimensions";
+
+describe("useDimensions", () => {
+  let listener;
+  let disconnectSpy;
+
+  beforeEach(() => {
+    // @ts-ignore next-line
+    window.requestAnimationFrame = jest.fn((cb) => cb()); // eslint-disable-line jest/prefer-spy-on
+    disconnectSpy = jest.fn();
+    // eslint-disable-next-line jest/prefer-spy-on
+    window.ResizeObserver = jest.fn().mockImplementation((l) => {
+      listener = l;
+      return {
+        observe: () => {},
+        disconnect: disconnectSpy,
+        unobserve: () => {},
+      };
+    });
+  });
+
+  it("validate default value", () => {
+    const { result } = renderHook(() => useDimensions({ current: null }));
+    expect(result.current).toMatchObject({
+      width: 0,
+      height: 0,
+    });
+  });
+
+  it("synchronously sets up ResizeObserver listener", () => {
+    renderHook(() => useDimensions({ current: null }));
+    expect(typeof listener).toBe("function");
+  });
+
+  it("tracks DOM dimensions", () => {
+    const { result } = renderHook(() =>
+      useDimensions({ current: document.createElement("div") })
+    );
+
+    act(() => {
+      listener!([
+        {
+          contentRect: {
+            width: 200,
+            height: 200,
+          },
+        },
+      ]);
+    });
+
+    expect(result.current).toMatchObject({
+      width: 200,
+      height: 200,
+    });
+  });
+
+  it("tracks multiple updates", () => {
+    const { result } = renderHook(() =>
+      useDimensions({ current: document.createElement("div") })
+    );
+
+    act(() => {
+      listener!([
+        {
+          contentRect: {
+            width: 200,
+            height: 200,
+          },
+        },
+      ]);
+    });
+
+    expect(result.current).toMatchObject({
+      width: 200,
+      height: 200,
+    });
+
+    act(() => {
+      listener!([
+        {
+          contentRect: {
+            width: 100,
+            height: 100,
+          },
+        },
+      ]);
+    });
+
+    expect(result.current).toMatchObject({
+      width: 100,
+      height: 100,
+    });
+  });
+
+  it("calls .disconnect() on ResizeObserver when component unmounts", () => {
+    const { unmount } = renderHook(() =>
+      useDimensions({ current: document.createElement("div") })
+    );
+
+    expect(disconnectSpy).toHaveBeenCalledTimes(0);
+
+    unmount();
+
+    expect(disconnectSpy).toHaveBeenCalledTimes(1);
+  });
+});
