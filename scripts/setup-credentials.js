76cd6e49162a4ceea10298d43e29ecfd4df3e613
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-04-04 17:57:48 -0400

    Setup prod credentials

--- scripts/setup-credentials.js
+++ scripts/setup-credentials.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const file = path.join(__dirname, "../env", ".cmdrc.json");
+const file = path.join(__dirname, "../", ".env-cmdrc.json");
 const prod = {
   REACT_APP_SIGNAL_PROCESSING_URL:
     "https://performance-monitoring-and-analysis.server-tig.prod.corp.mongodb.com",
