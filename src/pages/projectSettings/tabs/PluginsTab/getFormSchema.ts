e8cc66b78028fa9f4b39576e25b3d7a1667af7f5
Arjun Patel <arjun453@gmail.com>
2023-04-25 11:08:56 -0400

    update description

--- src/pages/projectSettings/tabs/PluginsTab/getFormSchema.ts
+++ src/pages/projectSettings/tabs/PluginsTab/getFormSchema.ts
@@ -142,7 +142,7 @@ export const getFormSchema = (
             type: "object" as "object",
             title: "",
             description:
-              "Add a URL to the patch metadata panel with the corresponding display name. Include {version_id} in the URL template and it will be replaced by an actual version ID.",
+              "Add a URL to the patch metadata panel to share a custom link with anyone viewing a patch from this project. Include {version_id} in the URL template and it will be replaced by an actual version ID.",
             properties: {
               displayName: {
                 type: "string" as "string",
