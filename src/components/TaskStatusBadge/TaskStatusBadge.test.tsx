2f496c090c1e10664ad464af9a84287ff0d7994a
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-04-25 10:38:52 -0400

    EVG-14506 EVG-17919 Link task status badges to task page (#1789)

---
+++ src/components/TaskStatusBadge/TaskStatusBadge.test.tsx
@@ -0,0 +1,37 @@
+import { renderWithRouterMatch, screen } from "test_utils";
+import { TaskStatus } from "types/task";
+import TaskStatusBadge from ".";
+
+describe("taskStatusBadge", () => {
+  it("should render", () => {
+    renderWithRouterMatch(<TaskStatusBadge status="success" />);
+    expect(screen.getByDataCy("task-status-badge")).toBeInTheDocument();
+  });
+  it("should render a badge if no task id is passed", () => {
+    renderWithRouterMatch(<TaskStatusBadge status="success" />);
+    expect(screen.getByDataCy("task-status-badge")).toBeInTheDocument();
+    expect(screen.queryByRole("link")).not.toBeInTheDocument();
+  });
+  it("should render a link if a task id is passed", () => {
+    renderWithRouterMatch(
+      <TaskStatusBadge status="success" id="123" execution={0} />
+    );
+    expect(screen.getByDataCy("task-status-badge")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      "/task/123?execution=0"
+    );
+  });
+  it("should render a link to the annotations tab if the status is known issue", () => {
+    renderWithRouterMatch(
+      <TaskStatusBadge status={TaskStatus.KnownIssue} id="123" execution={0} />
+    );
+    expect(screen.getByDataCy("task-status-badge")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      "/task/123/annotations?execution=0"
+    );
+  });
+});
