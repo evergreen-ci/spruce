19cd9af4153f64bad6489e448c248c902f45f53c
Arjun Patel <arjun453@gmail.com>
2023-05-02 13:59:08 -0400

    codegen

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -2538,13 +2538,19 @@ export type WebhookInput = {
 export type WebhookSubscriber = {
   __typename?: "WebhookSubscriber";
   headers: Array<Maybe<WebhookHeader>>;
+  minDelayMs: Scalars["Int"];
+  retries: Scalars["Int"];
   secret: Scalars["String"];
+  timeoutMs: Scalars["Int"];
   url: Scalars["String"];
 };
 
 export type WebhookSubscriberInput = {
   headers: Array<InputMaybe<WebhookHeaderInput>>;
+  minDelayMs?: InputMaybe<Scalars["Int"]>;
+  retries?: InputMaybe<Scalars["Int"]>;
   secret: Scalars["String"];
+  timeoutMs?: InputMaybe<Scalars["Int"]>;
   url: Scalars["String"];
 };
 
--- src/pages/projectSettings/tabs/NotificationsTab/transformers.ts
+++ src/pages/projectSettings/tabs/NotificationsTab/transformers.ts
@@ -1,10 +1,10 @@
 import { ProjectSettingsTabRoutes } from "constants/routes";
 import { projectTriggers } from "constants/triggers";
 import {
-  Subscriber,
   ProjectInput,
   SubscriptionInput,
   BannerTheme,
+  ProjectSettingsQuery,
 } from "gql/generated/types";
 import { NotificationMethods } from "types/subscription";
 import { TriggerType } from "types/triggers";
@@ -18,7 +18,10 @@ type Tab = ProjectSettingsTabRoutes.Notifications;
 
 const { toSentenceCase } = string;
 
-const getSubscriberText = (subscriberType: string, subscriber: Subscriber) => {
+const getSubscriberText = (
+  subscriberType: string,
+  subscriber: ProjectSettingsQuery["projectSettings"]["subscriptions"][0]["subscriber"]["subscriber"]
+) => {
   switch (subscriberType) {
     case NotificationMethods.JIRA_COMMENT:
       return subscriber.jiraCommentSubscriber;
