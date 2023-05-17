a6f7dfd53cc1b1e78091c39a5e6f06224b7fc652
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-05-17 14:42:25 -0400

    Create reusable tablecontrol component

---
+++ src/components/Table/TableControl/index.tsx
@@ -0,0 +1,84 @@
+import styled from "@emotion/styled";
+import Button from "@leafygreen-ui/button";
+import PageSizeSelector, {
+  usePageSizeSelector,
+} from "components/PageSizeSelector";
+import { Pagination } from "components/Pagination";
+import { ResultCountLabel } from "components/ResultCountLabel";
+import { TableControlOuterRow, TableControlInnerRow } from "components/styles";
+import { size } from "constants/tokens";
+
+interface Props {
+  filteredCount: number;
+  totalCount: number;
+  limit: number;
+  page: number;
+  label: string;
+  onClear: () => void;
+  onPageSizeChange?: (pageSize: number) => void;
+}
+
+const TableControl: React.VFC<Props> = ({
+  filteredCount,
+  totalCount,
+  limit,
+  page,
+  label,
+  onClear,
+  onPageSizeChange,
+}) => {
+  const setPageSize = usePageSizeSelector();
+
+  const handlePageSizeChange = (pageSize: number) => {
+    setPageSize(pageSize);
+    onPageSizeChange?.(pageSize);
+  };
+  const onClearAll = () => {
+    onClear();
+  };
+
+  return (
+    <TableControlOuterRow>
+      <FlexContainer>
+        <ResultCountLabel
+          dataCyNumerator="filtered-count"
+          dataCyDenominator="total-count"
+          label={label}
+          numerator={filteredCount}
+          denominator={totalCount}
+        />
+        <PaddedButton
+          onClick={onClearAll}
+          data-cy="clear-all-filters"
+          size="small"
+        >
+          Clear All Filters
+        </PaddedButton>
+      </FlexContainer>
+      <TableControlInnerRow>
+        <Pagination
+          data-cy="tasks-table-pagination"
+          pageSize={limit}
+          value={page}
+          totalResults={filteredCount}
+        />
+        <PageSizeSelector
+          data-cy="tasks-table-page-size-selector"
+          value={limit}
+          onChange={handlePageSizeChange}
+        />
+      </TableControlInnerRow>
+    </TableControlOuterRow>
+  );
+};
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PaddedButton = styled(Button)`
+  margin-left: ${size.m};
+`;
+
+export default TableControl;
