272fe209f208c9e6f93d9d39aca2aa892270499b
Arjun Patel <arjun453@gmail.com>
2023-05-23 16:46:10 -0400

    codegen

--- src/gql/generated/types.ts
+++ src/gql/generated/types.ts
@@ -687,6 +687,7 @@ export type Mutation = {
   createPublicKey: Array<PublicKey>;
   deactivateStepbackTask: Scalars["Boolean"];
   defaultSectionToRepo?: Maybe<Scalars["String"]>;
+  deleteProject: Scalars["Boolean"];
   detachProjectFromRepo: Project;
   detachVolumeFromHost: Scalars["Boolean"];
   editAnnotationNote: Scalars["Boolean"];
@@ -784,6 +785,10 @@ export type MutationDeactivateStepbackTaskArgs = {
 export type MutationDefaultSectionToRepoArgs = {
   projectId: Scalars["String"];
   section: ProjectSettingsSection;
+};
+
+export type MutationDeleteProjectArgs = {
+  projectId: Scalars["String"];
 };
 
 export type MutationDetachProjectFromRepoArgs = {
@@ -980,11 +985,17 @@ export type Note = {
 export type Notifications = {
   __typename?: "Notifications";
   buildBreak?: Maybe<Scalars["String"]>;
+  buildBreakId?: Maybe<Scalars["String"]>;
   commitQueue?: Maybe<Scalars["String"]>;
+  commitQueueId?: Maybe<Scalars["String"]>;
   patchFinish?: Maybe<Scalars["String"]>;
+  patchFinishId?: Maybe<Scalars["String"]>;
   patchFirstFailure?: Maybe<Scalars["String"]>;
+  patchFirstFailureId?: Maybe<Scalars["String"]>;
   spawnHostExpiration?: Maybe<Scalars["String"]>;
+  spawnHostExpirationId?: Maybe<Scalars["String"]>;
   spawnHostOutcome?: Maybe<Scalars["String"]>;
+  spawnHostOutcomeId?: Maybe<Scalars["String"]>;
 };
 
 export type NotificationsInput = {
@@ -1011,6 +1022,19 @@ export type Parameter = {
 export type ParameterInput = {
   key: Scalars["String"];
   value: Scalars["String"];
+};
+
+export type ParsleyFilter = {
+  __typename?: "ParsleyFilter";
+  caseSensitive: Scalars["Boolean"];
+  exactMatch: Scalars["Boolean"];
+  expression: Scalars["String"];
+};
+
+export type ParsleyFilterInput = {
+  caseSensitive: Scalars["Boolean"];
+  exactMatch: Scalars["Boolean"];
+  expression: Scalars["String"];
 };
 
 /** Patch is a manually initiated version submitted to test local code changes. */
@@ -1222,6 +1246,7 @@ export type Project = {
   manualPrTestingEnabled?: Maybe<Scalars["Boolean"]>;
   notifyOnBuildFailure?: Maybe<Scalars["Boolean"]>;
   owner: Scalars["String"];
+  parsleyFilters?: Maybe<Array<ParsleyFilter>>;
   patchTriggerAliases?: Maybe<Array<PatchTriggerAlias>>;
   patches: Patches;
   patchingDisabled?: Maybe<Scalars["Boolean"]>;
@@ -1304,8 +1329,6 @@ export type ProjectEventSettings = {
   aliases?: Maybe<Array<ProjectAlias>>;
   githubWebhooksEnabled: Scalars["Boolean"];
   projectRef?: Maybe<Project>;
-  /** @deprecated Use subscriptions instead */
-  projectSubscriptions?: Maybe<Array<ProjectSubscription>>;
   subscriptions?: Maybe<Array<GeneralSubscription>>;
   vars?: Maybe<ProjectVars>;
 };
@@ -1351,6 +1374,7 @@ export type ProjectInput = {
   manualPrTestingEnabled?: InputMaybe<Scalars["Boolean"]>;
   notifyOnBuildFailure?: InputMaybe<Scalars["Boolean"]>;
   owner?: InputMaybe<Scalars["String"]>;
+  parsleyFilters?: InputMaybe<Array<ParsleyFilterInput>>;
   patchTriggerAliases?: InputMaybe<Array<PatchTriggerAliasInput>>;
   patchingDisabled?: InputMaybe<Scalars["Boolean"]>;
   perfEnabled?: InputMaybe<Scalars["Boolean"]>;
@@ -1377,8 +1401,6 @@ export type ProjectSettings = {
   aliases?: Maybe<Array<ProjectAlias>>;
   githubWebhooksEnabled: Scalars["Boolean"];
   projectRef?: Maybe<Project>;
-  /** @deprecated Use subscriptions instead */
-  projectSubscriptions?: Maybe<Array<ProjectSubscription>>;
   subscriptions?: Maybe<Array<GeneralSubscription>>;
   vars?: Maybe<ProjectVars>;
 };
@@ -1412,20 +1434,9 @@ export enum ProjectSettingsSection {
   Plugins = "PLUGINS",
   Triggers = "TRIGGERS",
   Variables = "VARIABLES",
+  ViewsAndFilters = "VIEWS_AND_FILTERS",
   Workstation = "WORKSTATION",
 }
-
-export type ProjectSubscription = {
-  __typename?: "ProjectSubscription";
-  id: Scalars["String"];
-  ownerType: Scalars["String"];
-  regexSelectors: Array<Selector>;
-  resourceType: Scalars["String"];
-  selectors: Array<Selector>;
-  subscriber?: Maybe<SubscriberWrapper>;
-  trigger: Scalars["String"];
-  triggerData?: Maybe<Scalars["StringMap"]>;
-};
 
 export type ProjectVars = {
   __typename?: "ProjectVars";
@@ -1724,8 +1735,6 @@ export type RepoSettings = {
   aliases?: Maybe<Array<ProjectAlias>>;
   githubWebhooksEnabled: Scalars["Boolean"];
   projectRef?: Maybe<RepoRef>;
-  /** @deprecated Use subscriptions instead */
-  projectSubscriptions?: Maybe<Array<ProjectSubscription>>;
   subscriptions?: Maybe<Array<GeneralSubscription>>;
   vars?: Maybe<ProjectVars>;
 };
