72a73085dbf2623e0d24c9facd56a3026b63e3f5
Arjun Patel <arjun453@gmail.com>
2023-05-25 12:36:53 -0400

    add space

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,6 +1,7 @@
 import LeafyGreenProvider from "@leafygreen-ui/leafygreen-provider";
 import { AuthProvider } from "context/auth";
 import { ToastProvider } from "context/toast";
+
 export const ContextProviders: React.VFC<{ children: React.ReactNode }> = ({
   children,
 }) => (
