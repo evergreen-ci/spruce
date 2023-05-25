4b80257a14b2e501357c6c6095d03bd5badd2984
Arjun Patel <arjun453@gmail.com>
2023-05-25 12:16:49 -0400

    refactor tests

--- src/utils/patch/patch.test.ts
+++ src/utils/patch/patch.test.ts
@@ -2,16 +2,22 @@ import { commitQueueAlias } from "constants/patch";
 import { isPatchUnconfigured } from ".";
 
 describe("isPatchUnconfigured", () => {
-  it("returns true if patch is unconfigured and false otherwise", () => {
+  it("returns false for an unactivated commit queue patch", () => {
     expect(
       isPatchUnconfigured({ alias: commitQueueAlias, activated: false })
     ).toBe(false);
+  });
+  it("returns false for an activated commit queue patch", () => {
     expect(
       isPatchUnconfigured({ alias: commitQueueAlias, activated: true })
     ).toBe(false);
+  });
+  it("returns true for an unactivated non-commit queue patch", () => {
     expect(
       isPatchUnconfigured({ alias: "not-commitQueueAlias", activated: false })
     ).toBe(true);
+  });
+  it("returns false for an activated non-commit queue patch", () => {
     expect(
       isPatchUnconfigured({ alias: "not-commitQueueAlias", activated: true })
     ).toBe(false);
