6a6044af63dcf0c1c69b84956da121dde18eda32
minnakt <minna.kt@mongodb.com>
2023-05-26 14:00:57 -0400

    Undo `satisfies`, as it will be done in another ticket

--- src/pages/projectSettings/tabs/ViewsAndFiltersTab/transformers.ts
+++ src/pages/projectSettings/tabs/ViewsAndFiltersTab/transformers.ts
@@ -1,16 +1,23 @@
 import { ProjectSettingsTabRoutes } from "constants/routes";
+import { ProjectSettingsQuery } from "gql/generated/types";
 import { FormToGqlFunction, GqlToFormFunction } from "../types";
+import { FormState } from "./types";
 
 type Tab = ProjectSettingsTabRoutes.ViewsAndFilters;
 
-export const gqlToForm = ((data) => {
+export const gqlToForm: GqlToFormFunction<Tab> = (
+  data: ProjectSettingsQuery["projectSettings"]
+) => {
   if (!data) return null;
 
   return {};
-}) satisfies GqlToFormFunction<Tab>;
+};
 
-export const formToGql = ((_formState, id) => ({
+export const formToGql: FormToGqlFunction<Tab> = (
+  _formState: FormState,
+  id: string
+) => ({
   projectRef: {
     id,
   },
-})) satisfies FormToGqlFunction<Tab>;
+});
