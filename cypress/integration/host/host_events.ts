05995b293a2db4357bbd8d40b620eddbd03643d9
Arjun Patel <arjun453@gmail.com>
2023-05-26 14:28:04 -0400

    update tests

--- cypress/integration/host/host_events.ts
+++ cypress/integration/host/host_events.ts
@@ -93,15 +93,15 @@ describe("Host events", () => {
       },
       {
         hostType: "host-running-task-set",
-        text: "Assigned to run task evergreen_ubuntu1604_test_command_patch_5e823e1f28...",
+        text: "Assigned to run task evergreen_ubuntu1604_test_command_patch_5e823e1f28",
       },
       {
         hostType: "host-running-task-cleared",
-        text: "Current running task cleared (was:evergreen_ubuntu1604_test_command_patch_5e823e1f28...)",
+        text: "Current running task cleared (was:evergreen_ubuntu1604_test_command_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48)",
       },
       {
         hostType: "host-task-finished",
-        text: "Task evergreen_ubuntu1604_test_command_patch_5e823e1f28... completed with status: test-timed-out",
+        text: "Task evergreen_ubuntu1604_test_command_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48 completed with status: test-timed-out",
       },
     ];
     hostTypes.forEach(({ hostType, text }) => {
