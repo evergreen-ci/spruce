0be5c95dfb0faee7b9fbb015c393ad66c2479908
Sophie Stadler <sophie.stadler@mongodb.com>
2023-05-26 13:16:13 -0400

    Change default interval when toggling from cron

--- src/pages/projectSettings/tabs/PeriodicBuildsTab/transformers.ts
+++ src/pages/projectSettings/tabs/PeriodicBuildsTab/transformers.ts
@@ -64,7 +64,7 @@ export const gqlToForm = ((
                 }
               : {
                   specifier: IntervalSpecifier.Cron,
-                  intervalHours: 0,
+                  intervalHours: null,
                   cron,
                 },
         })
