4d80ed5ce3fe8896fdb82f52ea6b447b3f716af5
annie.black <annie.black@mongodb.com>
2023-06-09 17:33:16 -0400

    cr

--- src/pages/projectSettings/tabs/PatchAliasesTab/PatchAliasesTab.tsx
+++ src/pages/projectSettings/tabs/PatchAliasesTab/PatchAliasesTab.tsx
@@ -5,12 +5,36 @@ import {
   usePopulateForm,
   useProjectSettingsContext,
 } from "pages/projectSettings/Context";
+import { PatchStatus } from "../../../../types/patch";
 import { getFormSchema } from "./getFormSchema";
 import { TabProps } from "./types";
 
 const tab = ProjectSettingsTabRoutes.PatchAliases;
 
 const getInitialFormState = (projectData, repoData) => {
+  // Resolve legacy status value -- will remove after successful deploy
+  if (projectData) {
+    for (let i = 0; i < projectData.patchTriggerAliases.aliases.length; i++) {
+      if (
+        projectData.patchTriggerAliases.aliases[i].status ===
+        PatchStatus.LegacySuccess
+      ) {
+        // eslint-disable-next-line no-param-reassign
+        projectData.patchTriggerAliases.aliases[i].status = PatchStatus.Success;
+      }
+    }
+  }
+  if (repoData) {
+    for (let i = 0; i < repoData.patchTriggerAliases.aliases.length; i++) {
+      if (
+        repoData.patchTriggerAliases.aliases[i].status ===
+        PatchStatus.LegacySuccess
+      ) {
+        // eslint-disable-next-line no-param-reassign
+        repoData.patchTriggerAliases.aliases[i].status = PatchStatus.Success;
+      }
+    }
+  }
   if (!projectData) return repoData;
   if (repoData) {
     return {
