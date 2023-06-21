8f086f2c2e3453921497ffe37d9782de22a1b74b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-06-21 15:17:01 -0400

    CLEANUP NEVER ENDS

--- src/pages/configurePatch/configurePatchCore/ConfigureTasks/index.tsx
+++ src/pages/configurePatch/configurePatchCore/ConfigureTasks/index.tsx
@@ -14,10 +14,16 @@ import {
   ChildPatchAliased,
   PatchTriggerAlias,
   VariantTasksState,
-} from "hooks/useConfigurePatch";
+} from "../useConfigurePatch/types";
 import DisabledVariantTasksList from "./DisabledVariantTasksList";
 import { TaskLayoutGrid } from "./styles";
 import { CheckboxState } from "./types";
+import {
+  deduplicateTasks,
+  getSelectAllCheckboxState,
+  getVisibleAliases,
+  getVisibleChildPatches,
+} from "./utils";
 
 interface Props {
   selectedBuildVariants: string[];
@@ -257,107 +263,6 @@ const ConfigureTasks: React.VFC<Props> = ({
       )}
     </TabContentWrapper>
   );
-};
-
-const getSelectAllCheckboxState = (
-  buildVariants: {
-    [task: string]: CheckboxState;
-  },
-  aliases: {
-    [alias: string]: CheckboxState;
-  },
-  shouldShowChildPatchTasks: boolean
-): CheckboxState => {
-  if (shouldShowChildPatchTasks) {
-    return CheckboxState.CHECKED;
-  }
-
-  let state;
-  const allTaskStatuses = Object.values(buildVariants);
-  const allAliasStatuses = Object.values(aliases);
-
-  const hasSelectedTasks =
-    allTaskStatuses.includes(CheckboxState.CHECKED) ||
-    allAliasStatuses.includes(CheckboxState.CHECKED);
-  const hasUnselectedTasks =
-    allTaskStatuses.includes(CheckboxState.UNCHECKED) ||
-    allAliasStatuses.includes(CheckboxState.UNCHECKED);
-  if (hasSelectedTasks && !hasUnselectedTasks) {
-    state = CheckboxState.CHECKED;
-  } else if (!hasSelectedTasks && hasUnselectedTasks) {
-    state = CheckboxState.UNCHECKED;
-  } else {
-    state = CheckboxState.INDETERMINATE;
-  }
-
-  return state;
-};
-
-const getVisibleAliases = (
-  selectedAliases: AliasState,
-  selectedBuildVariants: string[]
-): { [key: string]: CheckboxState } => {
-  const visiblePatches = {};
-  Object.entries(selectedAliases).forEach(([alias]) => {
-    if (selectedBuildVariants.includes(alias)) {
-      visiblePatches[alias] = selectedAliases[alias]
-        ? CheckboxState.CHECKED
-        : CheckboxState.UNCHECKED;
-    }
-  });
-  return visiblePatches;
-};
-
-const getVisibleChildPatches = (
-  childPatches: ChildPatchAliased[],
-  selectedBuildVariants: string[]
-): ChildPatchAliased[] => {
-  if (!childPatches) {
-    return [];
-  }
-
-  return childPatches.filter(({ alias }) =>
-    selectedBuildVariants.includes(alias)
-  );
-};
-
-interface DeduplicateTasksResult {
-  [task: string]: CheckboxState;
-}
-const deduplicateTasks = (
-  currentTasks: {
-    [task: string]: boolean;
-  }[]
-): DeduplicateTasksResult => {
-  const visibleTasks: DeduplicateTasksResult = {};
-  currentTasks.forEach((bv) => {
-    Object.entries(bv).forEach(([taskName, value]) => {
-      switch (visibleTasks[taskName]) {
-        case CheckboxState.UNCHECKED:
-          // If a task is UNCHECKED and the next task of the same name is CHECKED it is INDETERMINATE
-          visibleTasks[taskName] = value
-            ? CheckboxState.INDETERMINATE
-            : CheckboxState.UNCHECKED;
-          break;
-        case CheckboxState.CHECKED:
-          // If a task is CHECKED and the next task of the same name is UNCHECKED it is INDETERMINATE
-          visibleTasks[taskName] = value
-            ? CheckboxState.CHECKED
-            : CheckboxState.INDETERMINATE;
-          break;
-        case CheckboxState.INDETERMINATE:
-          // If a task is INDETERMINATE because of previous task statuses
-          // it wouldn't change when subsequent statuses are considered
-          break;
-        default:
-          visibleTasks[taskName] = value
-            ? CheckboxState.CHECKED
-            : CheckboxState.UNCHECKED;
-          break;
-      }
-    });
-  });
-  return visibleTasks;
 };
 
 const Actions = styled.div`
