7c3d1ea639ce37a3b7d839c873f9dc3e9006ac7b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-07 15:22:58 -0400

    Move build variant card files

--- src/pages/commits/ActiveCommits/BuildVariantCard/index.tsx
+++ src/pages/commits/ActiveCommits/BuildVariantCard/index.tsx
@@ -5,10 +5,13 @@ import VisibilityContainer from "components/VisibilityContainer";
 import { getVariantHistoryRoute } from "constants/routes";
 import { size } from "constants/tokens";
 import { StatusCount } from "gql/generated/types";
-import { VariantGroupedTaskStatusBadges } from "pages/commits/ActiveCommits/buildVariantCard/VariantGroupedTaskStatusBadges";
-import { TASK_ICON_PADDING } from "../constants";
-import { WaterfallTaskStatusIcon } from "./buildVariantCard/WaterfallTaskStatusIcon";
-import { injectGlobalDimStyle, removeGlobalDimStyle } from "./utils";
+import { VariantGroupedTaskStatusBadges } from "pages/commits/ActiveCommits/BuildVariantCard/VariantGroupedTaskStatusBadges";
+import {
+  injectGlobalDimStyle,
+  removeGlobalDimStyle,
+} from "pages/commits/ActiveCommits/utils";
+import { TASK_ICON_PADDING } from "pages/commits/constants";
+import { WaterfallTaskStatusIcon } from "./WaterfallTaskStatusIcon";
 
 type taskList = {
   id: string;
