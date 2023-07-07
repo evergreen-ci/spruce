0aef0f9f7a9a7f376c79962a0366ff88293c2bc6
Jonathan Brill <jonathan.brill@10gen.com>
2023-07-07 15:43:38 -0400

    Guide Cue

--- src/pages/task/metadata/index.tsx
+++ src/pages/task/metadata/index.tsx
@@ -1,7 +1,10 @@
+import { useState, useRef } from "react";
 import { ApolloError } from "@apollo/client";
 import styled from "@emotion/styled";
+import { GuideCue } from "@leafygreen-ui/guide-cue";
 import { palette } from "@leafygreen-ui/palette";
 import { InlineCode } from "@leafygreen-ui/typography";
+import Cookies from "js-cookie";
 import { Link } from "react-router-dom";
 import { useTaskAnalytics } from "analytics";
 import {
@@ -10,6 +13,7 @@ import {
   MetadataTitle,
 } from "components/MetadataCard";
 import { StyledLink, StyledRouterLink } from "components/styles";
+import { SEEN_HONEYCOMB_GUIDE_CUE } from "constants/cookies";
 import {
   getDistroPageUrl,
   getTaskTraceUrl,
@@ -96,6 +100,14 @@ export const Metadata: React.VFC<Props> = ({
   const { id: podId } = pod ?? {};
   const isContainerTask = !!podId;
   const { metadataLinks } = annotation ?? {};
+  const [openGuideCue, setOpenGuideCue] = useState(
+    Cookies.get(SEEN_HONEYCOMB_GUIDE_CUE) !== "true"
+  );
+  const triggerRef = useRef(null);
+  const onHideCue = () => {
+    Cookies.set(SEEN_HONEYCOMB_GUIDE_CUE, "true");
+    setOpenGuideCue(false);
+  };
 
   return (
     <MetadataCard error={error} loading={loading}>
@@ -345,16 +357,32 @@ export const Metadata: React.VFC<Props> = ({
       ) : null}
       <MetadataItem>
         {taskTrace && startTime && (
-          <StyledLink
-            data-cy="task-trace-link"
-            href={getTaskTraceUrl(taskTrace, startTime)}
-            onClick={() =>
-              taskAnalytics.sendEvent({ name: "Click Trace Link" })
-            }
-            hideExternalIcon={false}
-          >
-            Task Trace
-          </StyledLink>
+          <>
+            <GuideCue
+              data-cy="migrate-cue"
+              open={openGuideCue}
+              setOpen={setOpenGuideCue}
+              title="Honeycomb!"
+              refEl={triggerRef}
+              numberOfSteps={1}
+              currentStep={1}
+              onPrimaryButtonClick={onHideCue}
+            >
+              Finished tasks link to Honeycomb.
+            </GuideCue>
+            <StyledLink
+              ref={triggerRef}
+              data-cy="task-trace-link"
+              href={getTaskTraceUrl(taskTrace, startTime)}
+              onClick={() => {
+                onHideCue();
+                taskAnalytics.sendEvent({ name: "Click Trace Link" });
+              }}
+              hideExternalIcon={false}
+            >
+              Honeycomb Trace
+            </StyledLink>
+          </>
         )}
       </MetadataItem>
       <MetadataItem>
@@ -362,12 +390,13 @@ export const Metadata: React.VFC<Props> = ({
           <StyledLink
             data-cy="task-metrics-link"
             href={getTaskSystemMetricsUrl(taskId, startTime, finishTime)}
-            onClick={() =>
-              taskAnalytics.sendEvent({ name: "Click Trace Metrics Link" })
-            }
+            onClick={() => {
+              onHideCue();
+              taskAnalytics.sendEvent({ name: "Click Trace Metrics Link" });
+            }}
             hideExternalIcon={false}
           >
-            System Metrics
+            Honeycomb System Metrics
           </StyledLink>
         )}
       </MetadataItem>
