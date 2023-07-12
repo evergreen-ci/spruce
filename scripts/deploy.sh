d12beb1452a114ad975e21ca7d7faf107aeed7b7
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-12 18:00:59 -0400

    Use the currently deployed commit hash as the basis for the commit message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,17 @@
+#!/bin/sh
+
 # This script runs the aws cli command to deploy the app to s3
 # It also uploads source maps to bugsnag
 
-# Try this step and throw an error if it fails
+REACT_APP_SPRUCE_URL=$REACT_APP_SPRUCE_URL sh scripts/get-current-deployed-commit.sh
+
+# Create a file in dist/ that contains the current commit hash
+CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+echo "$CURRENT_COMMIT_HASH" > dist/commit.txt
+
+# # Try this step and throw an error if it fails
 echo "Deploying to S3"
-aws s3 sync build/ s3://${BUCKET}/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json
+aws s3 sync dist/ s3://"${BUCKET}"/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
 echo "Deployed to S3"
 
 # If the above step succeeds, run this step
--- scripts/email.sh
+++ scripts/email.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+BASE_URL=$REACT_APP_SPRUCE_URL
 # If running locally (i.e. not on CI), fetch the email from the users git config
 if [ "$CI" != 'true' ]
 then
     echo "CI deploy not detected. Using local variables instead"
     # Fetch the variables from the git config.
     AUTHOR_EMAIL=$(git config user.email)
-    REACT_APP_DEPLOYS_EMAIL=$REACT_APP_DEPLOYS_EMAIL
 fi
 
 # Validate necessary variables are set
-if [ '$REACT_APP_DEPLOYS_EMAIL' == '' ]
+if [ "$DEPLOYS_EMAIL" == '' ]
 then
-    echo "REACT_APP_DEPLOYS_EMAIL is not set"
+    echo "DEPLOYS_EMAIL is not set"
     exit 1
 fi
 
-if [ '$AUTHOR_EMAIL' == '' ]
+if [ "$AUTHOR_EMAIL" == '' ]
 then
     echo "AUTHOR_EMAIL is not set"
     exit 1
@@ -51,15 +51,41 @@ fi
 
 # Fetch previous release tag and get the commits since that tag
 CURRENT_COMMIT_HASH=$(git rev-parse --short HEAD)
-PREVIOUS_TAG=$(git describe --abbrev=0 $CURRENT_COMMIT_HASH\^)
+PREVIOUS_DEPLOYED_COMMIT=''
+PREVIOUS_TAG=''
+BASE_COMMIT=''
+
+
+# Get the previous deployed commit hash from bin/previous_deploy.txt
+# If the file does not exist, then use the previous tag from git
+if [ -f bin/previous_deploy.txt ]
+then
+  echo "Found previous_deploy.txt"
+  PREVIOUS_DEPLOYED_COMMIT=$(cat bin/previous_deploy.txt)
+  echo "Previous deployed commit: $PREVIOUS_DEPLOYED_COMMIT"
+  PREVIOUS_TAG=$(git describe --abbrev=0 "$PREVIOUS_DEPLOYED_COMMIT"^)
+else
+  echo "Could not find previous_deploy.txt"
+  echo "Falling back to previous tag from git"
+  PREVIOUS_TAG=$(git describe --abbrev=0 "$CURRENT_COMMIT_HASH"^)
+fi
 
 # If this is a revert, then only include the currently deployed commit
 if [ "$IS_REVERT" == 'true' ]
 then
-  echo "spruce-$(git rev-parse HEAD)" > body.txt
+  echo "parsley-$(git rev-parse HEAD)" > body.txt
 else
-# get all commits since the previous tag
-  git log --no-merges $PREVIOUS_TAG..$CURRENT_COMMIT_HASH --pretty="%h %s" > body.txt
+  if [ "$PREVIOUS_DEPLOYED_COMMIT" == '' ]
+  then
+    echo "Using previous tag from git"
+    BASE_COMMIT=$PREVIOUS_TAG
+  else
+    echo "Using previous deployed commit from $BASE_URL"
+    BASE_COMMIT=$PREVIOUS_DEPLOYED_COMMIT
+  fi
+  echo "Getting commits between $BASE_COMMIT and $CURRENT_COMMIT_HASH"
+  # get all commits since the base commit
+  git log --no-merges "$BASE_COMMIT".."$CURRENT_COMMIT_HASH" --pretty="%h %s" > body.txt
 fi
 
 
@@ -85,11 +111,11 @@ esac
 echo "Commits Deployed:"
 cat body.txt
 
-TITLE="Spruce Deploy to $CURRENT_COMMIT_HASH"
-BODY_HTML=$(cat body.txt)$(echo "<br /> <br /><b> To revert to previous version rerun task from previous release tag ($PREVIOUS_TAG)</b>")
+TITLE="Parsley Deploy to $CURRENT_COMMIT_HASH"
+BODY_HTML=$(cat body.txt)"<br /> <br /><b> To revert to previous version rerun task from previous release tag ($PREVIOUS_TAG)</b>"
 DATE=$(date +'%Y-%m-%d')
 
-COMMAND="$EVERGREEN $CREDENTIALS notify email -f $AUTHOR_EMAIL -r $REACT_APP_DEPLOYS_EMAIL -s "
+COMMAND="$EVERGREEN $CREDENTIALS notify email -f $AUTHOR_EMAIL -r $DEPLOYS_EMAIL -s "
 COMMAND+="'"
 COMMAND+="$DATE"
 COMMAND+=" $TITLE"
@@ -99,5 +125,5 @@ COMMAND+=" '"
 COMMAND+="$BODY_HTML"
 COMMAND+="'"
 
-echo $COMMAND
-eval $COMMAND
+echo "$COMMAND"
+eval "$COMMAND"
