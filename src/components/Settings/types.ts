6560bd268d0811ddde145a0c636da3b217053dc7
Sophie Stadler <sophie.stadler@mongodb.com>
2023-07-17 11:44:47 -0400

    Remove unused type

--- src/components/Settings/types.ts
+++ src/components/Settings/types.ts
@@ -1,10 +1,6 @@
-import {
-  TabDataProps as ProjectSettingsTabDataProps,
-  WritableTabRoutes,
-} from "pages/projectSettings/tabs/types";
+import { WritableTabRoutes } from "pages/projectSettings/tabs/types";
 
 export type SettingsRoutes = WritableTabRoutes;
-export type TabData = ProjectSettingsTabDataProps;
 export type FormToGqlFunction<T extends SettingsRoutes> = (
   form: Record<T, any>,
   id?: string
