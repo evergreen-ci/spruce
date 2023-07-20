a87163e53940d24862ccf0d150dd087dbabf4f02
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-20 10:26:10 -0400

    EVG-19643 Use the currently deployed commit hash as the basis for the deploy email (#1940)

--- scripts/deploy-utils.js
+++ scripts/deploy-utils.js
@@ -15,10 +15,10 @@ const createNewTag = () =>
     });
   });
 
-const getCommitMessages = () =>
+const getCommitMessages = (currentlyDeployedCommit) =>
   new Promise((resolve, reject) => {
     exec(
-      "git log $(git describe --tags --abbrev=0)..HEAD --oneline",
+      `git log ${currentlyDeployedCommit}..HEAD --oneline`,
       (err, stdout) => {
         if (err) {
           reject(err);
@@ -27,6 +27,35 @@ const getCommitMessages = () =>
         resolve(stdout);
       }
     );
+  });
+
+const getCurrentlyDeployedCommit = () =>
+  new Promise((resolve, reject) => {
+    exec("bash scripts/get-current-deployed-commit.sh", (err, stdout) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+        return;
+      }
+      console.log(stdout);
+      // Regex for githash
+      const githashRegex = /[a-z0-9]{40}/gm;
+      // Regex for git tag
+      const gitTagRegex = /v[0-9]+\.[0-9]+\.[0-9]+/gm;
+      const githash = stdout.match(githashRegex);
+      const gitTag = stdout.match(gitTagRegex);
+      if (githash) {
+        resolve(githash[0]);
+      } else if (gitTag) {
+        resolve(gitTag[0]);
+      } else {
+        reject(
+          new Error(
+            "Could not find a githash or git tag in the output of get-current-deployed-commit.sh"
+          )
+        );
+      }
+    });
   });
 
 const getLatestTag = () =>
@@ -101,6 +130,7 @@ module.exports = {
   createNewTag,
   deleteTag,
   getCommitMessages,
+  getCurrentlyDeployedCommit,
   getLatestTag,
   isOnMainBranch,
   isWorkingDirectoryClean,
