262219d582e4daacb370e37003d0056720052515
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-25 16:30:01 -0400

    Test email on a patch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,11 @@ CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
 echo "$CURRENT_COMMIT_HASH" > build/commit.txt
 
 # # Try this step and throw an error if it fails
-echo "Deploying to S3"
-aws s3 sync build/ s3://"${BUCKET}"/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
-echo "Deployed to S3"
+# echo "Deploying to S3"
+# aws s3 sync build/ s3://"${BUCKET}"/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
+# echo "Deployed to S3"
 
 # # If the above step succeeds, run this step
-echo "Uploading source maps to Bugsnag"
-. ./scripts/app-version.sh && node ./scripts/upload-bugsnag-sourcemaps.js
-echo "Source maps uploaded to Bugsnag"
+# echo "Uploading source maps to Bugsnag"
+# . ./scripts/app-version.sh && node ./scripts/upload-bugsnag-sourcemaps.js
+# echo "Source maps uploaded to Bugsnag"
