8c637beaeb43a9f49ace4dc68e1c047e5a213042
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-25 16:42:16 -0400

    Fix comment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,12 @@ REACT_APP_SPRUCE_URL=$REACT_APP_SPRUCE_URL sh scripts/get-current-deployed-commi
 CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
 echo "$CURRENT_COMMIT_HASH" > build/commit.txt
 
-# # Try this step and throw an error if it fails
+# Try this step and throw an error if it fails
 echo "Deploying to S3"
 aws s3 sync build/ s3://"${BUCKET}"/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
 echo "Deployed to S3"
 
-# # If the above step succeeds, run this step
+# If the above step succeeds, run this step
 echo "Uploading source maps to Bugsnag"
 . ./scripts/app-version.sh && node ./scripts/upload-bugsnag-sourcemaps.js
 echo "Source maps uploaded to Bugsnag"
