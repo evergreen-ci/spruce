252034f934b4402c7bbb4df305d8cf18be3f367b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-25 16:02:05 -0400

    Create ci deploy step

--- scripts/run-deploy.js
+++ scripts/run-deploy.js
@@ -1,8 +1,16 @@
-const { evergreenDeploy, localDeploy } = require("./deploy-production");
+const {
+  evergreenDeploy,
+  localDeploy,
+  ciDeploy,
+} = require("./deploy-production");
 
 const main = async () => {
   if (process.argv.includes("--local")) {
     await localDeploy();
+    return;
+  }
+  if (process.argv.includes("--ci")) {
+    await ciDeploy();
     return;
   }
   await evergreenDeploy();
