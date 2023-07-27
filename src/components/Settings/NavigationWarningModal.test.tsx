caf74bca3a9735b29230fe8f799acf705c2928e6
Sophie Stadler <sophie.stadler@mongodb.com>
2023-07-27 13:44:05 -0400

    EVG-19940: Add distro settings boilerplate (#1955)

---
+++ src/components/Settings/NavigationWarningModal.test.tsx
@@ -0,0 +1,134 @@
+import {
+  createMemoryRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import { act, render, screen, userEvent } from "test_utils";
+import {
+  NavigationModalProps,
+  NavigationWarningModal,
+} from "./NavigationWarningModal";
+
+const getRouter = ({ shouldBlock, unsavedTabs }: NavigationModalProps) =>
+  createMemoryRouter(
+    [
+      {
+        Component() {
+          return (
+            <div>
+              <Link to="/about">About</Link>
+              <NavigationWarningModal
+                shouldBlock={shouldBlock}
+                unsavedTabs={unsavedTabs}
+              />
+              <Outlet />
+            </div>
+          );
+        },
+        children: [
+          {
+            path: "/",
+            element: <h1>Home Page</h1>,
+          },
+          {
+            path: "/about",
+            element: <h1>About Page</h1>,
+          },
+        ],
+      },
+    ],
+    {
+      initialEntries: ["/"],
+    }
+  );
+
+describe("navigation warning", () => {
+  it("does not warn when navigating and shouldBlock is false", () => {
+    const router = getRouter({ shouldBlock: false, unsavedTabs: [] });
+
+    render(<RouterProvider router={router} />);
+    expect(router.state.location.pathname).toBe("/");
+
+    userEvent.click(screen.getByRole("link"));
+    expect(router.state.location.pathname).toBe("/about");
+    expect(
+      screen.queryByDataCy("navigation-warning-modal")
+    ).not.toBeInTheDocument();
+    expect(screen.queryByRole("heading")).toHaveTextContent("About Page");
+  });
+
+  it("warns and shows the modal with unsaved pages", () => {
+    const router = getRouter({
+      shouldBlock: true,
+      unsavedTabs: [{ title: "An Unsaved Page", value: "foo" }],
+    });
+
+    render(<RouterProvider router={router} />);
+    expect(router.state.location.pathname).toBe("/");
+
+    userEvent.click(screen.getByRole("link"));
+    expect(router.state.location.pathname).toBe("/");
+    expect(screen.getByDataCy("navigation-warning-modal")).toBeInTheDocument();
+    expect(screen.getByText("An Unsaved Page")).toBeInTheDocument();
+  });
+
+  it("warns and shows the modal with unsaved pages when a function is provided to shouldBlock", () => {
+    const router = getRouter({
+      shouldBlock: () => true,
+      unsavedTabs: [{ title: "An Unsaved Page", value: "foo" }],
+    });
+
+    render(<RouterProvider router={router} />);
+    expect(router.state.location.pathname).toBe("/");
+
+    userEvent.click(screen.getByRole("link"));
+    expect(router.state.location.pathname).toBe("/");
+    expect(screen.getByDataCy("navigation-warning-modal")).toBeInTheDocument();
+    expect(screen.getByText("An Unsaved Page")).toBeInTheDocument();
+  });
+
+  it("navigates to the next page when 'Leave' button is clicked", () => {
+    const router = getRouter({
+      shouldBlock: true,
+      unsavedTabs: [{ title: "An Unsaved Page", value: "foo" }],
+    });
+
+    render(<RouterProvider router={router} />);
+    expect(router.state.location.pathname).toBe("/");
+
+    userEvent.click(screen.getByRole("link"));
+    expect(router.state.location.pathname).toBe("/");
+    expect(screen.getByDataCy("navigation-warning-modal")).toBeInTheDocument();
+    expect(screen.getByText("An Unsaved Page")).toBeInTheDocument();
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      userEvent.click(screen.getByRole("button", { name: "Leave" }));
+    });
+    expect(router.state.location.pathname).toBe("/about");
+    expect(screen.queryByRole("heading")).toHaveTextContent("About Page");
+  });
+
+  it("remains on the initial page when 'Cancel' button is clicked", () => {
+    const router = getRouter({
+      shouldBlock: true,
+      unsavedTabs: [{ title: "An Unsaved Page", value: "foo" }],
+    });
+
+    render(<RouterProvider router={router} />);
+    expect(router.state.location.pathname).toBe("/");
+
+    userEvent.click(screen.getByRole("link"));
+    expect(router.state.location.pathname).toBe("/");
+    expect(screen.getByDataCy("navigation-warning-modal")).toBeInTheDocument();
+    expect(screen.getByText("An Unsaved Page")).toBeInTheDocument();
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      userEvent.click(screen.getByRole("button", { name: "Cancel" }));
+    });
+    expect(router.state.location.pathname).toBe("/");
+    expect(screen.queryByRole("heading")).toHaveTextContent("Home Page");
+  });
+});
