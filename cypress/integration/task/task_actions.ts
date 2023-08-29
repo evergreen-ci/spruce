6b937c7356cf443ed700f3f0fadc01c1b0787bf1
Arjun Patel <arjun453@gmail.com>
2023-08-29 12:47:25 -0400

    test success toast instead of error toast

--- cypress/integration/task/task_actions.ts
+++ cypress/integration/task/task_actions.ts
@@ -11,12 +11,12 @@ describe("Task Action Buttons", () => {
       cy.validateToast("success", restartSuccessBannerText);
     });
 
-    it("Clicking Unschedule button should unschedule a task and display an error toast", () => {
-      cy.visit(tasks[6]);
+    it("Clicking Unschedule button should unschedule a task and display a success toast", () => {
+      cy.visit(tasks[5]);
       cy.dataCy("ellipsis-btn").click();
       cy.dataCy("card-dropdown").should("be.visible");
       cy.dataCy("unschedule-task").click();
-      cy.validateToast("error", unscheduleFailureBannerText);
+      cy.validateToast("success", unscheduleSuccessBannerText);
     });
 
     it("Abort button should be disabled on completed tasks", () => {
@@ -68,14 +68,11 @@ describe("Task Action Buttons", () => {
 
 const prioritySuccessBannerText = "Priority for task updated to 99";
 const restartSuccessBannerText = "Task scheduled to restart";
-const unscheduleFailureBannerText =
-  "Error unscheduling task: updating build and version status for task 'evergreen_ubuntu1604_otter_repotracker_5e823e1f28baeaa22ae00823d83e03082cd148ab_20_02_20_20_37_06': no build 'evergreen_ubuntu1604_5e823e1f28baeaa22ae00823d83e03082cd148ab_20_02_20_20_37_06' found for task 'evergreen_ubuntu1604_otter_repotracker_5e823e1f28baeaa22ae00823d83e03082cd148ab_20_02_20_20_37_06'";
-
+const unscheduleSuccessBannerText = "Task marked as unscheduled";
 const tasks = {
   1: "/task/evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48",
   2: "/task/evergreen_lint_lint_service_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48",
   3: "/task/evergreen_ubuntu1604_dist_patch_33016573166a36bd5f46b4111151899d5c4e95b1_5ecedafb562343215a7ff297_20_05_27_21_39_46",
   4: "/task/mci_ubuntu1604_display_asdf_patch_a1d2c8f70bf5c543de8b9641ac1ec08def1ddb26_5f74d99ab2373627c047c5e5_20_09_30_19_16_47/execution-tasks?execution=0&sorts=STATUS%3AASC",
-  5: "/task/spruce_ubuntu1604_test_e695f654c8b4b959d3e12e71696c3e318bcd4c33_22_03_02_15_57_32",
-  6: "/task/evergreen_ubuntu1604_otter_repotracker_5e823e1f28baeaa22ae00823d83e03082cd148ab_20_02_20_20_37_06",
+  5: "/task/evergreen_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48",
 };
