82e239b2be148e1968ebff685af7312033fa6b42
Arjun Patel <arjun453@gmail.com>
2023-09-12 13:53:10 -0400

    typo

--- cypress/integration/version/patch_with_aborted_task.ts
+++ cypress/integration/version/patch_with_aborted_task.ts
@@ -1,9 +1,9 @@
 describe("Patch with aborted task", () => {
   it("Shows status `aborted` in task table for tasks that were aborted", () => {
     cy.visit("version/5e94c2dfe3c3312519b59480");
-    const testId =
+    const taskId =
       "mongodb_mongo_master_merge_patch_977e984bf4ed5a406da11af800c12356d0975502_5e94c2dfe3c3312519b59480_20_04_13_19_52_11";
-    cy.get(`[data-row-key=${testId}] > .cy-task-table-col-STATUS`).within(
+    cy.get(`[data-row-key=${taskId}] > .cy-task-table-col-STATUS`).within(
       () => {
         cy.dataCy("task-status-badge").should("have.text", "Aborted");
       }
