a1be30ca48a668f3d1d4441fb98c69ba594ca16b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-09-14 22:22:55 -0400

    Empty state now takes in placeholder component

--- src/components/Table/BaseTable.tsx
+++ src/components/Table/BaseTable.tsx
@@ -17,57 +17,50 @@ import { TablePlaceholder } from "./TablePlaceholder";
 type SpruceTableProps = {
   "data-cy-row"?: string;
   "data-cy-table"?: string;
-  noDataPlaceholder?: string;
-  noDataGlyph?: keyof typeof glyphs;
-  noDataSpinner?: boolean;
+  emptyComponent?: React.ReactNode;
 };
 
 export const BaseTable = <T extends LGRowData>({
   "data-cy-row": dataCyRow,
   "data-cy-table": dataCyTable,
-  noDataGlyph,
-  noDataPlaceholder = "No data found",
-  noDataSpinner = false,
+  emptyComponent,
   table,
   ...args
-}: SpruceTableProps & TableProps<T>) => (
-  <>
-    <Table data-cy={dataCyTable} table={table} {...args}>
-      <TableHead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <HeaderRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <HeaderCell key={header.id} header={header}>
-                {flexRender(
-                  header.column.columnDef.header,
-                  header.getContext()
-                )}
-              </HeaderCell>
-            ))}
-          </HeaderRow>
-        ))}
-      </TableHead>
-      <TableBody>
-        {table.getRowModel().rows.map((row) => (
-          <Row key={row.id} row={row} data-cy={dataCyRow}>
-            {row.getVisibleCells().map((cell) => (
-              <Cell key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </Cell>
-            ))}
-            {row.original.renderExpandedContent && (
-              <ExpandedContent row={row} />
-            )}
-          </Row>
-        ))}
-      </TableBody>
-    </Table>
-    {table.getRowModel().rows.length === 0 && (
-      <TablePlaceholder
-        message={noDataPlaceholder}
-        glyph={noDataGlyph}
-        spin={noDataSpinner}
-      />
-    )}
-  </>
-);
+}: SpruceTableProps & TableProps<T>) => {
+  const noDataPlaceholder = emptyComponent || "No data to display";
+  return (
+    <>
+      <Table data-cy={dataCyTable} table={table} {...args}>
+        <TableHead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <HeaderRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <HeaderCell key={header.id} header={header}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </HeaderCell>
+              ))}
+            </HeaderRow>
+          ))}
+        </TableHead>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <Row key={row.id} row={row} data-cy={dataCyRow}>
+              {row.getVisibleCells().map((cell) => (
+                <Cell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Cell>
+              ))}
+              {row.original.renderExpandedContent && (
+                <ExpandedContent row={row} />
+              )}
+            </Row>
+          ))}
+        </TableBody>
+      </Table>
+      {table.getRowModel().rows.length === 0 && { noDataPlaceholder }}
+    </>
+  );
+};
