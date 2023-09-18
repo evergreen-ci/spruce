d387abe0cf6e772e48279882d3cd7142ed439a42
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-09-18 15:19:08 -0400

    cr

--- src/components/Table/BaseTable.tsx
+++ src/components/Table/BaseTable.tsx
@@ -24,41 +24,39 @@ export const BaseTable = <T extends LGRowData>({
   emptyComponent,
   table,
   ...args
-}: SpruceTableProps & TableProps<T>) => {
-  const noDataPlaceholder = emptyComponent || "No data to display";
-  return (
-    <>
-      <Table data-cy={dataCyTable} table={table} {...args}>
-        <TableHead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <HeaderRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <HeaderCell key={header.id} header={header}>
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </HeaderCell>
-              ))}
-            </HeaderRow>
-          ))}
-        </TableHead>
-        <TableBody>
-          {table.getRowModel().rows.map((row) => (
-            <Row key={row.id} row={row} data-cy={dataCyRow}>
-              {row.getVisibleCells().map((cell) => (
-                <Cell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </Cell>
-              ))}
-              {row.original.renderExpandedContent && (
-                <ExpandedContent row={row} />
-              )}
-            </Row>
-          ))}
-        </TableBody>
-      </Table>
-      {table.getRowModel().rows.length === 0 && { noDataPlaceholder }}
-    </>
-  );
-};
+}: SpruceTableProps & TableProps<T>) => (
+  <>
+    <Table data-cy={dataCyTable} table={table} {...args}>
+      <TableHead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <HeaderRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <HeaderCell key={header.id} header={header}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              </HeaderCell>
+            ))}
+          </HeaderRow>
+        ))}
+      </TableHead>
+      <TableBody>
+        {table.getRowModel().rows.map((row) => (
+          <Row key={row.id} row={row} data-cy={dataCyRow}>
+            {row.getVisibleCells().map((cell) => (
+              <Cell key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </Cell>
+            ))}
+            {row.original.renderExpandedContent && (
+              <ExpandedContent row={row} />
+            )}
+          </Row>
+        ))}
+      </TableBody>
+    </Table>
+    {table.getRowModel().rows.length === 0 &&
+      (emptyComponent || "No data to display")}
+  </>
+);
