105a9f025bd883111904bd7f200a87d0c89ca94d
Arjun Patel <arjun453@gmail.com>
2023-09-21 10:52:56 -0400

    isolate tests

--- cypress/integration/projectSettings/project_settings.ts
+++ cypress/integration/projectSettings/project_settings.ts
@@ -386,7 +386,165 @@ describe(
       cy.dataCy("default-to-repo-button").should("not.exist");
     });
 
-    it("Shows two radio boxes", () => {
+    it("Should redact and disable private variables on saving", () => {
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").type("sample_name");
+      saveButtonEnabled(false);
+      cy.dataCy("var-value-input").type("sample_value");
+      cy.contains("label", "Private").click();
+      cy.dataCy("var-private-input").should("be.checked");
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+      cy.dataCy("var-value-input").should("have.value", "{REDACTED}");
+      cy.dataCy("var-name-input").should("be.disabled");
+      cy.dataCy("var-value-input").should("be.disabled");
+      cy.dataCy("var-private-input").should("be.disabled");
+      cy.dataCy("var-admin-input").should("be.disabled");
+    });
+
+    it("Typing a duplicate variable name will disable saving and show an error message", () => {
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").type("sample_name");
+      cy.dataCy("var-value-input").type("sample_value");
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").first().type("sample_name");
+      cy.dataCy("var-value-input").first().type("sample_value_2");
+      cy.contains("Value already appears in project variables.").as(
+        "errorMessage"
+      );
+      saveButtonEnabled(false);
+      // Fix duplication
+      cy.dataCy("var-name-input").first().type("_2");
+      saveButtonEnabled();
+      cy.get("@errorMessage").should("not.exist");
+    });
+
+    it("Should correctly save an admin only variable", () => {
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").first().type("admin_var");
+      cy.dataCy("var-value-input").first().type("admin_value");
+      cy.contains("label", "Admin Only").click();
+      cy.dataCy("var-admin-input").should("be.checked");
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+    });
+
+    it("Should persist saved variables and allow deletion", () => {
+      // Add variables
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").type("sample_name");
+      cy.dataCy("var-value-input").type("sample_value");
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").first().type("sample_name_2");
+      cy.dataCy("var-value-input").first().type("sample_value");
+      cy.dataCy("add-button").click();
+      cy.dataCy("var-name-input").first().type("admin_var");
+      cy.dataCy("var-value-input").first().type("admin_value");
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+      // Verify persistence
+      cy.reload();
+      cy.dataCy("var-name-input").eq(0).should("have.value", "admin_var");
+      cy.dataCy("var-name-input").eq(1).should("have.value", "sample_name");
+      cy.dataCy("var-name-input").eq(2).should("have.value", "sample_name_2");
+      // Verify deletion
+      cy.dataCy("delete-item-button").first().click();
+      cy.dataCy("delete-item-button").first().click();
+      cy.dataCy("delete-item-button").first().click();
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+      cy.dataCy("var-name-input").should("not.exist");
+      // Verify persistence
+      cy.reload();
+      cy.dataCy("var-name-input").should("not.exist");
+    });
+  });
+
+  describe("GitHub/Commit Queue page", () => {
+    beforeEach(() => {
+      cy.dataCy("navitem-github-commitqueue").click();
+    });
+
+    it("Allows adding a git tag alias", () => {
+      cy.dataCy("git-tag-enabled-radio-box").children().first().click();
+      cy.dataCy("add-button").contains("Add Git Tag").parent().click();
+      cy.dataCy("git-tag-input").type("myGitTag");
+      cy.dataCy("remote-path-input").type("./evergreen.yml");
+
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+      cy.dataCy("remote-path-input").should("have.value", "./evergreen.yml");
+    });
+  });
+
+  describe("Periodic Builds page", () => {
+    beforeEach(() => {
+      cy.dataCy("navitem-periodic-builds").click();
+    });
+
+    it("Disables save button when interval is NaN or below minimum and allows saving a number in range", () => {
+      cy.dataCy("add-button").click();
+      cy.dataCy("interval-input").as("intervalInput").type("NaN");
+      cy.dataCy("config-file-input").type("config.yml");
+      saveButtonEnabled(false);
+      cy.contains("Value should be a number.");
+      cy.get("@intervalInput").clear();
+      cy.get("@intervalInput").type("0");
+      saveButtonEnabled(false);
+      cy.get("@intervalInput").clear();
+      cy.get("@intervalInput").type("12");
+      clickSave();
+      cy.validateToast("success", "Successfully updated project");
+    });
+  });
+
+  describe("Project Triggers page", () => {
+    beforeEach(() => {
+      cy.dataCy("navitem-project-triggers").click();
+    });
+
+    it("Saves a project trigger", () => {
+      cy.dataCy("add-button").click();
+      cy.dataCy("project-input").should("be.visible").should("not.be.disabled");
+      cy.dataCy("project-input").type("spruce");
+      cy.dataCy("config-file-input").type(".evergreen.yml");
+    });
+  });
+});
+
+xdescribe("Project Settings when defaulting to repo", () => {
+  const destination = getGeneralRoute(projectUseRepoEnabled);
+
+  beforeEach(() => {
+    cy.visit(destination);
+  });
+
+  describe("General Settings page", () => {
+    it("Save button is disabled on load and shows a link to the repo", () => {
+      saveButtonEnabled(false);
+      cy.dataCy("attached-repo-link")
+        .should("have.attr", "href")
+        .and("eq", `/${getGeneralRoute(repo)}`);
+    });
+
+    it("Preserves edits to the form when navigating between settings tabs and does not show a warning modal", () => {
+      cy.dataCy("spawn-host-input").should("have.value", "/path");
+      cy.dataCy("spawn-host-input").type("/test");
+      saveButtonEnabled();
+      cy.dataCy("navitem-access").click();
+      cy.dataCy("navigation-warning-modal").should("not.exist");
+      cy.dataCy("navitem-general").click();
+      cy.dataCy("spawn-host-input").should("have.value", "/path/test");
+      saveButtonEnabled();
+    });
+
+    it("Shows a 'Default to Repo' button on page", () => {
+      cy.dataCy("default-to-repo-button").should("exist");
+    });
+
+    it("Shows only two radio boxes even when rendering a project that inherits from repo", () => {
       cy.dataCy("enabled-radio-box").children().should("have.length", 2);
     });
 
@@ -896,7 +1054,7 @@ describe(
   }
 );
 
-describe("Attaching Spruce to a repo", { testIsolation: false }, () => {
+xdescribe("Attaching Spruce to a repo", { testIsolation: false }, () => {
   const destination = getGeneralRoute(project);
 
   before(() => {
@@ -955,7 +1113,7 @@ describe("Attaching Spruce to a repo", { testIsolation: false }, () => {
   });
 });
 
-describe("Renaming the identifier", { testIsolation: false }, () => {
+xdescribe("Renaming the identifier", { testIsolation: false }, () => {
   const destination = getGeneralRoute(project);
 
   before(() => {
@@ -981,7 +1139,7 @@ describe("Renaming the identifier", { testIsolation: false }, () => {
   });
 });
 
-describe(
+xdescribe(
   "A project that has GitHub webhooks disabled",
   { testIsolation: false },
   () => {
