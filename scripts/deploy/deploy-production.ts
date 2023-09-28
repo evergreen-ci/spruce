595d835cab9150aa3c46f76f4f00749874ce5a0d
Arjun Patel <arjun453@gmail.com>
2023-09-28 16:31:42 -0400

    wrap deploy function body in try/catch

--- scripts/deploy/deploy-production.ts
+++ scripts/deploy/deploy-production.ts
@@ -8,65 +8,66 @@ import { runDeploy } from "./utils/deploy";
 const { createNewTag, deleteTag, getLatestTag, pushTags } = tagUtils;
 /* Deploy by pushing a git tag, to be picked up and built by Evergreen, and deployed to S3. */
 const evergreenDeploy = async () => {
-  const currentlyDeployedCommit = getCurrentlyDeployedCommit();
-  console.log(`Currently Deployed Commit: ${currentlyDeployedCommit}`);
+  try {
+    const currentlyDeployedCommit = getCurrentlyDeployedCommit();
+    console.log(`Currently Deployed Commit: ${currentlyDeployedCommit}`);
 
-  const commitMessages = getCommitMessages(currentlyDeployedCommit);
+    const commitMessages = getCommitMessages(currentlyDeployedCommit);
 
-  // If there are no commit messages, ask the user if they want to delete and re-push the latest tag, thereby forcing a deploy with no new commits.
-  if (commitMessages.length === 0) {
-    const latestTag = getLatestTag();
-    const response = await prompts({
-      type: "confirm",
-      name: "value",
-      message: `No new commits. Do you want to trigger a deploy on the most recent existing tag? (${latestTag})`,
-      initial: false,
-    });
+    // If there are no commit messages, ask the user if they want to delete and re-push the latest tag, thereby forcing a deploy with no new commits.
+    if (commitMessages.length === 0) {
+      const latestTag = getLatestTag();
+      const response = await prompts({
+        type: "confirm",
+        name: "value",
+        message: `No new commits. Do you want to trigger a deploy on the most recent existing tag? (${latestTag})`,
+        initial: false,
+      });
 
-    const forceDeploy = response.value;
-    if (forceDeploy) {
-      console.log(`Deleting tag (${latestTag}) from remote...`);
-      deleteTag(latestTag);
-      console.log("Pushing tags...");
-      pushTags();
-      console.log("Check Evergreen for deploy progress.");
+      const forceDeploy = response.value;
+      if (forceDeploy) {
+        console.log(`Deleting tag (${latestTag}) from remote...`);
+        deleteTag(latestTag);
+        console.log("Pushing tags...");
+        pushTags();
+        console.log("Check Evergreen for deploy progress.");
+        return;
+      }
+
+      console.log(
+        "Deploy canceled. If systems are experiencing an outage and you'd like to push the deploy directly to S3, run yarn deploy:prod --local."
+      );
       return;
     }
 
-    console.log(
-      "Deploy canceled. If systems are experiencing an outage and you'd like to push the deploy directly to S3, run yarn deploy:prod --local."
-    );
-    return;
-  }
+    // Print all commits between the last tag and the current commit
+    console.log(`Commit messages:\n${commitMessages}`);
 
-  // Print all commits between the last tag and the current commit
-  console.log(`Commit messages:\n${commitMessages}`);
+    const response = await prompts({
+      type: "confirm",
+      name: "value",
+      message: "Are you sure you want to deploy to production?",
+    });
 
-  const response = await prompts({
-    type: "confirm",
-    name: "value",
-    message: "Are you sure you want to deploy to production?",
-  });
-
-  if (response.value) {
-    try {
-      console.log("Creating new tag...");
-      createNewTag();
-      console.log("Pushing tags...");
-      pushTags();
-      console.log("Pushed to remote. Should be deploying soon...");
-      console.log(
-        green(
-          `Track deploy progress at ${underline(
-            "https://spruce.mongodb.com/commits/spruce?requester=git_tag_request"
-          )}`
-        )
-      );
-    } catch (err) {
-      console.error(err);
-      console.error("Creating tag failed. Aborting.");
-      process.exit(1);
+    if (response.value) {
+      if (createNewTag()) {
+        console.log("Pushed to remote. Should be deploying soon...");
+        console.log(
+          green(
+            `Track deploy progress at ${underline(
+              "https://spruce.mongodb.com/commits/spruce?requester=git_tag_request"
+            )}`
+          )
+        );
+      } else {
+        console.log("Deploy failed.");
+        process.exit(1);
+      }
     }
+  } catch (err) {
+    console.error(err);
+    console.log("Deploy failed.");
+    process.exit(1);
   }
 };
 
