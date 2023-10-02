ffb55c4dab52fecc4c5e86514873e50b148c8fa1
minnakt <47064971+minnakt@users.noreply.github.com>
2023-10-02 12:10:30 -0400

    EVG-19254: Show details.description regardless of task status (#2070)

--- src/components/ExpandedText/index.tsx
+++ src/components/ExpandedText/index.tsx
@@ -1,25 +1,31 @@
 import styled from "@emotion/styled";
 import { palette } from "@leafygreen-ui/palette";
-import Tooltip, { TriggerEvent } from "@leafygreen-ui/tooltip";
+import Tooltip, { TriggerEvent, Align, Justify } from "@leafygreen-ui/tooltip";
 import { Disclaimer } from "@leafygreen-ui/typography";
 import { zIndex } from "constants/tokens";
 
 const { blue } = palette;
 
 interface ExpandedTextProps {
-  message: string;
-  triggerEvent?: (typeof TriggerEvent)[keyof typeof TriggerEvent];
-  popoverZIndex?: number;
+  align?: Align;
   ["data-cy"]?: string;
+  justify?: Justify;
+  message: string;
+  popoverZIndex?: number;
+  triggerEvent?: (typeof TriggerEvent)[keyof typeof TriggerEvent];
 }
 
 const ExpandedText: React.FC<ExpandedTextProps> = ({
+  align,
   "data-cy": dataCy,
+  justify,
   message,
   popoverZIndex = zIndex.popover,
   triggerEvent = TriggerEvent.Hover,
 }) => (
   <Tooltip
+    align={align}
+    justify={justify}
     trigger={<ButtonText>more</ButtonText>}
     triggerEvent={triggerEvent}
     popoverZIndex={popoverZIndex}
@@ -32,6 +38,8 @@ const ButtonText = styled(Disclaimer)`
   color: ${blue.dark2};
   text-decoration: underline;
   cursor: default;
+  width: fit-content;
+  display: inline-block;
 `;
 
 const MessageWrapper = styled.div`
