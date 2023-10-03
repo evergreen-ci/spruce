11633bfb6897310420fb8ccb1dcbbd02028e7662
SupaJoon <arjun453@gmail.com>
2023-10-03 11:06:23 -0400

    Update src/pages/projectSettings/index.tsx
    Co-authored-by: minnakt <47064971+minnakt@users.noreply.github.com>

--- src/pages/projectSettings/index.tsx
+++ src/pages/projectSettings/index.tsx
@@ -101,7 +101,7 @@ const ProjectSettings: React.FC = () => {
       : projectData?.projectSettings;
 
   const hasLoaded =
-    (projectData || isRepo) &&
+    (projectData ||  projectType === ProjectType.Repo) &&
     (repoData || projectType === ProjectType.Project) &&
     project;
 
