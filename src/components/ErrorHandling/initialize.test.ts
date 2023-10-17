0c8d0435c0e3c95fbf257e2ce99c7ac52a72631a
Sophie Stadler <sophie.stadler@mongodb.com>
2023-10-17 12:33:33 -0400

    Remove Bugsnag code

--- src/components/ErrorHandling/initialize.test.ts
+++ src/components/ErrorHandling/initialize.test.ts
@@ -1,5 +1,3 @@
-import Bugsnag from "@bugsnag/js";
-import BugsnagPluginReact from "@bugsnag/plugin-react";
 import * as Sentry from "@sentry/react";
 import { mockEnvironmentVariables } from "test_utils/utils";
 import { initializeErrorHandling } from ".";
@@ -8,8 +6,6 @@ const { cleanup, mockEnv } = mockEnvironmentVariables();
 
 describe("should initialize error handlers according to release stage", () => {
   beforeEach(() => {
-    jest.spyOn(Bugsnag, "start").mockImplementation(jest.fn());
-    jest.spyOn(Bugsnag, "isStarted").mockImplementation(jest.fn(() => false));
     jest.spyOn(Sentry, "init").mockImplementation(jest.fn());
     jest
       .spyOn(Sentry, "Replay")
@@ -27,7 +23,6 @@ describe("should initialize error handlers according to release stage", () => {
     mockEnv("REACT_APP_RELEASE_STAGE", "production");
     initializeErrorHandling();
 
-    expect(Bugsnag.start).not.toHaveBeenCalled();
     expect(Sentry.init).not.toHaveBeenCalled();
   });
 
@@ -35,16 +30,8 @@ describe("should initialize error handlers according to release stage", () => {
     mockEnv("NODE_ENV", "production");
     mockEnv("REACT_APP_VERSION", "1.0.0");
     mockEnv("REACT_APP_RELEASE_STAGE", "production");
-    mockEnv("REACT_APP_BUGSNAG_API_KEY", "i-am-a-fake-key");
     mockEnv("REACT_APP_SENTRY_DSN", "fake-sentry-key");
     initializeErrorHandling();
-
-    expect(Bugsnag.start).toHaveBeenCalledWith({
-      apiKey: "i-am-a-fake-key",
-      appVersion: "1.0.0",
-      releaseStage: "production",
-      plugins: [new BugsnagPluginReact()],
-    });
 
     expect(Sentry.init).toHaveBeenCalledWith({
       dsn: "fake-sentry-key",
@@ -61,16 +48,8 @@ describe("should initialize error handlers according to release stage", () => {
     mockEnv("REACT_APP_RELEASE_STAGE", "beta");
     mockEnv("NODE_ENV", "production");
     mockEnv("REACT_APP_VERSION", "1.0.0");
-    mockEnv("REACT_APP_BUGSNAG_API_KEY", "i-am-a-fake-key");
     mockEnv("REACT_APP_SENTRY_DSN", "fake-sentry-key");
     initializeErrorHandling();
-
-    expect(Bugsnag.start).toHaveBeenCalledWith({
-      apiKey: "i-am-a-fake-key",
-      appVersion: "1.0.0",
-      releaseStage: "beta",
-      plugins: [new BugsnagPluginReact()],
-    });
 
     expect(Sentry.init).toHaveBeenCalledWith({
       dsn: "fake-sentry-key",
@@ -87,16 +66,8 @@ describe("should initialize error handlers according to release stage", () => {
     mockEnv("NODE_ENV", "production");
     mockEnv("REACT_APP_VERSION", "1.0.0");
     mockEnv("REACT_APP_RELEASE_STAGE", "staging");
-    mockEnv("REACT_APP_BUGSNAG_API_KEY", "i-am-a-fake-key");
     mockEnv("REACT_APP_SENTRY_DSN", "fake-sentry-key");
     initializeErrorHandling();
-
-    expect(Bugsnag.start).toHaveBeenCalledWith({
-      apiKey: "i-am-a-fake-key",
-      appVersion: "1.0.0",
-      releaseStage: "staging",
-      plugins: [new BugsnagPluginReact()],
-    });
 
     expect(Sentry.init).toHaveBeenCalledWith({
       dsn: "fake-sentry-key",
@@ -112,8 +83,6 @@ describe("should initialize error handlers according to release stage", () => {
 
 describe("should not initialize if the client is already running", () => {
   beforeEach(() => {
-    jest.spyOn(Bugsnag, "start").mockImplementation(jest.fn());
-    jest.spyOn(Bugsnag, "isStarted").mockImplementation(jest.fn(() => false));
     jest.spyOn(Sentry, "init").mockImplementation(jest.fn());
     mockEnv("NODE_ENV", "production");
   });
@@ -123,19 +92,11 @@ describe("should not initialize if the client is already running", () => {
     cleanup();
   });
 
-  it("does not initialize Bugsnag twice", () => {
-    jest.spyOn(Bugsnag, "isStarted").mockImplementation(jest.fn(() => true));
-    initializeErrorHandling();
-    expect(Bugsnag.start).not.toHaveBeenCalled();
-    expect(Sentry.init).toHaveBeenCalledTimes(1);
-  });
-
   it("does not initialize Sentry twice", () => {
     const mockClient = { getClient: jest.fn(() => true) };
     // @ts-expect-error - Type error occurs because the entire return value of getCurrentHub is not mocked
     jest.spyOn(Sentry, "getCurrentHub").mockReturnValue(mockClient);
     initializeErrorHandling();
-    expect(Bugsnag.start).toHaveBeenCalledTimes(1);
     expect(Sentry.init).not.toHaveBeenCalled();
   });
 });
