aa95bc4cda9a3e2bcc4f34d5feeb94e3557bf162
Arjun Patel <arjun453@gmail.com>
2023-10-30 11:47:39 -0400

    remove tsx file (replaced by ts file)

--- src/components/styles/filters.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import { size } from "constants/tokens";
-
-export const FiltersWrapper = styled.div`
-  display: flex;
-  margin-bottom: ${size.s};
-  align-items: center;
-`;
