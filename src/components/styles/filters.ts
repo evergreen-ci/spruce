f43cd07364278a156849fafb82515178ba8f1e4a
Arjun Patel <arjun453@gmail.com>
2023-10-30 11:21:54 -0400

    more file name updates

---
+++ src/components/styles/filters.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { size } from "constants/tokens";
+
+export const FiltersWrapper = styled.div`
+  display: flex;
+  margin-bottom: ${size.s};
+  align-items: center;
+`;
