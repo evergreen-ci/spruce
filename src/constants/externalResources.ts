d1b5ca388a068d6323cd89bb1a75b879f673da82
Jonathan Brill <jonathan.brill@10gen.com>
2023-10-31 17:18:28 -0400

    add end time to the Honeycomb trace link

--- src/constants/externalResources.ts
+++ src/constants/externalResources.ts
@@ -103,10 +103,14 @@ export const getParsleyTestLogURL = (buildId: string, testId: string) =>
 export const getParsleyBuildLogURL = (buildId: string) =>
   `${getParsleyUrl()}/resmoke/${buildId}/all`;
 
-export const getHoneycombTraceUrl = (traceId: string, startTs: Date) =>
+export const getHoneycombTraceUrl = (
+  traceId: string,
+  startTs: Date,
+  endTs: Date
+) =>
   `${getHoneycombBaseURL()}/datasets/evergreen-agent/trace?trace_id=${traceId}&trace_start_ts=${getUnixTime(
     new Date(startTs)
-  )}`;
+  )}&trace_end_ts=${getUnixTime(new Date(endTs))}`;
 
 export const getHoneycombSystemMetricsUrl = (
   taskId: string,
