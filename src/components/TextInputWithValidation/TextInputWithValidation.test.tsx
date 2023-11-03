4da991bf6c49b86d27b09a803a046b1522f0d476
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-11-03 16:25:39 -0400

    Unit tests

--- src/components/TextInputWithValidation/TextInputWithValidation.test.tsx
+++ src/components/TextInputWithValidation/TextInputWithValidation.test.tsx
@@ -2,6 +2,23 @@ import { render, screen, userEvent } from "test_utils";
 import TextInputWithValidation from ".";
 
 describe("textInputWithValidation", () => {
+  it("should not be able to submit with an invalid input", async () => {
+    const user = userEvent.setup();
+    const onSubmit = jest.fn();
+    render(
+      <TextInputWithValidation
+        onSubmit={onSubmit}
+        label="textinput"
+        aria-label="textinput"
+        validator={(v) => v.length > 5}
+      />
+    );
+    const input = screen.getByRole("textbox", { name: "textinput" });
+    await user.type(input, "test");
+    expect(input).toHaveValue("test");
+    await user.type(input, "{enter}");
+    expect(onSubmit).not.toHaveBeenCalledWith("test");
+  });
   it("should not validate without a validation function", async () => {
     const user = userEvent.setup();
     const onSubmit = jest.fn();
@@ -12,12 +29,49 @@ describe("textInputWithValidation", () => {
         aria-label="textinput"
       />
     );
-    await user.type(screen.getByRole("textbox", { name: "textinput" }), "test");
-    expect(screen.getByRole("textbox", { name: "textinput" })).toHaveValue(
-      "test"
-    );
-    userEvent.click(screen.getByRole("button", { name: "Select plus button" }));
-
+    const input = screen.getByRole("textbox", { name: "textinput" });
+    await user.type(input, "test");
+    await user.type(input, "{enter}");
     expect(onSubmit).toHaveBeenCalledWith("test");
+  });
+  it("should call onChange only for valid inputs", async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+    render(
+      <TextInputWithValidation
+        onChange={onChange}
+        label="textinput"
+        aria-label="textinput"
+        validator={(v) => v.length >= 5}
+      />
+    );
+    const input = screen.getByRole("textbox", { name: "textinput" });
+    await user.type(input, "test");
+    expect(onChange).not.toHaveBeenCalledWith("test");
+    await user.type(input, "5");
+    expect(onChange).toHaveBeenCalledWith("test5");
+  });
+  it("clearOnSubmit should clear the input after a valid input is submitted", async () => {
+    const user = userEvent.setup();
+    const onChange = jest.fn();
+    const onSubmit = jest.fn();
+    render(
+      <TextInputWithValidation
+        onChange={onChange}
+        label="textinput"
+        aria-label="textinput"
+        validator={(v) => v.length >= 5}
+        onSubmit={onSubmit}
+        clearOnSubmit
+      />
+    );
+    const input = screen.getByRole("textbox", { name: "textinput" });
+    await user.type(input, "test");
+    expect(onChange).not.toHaveBeenCalledWith("test");
+    await user.type(input, "5");
+    expect(onChange).toHaveBeenCalledWith("test5");
+    await user.type(input, "{enter}");
+    expect(input).toHaveValue("");
+    expect(onSubmit).toHaveBeenCalledWith("test5");
   });
 });
