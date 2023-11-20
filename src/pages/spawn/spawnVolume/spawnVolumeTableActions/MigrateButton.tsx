c1e265c26110ca652db15bc7b29ae87c12e5b1f8
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-11-20 16:17:13 -0500

    DEVPROD-357 Make spawn volume tests atomic (#2158)

--- src/pages/spawn/spawnVolume/spawnVolumeTableActions/MigrateButton.tsx
+++ src/pages/spawn/spawnVolume/spawnVolumeTableActions/MigrateButton.tsx
@@ -1,8 +1,5 @@
 import { useRef, useState } from "react";
 import Button, { Size } from "@leafygreen-ui/button";
-import { GuideCue } from "@leafygreen-ui/guide-cue";
-import Cookies from "js-cookie";
-import { SEEN_MIGRATE_GUIDE_CUE } from "constants/cookies";
 import { TableVolume } from "types/spawn";
 import { MigrateVolumeModal } from "./MigrateVolumeModal";
 
@@ -12,26 +9,9 @@ interface Props {
 
 export const MigrateButton: React.FC<Props> = ({ volume }) => {
   const [openModal, setOpenModal] = useState(false);
-  const [openGuideCue, setOpenGuideCue] = useState(volume.showMigrateBtnCue);
   const triggerRef = useRef(null);
-  const onHideCue = () => {
-    Cookies.set(SEEN_MIGRATE_GUIDE_CUE, "true");
-    setOpenGuideCue(false);
-  };
   return (
     <>
-      <GuideCue
-        data-cy="migrate-cue"
-        open={openGuideCue}
-        setOpen={setOpenGuideCue}
-        title="New feature!"
-        refEl={triggerRef}
-        numberOfSteps={1}
-        currentStep={1}
-        onPrimaryButtonClick={onHideCue}
-      >
-        You can now migrate your home volume to a new spawn host!
-      </GuideCue>
       <Button
         ref={triggerRef}
         size={Size.XSmall}
@@ -39,7 +19,6 @@ export const MigrateButton: React.FC<Props> = ({ volume }) => {
         disabled={volume.migrating}
         onClick={(e) => {
           e.stopPropagation();
-          onHideCue();
           setOpenModal(true);
         }}
       >
