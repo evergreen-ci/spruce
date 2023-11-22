1f7b1065f8293a04d39768dab64386ee41069feb
Arjun Patel <arjun453@gmail.com>
2023-11-22 12:33:11 -0500

    increase retry

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 
 export default defineConfig({
   e2e: {
-    retries: 1,
+    retries: 2,
     baseUrl: "http://localhost:3000",
     projectId: "yshv48",
     reporterOptions: {
