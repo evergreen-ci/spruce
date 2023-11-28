2e57271ea3fd1c97ca0c02b8907bb12b0613bf9a
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-11-28 16:55:21 -0500

    Mocked provider with cache

--- src/gql/GQLWrapper.tsx
+++ src/gql/GQLWrapper.tsx
@@ -96,6 +96,12 @@ const cache = new InMemoryCache({
     User: {
       keyFields: ["userId"],
     },
+    UserSettings: {
+      keyFields: false,
+      merge(existing, incoming, { mergeObjects }) {
+        return mergeObjects(existing, incoming);
+      },
+    },
     Task: {
       keyFields: ["execution", "id"],
       fields: {
