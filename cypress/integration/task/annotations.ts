285d32164d832b58bbf0afe662fb1811a729ba38
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-12-12 17:25:03 -0500

    Use aliases to select rows

--- cypress/integration/task/annotations.ts
+++ cypress/integration/task/annotations.ts
@@ -2,24 +2,20 @@ const taskWithAnnotations =
   "evergreen_ubuntu1604_test_annotations_b_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48";
 const taskRoute = `/task/${taskWithAnnotations}/annotations`;
 
-const issuesList = "issues-list";
-const suspectedIssuesList = "suspected-issues-list";
-
-const validateAnnotationRowCount = (listName: string, count: number) => {
-  cy.dataCy(listName).within(() =>
-    cy.dataCy("annotation-ticket-row").should("have.length", count)
-  );
-};
 describe("Task Annotation Tab", () => {
   beforeEach(() => {
     cy.visit(taskRoute);
+    cy.dataCy("loading-annotation-ticket").should("not.exist");
+    const rowSelector = "[data-cy='annotation-ticket-row']";
+    cy.dataCy("issues-list").find(rowSelector).as("issueRows");
+    cy.dataCy("suspected-issues-list").find(rowSelector).as("susIssueRows");
   });
 
   it("annotations can be moved between lists", () => {
     cy.dataCy("loading-annotation-ticket").should("not.exist");
 
-    validateAnnotationRowCount(issuesList, 1);
-    validateAnnotationRowCount(suspectedIssuesList, 3);
+    cy.get("@issueRows").should("have.length", 1);
+    cy.get("@susIssueRows").should("have.length", 3);
 
     // move from suspectedIssues to Issues
     cy.dataCy("move-btn-AnotherOne").click();
@@ -27,8 +23,8 @@ describe("Task Annotation Tab", () => {
       .should("be.visible")
       .should("not.have.attr", "aria-disabled", "true");
     cy.contains("button", "Yes").click();
-    validateAnnotationRowCount(issuesList, 2);
-    validateAnnotationRowCount(suspectedIssuesList, 2);
+    cy.get("@issueRows").should("have.length", 2);
+    cy.get("@susIssueRows").should("have.length", 2);
     cy.validateToast("success", "Successfully moved suspected issue to issues");
 
     // move from Issues to suspectedIssues
@@ -37,23 +33,24 @@ describe("Task Annotation Tab", () => {
       .should("be.visible")
       .should("not.have.attr", "aria-disabled", "true");
     cy.contains("button", "Yes").click();
-    validateAnnotationRowCount(issuesList, 1);
-    validateAnnotationRowCount(suspectedIssuesList, 3);
+    cy.get("@issueRows").should("have.length", 1);
+    cy.get("@susIssueRows").should("have.length", 3);
 
     cy.validateToast("success", "Successfully moved issue to suspected issues");
   });
 
   it("annotations add and delete correctly", () => {
-    validateAnnotationRowCount(issuesList, 1);
-    validateAnnotationRowCount(suspectedIssuesList, 3);
+    cy.get("@issueRows").should("have.length", 1);
+    cy.get("@susIssueRows").should("have.length", 3);
+
     cy.dataCy("loading-annotation-ticket").should("have.length", 0);
 
     // add a ticket
     cy.dataCy("add-suspected-issue-button").click();
     cy.dataCy("issue-url").type("https://jira.example.com/browse/SERVER-1234");
     cy.contains("Add suspected issue").click();
-    validateAnnotationRowCount(issuesList, 1);
-    validateAnnotationRowCount(suspectedIssuesList, 4);
+    cy.get("@issueRows").should("have.length", 1);
+    cy.get("@susIssueRows").should("have.length", 4);
     cy.validateToast("success", "Successfully added suspected issue");
 
     // delete the added ticket
@@ -62,8 +59,8 @@ describe("Task Annotation Tab", () => {
       .should("be.visible")
       .should("not.have.attr", "aria-disabled", "true");
     cy.contains("button", "Yes").click();
-    validateAnnotationRowCount(issuesList, 1);
-    validateAnnotationRowCount(suspectedIssuesList, 3);
+    cy.get("@issueRows").should("have.length", 1);
+    cy.get("@susIssueRows").should("have.length", 3);
     cy.validateToast("success", "Successfully removed suspected issue");
   });
 });
