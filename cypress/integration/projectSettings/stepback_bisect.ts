fdd70a21e22c5ff331a59220e0612f148dfa14b4
ZackarySantana <zackzackyack@gmail.com>
2023-12-14 11:29:24 -0500

    Extract to helper functions

--- cypress/integration/projectSettings/stepback_bisect.ts
+++ cypress/integration/projectSettings/stepback_bisect.ts
@@ -4,12 +4,13 @@ import { getGeneralRoute, project, projectUseRepoEnabled } from "./constants";
 type stepbackButtonType = "enabled" | "disabled" | "repo";
 
 /**
- * Retrieves the stepback button associated with type. The stepback buttons are
- * inside of the div container with "stepback-bisect-group".
- * @param type The type of stepback button to get
- * @returns The stepback button corresponding to the type
+ * Retrieves the stepback label associated with type. The stepback labels are
+ * inside of the div container with "stepback-bisect-group". The labels contain
+ * the radio button (input) and a div with the text.
+ * @param type The type of stepback label to get
+ * @returns The stepback label corresponding to the type
  */
-function getStepbackButton(
+function getStepbackLabel(
   type: stepbackButtonType
 ): Cypress.Chainable<JQuery<HTMLElement>> {
   let i = 0;
@@ -20,6 +21,18 @@ function getStepbackButton(
     i = 2;
   }
   return cy.dataCy("stepback-bisect-group").children().eq(i);
+}
+
+/**
+ * Retrieves the stepback button associated with type. The stepback buttons are
+ * inside of the div container with "stepback-bisect-group", inside their label.
+ * @param type The type of stepback button to get
+ * @returns The stepback button corresponding to the type
+ */
+function getStepbackButton(
+  type: stepbackButtonType
+): Cypress.Chainable<JQuery<HTMLElement>> {
+  return getStepbackLabel(type).children().get("input");
 }
 
 describe("Stepback bisect setting", () => {
@@ -35,7 +48,7 @@ describe("Stepback bisect setting", () => {
     });
 
     it("Clicking on enabled and then save shows a success toast", () => {
-      getStepbackButton("enabled").click();
+      getStepbackLabel("enabled").click();
       clickSave();
       cy.validateToast("success", "Successfully updated project");
 
@@ -56,7 +69,7 @@ describe("Stepback bisect setting", () => {
     });
 
     it("Clicking on enabled and then save shows a success toast", () => {
-      getStepbackButton("enabled").click();
+      getStepbackLabel("enabled").click();
       clickSave();
       cy.validateToast("success", "Successfully updated project");
 
