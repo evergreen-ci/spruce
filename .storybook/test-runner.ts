22b2b82365afbb37869b1a38c6a67ef67875862b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-01-08 16:46:03 -0500

    install test runner

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,23 @@
+import { TestRunnerConfig, waitForPageReady } from '@storybook/test-runner';
+
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
+
+const config: TestRunnerConfig = {
+  setup() {
+    expect.extend({ toMatchImageSnapshot });
+  },
+  async postVisit(page, context) {
+    // Waits for the page to be ready before taking a screenshot to ensure consistent results
+    await waitForPageReady(page);
+
+    // To capture a screenshot for for different browsers, add page.context().browser().browserType().name() to get the browser name to prefix the file name
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir,
+      customSnapshotIdentifier: context.id,
+    });
+  },
+};
+export default config;
