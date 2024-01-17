0fbe42bd25a6ec679d2da4232cf22d0795963012
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-01-17 13:52:46 -0500

    Enable access page tests

--- cypress/integration/projectSettings/access.ts
+++ cypress/integration/projectSettings/access.ts
@@ -6,7 +6,7 @@ import {
 } from "./constants";
 import { clickSave } from "../../utils";
 
-xdescribe("Access page", () => {
+describe("Access page", () => {
   const origin = getAccessRoute(projectUseRepoEnabled);
   beforeEach(() => {
     cy.visit(origin);
