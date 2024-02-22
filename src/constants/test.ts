127802a7f0decbd14c648124b1dad5c27a9138bd
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-02-22 13:46:30 -0500

    Create new status for test timeouts

--- src/constants/test.ts
+++ src/constants/test.ts
@@ -1,19 +1,4 @@
-import { Variant } from "@leafygreen-ui/badge";
 import { TestStatus } from "types/test";
-
-export const statusToBadgeColor = {
-  [TestStatus.Pass]: Variant.Green,
-  [TestStatus.Fail]: Variant.Red,
-  [TestStatus.SilentFail]: Variant.Blue,
-  [TestStatus.Skip]: Variant.Yellow,
-};
-
-export const statusCopy = {
-  [TestStatus.Pass]: "Pass",
-  [TestStatus.Fail]: "Fail",
-  [TestStatus.Skip]: "Skip",
-  [TestStatus.SilentFail]: "Silent Fail",
-};
 
 export const testStatusesFilterTreeData = [
   {
