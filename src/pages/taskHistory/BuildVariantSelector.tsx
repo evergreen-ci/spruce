44d28360a7d1a3751a47ed587b8ef523673689a5
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-02-26 17:08:58 -0500

    Add onChange and onFilter callback comments to BuildVariantSelector

--- src/pages/taskHistory/BuildVariantSelector.tsx
+++ src/pages/taskHistory/BuildVariantSelector.tsx
@@ -36,6 +36,10 @@ const BuildVariantSelector: React.FC<BuildVariantSelectorProps> = ({
     },
   });
 
+  /**
+   * `onChange` is a callback function that is called when the user selects a build variant
+   * @param selectedBuildVariants - an array of build variants that the user has selected
+   */
   const onChange = (selectedBuildVariants: string[]) => {
     sendEvent({
       name: "Filter by build variant",
@@ -45,19 +49,20 @@ const BuildVariantSelector: React.FC<BuildVariantSelectorProps> = ({
   };
   const { buildVariantsForTaskName } = data || {};
 
+  /**
+   * `onFilter` is a callback function that is called when the user types in the search bar
+   */
   const onFilter = useCallback(
     (value: string) => {
       setFilteredOptions(
-        buildVariantsForTaskName
-          .filter(
-            (option) =>
-              option.buildVariant
-                .toLowerCase()
-                .includes(value.toLowerCase().trim()) ||
-              option.displayName
-                .toLowerCase()
-                .includes(value.toLowerCase().trim()),
-          )
+        (buildVariantsForTaskName || [])
+          .filter(({ buildVariant, displayName }) => {
+            const trimmedValue = value.toLowerCase().trim();
+            return (
+              buildVariant.toLowerCase().includes(trimmedValue) ||
+              displayName.toLowerCase().includes(trimmedValue)
+            );
+          })
           .map((option) => option.buildVariant) || [],
       );
     },
