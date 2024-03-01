0b5eda7a24b3dc5a66214668290e9987acccff53
Arjun Patel <arjun453@gmail.com>
2024-03-01 16:13:36 -0500

    update import after moving cache

--- src/test_utils/graphql.tsx
+++ src/test_utils/graphql.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MockedProvider, MockedProviderProps } from "@apollo/client/testing";
-import { cache } from "gql/GQLWrapper";
+import { cache } from "gql/client/cache";
 
 const CustomMockedProvider: React.FC<MockedProviderProps> = ({
   children,
