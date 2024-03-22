9dc51259922d4ffccaa84db24ea1ed18ed306a40
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-03-22 00:22:40 -0400

    Use correct slug for user patches redirect

--- src/components/Redirects/UserPatchesRedirect.tsx
+++ src/components/Redirects/UserPatchesRedirect.tsx
@@ -2,6 +2,6 @@ import { useParams, Navigate } from "react-router-dom";
 import { getUserPatchesRoute, slugs } from "constants/routes";
 
 export const UserPatchesRedirect: React.FC = () => {
-  const { [slugs.id]: id } = useParams();
-  return <Navigate replace to={getUserPatchesRoute(id)} />;
+  const { [slugs.userId]: userId } = useParams();
+  return <Navigate replace to={getUserPatchesRoute(userId)} />;
 };
